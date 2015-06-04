c7dd58b15d32704e8c55f1b302a2a81f5c6436db
Yuya Unno <unnonouno@gmail.com>
2015-06-04 13:47:00 +0900

    initial commit

---
+++ js/init.js
@@ -0,0 +1,122 @@
+/*
+	Ion by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+(function($) {
+
+	skel.init({
+		reset: 'full',
+		breakpoints: {
+			
+			// Global.
+				global: {
+					range: '*',
+					href: 'css/style.css',
+					containers: 1400,
+					grid: {
+						gutters: {
+							vertical: '4em',
+							horizontal: 0
+						}
+					}
+				},
+
+			// XLarge.
+				xlarge: {
+					range: '-1680',
+					href: 'css/style-xlarge.css',
+					containers: 1200
+				},
+
+			// Large.
+				large: {
+					range: '-1280',
+					href: 'css/style-large.css',
+					containers: 960,
+					grid: {
+						gutters: {
+							vertical: '2.5em'
+						}
+					},
+					viewport: {
+						scalable: false
+					}
+				},
+
+			// Medium.
+				medium: {
+					range: '-980',
+					href: 'css/style-medium.css',
+					containers: '90%',
+					grid: {
+						collapse: 1
+					}
+				},
+
+			// Small.
+				small: {
+					range: '-736',
+					href: 'css/style-small.css',
+					containers: '90%',
+					grid: {
+						gutters: {
+							vertical: '1.25em'
+						}
+					}
+				},
+
+			// XSmall.
+				xsmall: {
+					range: '-480',
+					href: 'css/style-xsmall.css',
+					grid: {
+						collapse: 2
+					}
+				}
+
+		},
+		plugins: {
+			layers: {
+				
+				// Config.
+					config: {
+						transform: true
+					},
+				
+				// Navigation Panel.
+					navPanel: {
+						animation: 'pushX',
+						breakpoints: 'medium',
+						clickToHide: true,
+						height: '100%',
+						hidden: true,
+						html: '<div data-action="moveElement" data-args="nav"></div>',
+						orientation: 'vertical',
+						position: 'top-left',
+						side: 'left',
+						width: 250
+					},
+
+				// Navigation Button.
+					navButton: {
+						breakpoints: 'medium',
+						height: '4em',
+						html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span>',
+						position: 'top-left',
+						side: 'top',
+						width: '6em'
+					}
+
+			}
+		}
+	});
+
+	$(function() {
+		
+		// jQuery ready stuff.
+		
+	});
+
+})(jQuery);
