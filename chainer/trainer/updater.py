5e1060bc4b90e8e9ba8862061ca2259f49d4b3de
Seiya Tokui <beam.web@gmail.com>
2016-02-16 18:43:03 +0900

    Change the name of argument

--- chainer/trainer/updater.py
+++ chainer/trainer/updater.py
@@ -10,7 +10,7 @@ class Updater(object):
     TODO(beam2d): document it.
 
     """
-    def __call__(self, inputs, model, optimizer):
+    def __call__(self, inputs, target, optimizer):
         raise NotImplementedError
 
     def serialize(self, serializer):
@@ -27,19 +27,19 @@ class StandardUpdater(Updater):
     def __init__(self, lossfun=None):
         self._lossfun = lossfun
 
-    def __call__(self, inputs, model, optimizer):
+    def __call__(self, inputs, target, optimizer):
         if not isinstance(inputs, tuple):
             inputs = inputs,
         in_vars = [variable.Variable(a) for a in inputs]
-        lossfun = model if self._lossfun is None else self._lossfun
+        lossfun = target if self._lossfun is None else self._lossfun
 
-        model.zerograds()
+        target.zerograds()
         loss = lossfun(*in_vars)
         loss.backward()
         optimizer.update()
 
         result = {'loss': loss.data}
-        for name, value in six.iteritems(model.__dict__):
+        for name, value in six.iteritems(target.__dict__):
             if isinstance(value, variable.Variable):
                 x = value.data
                 if x.size == 1:
