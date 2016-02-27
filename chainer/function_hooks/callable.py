d66a177cf9eb90c689ba2ec681d52c9de82e0126
Kenta OONO <k.oono.delta@gmail.com>
2016-02-27 13:10:51 +0900

    Remove name argument of CallableHook.__init__

--- chainer/function_hooks/callable.py
+++ chainer/function_hooks/callable.py
@@ -6,10 +6,14 @@ class CallableHook(function.FunctionHook):
 
     name = 'CallableHook'
 
-    def __init__(self, f, name=None):
+    def __init__(self, f):
+        """
+        Args:
+            f(callable): A callable object for preprocess
+                and postprocess. The ``f`` must be able to call
+                with arguments ``function``, ``in_data``, and ``out_grad``.
+        """
         self.f = f
-        if name is not None:
-            self.name = name
 
     def __call__(self, function, in_data, out_grad=None):
         self.f(function, in_data, out_grad)
