337632588b0a43b2210891a2381d7f00883ebb64
Yuya Unno <unnonouno@gmail.com>
2016-05-24 11:13:18 +0900

    Use parameterized test

--- tests/chainer_tests/functions_tests/pooling_tests/test_average_pooling_2d.py
+++ tests/chainer_tests/functions_tests/pooling_tests/test_average_pooling_2d.py
@@ -13,16 +13,25 @@ from chainer.testing import attr
 from chainer.testing import condition
 
 
-@testing.parameterize(*testing.product({
-    'dtype': [numpy.float16, numpy.float32, numpy.float64],
-}))
+@testing.parameterize(*testing.product_dict(
+    [
+        {'pad': 1, 'ksize': 3,
+         'in_shape': (2, 3, 4, 3), 'out_shape': (2, 3, 2, 2),
+         'x_ranges': [(0, 2), (1, 4)], 'y_ranges': [(0, 2), (1, 3)]},
+    ],
+    [
+        {'dtype': numpy.float16},
+        {'dtype': numpy.float32},
+        {'dtype': numpy.float64},
+    ]
+))
 class TestAveragePooling2D(unittest.TestCase):
 
     def setUp(self):
         self.x = numpy.random.uniform(-1, 1,
-                                      (2, 3, 4, 3)).astype(self.dtype)
+                                      self.in_shape).astype(self.dtype)
         self.gy = numpy.random.uniform(-1, 1,
-                                       (2, 3, 2, 2)).astype(self.dtype)
+                                       self.out_shape).astype(self.dtype)
         self.check_forward_options = {}
         self.check_backward_options = {'eps': 1e-2}
         if self.dtype == numpy.float16:
@@ -32,8 +41,8 @@ class TestAveragePooling2D(unittest.TestCase):
 
     def check_forward(self, x_data, use_cudnn=True):
         x = chainer.Variable(x_data)
-        y = functions.average_pooling_2d(x, 3, stride=2,
-                                         pad=1, use_cudnn=use_cudnn)
+        y = functions.average_pooling_2d(x, self.ksize, stride=2,
+                                         pad=self.pad, use_cudnn=use_cudnn)
         self.assertEqual(y.data.dtype, self.dtype)
         y_data = cuda.to_cpu(y.data)
 
@@ -41,9 +50,9 @@ class TestAveragePooling2D(unittest.TestCase):
         for k in six.moves.range(2):
             for c in six.moves.range(3):
                 x = self.x[k, c]
-                expect = numpy.array([
-                    [x[0:2, 0:2].sum(), x[0:2, 1:3].sum()],
-                    [x[1:4, 0:2].sum(), x[1:4, 1:3].sum()]]) / 9
+                expect = numpy.array(
+                    [[x[rx1:rx2, ry1:ry2].sum() for ry1, ry2 in self.y_ranges]
+                     for rx1, rx2 in self.x_ranges]) / (self.ksize ** 2)
                 testing.assert_allclose(
                     expect, y_data[k, c], **self.check_forward_options)
 
@@ -63,7 +72,8 @@ class TestAveragePooling2D(unittest.TestCase):
 
     def check_backward(self, x_data, y_grad, use_cudnn=True):
         gradient_check.check_backward(
-            functions.AveragePooling2D(3, 2, 1, False, use_cudnn),
+            functions.AveragePooling2D(
+                self.ksize, 2, self.pad, False, use_cudnn),
             x_data, y_grad, **self.check_backward_options)
 
     @condition.retry(3)
