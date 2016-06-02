3abd79a52203c654e25dc85b339fda6b97e6a602
Kenta OONO <oono@preferred.jp>
2016-06-02 22:11:23 +0900

    Add CPUTimer and rename Timer to GPUTimer

--- chainer/function_hooks/timer.py
+++ chainer/function_hooks/timer.py
@@ -6,7 +6,48 @@ from chainer import cuda
 from chainer import function
 
 
-class CPUTimer(object):
+def get_timer(xp, *args, **kwargs):
+    if xp is numpy:
+        return Timer(*args, **kwargs)
+    else:
+        return Timer(*args, **kwargs)
+
+
+class Timer(object):
+
+    def reset(self):
+        raise NotImplementedError
+
+    def start(self):
+        raise NotImplementedError
+
+    def stop(self):
+        raise NotImplementedError
+
+    def __enter__(self, *args, **kwargs):
+        self.reset()
+        self.start()
+        return self
+        
+    def __exit__(self, *args, **kwargs):
+        self.stop()
+        return self
+
+    def total_time(self):
+        raise NotImplementedError
+
+    def count(self):
+        raise NotImplementedError
+
+    def mean(self):
+        if self.count() == 0:
+            raise ValueError('Cannot calculate the mean elapsed time '
+                             'because this timer has never measure elapsed times.')
+        else:
+            return self.total_time() / self.count()
+
+
+class CPUTimer(Timer):
 
     def __init__(self):
         self.reset()
@@ -14,15 +55,6 @@ class CPUTimer(object):
     def reset(self):
         self.elapsed_times = []
         self.running = False
-
-    def __enter__(self, *args, **kwargs):
-        self.reset()
-        self.start()
-        return self
-
-    def __exit__(self, *args, **kwargs):
-        self.stop()
-        return self
 
     def start(self):
         if self.running:
@@ -44,13 +76,6 @@ class CPUTimer(object):
     def count(self):
         return len(self.stop)
 
-    def mean(self):
-        if self.count == 0:
-            raise ValueError('Cannot calculate the mean elapsed time '
-                             'because this timer has never measure elapsed times.')
-        else:
-            return self.total_time() / self.count()
-
 
 class GPUTimer(object):
 
@@ -63,21 +88,12 @@ class GPUTimer(object):
         self.blocking_method = blocking_method
         self.reset()
 
-    def __enter__(self, *args, **kwargs):
-        self.reset()
-        self.start()
-        return self
-
-    def __exit__(self, *args, **kwargs):
-        self.stop()
-        return self
-
     def reset(self):
         self.running = False
         self.start_events = []
         self.stop_events = []
         self.elapsed_times = None
-        self._synchronized = False
+        self.synchronized = False
 
     def start(self):
         if self.running:
@@ -89,10 +105,12 @@ class GPUTimer(object):
 
         start = cuda.Event()
         start.record()
+
         if ((self.blocking_method == 'block_first_time' and
-             not self.start_events) or
+             not self.start_events) or 
             (self.blocking_method == 'block_every_time')):
             start.synchronize()
+
         self.start_events.append(start)
         self.running = True
 
@@ -113,7 +131,7 @@ class GPUTimer(object):
 
         if len(self.stop_events) > 0:
             stop_events[-1].synchronize()
-        self._synchronized = True
+        self.synchronized = True
         self.elapsed_times = map(cuda.cupy.cuda.get_elapsed_time(start, stop) / 1000
                                  for start, stop in zip(self.start_events, self.stop_events))
 
@@ -121,20 +139,9 @@ class GPUTimer(object):
         self.synchronize()
         return sum(self.elapsed_times)
 
-    @property
-    def synchronized(self):
-        return self._synchronized
-
     def count(self):
         """Returns number of measurements that is already finish recording."""
         return len(self.stop_events)
-
-    def mean(self):
-        if self.count == 0:
-            raise ValueError('Cannot calculate the mean elapsed time '
-                             'because this timer has never measure elapsed times.')
-        else:
-            return self.total_time() / self.count()
 
 
 class TimerHook(function.FunctionHook):
@@ -151,7 +158,7 @@ class TimerHook(function.FunctionHook):
     name = 'TimerHook'
 
     def _preprocess(self, xp):
-        self.timer = Timer()
+        self.timer = get_timer(xp)
         self.timer.start()
 
     def forward_preprocess(self, function, in_data):
