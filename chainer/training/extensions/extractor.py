edb065ea5ade7262be3ac274db5e01808c25b53f
Amit Aides <amitibo@campus.technion.ac.il>
2016-07-10 10:13:16 -0400

    Documentation.

--- chainer/training/extensions/extractor.py
+++ chainer/training/extensions/extractor.py
@@ -13,43 +13,21 @@ from chainer import variable
 def concat_variables(variables, device=None):
     """Concatenates a list of variables into a single variable.
 
-    iterator yields a list of examples. If each example is an array,
-    this function concatenates them along the newly-inserted first axis (called
-    `batch dimension`) into one array. The basic behavior is same for examples
-    consisting of multiple arrays, i.e., corresponding arrays of all examples
-    are concatenated.
-
-    For instance, consider each example consists of two arrays ``(x, y)``.
-    Then, this function concatenates ``x`` 's into one array, and ``y`` 's
-    into another array, and returns a tuple of these two arrays. Another
-    example: consider each example is a dictionary of two entries whose keys
-    are ``'x'`` and ``'y'``, respectively, and values are arrays. Then, this
-    function concatenates ``x`` 's into one array, and ``y`` 's into another
-    array, and returns a dictionary with two entries ``x`` and ``y`` whose
-    values are the concatenated arrays.
-
-    When the arrays to concatenate have different shapes, the behavior depends
-    on the ``padding`` value. If ``padding`` is ``None`` (default), it raises
-    an error. Otherwise, it builds an array of the minimum shape that the
-    contents of all arrays can be substituted to. The padding value is then
-    used to the extra elements of the resulting arrays.
-
-    TODO(beam2d): Add an example.
+    The forward operation of a model yields a variable, tuple of variables
+    or a dict of variables for each batch. This function concatenates a list
+    of such variables to a single variable, tuple of varialbes or dict of
+    variables, respectively.
 
     Args:
-        batch (list): A list of examples. This is typically given by a dataset
-            iterator.
+        variables (list): A list of variables, list of tuples of variables
+            or a list of dicts of variables.
         device (int): Device ID to which each array is sent. Negative value
             indicates the host memory (CPU). If it is omitted, all arrays are
             left in the original device.
-        padding: Scalar value for extra elements. If this is None (default),
-            an error is raised on shape mismatch. Otherwise, an array of
-            minimum dimensionalities that can accomodate all arrays is created,
-            and elements outside of the examples are padded by this value.
 
     Returns:
-        Array, a tuple of arrays, or a dictionary of arrays. The type depends
-        on the type of each example in the batch.
+        Variable, a tuple of variables, or a dictionary of variables. The type
+        depends on the type of each example in the variables list.
 
     """
     if len(variables) == 0:
@@ -101,47 +79,32 @@ class Extractor(extension.Extension):
     extraction function.
 
     Extractor has a structure to customize similar to that of
-    :class:`~chainer.training.StandardUpdater`. The main differences are:
-
-    - There are no optimizers in an evaluator. Instead, it holds links
-      to evaluate.
-    - An evaluation loop function is used instead of an update function.
-    - Preparation routine can be customized, which is called before each
-      evaluation. It can be used, e.g., to initialize the state of stateful
-      recurrent networks.
-
-    There are two ways to modify the evaluation behavior besides setting a
-    custom evaluation function. One is by setting a custom evaluation loop via
-    the ``eval_loop`` argument. The other is by inheriting this class and
-    overriding the :meth:`evaluate` method. In latter case, users have to
-    create and handle a reporter object manually. Users also have to copy the
-    iterators before using them, in order to reuse them at the next time of
-    evaluation.
-
-    This extension is called at the end of each epoch by default.
+    :class:`~chainer.training.extensions.Evaluator`.
 
     Args:
-        iterator: Dataset iterator for the validation dataset. It can also be
+        iterator: Dataset iterator for extracting features. It can also be
             a dictionary of iterators. If this is just an iterator, the
             iterator is registered by the name ``'main'``.
-        target: Link object or a dictionary of links to evaluate. If this is
+        target: Link object or a dictionary of links to process. If this is
             just a link object, the link is registered by the name ``'main'``.
         converter: Converter function to build input arrays.
             :func:`~chainer.dataset.concat_examples` is used by default.
         device: Device to which the training data is sent. Negative value
             indicates the host memory (CPU).
-        eval_hook: Function to prepare for each evaluation process. It is
-            called at the beginning of the evaluation. The evaluator extension
+        extract_hook: Function to prepare for each extraction process. It is
+            called at the beginning of the extraction. The evxtractor extension
             object is passed at each call.
-        eval_func: Evaluation function called at each iteration. The target
-            link to evaluate as a callable is used by default.
+        extract_func: extraction function called at each iteration. The target
+            link to extract as a callable is used by default.
+        merger: Merger function to build output variable of features. By
+            default it concatenates all output features along the batch axis.
 
     Attributes:
         converter: Converter function.
         device: Device to which the training data is sent.
-        eval_hook: Function to prepare for each evaluation process.
-        eval_func: Evaluation function called at each iteration.
-
+        extract_hook: Function to prepare for each extraction process.
+        extract_func: Extraction function called at each iteration.
+        features: The extracted features. Created after calling the extension.
     """
     trigger = 1, 'epoch'
     default_name = 'extraction'
@@ -185,10 +148,7 @@ class Extractor(extension.Extension):
         """Executes the extractor extension.
 
         Unlike usual extensions, this extension can be executed without passing
-        a trainer object. This extension reports the performance on validation
-        dataset using the :func:`~chainer.report` function. Thus, users can use
-        this extension independently from any trainer by manutally configuring
-        a :class:`~chainer.Reporter` object.
+        a trainer object.
 
         Args:
             trainer (~chainer.training.Trainer): Trainer object that invokes
@@ -199,17 +159,15 @@ class Extractor(extension.Extension):
         self.features = self.extract()
 
     def extract(self):
-        """Evaluates the model and returns a result dictionary.
+        """Extractes features using the model.
 
         This method runs the extraction loop over the dataset. It accumulates
-        the extracted features to :class:`~chainer.DictSummary` and
-        returns a dictionary whose values are means computed by the summary.
+        the extracted features to :class:`~chainer.Variable`.
 
         Users can override this method to customize the extraction routine.
 
         Returns:
-            dict: Result dictionary. This dictionary is further reported via
-                :func:`~chainer.report` without specifying any observer.
+            dict: Features variable.
 
         """
         iterator = self._iterators['main']
