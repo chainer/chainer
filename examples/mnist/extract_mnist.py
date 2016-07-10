c6c8be7b799eda4b7c61ef5d0897e99c48b52e30
Amit Aides <amitibo@campus.technion.ac.il>
2016-07-10 07:35:36 -0400

    Added mnist example

---
+++ examples/mnist/extract_mnist.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import training
+from chainer.training import extensions
+
+
+# Network definition
+class MLP(chainer.Chain):
+
+    def __init__(self, n_in, n_units, n_out):
+        super(MLP, self).__init__(
+            l1=L.Linear(n_in, n_units),  # first layer
+            l2=L.Linear(n_units, n_units),  # second layer
+            l3=L.Linear(n_units, n_out),  # output layer
+        )
+
+    def __call__(self, x):
+        h1 = F.relu(self.l1(x))
+        h2 = F.relu(self.l2(h1))
+        return self.l3(h2)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Chainer example: MNIST')
+    parser.add_argument('--batchsize', '-b', type=int, default=100,
+                        help='Number of images in each mini batch')
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--out', '-o', default='result',
+                        help='Directory to output the result')
+    parser.add_argument('--unit', '-u', type=int, default=1000,
+                        help='Number of units')
+    parser.add_argument('model', help='Load the model from snapshot')
+    args = parser.parse_args()
+
+    print('GPU: {}'.format(args.gpu))
+    print('# unit: {}'.format(args.unit))
+    print('# Minibatch-size: {}'.format(args.batchsize))
+    print('')
+
+    # Set up a neural network to load trained model into.
+    model = MLP(784, args.unit, 10)
+
+    # Load the model from a snapshot
+    chainer.serializers.load_npz(args.model, model)
+
+    if args.gpu >= 0:
+        chainer.cuda.get_device(args.gpu).use()  # Make a specified GPU current
+        model.to_gpu()  # Copy the model to the GPU
+
+    # Load the MNIST dataset
+    train, _ = chainer.datasets.get_mnist(withlabel=False)
+    dataset_iter = chainer.iterators.SequentialIterator(train, args.batchsize,
+                                                        repeat=False)
+
+    # Evaluate the model with the test dataset for each epoch
+    extractor = extensions.Extractor(dataset_iter, model, device=args.gpu)
+
+    # Run the extraction
+    print(extractor.extract().data.shape)
+
+
+if __name__ == '__main__':
+    main()
