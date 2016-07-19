8fa6ece1059055a00c594e2ec1c7a5be1e8e5525
Masaki Saito <msaito@preferred.jp>
2016-07-19 23:23:25 +0900

    Add tests for Cholesky decomposition

--- tests/cupy_tests/linalg_tests/test_decomposition.py
+++ tests/cupy_tests/linalg_tests/test_decomposition.py
@@ -6,6 +6,24 @@ from cupy import testing
 
 
 @testing.gpu
+class TestCholeskyDecomposition(unittest.TestCase):
+
+    _multiprocess_can_split_ = True
+
+    @testing.for_float_dtypes(no_float16=True)
+    @testing.numpy_cupy_allclose(atol=1e-5)
+    def check_L(self, array, xp, dtype):
+        a = xp.asarray(array, dtype=dtype)
+        return getattr(xp, 'linalg').cholesky(a)
+
+    def test_all(self):
+        # A normal positive definite matrix
+        self.check_L(numpy.array([[2, -1, 0], [-1, 2, -1], [0, -1, 2]]))
+        # np.linalg.cholesky only uses a lower triangle of an array
+        self.check_L(numpy.array([[1, 2], [1, 9]]))
+
+
+@testing.gpu
 class TestSVD(unittest.TestCase):
 
     _multiprocess_can_split_ = True
