8064878b153f97e858e61ebb51471bd5c2c35e05
Kenta Oono <k.oono.delta@gmail.com>
2016-07-30 16:50:28 +0900

    Do not use metaclass to change classname

--- chainer/utils/rnn.py
+++ chainer/utils/rnn.py
@@ -13,70 +13,68 @@ def _force_tuple(x):
 
 def create_stateful_rnn(stateless_class, name):
 
-    class name_setter(type):
+    def __init__(self, *args, **kwargs):
+        link.Chain.__init__(
+            self, stateless=stateless_class(*args, **kwargs))
+        self.state_names = self.stateless.state_names
+        self.state_shapes = self.stateless.state_shapes
+        self.state_name_to_idx = dict((name, i) for i, name
+                                      in enumerate(self.state_names))
+        self.reset_state()
 
-        def __new__(cls, _, bases, dict):
-            return type.__new__(cls, name, bases, dict)
+    def to_cpu(self):
+        for s in self.states:
+            if s is not None:
+                s.to_cpu()
 
-    class Stateful(link.Chain):
+    def to_gpu(self, device=None):
+        for s in self.states:
+            if s is not None:
+                s.to_gpu(device)
 
-        __metaclass__ = name_setter
-
-        def __init__(self, *args, **kwargs):
-            super(Stateful, self).__init__(
-                stateless=stateless_class(*args, **kwargs))
-            self.state_names = self.stateless.state_names
-            self.state_shapes = self.stateless.state_shapes
-            self.state_name_to_idx = dict((name, i) for i, name
-                                          in enumerate(self.state_names))
-            self.reset_state()
-
-        def to_cpu(self):
-            for s in self.states:
-                if s is not None:
-                    s.to_cpu()
-
-        def to_gpu(self, device=None):
-            for s in self.states:
-                if s is not None:
-                    s.to_gpu(device)
-
-        def set_state(self, *values):
-            def _convert(x):
-                assert isinstance(x, variable.Variable)
-                x_ = x
-                if self.xp is numpy:
-                    x_.to_cpu()
-                else:
-                    x_.to_gpu()
-                return x_
-
-            assert len(values) == len(self.states)
-
-            for i, value in enumerate(values):
-                self.states[i] = _convert(value)
-
-        def reset_state(self):
-            self.states = [None] * len(self.state_names)
-
-        def __getattr__(self, name):
-            if name in self.state_names:
-                idx = self.state_name_to_idx[name]
-                return self.states[idx]
-            elif hasattr(self.stateless, name):
-                return getattr(self.stateless, name)
+    def set_state(self, *values):
+        def _convert(x):
+            assert isinstance(x, variable.Variable)
+            x_ = x
+            if self.xp is numpy:
+                x_.to_cpu()
             else:
-                return super(Stateful, self).__getattr__(name)
+                x_.to_gpu()
+            return x_
 
-        def __call__(self, x):
-            for i, val in enumerate(self.states):
-                if self.states[i] is None:
-                    self.states[i] = self.xp.zeros(
-                        (len(x.data),) + self.state_shapes[i],
-                        dtype=numpy.float32)
+        assert len(values) == len(self.states)
 
-            args = tuple(self.states) + (x,)
-            self.states = _force_tuple(self.stateless(*args))
-            return self.states[-1]
+        for i, value in enumerate(values):
+            self.states[i] = _convert(value)
 
-    return Stateful
+    def reset_state(self):
+        self.states = [None] * len(self.state_names)
+
+    def __getattr__(self, name):
+        if name in self.state_names:
+            idx = self.state_name_to_idx[name]
+            return self.states[idx]
+        elif hasattr(self.stateless, name):
+            return getattr(self.stateless, name)
+        else:
+            return link.Chain.__getattr__(self, name)
+
+    def __call__(self, x):
+        for i, val in enumerate(self.states):
+            if self.states[i] is None:
+                self.states[i] = self.xp.zeros(
+                    (len(x.data),) + self.state_shapes[i],
+                    dtype=numpy.float32)
+
+        args = tuple(self.states) + (x,)
+        self.states = _force_tuple(self.stateless(*args))
+        return self.states[-1]
+
+    return type(name, (link.Chain,),
+                {'__init__': __init__,
+                 'to_cpu': to_cpu,
+                 'to_gpu': to_gpu,
+                 'set_state': set_state,
+                 'reset_state': reset_state,
+                 '__getattr__': __getattr__,
+                 '__call__': __call__})
