1a1fb09e28278d2945b2dcc71fc78c1bcf768a58
Kenta OONO <oono@preferred.jp>
2016-08-10 16:33:34 +0900

    Fix SGD to use same code for CPU and GPU

--- chainer/optimizers/sgd.py
+++ chainer/optimizers/sgd.py
@@ -9,10 +9,5 @@ class SGD(optimizer.GradientMethod):
     def __init__(self, lr=0.01):
         self.lr = lr
 
-    def update_one_cpu(self, param, state):
+    def update_one(self, param, state):
         param.data -= self.lr * param.grad
-
-    def update_one_gpu(self, param, state):
-        cuda.elementwise('T grad, T lr', 'T param',
-                         'param -= lr * grad',
-                         'sgd')(param.grad, self.lr, param.data)
