602c70a2c73974976967493fb35ee4bcaf05f4ec
Yuya Unno <unnonouno@gmail.com>
2016-08-28 11:35:09 +0900

    Implement csc matrix

--- cupy/sparse/__init__.py
+++ cupy/sparse/__init__.py
@@ -1,6 +1,8 @@
 from cupy.sparse import coo
+from cupy.sparse import csc
 from cupy.sparse import csr
 
 
 coo_matrix = coo.coo_matrix
+csc_matrix = csc.csc_matrix
 csr_matrix = csr.csr_matrix
---
+++ cupy/sparse/csc.py
@@ -0,0 +1,58 @@
+import numpy
+import scipy.sparse
+
+import cupy
+from cupy.cuda import cusparse
+
+
+class csc_matrix(object):
+
+    format = 'csc'
+
+    def __init__(self, arg1, shape=None, dtype=None, copy=False):
+        if len(arg1) == 3:
+            data, indices, indptr = arg1
+            assert len(data) == len(indices)
+            self.data = data
+            self.indices = indices
+            self.indptr = indptr
+
+            if shape is None:
+                shape = (int(indices.max()) + 1, len(indptr) - 1)
+        else:
+            raise NotImplemented
+
+        assert shape[1] == len(indptr) - 1
+
+        self.handle = cusparse.create()
+        self._descr = cusparse.createMatDescr()
+        cusparse.setMatType(self._descr, cusparse.CUSPARSE_MATRIX_TYPE_GENERAL)
+        cusparse.setMatIndexBase(
+            self._descr, cusparse.CUSPARSE_INDEX_BASE_ZERO)
+        self.shape = shape
+
+    def get(self, stream=None):
+        data = self.data.get(stream)
+        indices = self.indices.get(stream)
+        indptr = self.indptr.get(stream)
+        return scipy.sparse.csc_matrix(
+            (data, indices, indptr), shape=self.shape)
+
+    def __str__(self):
+        return str(self.get())
+
+    @property
+    def ndim(self):
+        return 2
+
+    @property
+    def nnz(self):
+        return len(self.data)
+
+    def toarray(self, order=None, out=None):
+        A = cupy.zeros(self.shape, 'f')
+        cusparse.scsr2dense(
+            self.handle, self.shape[1], self.shape[0], self._descr,
+            self.data.data.ptr, self.indptr.data.ptr, self.indices.data.ptr,
+            A.data.ptr, self.shape[1])
+        return A
