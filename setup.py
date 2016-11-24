b270068443aa9c03da50e181f49a8d7e370df886
Seiya Tokui <beam.web@gmail.com>
2016-11-24 10:34:02 +0900

    Remove cupy from the source tree

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
               'chainer.training',
               'chainer.training.extensions',
               'chainer.training.triggers',
-              'chainer.utils'],
+              'chainer.utils',],
     zip_safe=False,
     setup_requires=setup_requires,
     install_requires=install_requires,
