34f7b6600f1491640f6f5de0269671c2efde78e8
Yuya Unno <unnonouno@gmail.com>
2017-01-19 01:00:21 +0900

    Fix GPU code

--- chainer/functions/array/thin_stack.py
+++ chainer/functions/array/thin_stack.py
@@ -1,3 +1,5 @@
+import numpy
+
 from chainer import cuda
 from chainer import function
 from chainer.utils import type_check
@@ -23,15 +25,48 @@ class ThinStackSet(function.Function):
         )
 
     def forward(self, inputs):
+        xp = cuda.get_array_module(*inputs)
         stack, indices, values = inputs
-        stack[range(len(indices)), indices] = values
+        if xp is numpy:
+            stack[range(len(indices)), indices] = values
+        else:
+            cuda.elementwise(
+                'S t, T v, int32 d',
+                'raw T s',
+                '''
+                int b = i / d;
+                int k = i - b * d;
+                int ind[] = {b, t, k};
+                s[ind] = v;
+                ''',
+                'thin_stack_set_fwd'
+            )(indices[:, None], values, values.shape[1], stack)
+
         return stack,
 
     def backward(self, inputs, grads):
+        xp = cuda.get_array_module(*inputs)
         _, indices, _ = inputs
         g = grads[0]
-        gv = g[range(len(indices)), indices]
-        g[range(len(indices)), indices] = 0
+        if xp is numpy:
+            gv = g[range(len(indices)), indices]
+            g[range(len(indices)), indices] = 0
+        else:
+            dim = g.shape[2]
+            shape = (indices.shape[0], dim)
+            gv = cuda.cupy.empty(shape, g.dtype)
+            cuda.elementwise(
+                'S t, int32 d',
+                'raw T s, T y',
+                '''
+                int b = i / d;
+                int k = i - b * d;
+                int ind[] = {b, t, k};
+                y = s[ind];
+                s[ind] = 0;
+                ''',
+                'thin_stack_set_bwd'
+            )(indices[:, None], dim, g, gv)
         return g, None, gv
 
 
@@ -53,17 +88,49 @@ class ThinStackGet(function.Function):
         )
 
     def forward(self, inputs):
+        xp = cuda.get_array_module(*inputs)
         stack, indices = inputs
-        return stack[range(len(indices)), indices], stack
+        if xp is numpy:
+            return stack[range(len(indices)), indices], stack
+        else:
+            dim = stack.shape[2]
+            shape = (indices.shape[0], dim)
+            y = cuda.cupy.empty(shape, stack.dtype)
+            cuda.elementwise(
+                'S t, int32 d, raw T s',
+                'T y',
+                '''
+                int b = i / d;
+                int k = i - b * d;
+                int ind[] = {b, t, k};
+                y = s[ind];
+                ''',
+                'thin_stack_get_fwd'
+            )(indices[:, None], dim, stack, y)
+            return y, stack
 
     def backward(self, inputs, grads):
+        xp = cuda.get_array_module(*inputs)
         stack, indices = inputs
         g, gs = grads
         if gs is None:
-            xp = cuda.get_array_module(stack)
             gs = xp.zeros_like(stack)
         if g is not None:
-            gs[range(len(indices)), indices] += g
+            if xp is numpy:
+                gs[range(len(indices)), indices] += g
+            else:
+                dim = stack.shape[2]
+                cuda.elementwise(
+                    'S t, int32 d',
+                    'raw T gs, T g',
+                    '''
+                    int b = i / d;
+                    int k = i - b * d;
+                    int ind[] = {b, t, k};
+                    gs[ind] += g;
+                    ''',
+                    'thin_stack_get_bwd'
+                )(indices[:, None], dim, gs, g)
         return gs, None
 
 
