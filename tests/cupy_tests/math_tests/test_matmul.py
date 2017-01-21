33135717277f71c70fa4d986b3083b1664742a76
fukatani <nannyakannya@gmail.com>
2017-01-21 22:56:50 +0900

    Flake8

--- tests/cupy_tests/math_tests/test_matmul.py
+++ tests/cupy_tests/math_tests/test_matmul.py
@@ -47,7 +47,7 @@ class TestMatmul(unittest.TestCase):
     @testing.numpy_cupy_allclose(rtol=1e-4, atol=1e-3)  # required for uint8
     def test_operator_matmul(self, xp, dtype1, dtype2):
         if not (numpy.result_type(dtype1, dtype2) == numpy.float32 or
-                    numpy.result_type(dtype1, dtype2) == numpy.float64):
+                numpy.result_type(dtype1, dtype2) == numpy.float64):
             return xp.array([])
         x1 = testing.shaped_arange(self.shape_pair[0], xp, dtype1)
         x2 = testing.shaped_arange(self.shape_pair[1], xp, dtype2)
@@ -59,7 +59,7 @@ class TestMatmul(unittest.TestCase):
     @testing.numpy_cupy_allclose(rtol=1e-4, atol=1e-3)  # required for uint8
     def test_cupy_matmul(self, xp, dtype1, dtype2):
         if not (numpy.result_type(dtype1, dtype2) == numpy.float32 or
-                    numpy.result_type(dtype1, dtype2) == numpy.float64):
+                numpy.result_type(dtype1, dtype2) == numpy.float64):
             return xp.array([])
         x1 = testing.shaped_arange(self.shape_pair[0], xp, dtype1)
         x2 = testing.shaped_arange(self.shape_pair[1], xp, dtype2)
