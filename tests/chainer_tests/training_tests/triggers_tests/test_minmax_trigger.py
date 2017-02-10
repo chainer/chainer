8fceb1728f48298fa569202626e612d98daac559
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-02-10 18:27:08 +0900

    add dummy epoch_detail

--- tests/chainer_tests/training_tests/triggers_tests/test_minmax_trigger.py
+++ tests/chainer_tests/training_tests/triggers_tests/test_minmax_trigger.py
@@ -26,6 +26,10 @@ class DummyUpdater(training.Updater):
         return 1
 
     @property
+    def epoch_detail(self):
+        return self.iteration
+
+    @property
     def is_new_epoch(self):
         return False
 
