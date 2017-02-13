31e96483359a8d148b05e3125b8223bf95831ab5
Masaki Saito <msaito@preferred.jp>
2017-02-13 18:11:53 +0900

    Update __init__.py

--- cupy/linalg/__init__.py
+++ cupy/linalg/__init__.py
@@ -2,14 +2,12 @@
 # http://docs.scipy.org/doc/numpy/reference/routines.linalg.html
 
 # "NOQA" to suppress flake8 warning
+from cupy.linalg import cholesky  # NOQA
 from cupy.linalg import decomposition  # NOQA
 from cupy.linalg import eigenvalue  # NOQA
 from cupy.linalg import norms  # NOQA
 from cupy.linalg.norms import norm  # NOQA
 from cupy.linalg import product  # NOQA
+from cupy.linalg import qr  # NOQA
 from cupy.linalg import solve  # NOQA
-
-
-cholesky = decomposition.cholesky
-qr = decomposition.qr
-svd = decomposition.svd
+from cupy.linalg import svd  # NOQA
