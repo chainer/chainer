a97866b7e3651700a152615e42b3461986bc9daf
Yuya Unno <unnonouno@gmail.com>
2017-03-02 19:13:49 +0900

    Remove volatile from tests

--- tests/chainer_tests/functions_tests/connection_tests/test_n_step_lstm.py
+++ tests/chainer_tests/functions_tests/connection_tests/test_n_step_lstm.py
@@ -62,13 +62,13 @@ class TestNStepLSTM(unittest.TestCase):
         self.dhy = numpy.random.uniform(-1, 1, h_shape).astype(numpy.float32)
 
     def check_forward(
-            self, h_data, c_data, xs_data, ws_data, bs_data, volatile):
-        h = chainer.Variable(h_data, volatile=volatile)
-        c = chainer.Variable(c_data, volatile=volatile)
-        xs = [chainer.Variable(x, volatile=volatile) for x in xs_data]
-        ws = [[chainer.Variable(w, volatile=volatile) for w in ws]
+            self, h_data, c_data, xs_data, ws_data, bs_data):
+        h = chainer.Variable(h_data)
+        c = chainer.Variable(c_data)
+        xs = [chainer.Variable(x) for x in xs_data]
+        ws = [[chainer.Variable(w) for w in ws]
               for ws in ws_data]
-        bs = [[chainer.Variable(b, volatile=volatile) for b in bs]
+        bs = [[chainer.Variable(b) for b in bs]
               for bs in bs_data]
         with chainer.using_config('use_cudnn', self.use_cudnn):
             hy, cy, ys = functions.n_step_lstm(
@@ -103,10 +103,7 @@ class TestNStepLSTM(unittest.TestCase):
         testing.assert_allclose(cy.data, e_cy, rtol=1e-4, atol=1e-4)
 
     def test_forward_cpu(self):
-        self.check_forward(self.hx, self.cx, self.xs, self.ws, self.bs, False)
-
-    def test_forward_cpu_volatile(self):
-        self.check_forward(self.hx, self.cx, self.xs, self.ws, self.bs, True)
+        self.check_forward(self.hx, self.cx, self.xs, self.ws, self.bs)
 
     @attr.gpu
     def test_forward_gpu(self):
@@ -114,17 +111,7 @@ class TestNStepLSTM(unittest.TestCase):
                            cuda.to_gpu(self.cx),
                            [cuda.to_gpu(x) for x in self.xs],
                            [[cuda.to_gpu(w) for w in ws] for ws in self.ws],
-                           [[cuda.to_gpu(b) for b in bs] for bs in self.bs],
-                           False)
-
-    @attr.gpu
-    def test_forward_gpu_volatile(self):
-        self.check_forward(cuda.to_gpu(self.hx),
-                           cuda.to_gpu(self.cx),
-                           [cuda.to_gpu(x) for x in self.xs],
-                           [[cuda.to_gpu(w) for w in ws] for ws in self.ws],
-                           [[cuda.to_gpu(b) for b in bs] for bs in self.bs],
-                           True)
+                           [[cuda.to_gpu(b) for b in bs] for bs in self.bs])
 
     def check_backward(self, h_data, c_data, xs_data, ws_data, bs_data,
                        dhy_data, dcy_data, dys_data):
@@ -215,13 +202,12 @@ class TestNStepLSTMCudnnCall(unittest.TestCase):
             self.expect = chainer.should_use_cudnn('>=auto', 5000)
 
     def forward(self, train):
-        volatile = not train
-        h = chainer.Variable(self.hx, volatile=volatile)
-        c = chainer.Variable(self.cx, volatile=volatile)
-        xs = [chainer.Variable(x, volatile=volatile) for x in self.xs]
-        ws = [[chainer.Variable(w, volatile=volatile) for w in ws]
+        h = chainer.Variable(self.hx)
+        c = chainer.Variable(self.cx)
+        xs = [chainer.Variable(x) for x in self.xs]
+        ws = [[chainer.Variable(w) for w in ws]
               for ws in self.ws]
-        bs = [[chainer.Variable(b, volatile=volatile) for b in bs]
+        bs = [[chainer.Variable(b) for b in bs]
               for bs in self.bs]
         with chainer.using_config('train', train):
             return functions.n_step_lstm(
