408c6ea09dbbe0194f8b1faebac6307f2afc4ebc
Yuya Unno <unnonouno@gmail.com>
2017-03-11 17:36:25 +0900

    Use view in array method when copy=False

--- cupy/core/core.pyx
+++ cupy/core/core.pyx
@@ -1674,6 +1674,9 @@ cpdef ndarray array(obj, dtype=None, bint copy=True, Py_ssize_t ndmin=0):
 
         ndim = a._shape.size()
         if ndmin > ndim:
+            if not copy:
+                # When `copy` is False, `a` is same as `obj`.
+                a = a.view()
             a.shape = (1,) * (ndmin - ndim) + a.shape
         return a
     else:
