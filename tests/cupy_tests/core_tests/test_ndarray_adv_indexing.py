4e11d1c8929ee5ff07367f46870e1018cabc83a8
Yusuke Niitani <yuyuniitani@gmail.com>
2017-03-17 13:39:38 +0900

    add  [[1], [1]] as test case

--- tests/cupy_tests/core_tests/test_ndarray_adv_indexing.py
+++ tests/cupy_tests/core_tests/test_ndarray_adv_indexing.py
@@ -85,6 +85,7 @@ class TestArrayAdvancedIndexingPerm(unittest.TestCase):
     {'shape': (2, 3, 4), 'indexes': [1, 1]},
     {'shape': (2, 3, 4), 'indexes': [[1]]},
     {'shape': (2, 3, 4), 'indexes': [[1, 1]]},
+    {'shape': (2, 3, 4), 'indexes': [[1], [1]]},
     {'shape': (2, 3, 4), 'indexes': [[1, 1], 1]},
     {'shape': (2, 3, 4), 'indexes': [[1], slice(1, 2)]},
     {'shape': (2, 3, 4), 'indexes': [[[1]], slice(1, 2)]},
