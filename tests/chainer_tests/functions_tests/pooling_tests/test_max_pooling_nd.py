be0842b6f0f89ce52fc232cdd385ab133572c823
Yuya Unno <unnonouno@gmail.com>
2017-04-14 23:26:10 +0900

    Support non-contiguous arrays in pooling functions

--- tests/chainer_tests/functions_tests/pooling_tests/test_max_pooling_nd.py
+++ tests/chainer_tests/functions_tests/pooling_tests/test_max_pooling_nd.py
@@ -104,6 +104,11 @@ class TestMaxPoolingND(unittest.TestCase):
     def test_forward_gpu(self):
         self.check_forward(cuda.to_gpu(self.x))
 
+    @attr.cudnn
+    @condition.retry(3)
+    def test_forward_gpu_non_contiguous(self):
+        self.check_forward(cuda.cupy.asfortranarray(cuda.to_gpu(self.x)))
+
     @attr.gpu
     @condition.retry(3)
     def test_forward_gpu_no_cudnn(self):
@@ -156,6 +161,13 @@ class TestMaxPoolingND(unittest.TestCase):
     @condition.retry(3)
     def test_backward_gpu(self):
         self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
+
+    @attr.cudnn
+    @condition.retry(3)
+    def test_backward_gpu(self):
+        self.check_backward(
+            cuda.cupy.asfortranarray(cuda.to_gpu(self.x)),
+            cuda.cupy.asfortranarray(cuda.to_gpu(self.gy)))
 
     @attr.gpu
     @condition.retry(3)
