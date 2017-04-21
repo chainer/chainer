5792d44f5159d3e4511c4687dee2a65c538aff6a
ysekky <s.yoshi.mobile.1123@gmail.com>
2017-04-21 20:13:48 +0900

    modify docstring for IDE friendly in deconvolution2D

--- chainer/links/connection/deconvolution_2d.py
+++ chainer/links/connection/deconvolution_2d.py
@@ -16,7 +16,7 @@ class Deconvolution2D(link.Link):
     and holds the filter weight and bias vector as parameters.
 
     Args:
-        in_channels (int): Number of channels of input arrays. If ``None``,
+        in_channels (int or None): Number of channels of input arrays. If ``None``,
             parameter initialization will be deferred until the first forward
             data pass at which time the size will be determined.
         out_channels (int): Number of channels of output arrays.
