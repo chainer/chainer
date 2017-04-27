7e4117cbaa0ccb1ed0134384252f443e47f3a3df
Akira Naruse <anaruse@nvidia.com>
2017-04-27 21:14:55 +0900

    Remove some redundant code

--- chainer/functions/normalization/batch_normalization.py
+++ chainer/functions/normalization/batch_normalization.py
@@ -56,24 +56,17 @@ class BatchNormalizationFunction(function.Function):
                 '%s == %s' % (in_types.size(), n_in))
         x_type, gamma_type, beta_type = in_types[:3]
         M = gamma_type.ndim.eval()
-        if x_type.dtype.eval() == numpy.float16:
+        type_check.expect(
+            x_type.dtype.kind == 'f',
+            x_type.ndim >= gamma_type.ndim + 1,
+            x_type.shape[1:1 + M] == gamma_type.shape,
+            beta_type.dtype == gamma_type.dtype,
+            beta_type.shape == gamma_type.shape,
+        )
+        if x_type.dtype.eval() != numpy.float16:
             type_check.expect(
-                x_type.dtype.kind == 'f',
-                x_type.ndim >= gamma_type.ndim + 1,
-                x_type.shape[1:1 + M] == gamma_type.shape,
                 # TODO(beam2d): Check shape
-                gamma_type.dtype == beta_type.dtype,
-                gamma_type.shape == beta_type.shape,
-            )
-        else:
-            type_check.expect(
-                x_type.dtype.kind == 'f',
-                x_type.ndim >= gamma_type.ndim + 1,
-                x_type.shape[1:1 + M] == gamma_type.shape,
-                # TODO(beam2d): Check shape
-                gamma_type.dtype == x_type.dtype,
-                beta_type.dtype == x_type.dtype,
-                gamma_type.shape == beta_type.shape,
+                x_type.dtype == gamma_type.dtype,
             )
         if len(in_types) == 5:
             mean_type, var_type = in_types[3:]
