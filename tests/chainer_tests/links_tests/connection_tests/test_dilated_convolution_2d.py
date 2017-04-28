f76cc9710356d8b6712d0a6141c45b5472356822
Shunta Saito <shunta.saito@gmail.com>
2017-04-28 17:17:41 +0900

    Fix parmetrizing way

--- tests/chainer_tests/links_tests/connection_tests/test_dilated_convolution_2d.py
+++ tests/chainer_tests/links_tests/connection_tests/test_dilated_convolution_2d.py
@@ -114,13 +114,12 @@ class TestDilatedConvolution2D(unittest.TestCase):
 
 
 @testing.parameterize(
-    {'init_args': ((2, 3), {'stride': 2, 'pad': 2, 'dilate': 2})},
-    {'init_args': ((None, 2, 3), {'stride': 2, 'pad': 2, 'dilate': 2})})
+    {'args': (2, 3), 'kwargs': {'stride': 2, 'pad': 2, 'dilate': 2}},
+    {'args': (None, 2, 3), 'kwargs': {'stride': 2, 'pad': 2, 'dilate': 2}})
 class TestDilatedConvolution2DParameterShapePlaceholder(unittest.TestCase):
 
     def setUp(self):
-        args, kwargs = self.init_args
-        self.link = links.DilatedConvolution2D(*args, **kwargs)
+        self.link = links.DilatedConvolution2D(*self.args, **self.kwargs)
         self.x = numpy.random.uniform(-1, 1,
                                       (2, 3, 4, 3)).astype(numpy.float32)
         self.link(chainer.Variable(self.x))
