62f71f7cd1600cf03df70d35a00979b049ce261e
Kenta OONO <oono@preferred.jp>
2017-04-27 22:57:57 -0700

    Remove reduce option from F.gaussian_nll

--- tests/chainer_tests/functions_tests/loss_tests/test_vae.py
+++ tests/chainer_tests/functions_tests/loss_tests/test_vae.py
@@ -4,6 +4,7 @@ import numpy
 
 import chainer
 from chainer import cuda
+from chainer import functions as F
 from chainer.functions import vae
 from chainer import testing
 from chainer.testing import attr
@@ -69,10 +70,6 @@ class TestBernoulliNLL(unittest.TestCase):
                                  cuda.to_gpu(self.y))
 
 
-@testing.parameterize(
-    {'reduce': 'no'},
-    {'reduce': 'sum'}
-)
 class TestGaussianNLL(unittest.TestCase):
 
     def setUp(self):
@@ -88,14 +85,12 @@ class TestGaussianNLL(unittest.TestCase):
         self.expect = (0.5 * numpy.log(2 * numpy.pi) +
                        0.5 * self.ln_var +
                        x_d * x_d / var * 0.5)
-        if self.reduce == 'sum':
-            self.expect = numpy.sum(self.expect)
 
     def check_gaussian_nll(self, x_data, mean_data, ln_var_data):
         x = chainer.Variable(x_data)
         mean = chainer.Variable(mean_data)
         ln_var = chainer.Variable(ln_var_data)
-        actual = cuda.to_cpu(F.gaussian_nll(x, mean, ln_var, self.reduce).data)
+        actual = cuda.to_cpu(F.gaussian_nll(x, mean, ln_var).data)
         testing.assert_allclose(self.expect, actual)
 
     @condition.retry(3)
@@ -108,29 +103,6 @@ class TestGaussianNLL(unittest.TestCase):
         self.check_gaussian_nll(cuda.to_gpu(self.x),
                                 cuda.to_gpu(self.mean),
                                 cuda.to_gpu(self.ln_var))
-
-
-class TestGaussianNLLInvalidReductionOption(unittest.TestCase):
-
-    def setUp(self):
-        self.x = numpy.random.uniform(-1, 1, (3,)).astype(numpy.float32)
-        self.mean = numpy.random.uniform(-1, 1, (3,)).astype(numpy.float32)
-        self.ln_var = numpy.random.uniform(-1, 1, (3,)).astype(numpy.float32)
-
-    def check_invalid_option(self, xp):
-        x = chainer.Variable(xp.asarray(self.x))
-        mean = chainer.Variable(xp.asarray(self.mean))
-        ln_var = chainer.Variable(xp.asarray(self.ln_var))
-
-        with self.assertRaises(ValueError):
-            F.gaussian_nll(x, mean, ln_var, 'invalid_option')
-
-    def test_invalid_option_cpu(self):
-        self.check_invalid_option(numpy)
-
-    @attr.gpu
-    def test_invalid_option_gpu(self):
-        self.check_invalid_option(cuda.cupy)
 
 
 testing.run_module(__name__, __file__)
