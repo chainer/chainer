98dd19753f43f0d552c99ecc11991dc89a60cca5
Yuya Unno <unnonouno@gmail.com>
2017-05-01 01:24:47 +0900

    Test grad type

--- tests/chainer_tests/functions_tests/array_tests/test_copy.py
+++ tests/chainer_tests/functions_tests/array_tests/test_copy.py
@@ -46,6 +46,7 @@ class Copy(unittest.TestCase):
         y.backward()
 
         x_grad = x.grad
+        self.assertEqual(cuda.get_device(x_grad).id, src_id)
         numpy.testing.assert_array_equal(
             cuda.to_cpu(x_grad), self.gy)
 
