64a5b29e15f633a29b753fa1114a9545e21c6ba2
Seiya Tokui <beam.web@gmail.com>
2017-05-01 15:34:00 +0900

    Fix extension tests

--- tests/chainer_tests/training_tests/test_extension.py
+++ tests/chainer_tests/training_tests/test_extension.py
@@ -14,15 +14,18 @@ class TestExtension(unittest.TestCase):
         self.assertEqual(ext.default_name, 'MyExtension')
 
     def test_make_extension(self):
+        def initialize(trainer):
+            pass
+
         @training.make_extension(trigger=(2, 'epoch'), default_name='my_ext',
-                                 priority=50, invoke_before_training=True)
+                                 priority=50, initializer=initialize)
         def my_extension(trainer):
             pass
 
         self.assertEqual(my_extension.trigger, (2, 'epoch'))
         self.assertEqual(my_extension.default_name, 'my_ext')
         self.assertEqual(my_extension.priority, 50)
-        self.assertTrue(my_extension.invoke_before_training)
+        self.assertIs(my_extension.initialize, initialize)
 
     def test_make_extension_default_values(self):
         @training.make_extension()
@@ -32,7 +35,7 @@ class TestExtension(unittest.TestCase):
         self.assertEqual(my_extension.trigger, (1, 'iteration'))
         self.assertEqual(my_extension.default_name, 'my_extension')
         self.assertEqual(my_extension.priority, training.PRIORITY_READER)
-        self.assertFalse(my_extension.invoke_before_training)
+        self.assertIsNone(my_extension.initialize)
 
 
 testing.run_module(__name__, __file__)
