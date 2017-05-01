7cf1d816cd1a324ffa19e89f58c7251b23b92c98
keisuke-umezawa <keisuke.umezawa@gmail.com>
2017-05-01 23:06:27 +0900

    Improve docs of flatten

--- chainer/functions/array/flatten.py
+++ chainer/functions/array/flatten.py
@@ -13,12 +13,34 @@ class Flatten(function.Function):
 
 
 def flatten(x):
-    """Flatten a given array.
+    """Flatten a given array into one dimension.
 
     Args:
-        x (~chainer.Varaiable): Input variable.
+        x (list of :class:`~chainer.Variable` or :class:`numpy.ndarray` or \
+        :class:`cupy.ndarray`): Input variable.
 
     Returns:
-        ~chainer.Variable: Output variable.
+        ~chainer.Variable: Output variable flatten to one dimension.
+
+    .. admonition:: Example
+
+        >>> x = np.array([[1, 2], [3, 4]])
+        >>> x.shape
+        (2, 2)
+        >>> y = F.flatten(x)
+        >>> y.shape
+        (4,)
+        >>> y.data
+        array([1, 2, 3, 4])
+
+        >>> x = np.arange(8).reshape(2, 2, 2)
+        >>> x.shape
+        (2, 2, 2)
+        >>> y = F.flatten(x)
+        >>> y.shape
+        (8,)
+        >>> y.data
+        array([0, 1, 2, 3, 4, 5, 6, 7])
+
     """
     return Flatten()(x)
