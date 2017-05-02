7c3ea34884c52cc95819c364b1aab3fab005e9b0
Kenta OONO <oono@preferred.jp>
2017-05-02 00:39:40 -0700

    Remove wscale option from MLPConvolution2D

--- chainer/links/connection/mlp_convolution_2d.py
+++ chainer/links/connection/mlp_convolution_2d.py
@@ -44,8 +44,7 @@ class MLPConvolution2D(link.ChainList):
     """
 
     def __init__(self, in_channels, out_channels, ksize, stride=1, pad=0,
-                 activation=relu.relu, wscale=1,
-                 conv_init=None, bias_init=None):
+                 activation=relu.relu, conv_init=None, bias_init=None):
         assert len(out_channels) > 0
         convs = [convolution_2d.Convolution2D(
             in_channels, out_channels[0], ksize, stride, pad,
