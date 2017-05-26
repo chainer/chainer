ccb7eacf62ab4ce5d523e53871b971ef9829d94a
Shunta Saito <shunta.saito@gmail.com>
2017-05-26 14:12:44 +0900

    Remove get_device

--- chainer/cuda.py
+++ chainer/cuda.py
@@ -176,41 +176,6 @@ def get_device_from_array(*arrays):
     return DummyDevice
 
 
-def get_device(*args):
-    """Gets the device from a device object, an ID integer or an array object.
-
-    .. note::
-
-        This API is deprecated. Please use
-        :func:`~chainer.cuda.get_device_from_id`
-        or :func:`~chainer.cuda.get_device_from_array` instead.
-
-    This is a convenient utility to select a correct device if the type of
-    ``arg`` is unknown (i.e., one can use this function on arrays that may be
-    on CPU or GPU). The returned device object supports the context management
-    protocol of Python for the *with* statement.
-
-    Args:
-        args: Values to specify a GPU device. The first device object, integer
-            or :class:`cupy.ndarray` object is used to select a device.
-            If it is a device object, it is returned. If it is an integer,
-            the corresponding device is returned. If it is a CuPy array,
-            the device on which this array reside is returned. If any
-            arguments are neither integers nor CuPy arrays, a dummy device
-            object representing CPU is returned.
-
-    Returns:
-        Device object specified by given ``args``.
-
-    .. seealso::
-       See :class:`cupy.cuda.Device` for the device selection not by arrays.
-
-    """
-    warnings.warn('get_device is deprecated. Please use get_device_from_id or'
-                  ' get_device_from_array instead.', DeprecationWarning)
-    return _get_device(*args)
-
-
 def _get_device(*args):
     for arg in args:
         if type(arg) in _integer_types:
@@ -229,7 +194,6 @@ def _get_device(*args):
 # ------------------------------------------------------------------------------
 # cupy.ndarray allocation and copy
 # ------------------------------------------------------------------------------
-
 def to_gpu(array, device=None, stream=None):
     """Copies the given CPU array to specified device.
 
