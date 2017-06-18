de712a7ac996845569e26aab4e9742a9bdd288d5
fukatani <nannyakannya@gmail.com>
2017-06-18 14:10:20 +0900

    Fix doc

--- chainer/functions/math/matmul.py
+++ chainer/functions/math/matmul.py
@@ -166,13 +166,17 @@ def matmul(a, b, transa=False, transb=False):
 
     Args:
         a (Variable): The left operand of the matrix multiplication.
-            A 1-D array of shape ``(N,)`` is considered as an
-            :math:`N \\times 1` matrix.
+            If ``a`` and ``b`` are both 1-D arrays, ``matmul`` returns a dot
+            product of vector `a` and vector `b`. If 2-D arrays, ``matmul``
+            returns matrix product of ``a`` and ``b``. If arrays' dimension is
+            larger than 2, they are treated as a stack of matrices residing in
+            the last two indexes. ``matmul`` returns a stack of each two
+            arrays. ``a`` and ``b`` must have the same dimension.
         b (Variable): The right operand of the matrix multiplication.
             Its array is treated as a matrix in the same way as ``a``'s array.
-        transa (bool): If ``True``, transpose ``a``.
+        transa (bool): If ``True``, each matrices in ``a`` will be transposed.
             If ``a.ndim == 1``, do nothing.
-        transb (bool): If ``True``, transpose ``b``.
+        transb (bool): If ``True``, each matrices in ``b`` will be transposed.
             If ``b.ndim == 1``, do nothing.
 
     Returns:
