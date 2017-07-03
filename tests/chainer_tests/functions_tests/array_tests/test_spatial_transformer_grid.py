8c97afa78a990dd928deae371929842f1cdac734
Yusuke Niitani <yuyuniitani@gmail.com>
2017-07-03 15:35:43 +0900

    use v2 api for spatial_transformer_*

--- tests/chainer_tests/functions_tests/array_tests/test_spatial_transformer_grid.py
+++ tests/chainer_tests/functions_tests/array_tests/test_spatial_transformer_grid.py
@@ -2,6 +2,7 @@ import unittest
 
 import numpy
 
+import chainer
 from chainer import cuda
 from chainer import functions
 from chainer import gradient_check
@@ -11,7 +12,7 @@ from chainer.testing import condition
 
 
 @testing.parameterize(*testing.product({
-    'use_cudnn': [True, False],
+    'use_cudnn': ['always', 'never'],
 }))
 class TestSpatialTransformerGrid(unittest.TestCase):
 
@@ -22,9 +23,10 @@ class TestSpatialTransformerGrid(unittest.TestCase):
         self.grads = numpy.random.uniform(
             size=(B, 2) + self.output_shape).astype(self.theta.dtype)
 
-    def check_forward(self, theta, output_shape, use_cudnn=True):
-        grid = functions.spatial_transformer_grid(
-            theta, output_shape, use_cudnn).data
+    def check_forward(self, theta, output_shape, use_cudnn='always'):
+        with chainer.using_config('use_cudnn', use_cudnn):
+            grid = functions.spatial_transformer_grid(
+                theta, output_shape).data
 
         theta = cuda.to_cpu(theta)
         B = theta.shape[0]
@@ -49,10 +51,11 @@ class TestSpatialTransformerGrid(unittest.TestCase):
         self.check_forward(
             cuda.to_gpu(self.theta), self.output_shape, self.use_cudnn)
 
-    def check_backward(self, theta, output_shape, grads, use_cudnn=True):
-        gradient_check.check_backward(
-            functions.SpatialTransformerGrid(output_shape, use_cudnn),
-            (theta,), (grads,), atol=1e-4, rtol=1e-3)
+    def check_backward(self, theta, output_shape, grads, use_cudnn='always'):
+        with chainer.using_config('use_cudnn', use_cudnn):
+            gradient_check.check_backward(
+                functions.SpatialTransformerGrid(output_shape),
+                (theta,), (grads,), atol=1e-4, rtol=1e-3)
 
     @condition.retry(3)
     def test_backward_cpu(self):
