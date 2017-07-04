81cc9837ec2c830e7964e904a3b39f136fb07222
Masaki Saito <msaito@preferred.jp>
2017-07-04 18:36:24 +0900

    Add tests

--- tests/chainer_tests/training_tests/extensions_tests/test_plot_report.py
+++ tests/chainer_tests/training_tests/extensions_tests/test_plot_report.py
@@ -9,7 +9,7 @@ class TestPlotReport(unittest.TestCase):
 
     def test_available(self):
         try:
-            from matplotlib import pyplot  # NOQA
+            import matplotlib  # NOQA
             available = True
         except ImportError:
             available = False
@@ -22,6 +22,15 @@ class TestPlotReport(unittest.TestCase):
             self.assertEqual(len(w), 0)
         else:
             self.assertEqual(len(w), 1)
+
+    def test_lazy_import(self):
+        if extensions.PlotReport.available():
+            # To support python2, we do not use self.assertWarns()
+            with warnings.catch_warnings(record=True) as w:
+                import matplotlib
+                matplotlib.use('Agg')
+
+            self.assertEqual(len(w), 0)
 
 
 testing.run_module(__name__, __file__)
