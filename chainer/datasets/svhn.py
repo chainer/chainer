02ae96e691c3d545059092d720e5dfb77d0ae6f1
rcalland <richard.calland@ipmu.jp>
2017-07-10 17:41:48 +0900

    fixed flake8 errors

--- chainer/datasets/svhn.py
+++ chainer/datasets/svhn.py
@@ -6,11 +6,14 @@ from scipy import io
 from chainer.dataset import download
 from chainer.datasets import tuple_dataset
 
-def get_svhn(withlabel=True, scale=1., dtype=numpy.float32, label_dtype=numpy.int32):
+
+def get_svhn(withlabel=True, scale=1., dtype=numpy.float32,
+             label_dtype=numpy.int32):
     """Gets the SVHN dataset.
 
-    `SVHN <http://ufldl.stanford.edu/housenumbers/>` is a dataset similar to MNIST but
-    composed of cropped images of house numbers. The functionality is identical to the MNIST dataset,
+    `SVHN <http://ufldl.stanford.edu/housenumbers/>` is a dataset
+    similar to MNIST but composed of cropped images of house numbers.
+    The functionality is identical to the MNIST dataset,
     with the exception that there is no ``ndim`` argument.
 
     Args:
@@ -30,38 +33,45 @@ def get_svhn(withlabel=True, scale=1., dtype=numpy.float32, label_dtype=numpy.in
     """
     train_raw = _retrieve_svhn_training()
     train = _preprocess_svhn(train_raw, withlabel, scale, dtype,
-                              label_dtype)
+                             label_dtype)
     test_raw = _retrieve_svhn_test()
     test = _preprocess_svhn(test_raw, withlabel, scale, dtype,
-                             label_dtype)
+                            label_dtype)
     return train, test
 
+
 def _preprocess_svhn(raw, withlabel, scale, image_dtype, label_dtype):
-    images = raw["x"].transpose(3,2,0,1)
+    images = raw["x"].transpose(3, 2, 0, 1)
     images = images.astype(image_dtype)
     images *= scale / 255.
 
     labels = raw["y"].astype(label_dtype).flatten()
     # labels go from 1-10, with the digit "0" having label 10.
     # Set "0" to be label 0 to restore expected ordering
-    labels[labels==10] = 0
+    labels[labels == 10] = 0
 
     if withlabel:
         return tuple_dataset.TupleDataset(images, labels)
     else:
         return images
 
+
 def _retrieve_svhn_training():
-    return _retrieve_svhn("train.npz", "http://ufldl.stanford.edu/housenumbers/train_32x32.mat")
+    url = "http://ufldl.stanford.edu/housenumbers/train_32x32.mat"
+    return _retrieve_svhn("train.npz", url)
+
 
 def _retrieve_svhn_test():
-    return _retrieve_svhn("test.npz", "http://ufldl.stanford.edu/housenumbers/test_32x32.mat")
+    url = "http://ufldl.stanford.edu/housenumbers/test_32x32.mat"
+    return _retrieve_svhn("test.npz", url)
+
 
 def _retrieve_svhn(name, url):
     root = download.get_dataset_directory('pfnet/chainer/svhn')
     path = os.path.join(root, name)
     return download.cache_or_load_file(
         path, lambda path: _make_npz(path, url), numpy.load)
+
 
 def _make_npz(path, url):
     _path = download.cached_download(url)
