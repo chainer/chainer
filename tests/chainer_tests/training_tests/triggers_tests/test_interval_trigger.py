7cf42e5f9577f9a6e8b8f304e589976f7476e29c
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-07-10 16:06:16 +0900

    trigger -> triggers

--- tests/chainer_tests/training_tests/triggers_tests/test_interval_trigger.py
+++ tests/chainer_tests/training_tests/triggers_tests/test_interval_trigger.py
@@ -37,7 +37,7 @@ class TestIntervalTrigger(unittest.TestCase):
     def test_trigger(self):
         trainer = testing.get_trainer_with_mock_updater(
             stop_trigger=None, iter_per_epoch=self.iter_per_epoch)
-        trigger = training.trigger.IntervalTrigger(*self.interval)
+        trigger = training.triggers.IntervalTrigger(*self.interval)
         # before the first iteration, trigger should be False
         for expected in [False] + self.expected:
             self.assertEqual(trigger(trainer), expected)
@@ -47,13 +47,13 @@ class TestIntervalTrigger(unittest.TestCase):
         trainer = testing.get_trainer_with_mock_updater(
             stop_trigger=None, iter_per_epoch=self.iter_per_epoch)
         with tempfile.NamedTemporaryFile(delete=False) as f:
-            trigger = training.trigger.IntervalTrigger(*self.interval)
+            trigger = training.triggers.IntervalTrigger(*self.interval)
             for expected in self.expected[:self.resume]:
                 trainer.updater.update()
                 self.assertEqual(trigger(trainer), expected)
             serializers.save_npz(f.name, trigger)
 
-            trigger = training.trigger.IntervalTrigger(*self.interval)
+            trigger = training.triggers.IntervalTrigger(*self.interval)
             serializers.load_npz(f.name, trigger)
             for expected in self.expected[self.resume:]:
                 trainer.updater.update()
@@ -63,7 +63,7 @@ class TestIntervalTrigger(unittest.TestCase):
     def test_trigger_sparse_call(self):
         trainer = testing.get_trainer_with_mock_updater(
             stop_trigger=None, iter_per_epoch=self.iter_per_epoch)
-        trigger = training.trigger.IntervalTrigger(*self.interval)
+        trigger = training.triggers.IntervalTrigger(*self.interval)
         accumulated = False
         # before the first iteration, trigger should be False
         for expected in [False] + self.expected:
@@ -79,7 +79,7 @@ class TestIntervalTrigger(unittest.TestCase):
             stop_trigger=None, iter_per_epoch=self.iter_per_epoch)
         accumulated = False
         with tempfile.NamedTemporaryFile(delete=False) as f:
-            trigger = training.trigger.IntervalTrigger(*self.interval)
+            trigger = training.triggers.IntervalTrigger(*self.interval)
             for expected in self.expected[:self.resume]:
                 trainer.updater.update()
                 accumulated = accumulated or expected
@@ -88,7 +88,7 @@ class TestIntervalTrigger(unittest.TestCase):
                     accumulated = False
             serializers.save_npz(f.name, trigger)
 
-            trigger = training.trigger.IntervalTrigger(*self.interval)
+            trigger = training.triggers.IntervalTrigger(*self.interval)
             serializers.load_npz(f.name, trigger)
             for expected in self.expected[self.resume:]:
                 trainer.updater.update()
@@ -101,14 +101,14 @@ class TestIntervalTrigger(unittest.TestCase):
         trainer = testing.get_trainer_with_mock_updater(
             stop_trigger=None, iter_per_epoch=self.iter_per_epoch)
         with tempfile.NamedTemporaryFile(delete=False) as f:
-            trigger = training.trigger.IntervalTrigger(*self.interval)
+            trigger = training.triggers.IntervalTrigger(*self.interval)
             for expected in self.expected[:self.resume]:
                 trainer.updater.update()
                 self.assertEqual(trigger(trainer), expected)
             # old version does not save anything
             np.savez(f, dummy=0)
 
-            trigger = training.trigger.IntervalTrigger(*self.interval)
+            trigger = training.triggers.IntervalTrigger(*self.interval)
             serializers.load_npz(f.name, trigger)
             for expected in self.expected[self.resume:]:
                 trainer.updater.update()
