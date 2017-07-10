560b4e6d5fe7416567cb408b436578f7b410c605
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-07-10 16:05:49 +0900

    interval -> interval_trigger

--- chainer/training/triggers/__init__.py
+++ chainer/training/triggers/__init__.py
@@ -1,9 +1,9 @@
-from chainer.training.triggers import interval  # NOQA
+from chainer.training.triggers import interval_trigger  # NOQA
 from chainer.training.triggers import minmax_value_trigger  # NOQA
 
 
 # import class and function
-from chainer.training.triggers.interval import IntervalTrigger  # NOQA
+from chainer.training.triggers.interval_trigger import IntervalTrigger  # NOQA
 from chainer.training.triggers.manual_schedule_trigger import ManualScheduleTrigger  # NOQA
 from chainer.training.triggers.minmax_value_trigger import MaxValueTrigger  # NOQA
 from chainer.training.triggers.minmax_value_trigger import MinValueTrigger  # NOQA
