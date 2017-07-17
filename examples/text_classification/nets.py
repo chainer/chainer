aba35ecfeba64129575a7367f5eb87a5c38f1762
Sosuke Kobayashi <sosk@preferred.jp>
2017-07-17 10:51:25 +0900

    add text classification example

---
+++ examples/text_classification/nets.py
@@ -0,0 +1,144 @@
+# encoding: utf-8
+import numpy
+
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import reporter
+
+
+def sequence_embed(embed, xs, dropout=0.):
+    x_len = [len(x) for x in xs]
+    x_section = numpy.cumsum(x_len[:-1])
+    ex = embed(F.concat(xs, axis=0))
+    ex = F.dropout(ex, ratio=dropout)
+    exs = F.split_axis(ex, x_section, 0)
+    return exs
+
+
+def block_embed(embed, x, dropout=0.):
+    batch, length = x.shape
+    e = embed(x.reshape((batch * length, )))
+    # (batch * length, units)
+    e = F.transpose(F.stack(F.split_axis(e, batch, axis=0), axis=0), (0, 2, 1))
+    # (batch, units, length)
+    e = e[:, :, :, None]
+    # (batch, units, length, 1)
+    e = F.dropout(e, ratio=dropout)
+    return e
+
+
+class Classifier(chainer.Chain):
+
+    def __init__(self, encoder, n_class, dropout=0.1):
+        super(Classifier, self).__init__()
+        with self.init_scope():
+            self.encoder = encoder
+            self.output = L.Linear(encoder.out_units, n_class)
+        self.dropout = dropout
+
+    def __call__(self, xs, ys):
+        concat_encodings = F.dropout(self.encoder(xs), ratio=self.dropout)
+        concat_outputs = self.output(concat_encodings)
+        concat_truths = F.concat(ys, axis=0)
+
+        loss = F.softmax_cross_entropy(concat_outputs, concat_truths)
+        accuracy = F.accuracy(concat_outputs, concat_truths)
+        reporter.report({'loss': loss.data}, self)
+        reporter.report({'accuracy': accuracy.data}, self)
+        return loss
+
+
+class RNNEncoder(chainer.Chain):
+
+    def __init__(self, n_layers, n_vocab, n_units, dropout=0.1):
+        super(RNNEncoder, self).__init__(
+            embed=L.EmbedID(n_vocab, n_units),
+            encoder=L.NStepLSTM(n_layers, n_units, n_units, dropout),
+        )
+        self.out_units = n_units
+        self.dropout = dropout
+
+    def __call__(self, xs):
+        exs = sequence_embed(self.embed, xs, self.dropout)
+        last_h, last_c, ys = self.encoder(None, None, exs)
+        # last_h's shape = (n_layer, batchsize, n_units)
+        concat_outputs = last_h[-1]
+        return concat_outputs
+
+
+class CNNEncoder(chainer.Chain):
+
+    def __init__(self, n_layers, n_vocab, n_units, dropout=0.1):
+        out_units = n_units // 3
+        super(CNNEncoder, self).__init__(
+            embed=L.EmbedID(n_vocab, n_units, ignore_label=-1),
+            encoder_w2=L.Convolution2D(
+                n_units, out_units, ksize=(2, 1), stride=1, pad=(1, 0)),
+            encoder_w3=L.Convolution2D(
+                n_units, out_units, ksize=(3, 1), stride=1, pad=(1, 0)),
+            encoder_w4=L.Convolution2D(
+                n_units, out_units, ksize=(4, 1), stride=1, pad=(2, 0)),
+            mlp=MLP(n_layers, out_units * 3, dropout)
+        )
+        self.out_units = out_units * 3
+        self.dropout = dropout
+
+    def __call__(self, xs):
+        x_block = chainer.dataset.convert.concat_examples(xs, padding=-1)
+        ex_block = block_embed(self.embed, x_block, self.dropout)
+        h_w2 = F.max(self.encoder_w2(ex_block), axis=2)
+        h_w3 = F.max(self.encoder_w3(ex_block), axis=2)
+        h_w4 = F.max(self.encoder_w4(ex_block), axis=2)
+        h = F.concat([h_w2, h_w3, h_w4], axis=1)
+        h = F.relu(h)
+        h = F.dropout(h, ratio=self.dropout)
+        h = self.mlp(h)
+        return h
+
+
+class MLP(chainer.ChainList):
+
+    def __init__(self, n_layers, n_units, dropout=0.1):
+        super(MLP, self).__init__()
+        for i in range(n_layers):
+            self.add_link(L.Linear(None, n_units))
+        self.dropout = dropout
+        self.out_units = n_units
+
+    def __call__(self, x):
+        for i, link in enumerate(self.children()):
+            x = F.dropout(x, ratio=self.dropout)
+            x = F.relu(link(x))
+        return x
+
+
+class BOWEncoder(chainer.Chain):
+
+    def __init__(self, n_layers, n_vocab, n_units, dropout=0.1):
+        super(BOWEncoder, self).__init__(
+            embed=L.EmbedID(n_vocab, n_units, ignore_label=-1),
+        )
+        self.out_units = n_units
+        self.dropout = dropout
+
+    def __call__(self, xs):
+        x_block = chainer.dataset.convert.concat_examples(xs, padding=-1)
+        ex_block = block_embed(self.embed, x_block)
+        x_len = self.xp.array([len(x) for x in xs], 'i')[:, None, None]
+        h = F.sum(ex_block, axis=2) / x_len
+        return h
+
+
+class BOWMLPEncoder(chainer.Chain):
+    def __init__(self, n_layers, n_vocab, n_units, dropout=0.1):
+        super(BOWMLPEncoder, self).__init__(
+            bow_encoder=BOWEncoder(n_layers, n_vocab, n_units, dropout),
+            mlp_encoder=MLP(n_layers, n_units, dropout)
+        )
+        self.out_units = n_units
+
+    def __call__(self, xs):
+        h = self.bow_encoder(xs)
+        h = self.mlp_encoder(h)
+        return h
---
+++ examples/text_classification/text_datasets.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+import csv
+import glob
+import io
+import os
+import os.path
+from six.moves.urllib import request
+import tarfile
+
+import numpy
+
+from nlp_utils import make_vocab
+from nlp_utils import normalize_text
+from nlp_utils import split_text
+from nlp_utils import transform_to_array
+
+
+def download_dbpedia():
+    request.urlretrieve(
+        'https://github.com/le-scientifique/torchDatasets/raw/master/dbpedia_csv.tar.gz',  # NOQA
+        'dbpedia_csv.tar.gz')
+    tf = tarfile.open('dbpedia_csv.tar.gz', 'r')
+    tf.extractall('./')
+    os.remove('dbpedia_csv.tar.gz')
+
+
+def read_dbpedia(path, shrink=1, char_based=False):
+    dataset = []
+    with io.open(path, encoding='utf-8', errors='ignore') as f:
+        for i, (cls, title, text) in enumerate(csv.reader(f)):
+            if i % shrink != 0:
+                continue
+            tokens = split_text(normalize_text(text), char_based)
+            label = int(cls) - 1
+            dataset.append((tokens, label))
+    return dataset
+
+
+def get_dbpedia(path='./dbpedia_csv', vocab=None, shrink=1, char_based=False):
+    if not os.path.isdir(path):
+        print('download dbpedia')
+        download_dbpedia()
+    train_path = os.path.join(path, 'train.csv')
+    test_path = os.path.join(path, 'test.csv')
+
+    print('read dbpedia')
+    train = read_dbpedia(train_path, shrink=shrink, char_based=char_based)
+    test = read_dbpedia(test_path, shrink=shrink, char_based=char_based)
+
+    if vocab is None:
+        print('constract vocabulary based on frequency')
+        vocab = make_vocab(train)
+
+    train = transform_to_array(train, vocab)
+    test = transform_to_array(test, vocab)
+
+    return train, test, vocab
+
+
+def download_imdb():
+    request.urlretrieve(
+        'http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz',
+        'aclImdb_v1.tar.gz')
+    tf = tarfile.open('aclImdb_v1.tar.gz', 'r')
+    tf.extractall('./')
+    os.remove('aclImdb_v1.tar.gz')
+
+
+def read_imdb(base_path, shrink=1, fine_grained=False, char_based=False):
+    fg_label_dict = {'1': 0, '2': 0, '3': 1, '4': 1,
+                     '7': 2, '8': 2, '9': 3, '10': 3}
+
+    def read_and_label(path, label):
+        dataset = []
+        for i, f_path in enumerate(glob.glob(os.path.join(path, '*'))):
+            if i % shrink != 0:
+                continue
+            with io.open(f_path, encoding='utf-8', errors='ignore') as f:
+                text = f.read().strip()
+            tokens = split_text(normalize_text(text), char_based)
+            if fine_grained:
+                # extract from f_path. e.g. /pos/200_8.txt -> 8
+                label = fg_label_dict[f_path.split('_')[-1][:-4]]
+                dataset.append((tokens, label))
+            else:
+                dataset.append((tokens, label))
+        return dataset
+
+    pos_dataset = read_and_label(os.path.join(base_path, 'pos'), 0)
+    neg_dataset = read_and_label(os.path.join(base_path, 'neg'), 1)
+    return pos_dataset + neg_dataset
+
+
+def get_imdb(path='./aclImdb', vocab=None, shrink=1, fine_grained=False,
+             char_based=False):
+    if not os.path.isdir(path):
+        print('download imdb')
+        download_imdb()
+    train_path = os.path.join(path, 'train')
+    test_path = os.path.join(path, 'test')
+
+    print('read imdb')
+    train = read_imdb(train_path, shrink=shrink, fine_grained=fine_grained,
+                      char_based=char_based)
+    test = read_imdb(test_path, shrink=shrink, fine_grained=fine_grained,
+                     char_based=char_based)
+
+    if vocab is None:
+        print('constract vocabulary based on frequency')
+        vocab = make_vocab(train)
+
+    train = transform_to_array(train, vocab)
+    test = transform_to_array(test, vocab)
+
+    return train, test, vocab
+
+
+def download_other_dataset(path, name):
+    os.mkdir(path)
+    base_url = 'https://raw.githubusercontent.com/harvardnlp/sent-conv-torch/master/data/'  # NOQA
+    if name in ['custrev', 'mpqa', 'rt-polarity', 'subj']:
+        files = [name + '.all']
+    elif name == 'TREC':
+        files = [name + suff for suff in ['.train.all', '.test.all']]
+    else:
+        files = [name + suff for suff in ['.train', '.test']]
+    for f_name in files:
+        request.urlretrieve(
+            os.path.join(base_url, f_name),
+            os.path.join(path, f_name))
+
+
+def read_other_dataset(path, shrink=1, char_based=False):
+    dataset = []
+    with io.open(path, encoding='utf-8', errors='ignore') as f:
+        for i, l in enumerate(f):
+            if i % shrink != 0 or not len(l.strip()) >= 3:
+                continue
+            cls, text = l.strip().split(None, 1)
+            tokens = split_text(normalize_text(text), char_based)
+            label = int(cls)
+            dataset.append((tokens, label))
+    return dataset
+
+
+def get_other_text_dataset(name, path=None, vocab=None, shrink=1,
+                           char_based=False, seed=777):
+    assert(name in ['TREC', 'stsa.binary', 'stsa.fine',
+                    'custrev', 'mpqa', 'rt-polarity', 'subj'])
+
+    if path is None:
+        path = './' + name
+
+    if not os.path.isdir(path):
+        print('download ' + name)
+        download_other_dataset(path, name)
+
+    print('read ' + name)
+    if name in ['custrev', 'mpqa', 'rt-polarity', 'subj']:
+        print('split {} to train:test=9:1 by rand seed {}'.format(name, seed))
+        numpy.random.seed(seed)
+        alldata = numpy.random.permutation(
+            read_other_dataset(os.path.join(path, name + '.all'),
+                               shrink=shrink, char_based=char_based))
+        train = alldata[:-len(alldata) // 10]
+        test = alldata[-len(alldata) // 10:]
+    elif name == 'TREC':
+        train = read_other_dataset(os.path.join(path, name + '.train.all'),
+                                   shrink=shrink, char_based=char_based)
+        test = read_other_dataset(os.path.join(path, name + '.test.all'),
+                                  shrink=shrink, char_based=char_based)
+    else:
+        train = read_other_dataset(os.path.join(path, name + '.train'),
+                                   shrink=shrink, char_based=char_based)
+        test = read_other_dataset(os.path.join(path, name + '.test'),
+                                  shrink=shrink, char_based=char_based)
+
+    if vocab is None:
+        print('constract vocabulary based on frequency')
+        vocab = make_vocab(train)
+
+    train = transform_to_array(train, vocab)
+    test = transform_to_array(test, vocab)
+
+    return train, test, vocab
