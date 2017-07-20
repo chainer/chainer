9d318d4819c5ef82fed66db3191595bea129af86
fiarabbit <fiarabbit@gmail.com>
2017-07-20 15:42:45 +0900

    Update updater.py
    chaged the document according to your comment.
    I'm newbee of sphinx and rst format. Please check if the :module:`~chainer.dataset` is ok.

--- chainer/training/updater.py
+++ chainer/training/updater.py
@@ -15,7 +15,8 @@ class Updater(object):
 
     Typically, the updating iteration proceeds as follows.
 
-    - Fetch a minibatch from :class:`~chainer.dataset.DatasetMixin`.
+    - Fetch a minibatch from :module:`~chainer.dataset`
+        via :class:`~chainer.dataset.Iterator`.
     - Run forward and backward process of :class:`~chainer.Chain`.
     - Update parameters according to their :class:`~chainer.UpdateRule`.
 
