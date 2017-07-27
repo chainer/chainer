286efb620dccf668ea39870c738b42ef9847f450
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-07-27 09:33:43 +0900

    fix typo

--- chainer/link.py
+++ chainer/link.py
@@ -519,7 +519,7 @@ Assign a Parameter object directly to an attribute within a \
     def disable_update(self):
         """Disables update rules of all parameters under the link hierarchy.
 
-        This method sets the :attr:~chainer.UpdateRule.enabled` flag of the
+        This method sets the :attr:`~chainer.UpdateRule.enabled` flag of the
         update rule of each parameter variable to ``False``.
 
         """
