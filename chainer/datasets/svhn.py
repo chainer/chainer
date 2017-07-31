f46bacf20a2216af2a97b4619b206e91506ab1d8
rcalland <richard.calland@ipmu.jp>
2017-07-31 13:46:23 +0900

    fix alignment issue

--- chainer/datasets/svhn.py
+++ chainer/datasets/svhn.py
@@ -36,7 +36,7 @@ def get_svhn(withlabel=True, scale=1., dtype=numpy.float32,
 
     """
     if not _scipy_available:
-            raise RuntimeError('scipy is not available')
+        raise RuntimeError('scipy is not available')
 
     train_raw = _retrieve_svhn_training()
     train = _preprocess_svhn(train_raw, withlabel, scale, dtype,
