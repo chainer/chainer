9ee24574e5381a90f27be39fb7f2dfbf25e7e1f3
Kai Katsumata <imperialroyalguard38@gmail.com>
2017-08-04 17:03:44 +0900

    fix unalphabetical order

--- chainer/functions/__init__.py
+++ chainer/functions/__init__.py
@@ -16,10 +16,10 @@ from chainer.functions.activation.lstm import lstm  # NOQA
 from chainer.functions.activation.lstm import LSTM  # NOQA
 from chainer.functions.activation.maxout import maxout  # NOQA
 from chainer.functions.activation.prelu import prelu  # NOQA
-from chainer.functions.activation.rrelu import randomized_leaky_relu  # NOQA
-from chainer.functions.activation.rrelu import RReLU  # NOQA
 from chainer.functions.activation.relu import relu  # NOQA
 from chainer.functions.activation.relu import ReLU  # NOQA
+from chainer.functions.activation.rrelu import randomized_leaky_relu  # NOQA
+from chainer.functions.activation.rrelu import RReLU  # NOQA
 from chainer.functions.activation.selu import selu  # NOQA
 from chainer.functions.activation.sigmoid import sigmoid  # NOQA
 from chainer.functions.activation.sigmoid import Sigmoid  # NOQA
