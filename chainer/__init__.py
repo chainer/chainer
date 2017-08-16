9747e452dcad75de3ea6d580167f632ef0d2e5be
Shunta Saito <shunta.saito@gmail.com>
2017-08-16 14:02:46 +0900

    Update __init__.py

--- chainer/__init__.py
+++ chainer/__init__.py
@@ -32,8 +32,8 @@ from chainer.configuration import global_config  # NOQA
 from chainer.configuration import using_config  # NOQA
 from chainer.function import force_backprop_mode  # NOQA
 from chainer.function import Function  # NOQA
-from chainer.function import no_backprop_mode  # NOQA
 from chainer.function import FunctionAdapter  # NOQA
+from chainer.function import no_backprop_mode  # NOQA
 from chainer.function_node import FunctionNode  # NOQA
 from chainer.functions import array  # NOQA
 from chainer.functions.math import basic_math  # NOQA
