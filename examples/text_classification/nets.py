cdd20dd7a1068bc85ff453a10a05373049460989
Sosuke Kobayashi <sosk@preferred.jp>
2017-08-21 10:02:39 +0900

    update embed block and doc

--- examples/text_classification/nets.py
+++ examples/text_classification/nets.py
@@ -7,6 +7,26 @@ from chainer import reporter
 
 
 def sequence_embed(embed, xs, dropout=0.):
+    """Efficient embedding function for variable-length sequences
+
+    This output is equally to
+    "return [F.dropout(embed(x), ratio=dropout) for x in xs]".
+    However, calling the functions is one-shot and faster.
+
+    Args:
+        embed (callable): A :func:`~chainer.functions.embed_id` function
+            or :class:`~chainer.links.EmbedID` link.
+        xs (list of :class:`~chainer.Variable` or :class:`numpy.ndarray` or \
+        :class:`cupy.ndarray`): i-th element in the list is an input variable,
+            which is a :math:`(L_i, )`-shaped int array.
+        dropout (float): Dropout ratio.
+
+    Returns:
+        list of ~chainer.Variable: Output variables. i-th element in the
+        list is an output variable, which is a :math:`(L_i, N)`-shaped
+        float array. :math:`(N)` is the number of dimensions of word embedding.
+
+    """
     x_len = [len(x) for x in xs]
     x_section = numpy.cumsum(x_len[:-1])
     ex = embed(F.concat(xs, axis=0))
@@ -16,14 +36,29 @@ def sequence_embed(embed, xs, dropout=0.):
 
 
 def block_embed(embed, x, dropout=0.):
-    batch, length = x.shape
-    e = embed(x.reshape((batch * length, )))
-    # shape=(batch * length, units)
-    e = F.transpose(F.stack(F.split_axis(e, batch, axis=0), axis=0), (0, 2, 1))
-    # shape=(batch, units, length)
-    e = e[:, :, :, None]
-    # shape=(batch, units, length, 1)
+    """Embedding function followed by convolution
+
+    Args:
+        embed (callable): A :func:`~chainer.functions.embed_id` function
+            or :class:`~chainer.links.EmbedID` link.
+        x (:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
+        :class:`cupy.ndarray`): Input variable, which
+            is a :math:`(B, L)`-shaped int array. Its first dimension
+            :math:`(B)` is assumed to be the *minibatch dimension*.
+            The second dimension :math:`(L)` is the length of padded
+            sentences.
+        dropout (float): Dropout ratio.
+
+    Returns:
+        ~chainer.Variable: Output variable. A float array with shape
+        of :math:`(B, N, L, 1)`. :math:`(N)` is the number of dimensions
+        of word embedding.
+
+    """
+    e = embed(x)
     e = F.dropout(e, ratio=dropout)
+    e = F.transpose(e, (0, 2, 1))
+    e = e[:, :, :, None]
     return e
 
 
