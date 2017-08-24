c77db0f260dfc98b8dc7f746aa798a138ef69b23
Seiya Tokui <beam.web@gmail.com>
2017-08-24 09:15:17 +0900

    Fix import order

--- chainer/__init__.py
+++ chainer/__init__.py
@@ -55,8 +55,8 @@ from chainer.reporter import Summary  # NOQA
 from chainer.serializer import AbstractSerializer  # NOQA
 from chainer.serializer import Deserializer  # NOQA
 from chainer.serializer import Serializer  # NOQA
-from chainer.variable import Parameter  # NOQA
 from chainer.variable import as_variable  # NOQA
+from chainer.variable import Parameter  # NOQA
 from chainer.variable import Variable  # NOQA
 
 
