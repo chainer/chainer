95fbb079bf3fe643fbec62a179ac38e1dde0557a
niboshi <niboshi000@gmail.com>
2017-08-29 22:07:41 +0900

    Fix to_gpu on Variable test

--- tests/chainer_tests/test_cuda.py
+++ tests/chainer_tests/test_cuda.py
@@ -285,10 +285,10 @@ class TestToGPU(unittest.TestCase):
         self.assertIsNot(x, y)  # Do copy
         cuda.cupy.testing.assert_array_equal(x, y)
 
-    def test_variable_cpu(self):
+    def test_variable_gpu(self):
         x = chainer.Variable(self.x)
         with self.assertRaises(TypeError):
-            cuda.to_cpu(x)
+            cuda.to_gpu(x)
 
 
 testing.run_module(__name__, __file__)
