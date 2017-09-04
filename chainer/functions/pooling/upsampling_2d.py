108e66898b7e01e5dde4e2e3443829cd5986ee37
Shunta Saito <shunta.saito@gmail.com>
2017-09-04 16:49:17 +0900

    Revert imports

--- chainer/functions/pooling/upsampling_2d.py
+++ chainer/functions/pooling/upsampling_2d.py
@@ -1,9 +1,9 @@
-import numpy
-
 from chainer import cuda
 from chainer.functions.pooling import pooling_2d
 from chainer.utils import conv
 from chainer.utils import type_check
+
+import numpy
 
 
 class Upsampling2D(pooling_2d.Pooling2D):
