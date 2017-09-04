cbf5a12f18f616eb14e105e5b2f9aa0f8272aee2
Yuya Unno <unnonouno@gmail.com>
2017-09-04 11:02:43 +0900

    Add +2 for special characters

--- examples/seq2seq/seq2seq.py
+++ examples/seq2seq/seq2seq.py
@@ -157,7 +157,11 @@ def count_lines(path):
 
 def load_vocabulary(path):
     with open(path) as f:
-        return {line.strip(): i for i, line in enumerate(f)}
+        # +2 for UNK and EOS
+        word_ids = {line.strip(): i + 2 for i, line in enumerate(f)}
+    word_ids['<UNK>'] = 0
+    word_ids['<EOS>'] = 1
+    return word_ids
 
 
 def load_data(vocabulary, path):
