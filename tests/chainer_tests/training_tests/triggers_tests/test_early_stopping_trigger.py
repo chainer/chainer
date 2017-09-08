bd08069af78aef0d67b2292e5caf14b438375d76
himkt <himkt@klis.tsukuba.ac.jp>
2017-09-08 22:38:17 +0900

    Add test for early stopping with loss

--- tests/chainer_tests/training_tests/triggers_tests/test_early_stopping_trigger.py
+++ tests/chainer_tests/training_tests/triggers_tests/test_early_stopping_trigger.py
@@ -43,7 +43,7 @@ def _test_trigger(self, trigger, key, accuracies, expected):
 
 class TestEarlyStoppingTrigger(unittest.TestCase):
 
-    def test_early_stopping_trigger(self):
+    def test_early_stopping_trigger_with_accuracy(self):
         key = 'main/accuracy'
         trigger = triggers.EarlyStoppingTrigger(monitor=key, patients=3,
                                                 trigger=(1, 'iteration'),
@@ -51,6 +51,21 @@ class TestEarlyStoppingTrigger(unittest.TestCase):
         trigger = util.get_trigger(trigger)
 
         accuracies = [0.5, 0.5, 0.6, 0.7, 0.6, 0.4, 0.3, 0.2]
+        accuracies = numpy.asarray([
+            chainer.Variable(numpy.asarray(acc, dtype=numpy.float32))
+            for acc in accuracies])
+
+        expected = [False, False, False, False, False, False, False, True]
+        _test_trigger(self, trigger, key, accuracies, expected)
+
+    def test_early_stopping_trigger_with_loss(self):
+        key = 'main/loss'
+        trigger = triggers.EarlyStoppingTrigger(monitor=key, patients=3,
+                                                trigger=(1, 'iteration'),
+                                                verbose=True)
+        trigger = util.get_trigger(trigger)
+
+        accuracies = [100, 80, 30, 10, 20, 24, 30, 35]
         accuracies = numpy.asarray([
             chainer.Variable(numpy.asarray(acc, dtype=numpy.float32))
             for acc in accuracies])
