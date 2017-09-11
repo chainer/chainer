cb4da3354be08d61f9304c0ed63e9e0413416b00
niboshi <niboshi000@gmail.com>
2017-09-11 11:03:41 +0900

    flake8

--- tests/chainer_tests/functions_tests/normalization_tests/test_local_response_normalization.py
+++ tests/chainer_tests/functions_tests/normalization_tests/test_local_response_normalization.py
@@ -26,7 +26,7 @@ class TestLocalResponseNormalization(unittest.TestCase):
             self.check_backward_options = {'atol': 5e-3, 'rtol': 5e-3}
         else:
             self.check_forward_options = {}
-            self.check_backward_options = {'atol':3e-4, 'rtol': 3e-3}
+            self.check_backward_options = {'atol': 3e-4, 'rtol': 3e-3}
 
     def check_forward(self, x_data):
         x = chainer.Variable(x_data)
