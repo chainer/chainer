dddf89e519e40ce118509dcb5823ad932fea88f8
himkt <himkt@klis.tsukuba.ac.jp>
2017-09-13 19:27:38 +0900

    Fix the order of importing

--- chainer/training/triggers/__init__.py
+++ chainer/training/triggers/__init__.py
@@ -3,9 +3,9 @@ from chainer.training.triggers import minmax_value_trigger  # NOQA
 
 
 # import class and function
+from chainer.training.triggers.early_stopping_trigger import EarlyStoppingTrigger  # NOQA
 from chainer.training.triggers.interval_trigger import IntervalTrigger  # NOQA
 from chainer.training.triggers.manual_schedule_trigger import ManualScheduleTrigger  # NOQA
 from chainer.training.triggers.minmax_value_trigger import BestValueTrigger  # NOQA
 from chainer.training.triggers.minmax_value_trigger import MaxValueTrigger  # NOQA
 from chainer.training.triggers.minmax_value_trigger import MinValueTrigger  # NOQA
-from chainer.training.triggers.early_stopping_trigger import EarlyStoppingTrigger  # NOQA
