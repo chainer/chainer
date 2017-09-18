7f87feb04e213d4f9526a78184d06cc72df1aaf1
Hiroki11x <hiroki11x@gmail.com>
2017-09-18 23:20:17 +0900

    copy?model.py

--- examples/modelzoo/copy_model.py
+++ examples/modelzoo/copy_model.py
@@ -12,18 +12,18 @@ def copy_model(src, dst):
         if type(child) != type(dst_child): continue
         if isinstance(child, chainer.Chain):
             copy_model(child, dst_child)
-        if isinstance(child, chainer.Link):
-            match = True
-            for a, b in zip(child.namedparams(), dst_child.namedparams()):
-                if a[0] != b[0]:
-                    match = False
-                    break
-                if a[1].data.shape != b[1].data.shape:
-                    match = False
-                    break
-            if not match:
-                print 'Ignore %s because of parameter mismatch' % child.name
-                continue
-            for a, b in zip(child.namedparams(), dst_child.namedparams()):
-                b[1].data = a[1].data
-            print 'Copy %s' % child.name
+            if isinstance(child, chainer.Link):
+                match = True
+                for a, b in zip(child.namedparams(), dst_child.namedparams()):
+                    if a[0] != b[0]:
+                        match = False
+                        break
+                    if a[1].data.shape != b[1].data.shape:
+                        match = False
+                        break
+                    if not match:
+                        print('Ignore %s because of parameter mismatch' % child.name)
+                        continue
+                    for a, b in zip(child.namedparams(), dst_child.namedparams()):
+                        b[1].data = a[1].data
+                        print('Copy %s' % child.name)
