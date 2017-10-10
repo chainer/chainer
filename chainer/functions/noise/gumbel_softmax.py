cf5f341d67dc3ba5988d0e662038d8d1bdb0f17d
Tatsuma Ishihara <ashitahamatsuri@gmail.com>
2017-10-10 23:33:58 +0900

    fix document

--- chainer/functions/noise/gumbel_softmax.py
+++ chainer/functions/noise/gumbel_softmax.py
@@ -9,21 +9,24 @@ def gumbel_softmax(log_pi, tau=0.1, axis=1):
     """Gumbel-Softmax sampling function.
 
     This function draws samples :math:`y_i` from Gumbel-Softmax distribution,
-        :math:`y_i = {\\exp((g_i + \\log\\pi_i)/\\tau)
-                    \\over \\sum_{j}\\exp((g_j + \\log\\pi_j)/\\tau)}`,
-        where :math:`\\tau` is a temperature parameter and
-            :math:`g_i` s are samples drawn from \
-            Gumbel distribution :math:`Gumbel(0, 1)`
+
+    .. math::
+        y_i = {\\exp((g_i + \\log\\pi_i)/\\tau) 
+        \\over \\sum_{j}\\exp((g_j + \\log\\pi_j)/\\tau)},
+
+    where :math:`\\tau` is a temperature parameter and
+    :math:`g_i` s are samples drawn from
+    Gumbel distribution :math:`Gumbel(0, 1)`
 
     See `Categorical Reparameterization with Gumbel-Softmax \
     <https://arxiv.org/abs/1611.01144>`_.
 
     Args:
         log_pi (:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
-        :class:`cupy.ndarray`): Input variable representing pre-normalized
+            :class:`cupy.ndarray`): Input variable representing pre-normalized
             log-probability :math:`\\log\\pi`.
-        tau (:class:`~float`): Input variable representing \
-            temperature :math:`\\tau`.
+        tau (:class:`~float` or :class:`~chainer.Variable`): \
+            Input variable representing temperature :math:`\\tau`.
 
     Returns:
         ~chainer.Variable: Output variable.
