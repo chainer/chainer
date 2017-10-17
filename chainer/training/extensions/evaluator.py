d772435dc76b0f4e5008f6c8ce1ab2f251875e2f
Shunta Saito <shunta.saito@gmail.com>
2017-10-17 18:56:46 +0900

    reflect a review

--- chainer/training/extensions/evaluator.py
+++ chainer/training/extensions/evaluator.py
@@ -151,7 +151,7 @@ class Evaluator(extension.Extension):
         .. note::
 
             This method encloses :attr:`eval_func` calls with
-            :func:`function.no_backprop_mode` context. So, all calculations
+            :func:`function.no_backprop_mode` context, so all calculations
             using :class:`~chainer.Function`\ s inside :attr:`eval_func` don't
             make computational graphs. It is for reducing the memory
             consumption.
