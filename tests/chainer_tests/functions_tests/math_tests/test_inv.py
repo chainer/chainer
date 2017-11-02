0a005560e2d5d6d38ae01c572a3eb7b5ebb8a222
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2017-11-02 17:34:55 +0900

    make test data generation out of with scope

--- tests/chainer_tests/functions_tests/math_tests/test_inv.py
+++ tests/chainer_tests/functions_tests/math_tests/test_inv.py
@@ -149,23 +149,27 @@ class BatchInvFunctionTest(unittest.TestCase):
 class InvFunctionRaiseTest(unittest.TestCase):
 
     def test_invalid_ndim(self):
+        x = chainer.Variable(numpy.zeros((1, 2, 2), dtype=numpy.float32))
         with self.assertRaises(type_check.InvalidType):
-            functions.inv(chainer.Variable(numpy.zeros((1, 2, 2), dtype=numpy.float32)))
+            functions.inv(x)
 
     def test_invalid_shape(self):
+        x = chainer.Variable(numpy.zeros((1, 2), dtype=numpy.float32))
         with self.assertRaises(type_check.InvalidType):
-            functions.inv(chainer.Variable(numpy.zeros((1, 2), dtype=numpy.float32)))
+            functions.inv(x)
 
 
 class BatchInvFunctionRaiseTest(unittest.TestCase):
 
     def test_invalid_ndim(self):
+        x = chainer.Variable(numpy.zeros((2, 2), dtype=numpy.float32))
         with self.assertRaises(type_check.InvalidType):
-            functions.batch_inv(chainer.Variable(numpy.zeros((2, 2), dtype=numpy.float32)))
+            functions.batch_inv(x)
 
     def test_invalid_shape(self):
+        x = chainer.Variable(numpy.zeros((1, 2, 1), dtype=numpy.float32))
         with self.assertRaises(type_check.InvalidType):
-            functions.batch_inv(chainer.Variable(numpy.zeros((1, 2, 1), dtype=numpy.float32)))
+            functions.batch_inv(x)
 
 
 testing.run_module(__name__, __file__)
