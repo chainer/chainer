ca77c9a91ea52334aca244abfd01c4595074eb04
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2017-11-02 14:46:09 +0900

    Fix PEP8

--- tests/chainer_tests/functions_tests/loss_tests/test_triplet.py
+++ tests/chainer_tests/functions_tests/loss_tests/test_triplet.py
@@ -9,7 +9,6 @@ from chainer import functions
 from chainer import gradient_check
 from chainer import testing
 from chainer.testing import attr
-from chainer.testing import condition
 
 
 @testing.parameterize(
