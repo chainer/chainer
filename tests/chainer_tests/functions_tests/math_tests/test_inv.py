6e55ae9029c4015c3b1bbb0ec2ac9cf532d3f073
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2017-11-02 17:15:47 +0900

    fix inv_test does not test assertion as expected

--- tests/chainer_tests/functions_tests/math_tests/test_inv.py
+++ tests/chainer_tests/functions_tests/math_tests/test_inv.py
@@ -9,6 +9,7 @@ from chainer import gradient_check
 from chainer import testing
 from chainer.testing import attr
 from chainer.testing import condition
+from chainer.utils import type_check
 
 
 def _inv(x):
@@ -148,23 +149,23 @@ class BatchInvFunctionTest(unittest.TestCase):
 class InvFunctionRaiseTest(unittest.TestCase):
 
     def test_invalid_ndim(self):
-        with self.assertRaises(TypeError):
-            functions.inv(chainer.Variable(numpy.zeros(1, 2, 2)))
+        with self.assertRaises(type_check.InvalidType):
+            functions.inv(chainer.Variable(numpy.zeros((1, 2, 2), dtype=numpy.float32)))
 
     def test_invalid_shape(self):
-        with self.assertRaises(TypeError):
-            functions.inv(chainer.Variable(numpy.zeros(1, 2)))
+        with self.assertRaises(type_check.InvalidType):
+            functions.inv(chainer.Variable(numpy.zeros((1, 2), dtype=numpy.float32)))
 
 
 class BatchInvFunctionRaiseTest(unittest.TestCase):
 
     def test_invalid_ndim(self):
-        with self.assertRaises(TypeError):
-            functions.batch_inv(chainer.Variable(numpy.zeros(2, 2)))
+        with self.assertRaises(type_check.InvalidType):
+            functions.batch_inv(chainer.Variable(numpy.zeros((2, 2), dtype=numpy.float32)))
 
     def test_invalid_shape(self):
-        with self.assertRaises(TypeError):
-            functions.batch_inv(chainer.Variable(numpy.zeros(1, 2, 1)))
+        with self.assertRaises(type_check.InvalidType):
+            functions.batch_inv(chainer.Variable(numpy.zeros((1, 2, 1), dtype=numpy.float32)))
 
 
 testing.run_module(__name__, __file__)
