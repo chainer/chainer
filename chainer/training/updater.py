1afcb8b61ff676a10cd2b0727e176e735804c6df
Kenta OONO <oono@preferred.jp>
2017-11-06 15:07:56 +0900

    Fix updater.py

--- chainer/training/updater.py
+++ chainer/training/updater.py
@@ -85,5 +85,5 @@ class Updater(object):
 
 
 # For backward compatibility
-from chainer.training.updaters.parallel_updater import ParallelUpdater  # NOQA
-from chainer.training.updaters.standard_updater import StandardUpdater  # NOQA
+from chainer.training.updaters.parallel_updater import *  # NOQA
+from chainer.training.updaters.standard_updater import *  # NOQA
