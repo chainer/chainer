1c247e9b60a7921694607c0077e49d3c2bf37e18
niboshi <niboshi000@gmail.com>
2017-11-06 14:14:55 +0900

    Merge pull request #3652 from unnonouno/directional-derivertive-2
    Directional derivative

--- tests/chainer_tests/functions_tests/activation_tests/test_clipped_relu.py
+++ tests/chainer_tests/functions_tests/activation_tests/test_clipped_relu.py
@@ -27,6 +27,13 @@ class TestClippedReLU(unittest.TestCase):
         self.ggx = numpy.random.uniform(-1, 1, x.shape).astype(self.dtype)
         self.z = 0.75
 
+        if self.dtype == numpy.float16:
+            self.check_backward_options = {}
+            self.check_double_backward_options = {'atol': 1e-3, 'rtol': 1e-2}
+        else:
+            self.check_backward_options = {}
+            self.check_double_backward_options = {}
+
     def check_forward(self, x_data):
         x = chainer.Variable(x_data)
         y = functions.clipped_relu(x, self.z)
@@ -48,7 +55,8 @@ class TestClippedReLU(unittest.TestCase):
             return functions.clipped_relu(x, self.z)
 
         gradient_check.check_backward(
-            f, x_data, y_grad, dtype=numpy.float64)
+            f, x_data, y_grad, dtype=numpy.float64,
+            **self.check_backward_options)
 
     def test_backward_cpu(self):
         self.check_backward(self.x, self.gy)
@@ -63,7 +71,8 @@ class TestClippedReLU(unittest.TestCase):
             return y * y
 
         gradient_check.check_double_backward(
-            f, x_data, y_grad, x_grad_grad, dtype=numpy.float64)
+            f, x_data, y_grad, x_grad_grad, dtype=numpy.float64,
+            **self.check_double_backward_options)
 
     def test_double_backward_cpu(self):
         self.check_double_backward(self.x, self.gy, self.ggx)
