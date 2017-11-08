cd74b606bce3ef813bee9ca905c13275bc61b4ee
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2017-11-08 12:29:14 +0900

    remove backward test cases

--- tests/chainer_tests/functions_tests/math_tests/test_fix.py
+++ tests/chainer_tests/functions_tests/math_tests/test_fix.py
@@ -18,7 +18,7 @@ class UnaryFunctionsTestBase(unittest.TestCase):
     def setUp(self):
         self.eps = 1e-3
         while True:
-            self.x, self.gy, self.ggx = self.make_data()
+            self.x = self.make_data()
             if (numpy.abs(self.x - numpy.round(self.x)) > self.eps * 10).all():
                 break
 
@@ -27,42 +27,13 @@ class UnaryFunctionsTestBase(unittest.TestCase):
         y = op(x)
         self.assertEqual(x.data.dtype, y.data.dtype)
         v = op_xp(x_data)
-        testing.assert_allclose(
-            v, y.data, atol=1e-7, rtol=1e-7)
+        testing.assert_allclose(v, y.data, atol=1e-7, rtol=1e-7)
 
     def check_forward_cpu(self, op, op_xp):
         self.check_forward(op, op_xp, self.x)
 
     def check_forward_gpu(self, op, op_xp):
         self.check_forward(op, op_xp, cuda.to_gpu(self.x))
-
-    def check_backward(self, op, x_data, y_grad):
-        gradient_check.check_backward(op, x_data, y_grad, atol=5e-4,
-                                      rtol=5e-3, dtype=numpy.float64,
-                                      eps=self.eps)
-
-    def check_backward_cpu(self, op):
-        self.check_backward(op, self.x, self.gy)
-
-    def check_backward_gpu(self, op):
-        self.check_backward(op, cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
-
-    def check_double_backward(self, op, x_data, y_grad, y_grad_grad):
-        def f(x):
-            x = op(x)
-            return x * x
-        gradient_check.check_double_backward(
-            f, x_data, y_grad, y_grad_grad, dtype=numpy.float64)
-
-    def check_double_backward_cpu(self, op):
-        self.check_double_backward(op, self.x, self.gy, self.ggx)
-
-    def check_double_backward_gpu(self, op):
-        self.check_double_backward(op, cuda.to_gpu(
-            self.x), cuda.to_gpu(self.gy), cuda.to_gpu(self.ggx))
-
-    def check_label(self, op, expected):
-        self.assertEqual(op().label, expected)
 
 
 @testing.parameterize(*testing.product({
@@ -73,9 +44,7 @@ class TestFix(UnaryFunctionsTestBase):
 
     def make_data(self):
         x = numpy.random.uniform(-10.0, 10.0, self.shape).astype(self.dtype)
-        gy = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
-        ggx = numpy.random.uniform(-10.0, 10.0, self.shape).astype(self.dtype)
-        return x, gy, ggx
+        return x
 
     def test_forward_cpu(self):
         self.check_forward_cpu(F.fix, numpy.fix)
@@ -83,23 +52,6 @@ class TestFix(UnaryFunctionsTestBase):
     @attr.gpu
     def test_forward_gpu(self):
         self.check_forward_gpu(F.fix, cuda.cupy.fix)
-
-    def test_backward_cpu(self):
-        self.check_backward_cpu(F.fix)
-
-    @attr.gpu
-    def test_backward_gpu(self):
-        self.check_backward_gpu(F.fix)
-
-    def test_double_backward_cpu(self):
-        self.check_double_backward_cpu(F.fix)
-
-    @attr.gpu
-    def test_double_backward_gpu(self):
-        self.check_double_backward_gpu(F.fix)
-
-    def test_label(self):
-        self.check_label(F.Fix, 'fix')
 
 
 testing.run_module(__name__, __file__)
