b7e27629dd7e9a0a7026a19d25880a2bbeb8b427
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2017-11-10 10:04:09 +0900

    Fix sign function test

--- tests/chainer_tests/functions_tests/math_tests/test_sign.py
+++ tests/chainer_tests/functions_tests/math_tests/test_sign.py
@@ -10,21 +10,17 @@ from chainer import testing
 from chainer.testing import attr
 
 
-class UnaryFunctionsTestBase(unittest.TestCase):
-
-    @property
-    def no_grads(self):
-        return (False,)
-
-    def make_data(self):
-        raise NotImplementedError
+@testing.parameterize(*testing.product({
+    'shape': [(3, 2), ()],
+    'dtype': [numpy.float16, numpy.float32, numpy.float64],
+}))
+class TestSign(unittest.TestCase):
 
     def setUp(self):
-        self.eps = 1e-3
-        while True:
-            self.x, self.gy, self.ggx = self.make_data()
-            if (numpy.abs(self.x - numpy.round(self.x)) > self.eps * 10).all():
-                break
+        self.x = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        self.gy = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        self.ggx = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        self.no_grads = (True,)  # Always assert that gradients are `None`
 
     def check_forward(self, op, op_xp, x_data):
         x = chainer.Variable(x_data)
@@ -47,41 +43,10 @@ class UnaryFunctionsTestBase(unittest.TestCase):
         self.check_forward(op, op_xp, self.x)
         self.check_forward_ndarray(op, op_xp, self.x)
 
+    @attr.gpu
     def check_forward_gpu(self, op, op_xp):
         self.check_forward(op, op_xp, cuda.to_gpu(self.x))
         self.check_forward_ndarray(op, op_xp, cuda.to_gpu(self.x))
-
-    def check_backward(self, op, x_data, y_grad, no_grads):
-        gradient_check.check_backward(op, x_data, y_grad, no_grads=no_grads)
-
-        if no_grads and no_grads[0]:
-            x = chainer.Variable(x_data)
-            op(x).backward()
-            assert x.grad is None
-
-    def check_backward_cpu(self, op):
-        self.check_backward(op, self.x, self.gy, self.no_grads)
-
-    def check_backward_gpu(self, op):
-        self.check_backward(
-            op, cuda.to_gpu(self.x), cuda.to_gpu(self.gy), self.no_grads)
-
-
-@testing.parameterize(*testing.product({
-    'shape': [(3, 2), ()],
-    'dtype': [numpy.float16, numpy.float32, numpy.float64],
-}))
-class TestSign(UnaryFunctionsTestBase):
-
-    def make_data(self):
-        x = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
-        gy = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
-        ggx = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
-        return x, gy, ggx
-
-    @property
-    def no_grads(self):
-        return (True,)
 
     def test_forward_cpu(self):
         self.check_forward_cpu(F.sign, numpy.sign)
@@ -89,6 +54,22 @@ class TestSign(UnaryFunctionsTestBase):
     @attr.gpu
     def test_forward_gpu(self):
         self.check_forward_gpu(F.sign, cuda.cupy.sign)
+
+    def check_backward(self, op, x_data, y_grad, no_grads):
+        gradient_check.check_backward(op, x_data, y_grad, no_grads=no_grads)
+
+        # Explicitly check that gradients are `None`
+        x = chainer.Variable(x_data)
+        op(x).backward()
+        assert x.grad is None
+
+    def check_backward_cpu(self, op):
+        self.check_backward(op, self.x, self.gy, self.no_grads)
+
+    @attr.gpu
+    def check_backward_gpu(self, op):
+        self.check_backward(
+            op, cuda.to_gpu(self.x), cuda.to_gpu(self.gy), self.no_grads)
 
     def test_backward_cpu(self):
         self.check_backward_cpu(F.sign)
