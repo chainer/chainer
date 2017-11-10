d1c216c0cad3207bcf9293b5a74d2f4b7708e256
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2017-11-10 11:04:28 +0900

    Clean up sign function test

--- tests/chainer_tests/functions_tests/math_tests/test_sign.py
+++ tests/chainer_tests/functions_tests/math_tests/test_sign.py
@@ -22,59 +22,53 @@ class TestSign(unittest.TestCase):
         self.ggx = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
         self.no_grads = (True,)  # Always assert that gradients are `None`
 
-    def check_forward(self, op, op_xp, x_data):
+    def check_forward(self, x_data, xp):
         x = chainer.Variable(x_data)
-        y = op(x)
-        v = op_xp(x_data)
+        y = F.sign(x)
+        v = xp.sign(x_data)
 
         assert x.data.dtype == y.data.dtype
         testing.assert_allclose(v, y.data, atol=1e-7, rtol=1e-7)
 
-    def check_forward_ndarray(self, op, op_xp, x_data):
-        y = op(x_data)
-        v = op_xp(x_data)
+    def test_forward_cpu(self):
+        self.check_forward(self.x, numpy)
+
+    @attr.gpu
+    def test_forward_gpu(self):
+        self.check_forward(cuda.to_gpu(self.x), cuda.cupy)
+
+    def check_forward_ndarray(self, x_data, xp):
+        y = F.sign(x_data)
+        v = xp.sign(x_data)
 
         assert x_data.dtype == y.data.dtype
         assert x_data.dtype == v.dtype
         assert isinstance(y, chainer.Variable)
         testing.assert_allclose(v, y.data, atol=1e-7, rtol=1e-7)
 
-    def check_forward_cpu(self, op, op_xp):
-        self.check_forward(op, op_xp, self.x)
-        self.check_forward_ndarray(op, op_xp, self.x)
-
-    def check_forward_gpu(self, op, op_xp):
-        self.check_forward(op, op_xp, cuda.to_gpu(self.x))
-        self.check_forward_ndarray(op, op_xp, cuda.to_gpu(self.x))
-
-    def test_forward_cpu(self):
-        self.check_forward_cpu(F.sign, numpy.sign)
+    def test_forward_ndarray_cpu(self):
+        self.check_forward_ndarray(self.x, numpy)
 
     @attr.gpu
-    def test_forward_gpu(self):
-        self.check_forward_gpu(F.sign, cuda.cupy.sign)
+    def test_forward_ndarray_gpu(self):
+        self.check_forward_ndarray(cuda.to_gpu(self.x), cuda.cupy)
 
-    def check_backward(self, op, x_data, y_grad, no_grads):
-        gradient_check.check_backward(op, x_data, y_grad, no_grads=no_grads)
+    def check_backward(self, x_data, y_grad, no_grads):
+        gradient_check.check_backward(
+            F.sign, x_data, y_grad, no_grads=no_grads)
 
         # Explicitly check that gradients are `None`
         x = chainer.Variable(x_data)
-        op(x).backward()
+        F.sign(x).backward()
         assert x.grad is None
 
-    def check_backward_cpu(self, op):
-        self.check_backward(op, self.x, self.gy, self.no_grads)
-
-    def check_backward_gpu(self, op):
-        self.check_backward(
-            op, cuda.to_gpu(self.x), cuda.to_gpu(self.gy), self.no_grads)
-
     def test_backward_cpu(self):
-        self.check_backward_cpu(F.sign)
+        self.check_backward(self.x, self.gy, self.no_grads)
 
     @attr.gpu
     def test_backward_gpu(self):
-        self.check_backward_gpu(F.sign)
+        self.check_backward(
+            cuda.to_gpu(self.x), cuda.to_gpu(self.gy), self.no_grads)
 
 
 testing.run_module(__name__, __file__)
