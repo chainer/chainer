4f04d80b0e20cd008e2450fe5f9844a381fcd9a5
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-11-30 14:35:10 +0900

    use getitem

--- chainer/datasets/sliceable/transform_dataset.py
+++ chainer/datasets/sliceable/transform_dataset.py
@@ -6,7 +6,7 @@ class TransformDataset(GetterDataset):
     def __init__(self, dataset, keys, func):
         super(TransformDataset, self).__init__()
         self._dataset = dataset
-        self.add_getter(keys, lambda i: func(dataset.get_example(i)))
+        self.add_getter(keys, lambda i: func(dataset[i]))
 
     def __len__(self):
         return len(self._dataset)
