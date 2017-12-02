a26366e80998fcab4b7d318c8555160264ba1617
Kai Katsumata <imperialroyalguard38@gmail.com>
2017-12-03 04:18:15 +0900

    Pass test

--- chainer/functions/activation/rrelu.py
+++ chainer/functions/activation/rrelu.py
@@ -1,6 +1,5 @@
 import chainer
 from chainer import cuda
-from chainer import function
 from chainer import function_node
 from chainer.utils import type_check
 import numpy as np
@@ -29,25 +28,31 @@ class RReLU(function_node.FunctionNode):
         type_check.expect(x_type.dtype.kind == 'f')
 
     def forward_cpu(self, x):
-        y = x[0].copy()
-        if chainer.config.train:
-            self.r = np.random.uniform(self.lower, self.upper, x[0].shape)
+        if hasattr(self, 'r'):
+            y = np.where(x[0] >= 0, x[0], x[0]*self.r)
         else:
-            self.r = np.empty(x[0].shape)
-            self.r.fill((self.lower + self.upper) / 2)
-        y *= np.where(x[0] < 0, self.r, 1)
-        self.retain_inputs(())
+            if chainer.config.train:
+                self.r = np.random.uniform(
+                    self.lower, self.upper, x[0].shape).astype(x[0].dtype)
+            else:
+                self.r = np.empty(x[0].shape).astype(x[0].dtype)
+                self.r.fill((self.lower + self.upper) / 2)
+            y = np.where(x[0] >= 0, x[0], x[0]*self.r)
         self.retain_outputs((0,))
         return y,
 
     def forward_gpu(self, x):
         xp = cuda.cupy
-        if chainer.config.train:
-            self.r = xp.random.uniform(self.lower, self.upper, x[0].shape)
+        if hasattr(self, 'r'):
+            y = _kern()(x[0], x[0], self.r.astype(x[0].dtype))
         else:
-            self.r = xp.empty(x[0].shape)
-            self.r.fill((self.lower + self.upper) / 2)
-        y = _kern(x[0], x[0], self.r)
+            if chainer.config.train:
+                self.r = xp.random.uniform(
+                    self.lower, self.upper, x[0].shape).astype(x[0].dtype)
+            else:
+                self.r = xp.empty(x[0].shape).astype(x[0].dtype)
+                self.r.fill((self.lower + self.upper) / 2)
+            y = _kern()(x[0], x[0], self.r)
         self.retain_inputs(())
         self.retain_outputs((0,))
         return y,
@@ -55,28 +60,30 @@ class RReLU(function_node.FunctionNode):
     def backward(self, indexes, grad_outputs):
         x = None
         y = self.get_retained_outputs()[0].data
-        return _RReLUGrad(x, y, self.r).apply(grad_putputs)
+        return _RReLUGrad(x, y, self.r).apply(grad_outputs)
 
 
 class _RReLUGrad(function_node.FunctionNode):
-    def  __init__(self, x, y, slope):
-        self.slope = slope
+    def __init__(self, x, y, r):
+        self.r = r
         self.x = x
         self.y = y
 
     def forward_cpu(self, inputs):
         gy, = inputs
         gy = gy.copy()
-        gy *= np.where(gy[0] < 0, self.r, 1)
+        gy = np.where(self.y >= 0, gy, gy*self.r)
         return gy,
 
     def forward_gpu(self, inputs):
-         gy, = inputs
-         gy = _kern()(self.y, gy, self.slope)
-         return gy,
+        gy, = inputs
+        gy = gy.copy()
+        gy = _kern()(self.y, gy, self.r)
+        return gy,
 
     def backward(self, indexes, grad_outputs):
-        return _LeakyReLUGrad(self.x, self.y, self.slope).apply(grad_outputs)
+        return _RReLUGrad(self.x, self.y, self.r).apply(grad_outputs)
+
 
 def rrelu(x, l=1. / 8, u=1. / 3):
     """Randomized Leaky Rectified Liner Unit function.
--- tests/chainer_tests/functions_tests/activation_tests/test_rrelu.py
+++ tests/chainer_tests/functions_tests/activation_tests/test_rrelu.py
@@ -11,12 +11,6 @@ from chainer.testing import attr
 from chainer.testing import condition
 
 
-def _rrelu(x, creator, train):
-    r = creator.r if train else (creator.lower + creator.upper) / 2.0
-    xp = cuda.get_array_module(x)    
-    return x * xp.where(x < 0.0, r, 1.0)
-
-
 @testing.parameterize(*testing.product({
     'train': [True, False],
     'shape': [(3, 2)],
@@ -26,11 +20,9 @@ class TestRReLU(unittest.TestCase):
 
     def setUp(self):
         # Avoid unstability of numeraical grad
-        self.x = numpy.random.uniform(-100, 100, self.shape).astype(self.dtype)
-        for i in numpy.ndindex(self.shape):
-            if -0.05 < self.x[i] < 0.05:
-                self.x[i] = 0.5
-        self.gy = numpy.random.uniform(-100, 100, self.shape).astype(self.dtype)
+        self.x = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        self.x[(-0.05 < self.x) & (self.x < 0.05)] = 0.5
+        self.gy = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
         # Asummption l < u
         self.l = numpy.random.uniform(0, 1)
         self.u = numpy.random.uniform(0, 1)
@@ -40,21 +32,15 @@ class TestRReLU(unittest.TestCase):
         self.check_backward_options = {}
         if self.dtype == numpy.float16:
             self.check_forward_options = {'atol': 1e-4, 'rtol': 1e-3}
-            self.check_backward_options = {'atol': 1e-4, 'rtol': 1e-3}
+            self.check_backward_options = {'atol': 5e-4, 'rtol': 5e-3}
 
-    def check_forward(self, x_data, use_cudnn='always'):
+    def check_forward(self, x_data):
         x = chainer.Variable(x_data)
-        chainer.config.train = self.train        
-        with chainer.using_config('use_cudnn', use_cudnn):        
-            y = functions.rrelu(x, l=self.l, u=self.u)
+        xp = cuda.get_array_module(x)
+        chainer.config.train = self.train
+        y = functions.rrelu(x, l=self.l, u=self.u)
         self.assertEqual(y.data.dtype, self.dtype)
-
-        expected = self.x.copy()
-        for i in numpy.ndindex(self.x.shape):
-            if self.x[i] < 0.0:
-                expected[i] *= y.creator.r[i] if self.train else (
-                    self.l + self.u) / 2.0
-
+        expected = xp.where(x_data >= 0, x_data, x_data * y.creator.r)
         testing.assert_allclose(
             expected, y.data, **self.check_forward_options)
 
@@ -64,21 +50,18 @@ class TestRReLU(unittest.TestCase):
 
     @attr.gpu
     @condition.retry(3)
-    def test_forward_gpu_no_cudnn(self):
-        self.check_forward(cuda.to_gpu(self.x), 'never')
-
-    @attr.gpu
-    @condition.retry(3)
     def test_forward_gpu(self):
         self.check_forward(cuda.to_gpu(self.x))
 
+    def check_backward(self, x_data, y_grad):
+        rrelu = functions.RReLU(self.l, self.u)
+        chainer.config.train = self.train
 
-
-    def check_backward(self, x_data, y_grad, use_cudnn='always'):
-        with chainer.using_config('use_cudnn', use_cudnn):                
-            gradient_check.check_backward(
-                functions.rrelu, x_data, y_grad, dtype=numpy.float64,
-                **self.check_backward_options)
+        def f(x):
+            return rrelu.apply((x,))[0]
+        gradient_check.check_backward(
+            f, x_data, y_grad, dtype=numpy.float64,
+            **self.check_backward_options)
 
     @condition.retry(10)
     def test_backward_cpu(self):
@@ -88,11 +71,6 @@ class TestRReLU(unittest.TestCase):
     @condition.retry(10)
     def test_backward_gpu(self):
         self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
-
-    @attr.gpu
-    @condition.retry(10)
-    def test_backward_gpu_no_cudnn(self):
-        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy), 'never')        
 
 
 testing.run_module(__name__, __file__)
