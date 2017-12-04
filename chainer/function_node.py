c54d015b124dd0405858aa4a6d7fbc0698f01365
niboshi <niboshi000@gmail.com>
2017-12-04 20:11:02 +0900

    Omit ndarray type checks because some built-in functions will fail

--- chainer/function_node.py
+++ chainer/function_node.py
@@ -284,12 +284,6 @@ Use apply() method instead.\
         return ret
 
     def _check_data_type_forward(self, in_data):
-        xp = cuda.get_array_module(*in_data)
-        if not all([_ is None or isinstance(_, xp.ndarray) for _ in in_data]):
-            raise ValueError('numpy and cupy must not be used together\n'
-                             '{}'
-                             .format(', '.join(str(type(_)) for _ in in_data)))
-
         in_type = type_check.get_light_types(in_data)
         try:
             with type_check.light_mode:
