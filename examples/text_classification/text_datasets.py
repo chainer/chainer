fd928d4c0cd90e5a50def0a9fc4a49e93e913a21
Sosuke <sosk@preferred.jp>
2017-12-06 23:46:53 +0900

    use cached_download

--- examples/text_classification/text_datasets.py
+++ examples/text_classification/text_datasets.py
@@ -2,48 +2,48 @@ import csv
 import glob
 import io
 import os
+import shutil
 import tarfile
+import tempfile
 
 import numpy
-from six.moves.urllib import request
+
+import chainer
 
 from nlp_utils import make_vocab
 from nlp_utils import normalize_text
 from nlp_utils import split_text
 from nlp_utils import transform_to_array
 
+URL_DBPEDIA = 'https://github.com/le-scientifique/torchDatasets/raw/master/dbpedia_csv.tar.gz'  # NOQA
+URL_IMDB = 'http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz'
+URL_OTHER_BASE = 'https://raw.githubusercontent.com/harvardnlp/sent-conv-torch/master/data/'  # NOQA
+
 
 def download_dbpedia():
-    request.urlretrieve(
-        'https://github.com/le-scientifique/torchDatasets/raw/master/dbpedia_csv.tar.gz',  # NOQA
-        'dbpedia_csv.tar.gz')
-    tf = tarfile.open('dbpedia_csv.tar.gz', 'r')
-    tf.extractall('./')
-    os.remove('dbpedia_csv.tar.gz')
+    path = chainer.dataset.cached_download(URL_DBPEDIA)
+    tf = tarfile.open(path, 'r')
+    return tf
 
 
-def read_dbpedia(path, shrink=1, char_based=False):
+def read_dbpedia(tf, split, shrink=1, char_based=False):
     dataset = []
-    with io.open(path, encoding='utf-8', errors='ignore') as f:
-        for i, (label, title, text) in enumerate(csv.reader(f)):
-            if i % shrink != 0:
-                continue
-            label = int(label) - 1  # Index begins from 1
-            tokens = split_text(normalize_text(text), char_based)
-            dataset.append((tokens, label))
+    f = tf.extractfile('dbpedia_csv/{}.csv'.format(split))
+    for i, (label, title, text) in enumerate(csv.reader(f)):
+        if i % shrink != 0:
+            continue
+        label = int(label) - 1  # Index begins from 1
+        tokens = split_text(normalize_text(text), char_based)
+        dataset.append((tokens, label))
     return dataset
 
 
-def get_dbpedia(path='./dbpedia_csv', vocab=None, shrink=1, char_based=False):
-    if not os.path.isdir(path):
-        print('download dbpedia')
-        download_dbpedia()
-    train_path = os.path.join(path, 'train.csv')
-    test_path = os.path.join(path, 'test.csv')
+def get_dbpedia(vocab=None, shrink=1, char_based=False):
+    tf = download_dbpedia()
 
     print('read dbpedia')
-    train = read_dbpedia(train_path, shrink=shrink, char_based=char_based)
-    test = read_dbpedia(test_path, shrink=shrink, char_based=char_based)
+    train = read_dbpedia(tf, 'train', shrink=shrink, char_based=char_based)
+    test = read_dbpedia(tf, 'test', shrink=shrink, char_based=char_based)
 
     if vocab is None:
         print('constract vocabulary based on frequency')
@@ -56,21 +56,23 @@ def get_dbpedia(path='./dbpedia_csv', vocab=None, shrink=1, char_based=False):
 
 
 def download_imdb():
-    request.urlretrieve(
-        'http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz',
-        'aclImdb_v1.tar.gz')
-    tf = tarfile.open('aclImdb_v1.tar.gz', 'r')
-    tf.extractall('./')
-    os.remove('aclImdb_v1.tar.gz')
+    path = chainer.dataset.cached_download(URL_IMDB)
+    tf = tarfile.open(path, 'r')
+    # To read many files fast, tarfile is untared
+    path = tempfile.mkdtemp()
+    tf.extractall(path)
+    return path
 
 
-def read_imdb(base_path, shrink=1, fine_grained=False, char_based=False):
+def read_imdb(path, split,
+              shrink=1, fine_grained=False, char_based=False):
     fg_label_dict = {'1': 0, '2': 0, '3': 1, '4': 1,
                      '7': 2, '8': 2, '9': 3, '10': 3}
 
-    def read_and_label(path, label):
+    def read_and_label(posneg, label):
         dataset = []
-        for i, f_path in enumerate(glob.glob(os.path.join(path, '*'))):
+        target = os.path.join(path, 'aclImdb', split, posneg, '*')
+        for i, f_path in enumerate(glob.glob(target)):
             if i % shrink != 0:
                 continue
             with io.open(f_path, encoding='utf-8', errors='ignore') as f:
@@ -84,24 +86,24 @@ def read_imdb(base_path, shrink=1, fine_grained=False, char_based=False):
                 dataset.append((tokens, label))
         return dataset
 
-    pos_dataset = read_and_label(os.path.join(base_path, 'pos'), 0)
-    neg_dataset = read_and_label(os.path.join(base_path, 'neg'), 1)
+    pos_dataset = read_and_label('pos', 0)
+    neg_dataset = read_and_label('neg', 1)
     return pos_dataset + neg_dataset
 
 
-def get_imdb(path='./aclImdb', vocab=None, shrink=1, fine_grained=False,
+def get_imdb(vocab=None, shrink=1, fine_grained=False,
              char_based=False):
-    if not os.path.isdir(path):
-        print('download imdb')
-        download_imdb()
-    train_path = os.path.join(path, 'train')
-    test_path = os.path.join(path, 'test')
+    tmp_path = download_imdb()
 
     print('read imdb')
-    train = read_imdb(train_path, shrink=shrink, fine_grained=fine_grained,
+    train = read_imdb(tmp_path, 'train',
+                      shrink=shrink, fine_grained=fine_grained,
                       char_based=char_based)
-    test = read_imdb(test_path, shrink=shrink, fine_grained=fine_grained,
+    test = read_imdb(tmp_path, 'test',
+                     shrink=shrink, fine_grained=fine_grained,
                      char_based=char_based)
+
+    shutil.rmtree(tmp_path)
 
     if vocab is None:
         print('constract vocabulary based on frequency')
@@ -113,19 +115,19 @@ def get_imdb(path='./aclImdb', vocab=None, shrink=1, fine_grained=False,
     return train, test, vocab
 
 
-def download_other_dataset(path, name):
-    os.mkdir(path)
-    base_url = 'https://raw.githubusercontent.com/harvardnlp/sent-conv-torch/master/data/'  # NOQA
+def download_other_dataset(name):
     if name in ['custrev', 'mpqa', 'rt-polarity', 'subj']:
         files = [name + '.all']
     elif name == 'TREC':
         files = [name + suff for suff in ['.train.all', '.test.all']]
     else:
         files = [name + suff for suff in ['.train', '.test']]
+    file_paths = []
     for f_name in files:
-        request.urlretrieve(
-            os.path.join(base_url, f_name),
-            os.path.join(path, f_name))
+        url = os.path.join(URL_OTHER_BASE, f_name)
+        path = chainer.dataset.cached_download(url)
+        file_paths.append(path)
+    return file_paths
 
 
 def read_other_dataset(path, shrink=1, char_based=False):
@@ -141,37 +143,21 @@ def read_other_dataset(path, shrink=1, char_based=False):
     return dataset
 
 
-def get_other_text_dataset(name, path=None, vocab=None, shrink=1,
+def get_other_text_dataset(name, vocab=None, shrink=1,
                            char_based=False, seed=777):
     assert(name in ['TREC', 'stsa.binary', 'stsa.fine',
                     'custrev', 'mpqa', 'rt-polarity', 'subj'])
-
-    if path is None:
-        path = './' + name
-
-    if not os.path.isdir(path):
-        print('download ' + name)
-        download_other_dataset(path, name)
-
-    print('read ' + name)
-    if name in ['custrev', 'mpqa', 'rt-polarity', 'subj']:
-        print('split {} to train:test=9:1 by rand seed {}'.format(name, seed))
+    datasets = download_other_dataset(name)
+    train = read_other_dataset(
+        datasets[0], shrink=shrink, char_based=char_based)
+    if len(datasets) == 2:
+        test = read_other_dataset(
+            datasets[1], shrink=shrink, char_based=char_based)
+    else:
         numpy.random.seed(seed)
-        alldata = numpy.random.permutation(
-            read_other_dataset(os.path.join(path, name + '.all'),
-                               shrink=shrink, char_based=char_based))
+        alldata = numpy.random.permutation(train)
         train = alldata[:-len(alldata) // 10]
         test = alldata[-len(alldata) // 10:]
-    elif name == 'TREC':
-        train = read_other_dataset(os.path.join(path, name + '.train.all'),
-                                   shrink=shrink, char_based=char_based)
-        test = read_other_dataset(os.path.join(path, name + '.test.all'),
-                                  shrink=shrink, char_based=char_based)
-    else:
-        train = read_other_dataset(os.path.join(path, name + '.train'),
-                                   shrink=shrink, char_based=char_based)
-        test = read_other_dataset(os.path.join(path, name + '.test'),
-                                  shrink=shrink, char_based=char_based)
 
     if vocab is None:
         print('constract vocabulary based on frequency')
