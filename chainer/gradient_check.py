ce5b0363c2429a0f25c1297d15413a8f96014912
Crissman Loomis <crissman@preferred.jp>
2017-12-06 19:06:36 +0900

    Fix python function links

--- chainer/gradient_check.py
+++ chainer/gradient_check.py
@@ -33,7 +33,7 @@ def numerical_grad(
     ``eps``.
 
     Args:
-        f (function): Python function with no arguments that runs forward
+        f (`Python function`_): Python function with no arguments that runs forward
             computation and returns the result.
         inputs (tuple of arrays): Tuple of arrays that should be treated as
             inputs. Each element of them is slightly modified to realize
@@ -67,6 +67,8 @@ def numerical_grad(
 
     Returns:
         tuple: Numerical gradient arrays corresponding to ``inputs``.
+
+    .. _Python function: https://docs.python.org/3/reference/compound_stmts.html#function-definitions
 
     """
     assert eps > 0
