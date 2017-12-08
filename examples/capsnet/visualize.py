120366ef3bce926a18bf24558d0f5cb152aded3c
Sosuke <sosk@preferred.jp>
2017-12-08 11:57:42 +0900

    simplify visualizer

--- examples/capsnet/visualize.py
+++ examples/capsnet/visualize.py
@@ -10,46 +10,44 @@ import numpy as np
 
 import chainer
 from chainer.dataset.convert import concat_examples
+from chainer import cuda
 from chainer import serializers
 
 import nets
 
 
-def save_images(xs, filename, marked_row=0):
+def save_images(xs, filename):
     width = xs[0].shape[0]
     height = len(xs)
 
-    xs = [np.array(x.tolist(), np.float32) for x in xs]
+    xs = np.concatenate([cuda.to_cpu(x) for x in xs], axis=0)
     fig, ax = plt.subplots(
         height, width, figsize=(1 * width / 2.5, height / 2.5))
-    xs = np.concatenate(xs, axis=0)
     for i, (ai, xi) in enumerate(zip(ax.ravel(), xs)):
         ai.set_xticklabels([])
         ai.set_yticklabels([])
         ai.set_axis_off()
-        color = 'Greens_r' if i // width == marked_row else 'Blues_r'
+        color = 'Greens_r' if i // width == 0 else 'Blues_r'  # colored rows
         ai.imshow(xi.reshape(28, 28), cmap=color, vmin=0., vmax=1.)
 
-    plt.subplots_adjust(
-        left=None, bottom=None, right=None, top=None, wspace=0.05, hspace=0.05)
+    plt.subplots_adjust(wspace=0.05, hspace=0.05)
     fig.savefig(filename, bbox_inches='tight', pad=0.)
     plt.clf()
     plt.close('all')
 
 
 def visualize_reconstruction(model, x, t, filename='vis.png'):
-    print('visualize', filename)
     vs_norm, vs = model.output(x)
     x_recon = model.reconstruct(vs, t)
-    save_images([x, x_recon.data],
-                filename)
+    save_images([x, x_recon.data], filename)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='CapsNet: MNIST reconstruction')
     parser.add_argument('--gpu', '-g', type=int, default=-1)
-    parser.add_argument('--load')
+    parser.add_argument('--load', required=True)
+    parser.add_argument('--save-image', default='vis.png')
     args = parser.parse_args()
     print(json.dumps(args.__dict__, indent=2))
 
@@ -65,4 +63,4 @@ if __name__ == '__main__':
 
     with chainer.no_backprop_mode():
         with chainer.using_config('train', False):
-            visualize_reconstruction(model, x, t)
+            visualize_reconstruction(model, x, t, args.save_image)
