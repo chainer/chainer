da42ed97a7309562c1d0e52908bf3ee048a8831f
Masayuki Takagi <kamonama@gmail.com>
2017-12-13 23:22:15 +0900

    Add iterators

--- xchainer/shape.h
+++ xchainer/shape.h
@@ -15,6 +15,11 @@ class Shape {
     using DimsType = std::array<int64_t, kMaxNdim>;
 
 public:
+    using iterator = DimsType::iterator;
+    using const_iterator = DimsType::const_iterator;
+    using reverse_iterator = DimsType::reverse_iterator;
+    using const_reverse_iterator = DimsType::const_reverse_iterator;
+
     // by gsl:span
     Shape(gsl::span<const int64_t> dims) : ndim_(gsl::narrow_cast<int8_t>(dims.size())) {
         CheckNdim();
@@ -45,6 +50,19 @@ public:
         return dims_[index];
     }
 
+    // iterators
+    const_iterator cbegin() const noexcept { return dims_.cbegin(); }
+    const_iterator cend() const noexcept { return dims_.cbegin() + ndim_; }
+    const_iterator begin() const noexcept { return cbegin(); }
+    const_iterator end() const noexcept { return cend(); }
+
+    // reverse iterators
+    const_reverse_iterator crbegin() const noexcept { return dims_.crend() - ndim_; }
+    const_reverse_iterator crend() const noexcept { return dims_.crend(); }
+    const_reverse_iterator rbegin() const noexcept { return crbegin(); }
+    const_reverse_iterator rend() const noexcept { return crend(); }
+
+    // span
     gsl::span<const int64_t> span() const { return {&dims_[0], static_cast<size_t>(ndim_)}; }
 
 private:
