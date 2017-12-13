64740c2f1e1c6a0b9282a4f06192e50458e1721b
sonots <sonots@gmail.com>
2017-12-13 15:57:48 +0900

    Add python binding to Dtype

--- xchainer/python/core_module.cc
+++ xchainer/python/core_module.cc
@@ -1,5 +1,91 @@
+#include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <algorithm>
+#include <gsl/gsl>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include "xchainer/dtype.h"
+#include "xchainer/error.h"
 
-PYBIND11_MODULE(_core, m) {
-    m.attr("__name__") = "xchainer";  // Show each member as "xchainer.*" instead of "xchainer._core.*"
+namespace xchainer {
+namespace {
+
+namespace py = pybind11;  // standard convention
+
+Dtype ConvertDtype(py::dtype npdtype) {
+    switch (npdtype.kind()) {
+        case 'b':
+            return Dtype::kBool;
+
+        case 'i':
+            switch (npdtype.itemsize()) {
+                case 1:
+                    return Dtype::kInt8;
+                case 2:
+                    return Dtype::kInt16;
+                case 4:
+                    return Dtype::kInt32;
+                case 8:
+                    return Dtype::kInt64;
+                default:
+                    break;
+            }
+            break;
+
+        case 'u':
+            switch (npdtype.itemsize()) {
+                case 1:
+                    return Dtype::kUInt8;
+                default:
+                    break;
+            }
+            break;
+
+        case 'f':
+            switch (npdtype.itemsize()) {
+                case 4:
+                    return Dtype::kFloat32;
+                case 8:
+                    return Dtype::kFloat64;
+                default:
+                    break;
+            }
+            break;
+
+        default:
+            break;
+    }
+    throw DtypeError("unsupported numpy dtype");
 }
+
+void InitXchainerModule(pybind11::module& m) {
+    m.doc() = "Xchainer";
+    m.attr("__name__") = "xchainer";  // Show each member as "xchainer.*" instead of "xchainer.core.*"
+
+    py::register_exception<XchainerError>(m, "XchainerError");
+    py::register_exception<DtypeError>(m, "DtypeError");
+
+    //
+    // Types
+    //
+
+    {
+        py::enum_<Dtype> dtype_type(m, "Dtype");
+        for (Dtype dtype : GetAllDtypes()) {
+            dtype_type.value(GetDtypeName(dtype), dtype);
+        }
+        dtype_type.export_values();
+        dtype_type.def(py::init(&GetDtype));
+        dtype_type.def_property_readonly("char", [](Dtype dtype) { return std::string(1, GetCharCode(dtype)); });
+        dtype_type.def_property_readonly("itemsize", &GetElementSize);
+    }
+
+    m.def("array", []() {});
+}
+
+}  // namespace
+}  // namespace xchainer
+
+PYBIND11_MODULE(_core, m) { xchainer::InitXchainerModule(m); }
