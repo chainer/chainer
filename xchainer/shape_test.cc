ea3993f0054b75be93d7c3ade3d3b4ed1f9cce18
Masayuki Takagi <kamonama@gmail.com>
2017-12-13 12:43:18 +0900

    Fix test

--- xchainer/shape_test.cc
+++ xchainer/shape_test.cc
@@ -5,26 +5,26 @@ namespace xchainer {
 namespace {
 
 void CheckSpanEqual(std::initializer_list<int64_t> expect, gsl::span<const int64_t> actual) {
-    EXPECT_EQ(gsl::make_span(expect.begin(), expect.end()), actual);
+    ASSERT_EQ(gsl::make_span(expect.begin(), expect.end()), actual);
 }
 
 TEST(ShapeTest, Ctor) {
     {
         const Shape shape = {2, 3, 4};
-        EXPECT_EQ(3, shape.ndim());
-        EXPECT_EQ(static_cast<size_t>(3), shape.size());
+        ASSERT_EQ(3, shape.ndim());
+        ASSERT_EQ(static_cast<size_t>(3), shape.size());
         CheckSpanEqual({2, 3, 4}, shape.span());
-        EXPECT_EQ(2 * 3 * 4, shape.total_size());
+        ASSERT_EQ(2 * 3 * 4, shape.total_size());
     }
     {
         const std::array<int64_t, 3> dims = {2, 3, 4};
         const Shape shape(gsl::make_span(dims));
-        EXPECT_EQ(3, shape.ndim());
+        ASSERT_EQ(3, shape.ndim());
         CheckSpanEqual({2, 3, 4}, shape.span());
     }
     {
         const std::array<int64_t, kMaxNdim + 1> too_long = {1};
-        EXPECT_THROW(Shape(gsl::make_span(too_long)), DimensionError);
+        ASSERT_THROW(Shape(gsl::make_span(too_long)), DimensionError);
     }
 }
 
