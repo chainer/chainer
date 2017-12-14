ce9992db73b671b53069585af083705c6cab3e86
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2017-12-14 16:12:40 +0900

    code style fix

--- docs/source/_autosummary_check.py
+++ docs/source/_autosummary_check.py
@@ -16,30 +16,31 @@ def process(app, what, name, obj, options, lines):
 def check(app, exception):
     missing_entities = []
 
-    for name in _list_chainer_functions():
-        if name not in _autodoc_entities['function']:
-            missing_entities.append(name)
+    missing_entities += [
+        name for name in _list_chainer_functions()
+        if name not in _autodoc_entities['function']]
 
-    for name in _list_chainer_links():
-        if name not in _autodoc_entities['class']:
-            missing_entities.append(name)
+    missing_entities += [
+        name for name in _list_chainer_links()
+        if name not in _autodoc_entities['class']]
 
     if len(missing_entities) != 0:
         app.warn('\n'.join([
             'Undocumented entities found.',
             '(Note: be sure to use `make clean html` to run this check):',
             '',
-            ] + missing_entities))
+        ] + missing_entities))
 
 
 def _list_chainer_functions():
+    # List exported functions under chainer.functions.
     return ['chainer.functions.{}'.format(name)
-        for (name, func) in chainer.functions.__dict__.items()
-        if isinstance(func, types.FunctionType)]
+            for (name, func) in chainer.functions.__dict__.items()
+            if isinstance(func, types.FunctionType)]
 
 
 def _list_chainer_links():
     # List exported classes under chainer.links.
     return ['chainer.links.{}'.format(name)
-        for (name, link) in chainer.links.__dict__.items()
-        if inspect.isclass(link)]
+            for (name, link) in chainer.links.__dict__.items()
+            if inspect.isclass(link)]
