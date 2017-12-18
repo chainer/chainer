bda2289e3df320ec58ec1689dc0aae342bd8542f
sonots <sonots@gmail.com>
2017-12-18 12:08:08 +0900

    s/EXPECT/ASSERT/ because I think it is fatal if they fail

--- xchainer/array_test.cc
+++ xchainer/array_test.cc
@@ -23,22 +23,22 @@ private:
 TEST_F(ArrayTest, Ctor) {
     std::shared_ptr<void> data = std::unique_ptr<float[]>(new float[2 * 3 * 4]);
     Array x = MakeArray({2, 3, 4}, data);
-    EXPECT_EQ(dtype(), x.dtype());
-    EXPECT_EQ(3, x.ndim());
-    EXPECT_EQ(2 * 3 * 4, x.total_size());
-    EXPECT_EQ(4, x.element_bytes());
-    EXPECT_EQ(2 * 3 * 4 * 4, x.total_bytes());
+    ASSERT_EQ(dtype(), x.dtype());
+    ASSERT_EQ(3, x.ndim());
+    ASSERT_EQ(2 * 3 * 4, x.total_size());
+    ASSERT_EQ(4, x.element_bytes());
+    ASSERT_EQ(2 * 3 * 4 * 4, x.total_bytes());
     const std::shared_ptr<void> x_data = x.data();
-    EXPECT_EQ(data, x_data);
-    EXPECT_EQ(0, x.offset());
-    EXPECT_TRUE(x.is_contiguous());
+    ASSERT_EQ(data, x_data);
+    ASSERT_EQ(0, x.offset());
+    ASSERT_TRUE(x.is_contiguous());
 }
 
 TEST_F(ArrayTest, ConstArray) {
     std::shared_ptr<void> data = std::unique_ptr<float[]>(new float[2 * 3 * 4]);
     const Array x = MakeArray({2, 3, 4}, data);
     std::shared_ptr<const void> x_data = x.data();
-    EXPECT_EQ(data, x_data);
+    ASSERT_EQ(data, x_data);
 }
 
 }  // namespace
