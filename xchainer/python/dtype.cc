a84b3bb1690f607ab8bf0c7a95f4d75182d02fc0
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2017-12-19 14:35:00 +0900

    Modularize Python bindings

---
+++ xchainer/python/dtype.cc
@@ -0,0 +1,22 @@
+#include "xchainer/python/dtype.h"
+
+#include "xchainer/dtype.h"
+
+namespace xchainer {
+
+namespace py = pybind11;  // standard convention
+
+void InitXchainerDtype(pybind11::module& m) {
+    {
+        py::enum_<Dtype> dtype_type(m, "Dtype");
+        for (Dtype dtype : GetAllDtypes()) {
+            dtype_type.value(GetDtypeName(dtype), dtype);
+        }
+        dtype_type.export_values();
+        dtype_type.def(py::init(&GetDtype));
+        dtype_type.def_property_readonly("char", [](Dtype dtype) { return std::string(1, GetCharCode(dtype)); });
+        dtype_type.def_property_readonly("itemsize", &GetElementSize);
+        dtype_type.def_property_readonly("name", &GetDtypeName);
+    }
+}
+}  // namespace xchainer
---
+++ xchainer/python/scalar.cc
@@ -0,0 +1,24 @@
+#include "xchainer/python/error.h"
+
+#include <pybind11/operators.h>
+
+#include "xchainer/scalar.h"
+
+namespace xchainer {
+
+namespace py = pybind11;  // standard convention
+
+void InitXchainerScalar(pybind11::module& m) {
+    py::class_<Scalar>(m, "Scalar")
+        .def(py::init<bool>())
+        .def(py::init<int64_t>())
+        .def(py::init<double>())
+        .def(+py::self)
+        .def(-py::self)
+        .def("__bool__", &Scalar::operator bool)
+        .def("__int__", &Scalar::operator int64_t)
+        .def("__float__", &Scalar::operator double)
+        .def("__repr__", &Scalar::ToString)
+        .def_property_readonly("dtype", &Scalar::dtype);
+}
+}  // namespace xchainer
