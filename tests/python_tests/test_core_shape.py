5b370f39bec9e65afc4aa5d8a58cb6f5f04ad611
niboshi <niboshi000@gmail.com>
2017-12-20 12:25:15 +0900

    Add shape inequality tests

--- tests/python_tests/test_core_shape.py
+++ tests/python_tests/test_core_shape.py
@@ -17,11 +17,21 @@ import xchainer
 def test_shape(shape_tup):
     shape = xchainer.Shape(shape_tup)
 
+    assert shape.ndim == len(shape_tup)
+    assert shape.size == len(shape_tup)
+
+    # equality
     assert shape == xchainer.Shape(shape_tup)
     assert shape == shape_tup
     assert shape_tup == shape
-    assert shape.ndim == len(shape_tup)
-    assert shape.size == len(shape_tup)
+    # inequality
+    assert shape != xchainer.Shape(shape_tup + (1,))
+    assert shape != shape_tup + (1,)
+    assert shape_tup + (1,) != shape_tup
+    if shape_tup != ():
+        assert shape != tuple(['a' for _ in shape_tup])
+        # Note: this behavior is different from NumPy
+        assert shape != tuple([float(d) for d in shape_tup])
 
     expected_total_size = functools.reduce(operator.mul, shape_tup, 1)
     assert shape.total_size == expected_total_size
