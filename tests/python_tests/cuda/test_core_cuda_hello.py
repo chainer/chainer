a59bc467f73a25b11176160ba01ec62e2ff5c130
Masayuki Takagi <kamonama@gmail.com>
2017-12-21 15:11:02 +0900

    Add Python test

---
+++ tests/python_tests/cuda/test_core_cuda_hello.py
@@ -0,0 +1,22 @@
+import pytest
+
+import xchainer
+
+
+cuda = pytest.mark.cuda
+
+@cuda
+def test_hello(capfd):
+    device = xchainer.get_current_device()
+
+    xchainer.set_current_device('cpu')
+    xchainer.hello()
+    out, _ = capfd.readouterr()
+    assert out == 'Hello, World!\n'
+
+    xchainer.set_current_device('cuda')
+    xchainer.hello()
+    out, _ = capfd.readouterr()
+    assert out == 'Hello, CUDA!\n'
+
+    xchainer.set_current_device(device)
