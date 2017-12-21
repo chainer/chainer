72fd1bc2b3dc76273fa8864a489ef96c49ed7166
Masayuki Takagi <kamonama@gmail.com>
2017-12-22 00:13:18 +0900

    Take dummy API output

--- tests/python_tests/cuda/test_core_cuda_hello.py
+++ tests/python_tests/cuda/test_core_cuda_hello.py
@@ -1,4 +1,5 @@
 import pytest
+import subprocess
 
 import xchainer
 
@@ -7,17 +8,15 @@ cuda = pytest.mark.cuda
 
 
 @cuda
-def test_hello(capfd):
-    device = xchainer.get_current_device()
+def test_hello():
+    proc = subprocess.Popen(['python', '-c',
+                             'import xchainer; '
+                             'xchainer.set_current_device("cpu"); '
+                             'xchainer.hello()'], stdout=subprocess.PIPE)
+    assert proc.stdout.read() == b'Hello, World!\n'
 
-    xchainer.set_current_device('cpu')
-    xchainer.hello()
-    out, _ = capfd.readouterr()
-    assert out == 'Hello, World!\n'
-
-    xchainer.set_current_device('cuda')
-    xchainer.hello()
-    out, _ = capfd.readouterr()
-    assert out == 'Hello, CUDA!\n'
-
-    xchainer.set_current_device(device)
+    proc = subprocess.Popen(['python', '-c',
+                             'import xchainer; '
+                             'xchainer.set_current_device("cuda"); '
+                             'xchainer.hello()'], stdout=subprocess.PIPE)
+    assert proc.stdout.read() == b'Hello, CUDA!\n'
