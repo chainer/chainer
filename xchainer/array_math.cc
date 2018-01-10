13dbbd5e88ee27653813f01adf90d40bfcf0f991
niboshi <niboshi000@gmail.com>
2018-01-10 19:26:59 +0900

    Use auto (modernize-use-auto)

--- xchainer/array_math.cc
+++ xchainer/array_math.cc
@@ -11,9 +11,9 @@ void Add(const Array& lhs, const Array& rhs, Array& out) {
         using T = typename decltype(pt)::type;
 
         auto total_size = lhs.shape().total_size();
-        const T* ldata = static_cast<const T*>(lhs.data().get());
-        const T* rdata = static_cast<const T*>(rhs.data().get());
-        T* odata = static_cast<T*>(out.data().get());
+        auto* ldata = static_cast<const T*>(lhs.data().get());
+        auto* rdata = static_cast<const T*>(rhs.data().get());
+        auto* odata = static_cast<T*>(out.data().get());
 
         for (decltype(total_size) i = 0; i < total_size; i++) {
             odata[i] = ldata[i] + rdata[i];
@@ -26,9 +26,9 @@ void Mul(const Array& lhs, const Array& rhs, Array& out) {
         using T = typename decltype(pt)::type;
 
         auto total_size = lhs.shape().total_size();
-        const T* ldata = static_cast<const T*>(lhs.data().get());
-        const T* rdata = static_cast<const T*>(rhs.data().get());
-        T* odata = static_cast<T*>(out.data().get());
+        auto* ldata = static_cast<const T*>(lhs.data().get());
+        auto* rdata = static_cast<const T*>(rhs.data().get());
+        auto* odata = static_cast<T*>(out.data().get());
 
         for (decltype(total_size) i = 0; i < total_size; i++) {
             odata[i] = ldata[i] * rdata[i];
