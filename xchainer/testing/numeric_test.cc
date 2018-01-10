9bffb38d47aae7d6c25b8a819dad4a6ce7de6dd1
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-01-10 19:48:12 +0900

    Swap order of atol, rtol in test

--- xchainer/testing/numeric_test.cc
+++ xchainer/testing/numeric_test.cc
@@ -23,21 +23,21 @@ public:
     }
 
     template <typename T>
-    void CheckAllClose(const Shape& shape, std::initializer_list<T> adata, std::initializer_list<T> bdata, double atol, double rtol) {
+    void CheckAllClose(const Shape& shape, std::initializer_list<T> adata, std::initializer_list<T> bdata, double rtol, double atol) {
         Array a = MakeArray<T>(shape, adata);
         Array b = MakeArray<T>(shape, bdata);
         ASSERT_TRUE(AllClose(a, b, rtol, atol));
     }
 
     template <typename T>
-    void CheckNotAllClose(const Shape& shape, std::initializer_list<T> adata, std::initializer_list<T> bdata, double atol, double rtol) {
+    void CheckNotAllClose(const Shape& shape, std::initializer_list<T> adata, std::initializer_list<T> bdata, double rtol, double atol) {
         Array a = MakeArray<T>(shape, adata);
         Array b = MakeArray<T>(shape, bdata);
         ASSERT_FALSE(AllClose(a, b, rtol, atol));
     }
 
     template <typename T, typename U>
-    void CheckAllCloseThrow(const Shape& shape, std::initializer_list<T> adata, std::initializer_list<U> bdata, double atol, double rtol) {
+    void CheckAllCloseThrow(const Shape& shape, std::initializer_list<T> adata, std::initializer_list<U> bdata, double rtol, double atol) {
         Array a = MakeArray<T>(shape, adata);
         Array b = MakeArray<U>(shape, bdata);
         ASSERT_THROW(AllClose(a, b, rtol, atol), DtypeError);
@@ -45,83 +45,83 @@ public:
 };
 
 TEST_F(NumericTest, AllClose) {
-    CheckAllClose<bool>({2}, {true, false}, {true, false}, false, false);
-    CheckAllClose<bool>({2}, {true, false}, {false, true}, true, false);
-    CheckAllClose<bool>({2}, {false, false}, {true, true}, false, true);
-    CheckAllClose<bool>({2}, {false, true}, {true, false}, true, true);
-    CheckNotAllClose<bool>({2}, {true, false}, {false, true}, false, false);
-    CheckNotAllClose<bool>({2}, {true, false}, {false, true}, false, true);
+    CheckAllClose<bool>({2}, {true, false}, {true, false}, 0., 0.);
+    CheckAllClose<bool>({2}, {true, false}, {false, true}, 0., 1.);
+    CheckAllClose<bool>({2}, {false, false}, {true, true}, 1., 0.);
+    CheckAllClose<bool>({2}, {false, true}, {true, false}, 1., 1.);
+    CheckNotAllClose<bool>({2}, {true, false}, {false, true}, 0., 0.);
+    CheckNotAllClose<bool>({2}, {true, false}, {false, true}, 1., 0.);
 
-    CheckAllClose<int8_t>({2}, {1, 2}, {1, 2}, 0, 0);
-    CheckAllClose<int8_t>({2}, {1, 2}, {3, 4}, 2, 0);
-    CheckAllClose<int8_t>({2}, {1, 2}, {3, 4}, 0, 2);
-    CheckAllClose<int8_t>({2}, {1, 2}, {3, 4}, 1, 1);
-    CheckNotAllClose<int8_t>({2}, {1, 2}, {3, 4}, 0, 0);
-    CheckNotAllClose<int8_t>({2}, {1, 2}, {3, 4}, 1, 0);
-    CheckNotAllClose<int8_t>({2}, {3, 4}, {1, 2}, 0, 1);
+    CheckAllClose<int8_t>({2}, {1, 2}, {1, 2}, 0., 0.);
+    CheckAllClose<int8_t>({2}, {1, 2}, {3, 4}, 0., 2.);
+    CheckAllClose<int8_t>({2}, {1, 2}, {3, 4}, 2., 0.);
+    CheckAllClose<int8_t>({2}, {1, 2}, {3, 4}, 1., 1.);
+    CheckNotAllClose<int8_t>({2}, {1, 2}, {3, 4}, 0., 0.);
+    CheckNotAllClose<int8_t>({2}, {1, 2}, {3, 4}, 0., 1.);
+    CheckNotAllClose<int8_t>({2}, {3, 4}, {1, 2}, 1., 0.);
 
-    CheckAllClose<int16_t>({2}, {1, 2}, {1, 2}, 0, 0);
-    CheckAllClose<int16_t>({2}, {1, 2}, {3, 4}, 2, 0);
-    CheckAllClose<int16_t>({2}, {1, 2}, {3, 4}, 0, 2);
-    CheckAllClose<int16_t>({2}, {1, 2}, {3, 4}, 1, 1);
-    CheckNotAllClose<int16_t>({2}, {1, 2}, {3, 4}, 0, 0);
-    CheckNotAllClose<int16_t>({2}, {1, 2}, {3, 4}, 1, 0);
-    CheckNotAllClose<int16_t>({2}, {3, 4}, {1, 2}, 0, 1);
+    CheckAllClose<int16_t>({2}, {1, 2}, {1, 2}, 0., 0.);
+    CheckAllClose<int16_t>({2}, {1, 2}, {3, 4}, 0., 2.);
+    CheckAllClose<int16_t>({2}, {1, 2}, {3, 4}, 2., 0.);
+    CheckAllClose<int16_t>({2}, {1, 2}, {3, 4}, 1., 1.);
+    CheckNotAllClose<int16_t>({2}, {1, 2}, {3, 4}, 0., 0.);
+    CheckNotAllClose<int16_t>({2}, {1, 2}, {3, 4}, 0., 1.);
+    CheckNotAllClose<int16_t>({2}, {3, 4}, {1, 2}, 1., 0.);
 
-    CheckAllClose<int32_t>({2}, {1, 2}, {1, 2}, 0, 0);
-    CheckAllClose<int32_t>({2}, {-1, 0}, {1, 2}, 2, 0);
-    CheckAllClose<int32_t>({2}, {-1, 0}, {1, 2}, 0, 2);
-    CheckAllClose<int32_t>({2}, {-1, 0}, {1, 2}, 1, 1);
-    CheckNotAllClose<int32_t>({2}, {-1, 0}, {1, 2}, 0, 0);
-    CheckNotAllClose<int32_t>({2}, {-1, 0}, {1, 2}, 1, 0);
-    CheckNotAllClose<int32_t>({2}, {-1, 0}, {1, 2}, 0, 1);
+    CheckAllClose<int32_t>({2}, {1, 2}, {1, 2}, 0., 0.);
+    CheckAllClose<int32_t>({2}, {-1, 0}, {1, 2}, 0., 2.);
+    CheckAllClose<int32_t>({2}, {-1, 0}, {1, 2}, 2., 0.);
+    CheckAllClose<int32_t>({2}, {-1, 0}, {1, 2}, 1., 1.);
+    CheckNotAllClose<int32_t>({2}, {-1, 0}, {1, 2}, 0., 0.);
+    CheckNotAllClose<int32_t>({2}, {-1, 0}, {1, 2}, 0., 1.);
+    CheckNotAllClose<int32_t>({2}, {-1, 0}, {1, 2}, 1., 0.);
 
-    CheckAllClose<int64_t>({2}, {1, 2}, {1, 2}, 0, 0);
-    CheckAllClose<int64_t>({2}, {-1, 0}, {1, 2}, 2, 0);
-    CheckAllClose<int64_t>({2}, {-1, 0}, {1, 2}, 0, 2);
-    CheckAllClose<int64_t>({2}, {-1, 0}, {1, 2}, 1, 1);
-    CheckNotAllClose<int64_t>({2}, {-1, 0}, {1, 2}, 0, 0);
-    CheckNotAllClose<int64_t>({2}, {-1, 0}, {1, 2}, 1, 0);
-    CheckNotAllClose<int64_t>({2}, {-1, 0}, {1, 2}, 0, 1);
+    CheckAllClose<int64_t>({2}, {1, 2}, {1, 2}, 0., 0.);
+    CheckAllClose<int64_t>({2}, {-1, 0}, {1, 2}, 0., 2.);
+    CheckAllClose<int64_t>({2}, {-1, 0}, {1, 2}, 2., 0.);
+    CheckAllClose<int64_t>({2}, {-1, 0}, {1, 2}, 1., 1.);
+    CheckNotAllClose<int64_t>({2}, {-1, 0}, {1, 2}, 0., 0.);
+    CheckNotAllClose<int64_t>({2}, {-1, 0}, {1, 2}, 0., 1.);
+    CheckNotAllClose<int64_t>({2}, {-1, 0}, {1, 2}, 1., 0.);
 
-    CheckAllClose<uint8_t>({2}, {1, 2}, {1, 2}, 0, 0);
-    CheckAllClose<uint8_t>({2}, {1, 2}, {3, 4}, 2, 0);
-    CheckAllClose<uint8_t>({2}, {1, 2}, {3, 4}, 0, 2);
-    CheckAllClose<uint8_t>({2}, {1, 2}, {3, 4}, 1, 1);
-    CheckNotAllClose<uint8_t>({2}, {1, 2}, {3, 4}, 0, 0);
-    CheckNotAllClose<uint8_t>({2}, {1, 2}, {3, 4}, 1, 0);
-    CheckNotAllClose<uint8_t>({2}, {3, 4}, {1, 2}, 0, 1);
+    CheckAllClose<uint8_t>({2}, {1, 2}, {1, 2}, 0., 0.);
+    CheckAllClose<uint8_t>({2}, {1, 2}, {3, 4}, 0., 2.);
+    CheckAllClose<uint8_t>({2}, {1, 2}, {3, 4}, 2., 0.);
+    CheckAllClose<uint8_t>({2}, {1, 2}, {3, 4}, 1., 1.);
+    CheckNotAllClose<uint8_t>({2}, {1, 2}, {3, 4}, 0., 0.);
+    CheckNotAllClose<uint8_t>({2}, {1, 2}, {3, 4}, 0., 1.);
+    CheckNotAllClose<uint8_t>({2}, {3, 4}, {1, 2}, 1., 0.);
 
     {
         float eps = 1e-5f;
-        CheckAllClose<float>({2}, {1.f, 2.f}, {1.f, 2.f}, 0.f + eps, 0.f);
-        CheckAllClose<float>({2}, {1.f, 2.f}, {1.2f, 2.f}, .2f + eps, 0.f);
-        CheckAllClose<float>({2}, {1.f, 2.f}, {1.2f, 2.5f}, 0.f + eps, .3f);
-        CheckAllClose<float>({2}, {1.f, 2.f}, {1.2f, 2.2f}, .2f + eps, .2f);
-        CheckNotAllClose<float>({2}, {1.f, 2.f}, {1.1f, 2.2f}, 0.f + eps, 0.f);
-        CheckNotAllClose<float>({2}, {1.f, 2.f}, {1.1f, 2.2f}, 1e-2f + eps, 0.f);
-        CheckNotAllClose<float>({2}, {1.f, 2.f}, {1.1f, 2.2f}, 0.f + eps, 1e-2f);
+        CheckAllClose<float>({2}, {1.f, 2.f}, {1.f, 2.f}, 0., 0. + eps);
+        CheckAllClose<float>({2}, {1.f, 2.f}, {1.2f, 2.f}, 0., .2 + eps);
+        CheckAllClose<float>({2}, {1.f, 2.f}, {1.2f, 2.5f}, .3, 0. + eps);
+        CheckAllClose<float>({2}, {1.f, 2.f}, {1.2f, 2.2f}, .2, .2 + eps);
+        CheckNotAllClose<float>({2}, {1.f, 2.f}, {1.1f, 2.2f}, 0., 0. + eps);
+        CheckNotAllClose<float>({2}, {1.f, 2.f}, {1.1f, 2.2f}, 0., 1e-2 + eps);
+        CheckNotAllClose<float>({2}, {1.f, 2.f}, {1.1f, 2.2f}, 1e-2, 0. + eps);
     }
 
     {
         double eps = 1e-8;
-        CheckAllClose<double>({2}, {1., 2.}, {1., 2.}, 0. + eps, 0.);
-        CheckAllClose<double>({2}, {1., 2.}, {1.2, 2.}, .2 + eps, 0.);
-        CheckAllClose<double>({2}, {1., 2.}, {1.2, 2.5}, 0. + eps, .3);
-        CheckAllClose<double>({2}, {1., 2.}, {1.2, 2.2}, .2 + eps, .2);
-        CheckNotAllClose<double>({2}, {1., 2.}, {1.1, 2.2}, 0. + eps, 0.);
-        CheckNotAllClose<double>({2}, {1., 2.}, {1.1, 2.2}, 1e-2 + eps, 0.);
-        CheckNotAllClose<double>({2}, {1., 2.}, {1.1, 2.2}, 0. + eps, 1e-2);
+        CheckAllClose<double>({2}, {1., 2.}, {1., 2.}, 0., 0. + eps);
+        CheckAllClose<double>({2}, {1., 2.}, {1.2, 2.}, 0., .2 + eps);
+        CheckAllClose<double>({2}, {1., 2.}, {1.2, 2.5}, .3, 0. + eps);
+        CheckAllClose<double>({2}, {1., 2.}, {1.2, 2.2}, .2, .2 + eps);
+        CheckNotAllClose<double>({2}, {1., 2.}, {1.1, 2.2}, 0., 0. + eps);
+        CheckNotAllClose<double>({2}, {1., 2.}, {1.1, 2.2}, 0., 1e-2 + eps);
+        CheckNotAllClose<double>({2}, {1., 2.}, {1.1, 2.2}, 1e-2, 0. + eps);
     }
 }
 
 TEST_F(NumericTest, AllCloseMixed) {
-    CheckAllCloseThrow<bool, int8_t>({3}, {true, false, true}, {1, 2, 3}, 1, 2);
-    CheckAllCloseThrow<int16_t, int8_t>({3}, {1, 2, 3}, {4, 5, 6}, 7, 8);
-    CheckAllCloseThrow<int8_t, int32_t>({3}, {1, 2, 3}, {4, 5, 6}, 7, 8);
-    CheckAllCloseThrow<int64_t, int8_t>({3}, {1, 2, 3}, {1, 2, 3}, 1, 2);
-    CheckAllCloseThrow<int32_t, float>({3}, {1, 2, 3}, {1.f, 2.f, 3.f}, 1, 2);
-    CheckAllCloseThrow<double, int32_t>({3}, {1., 2., 3.}, {1, 2, 3}, 1., 2.);
+    CheckAllCloseThrow<bool, int8_t>({3}, {true, false, true}, {1, 2, 3}, 2., 1.);
+    CheckAllCloseThrow<int16_t, int8_t>({3}, {1, 2, 3}, {4, 5, 6}, 8., 7.);
+    CheckAllCloseThrow<int8_t, int32_t>({3}, {1, 2, 3}, {4, 5, 6}, 8., 7.);
+    CheckAllCloseThrow<int64_t, int8_t>({3}, {1, 2, 3}, {1, 2, 3}, 2., 1.);
+    CheckAllCloseThrow<int32_t, float>({3}, {1, 2, 3}, {1.f, 2.f, 3.f}, 2., 1.);
+    CheckAllCloseThrow<double, int32_t>({3}, {1., 2., 3.}, {1, 2, 3}, 2., 1.);
 }
 
 }  // namespace testing
