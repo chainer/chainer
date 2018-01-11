e9d007d6f8a2fd50968b9b2aa1751f10a410a1f8
niboshi <niboshi000@gmail.com>
2018-01-11 12:52:27 +0900

    Use Array::total_size(), remove const

--- xchainer/cuda/array_fill.cu
+++ xchainer/cuda/array_fill.cu
@@ -10,7 +10,7 @@ namespace cuda {
 namespace {
 
 template <typename T>
-__global__ void FillKernel(T* odata, const T value, const int64_t total_size) {
+__global__ void FillKernel(T* odata, T value, int64_t total_size) {
     for (int64_t i = blockIdx.x * blockDim.x + threadIdx.x; i < total_size; i += blockDim.x * gridDim.x) {
         odata[i] = value;
     }
@@ -23,9 +23,9 @@ void Fill(Array& out, Scalar value) {
         using T = typename decltype(pt)::type;
         static const int kMaxBlockSize = CudaOccupancyMaxPotentialBlockSize(&FillKernel<T>).block_size;
 
-        const int64_t total_size = out.shape().total_size();
-        const int64_t grid_size = (total_size + kMaxBlockSize - 1) / kMaxBlockSize;
-        const int64_t block_size = std::min<int64_t>(total_size, kMaxBlockSize);
+        int64_t total_size = out.total_size();
+        int64_t grid_size = (total_size + kMaxBlockSize - 1) / kMaxBlockSize;
+        int64_t block_size = std::min<int64_t>(total_size, kMaxBlockSize);
 
         auto* odata = static_cast<T*>(out.data().get());
         FillKernel<<<grid_size, block_size>>>(odata, static_cast<T>(value), total_size);
--- xchainer/cuda/array_math.cu
+++ xchainer/cuda/array_math.cu
@@ -10,14 +10,14 @@ namespace cuda {
 namespace {
 
 template <typename T>
-__global__ void AddKernel(const T* ldata, const T* rdata, T* odata, const int64_t total_size) {
+__global__ void AddKernel(const T* ldata, const T* rdata, T* odata, int64_t total_size) {
     for (int64_t i = blockIdx.x * blockDim.x + threadIdx.x; i < total_size; i += blockDim.x * gridDim.x) {
         odata[i] = ldata[i] + rdata[i];
     }
 }
 
 template <typename T>
-__global__ void MulKernel(const T* ldata, const T* rdata, T* odata, const int64_t total_size) {
+__global__ void MulKernel(const T* ldata, const T* rdata, T* odata, int64_t total_size) {
     for (int64_t i = blockIdx.x * blockDim.x + threadIdx.x; i < total_size; i += blockDim.x * gridDim.x) {
         odata[i] = ldata[i] * rdata[i];
     }
@@ -31,9 +31,9 @@ void Add(const Array& lhs, const Array& rhs, Array& out) {
         using T = typename decltype(pt)::type;
         static const int kMaxBlockSize = CudaOccupancyMaxPotentialBlockSize(&AddKernel<T>).block_size;
 
-        const int64_t total_size = lhs.shape().total_size();
-        const int64_t grid_size = (total_size + kMaxBlockSize - 1) / kMaxBlockSize;
-        const int64_t block_size = std::min<int64_t>(total_size, kMaxBlockSize);
+        int64_t total_size = lhs.total_size();
+        int64_t grid_size = (total_size + kMaxBlockSize - 1) / kMaxBlockSize;
+        int64_t block_size = std::min<int64_t>(total_size, kMaxBlockSize);
 
         auto* ldata = static_cast<const T*>(lhs.data().get());
         auto* rdata = static_cast<const T*>(rhs.data().get());
@@ -48,9 +48,9 @@ void Mul(const Array& lhs, const Array& rhs, Array& out) {
         using T = typename decltype(pt)::type;
         static const int kMaxBlockSize = CudaOccupancyMaxPotentialBlockSize(&MulKernel<T>).block_size;
 
-        const int64_t total_size = lhs.shape().total_size();
-        const int64_t grid_size = (total_size + kMaxBlockSize - 1) / kMaxBlockSize;
-        const int64_t block_size = std::min<int64_t>(total_size, kMaxBlockSize);
+        int64_t total_size = lhs.total_size();
+        int64_t grid_size = (total_size + kMaxBlockSize - 1) / kMaxBlockSize;
+        int64_t block_size = std::min<int64_t>(total_size, kMaxBlockSize);
 
         auto* ldata = static_cast<const T*>(lhs.data().get());
         auto* rdata = static_cast<const T*>(rhs.data().get());
