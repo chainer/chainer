5641bdbfed7179eea853f5267b677b964e67b849
Takeshi Kanmae <tkanmae@gmail.com>
2018-01-11 13:23:53 +0900

    Revert "Add a test for avg_mean and avg_var attributes"
    This reverts commit 5b1619710f99bed0983f4bf3814a3cb502b4bc6b.

--- tests/chainer_tests/links_tests/normalization_tests/test_batch_normalization.py
+++ tests/chainer_tests/links_tests/normalization_tests/test_batch_normalization.py
@@ -390,12 +390,8 @@ class TestChannalSizeInference(unittest.TestCase):
         self.link_no_beta = links.BatchNormalization(None, use_beta=False)
         self.x = numpy.random.randn(1, 16).astype('f')
 
-    def test_initialization(self):
-        assert hasattr(self.link, 'avg_mean')
-        assert hasattr(self.link, 'avg_var')
-
     def test_inference(self):
-        self.link(self.x)
+        self.link(self.x, test=True)
         assert self.link.beta.shape == (16,)
         assert self.link.gamma.shape == (16,)
         assert self.link.avg_mean.shape == (16,)
