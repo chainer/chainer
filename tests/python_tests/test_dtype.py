b77643951dcb65c58ebb84286005307006251ffe
niboshi <niboshi000@gmail.com>
2018-01-12 11:39:23 +0900

    Fix dtype fixture to provide only dtype instances

--- tests/python_tests/test_dtype.py
+++ tests/python_tests/test_dtype.py
@@ -3,18 +3,26 @@ import pytest
 import xchainer
 
 
-@pytest.fixture
-def dtype_init_inputs(dtype_data):
-    name = dtype_data['name']
-    char = dtype_data['char']
-    itemsize = dtype_data['itemsize']
-    return name, char, itemsize
+_dtypes_data = [
+    (xchainer.bool, 'bool', '?', 1),
+    (xchainer.int8, 'int8', 'b', 1),
+    (xchainer.int16, 'int16', 'h', 2),
+    (xchainer.int32, 'int32', 'i', 4),
+    (xchainer.int64, 'int64', 'q', 8),
+    (xchainer.uint8, 'uint8', 'B', 1),
+    (xchainer.float32, 'float32', 'f', 4),
+    (xchainer.float64, 'float64', 'd', 8),
+]
 
 
-def test_inti_eq(dtype_init_inputs):
-    name, char, itemsize = dtype_init_inputs
-    dtype = getattr(xchainer, name)
+def test_dtypes_covered(dtype):
+    # This test ensures _dtypes_data covers all dtypes
+    assert any(tup[0] == dtype for tup in _dtypes_data), "Not all dtypes are covered in _dtypes_data in dtypes test."
 
+
+@pytest.mark.parametrize("dtype,name,char,itemsize", _dtypes_data)
+def test_dtypes(dtype, name, char, itemsize):
+    assert dtype == getattr(xchainer, name)
     assert dtype.name == name
     assert dtype.char == char
     assert dtype.itemsize == itemsize
