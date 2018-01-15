5a583fd5754802034ff2263e140af7fad819db07
Masayuki Takagi <kamonama@gmail.com>
2018-01-15 18:08:18 +0900

    Use const&

--- xchainer/backprop.cc
+++ xchainer/backprop.cc
@@ -47,11 +47,10 @@ public:
 private:
     std::vector<nonstd::optional<Array>> ComputeNextGradients() {
         const std::shared_ptr<const OpNode>& op_node = TopOpNode();
-        std::shared_ptr<ArrayNode> previous_array_node = PreviousArrayNode(op_node);
+        const std::shared_ptr<ArrayNode>& previous_array_node = PreviousArrayNode(op_node);
 
-        nonstd::optional<Array> gy = previous_array_node->grad();
+        const nonstd::optional<Array>& gy = previous_array_node->grad();
         assert(gy);
-        previous_array_node->ClearGrad();
 
         std::vector<nonstd::optional<Array>> gxs;
         for (const auto& backward_function : op_node->backward_functions()) {
@@ -61,6 +60,9 @@ private:
                 gxs.emplace_back(nonstd::nullopt);
             }
         }
+
+        previous_array_node->ClearGrad();
+
         return gxs;
     }
 
@@ -70,7 +72,7 @@ private:
         auto next_size = next_array_nodes.size();
         for (decltype(next_size) i = 0; i < next_size; ++i) {
             const nonstd::optional<Array>& gx = gxs[i];
-            std::shared_ptr<ArrayNode> next_array_node = next_array_nodes[i];
+            const std::shared_ptr<ArrayNode>& next_array_node = next_array_nodes[i];
             if (gx) {
                 const nonstd::optional<Array>& grad = next_array_node->grad();
                 if (grad) {
@@ -83,7 +85,7 @@ private:
     }
 
     void PushNextOpNode(std::shared_ptr<ArrayNode> array_node) {
-        std::shared_ptr<const OpNode> next_op_node = array_node->next_node();
+        const std::shared_ptr<const OpNode>& next_op_node = array_node->next_node();
         if (next_op_node) {
             PushOpNode(next_op_node);
             InsertPreviousArrayNode(next_op_node, array_node);
