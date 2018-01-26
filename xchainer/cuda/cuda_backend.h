e2fc825197a3fb0d8066de42d18fcae19f1998df
Naotoshi Seo <sonots@gmail.com>
2018-01-26 13:59:31 +0900

    Add function declarations to avoid `the following virtual functions are pure` error

--- xchainer/cuda/cuda_backend.h
+++ xchainer/cuda/cuda_backend.h
@@ -7,6 +7,12 @@ namespace cuda {
 
 class CudaBackend : public Backend {
 public:
+    void Fill(Array& out, Scalar value);
+    void Add(const Array& lhs, const Array& rhs, Array& out);
+    void Mul(const Array& lhs, const Array& rhs, Array& out);
+    std::shared_ptr<void> Allocate(const Device& device, size_t bytesize);
+    void MemoryCopy(void* dst_ptr, const void* src_ptr, size_t bytesize);
+    std::shared_ptr<void> FromBuffer(const Device& device, const std::shared_ptr<void>& src_ptr, size_t bytesize);
     void Synchronize();
 };
 
--- xchainer/native_backend.h
+++ xchainer/native_backend.h
@@ -1,4 +1,4 @@
-#pragma onnce
+#pragma once
 
 #include "xchainer/backend.h"
 
@@ -6,6 +6,12 @@ namespace xchainer {
 
 class NativeBackend : public Backend {
 public:
+    void Fill(Array& out, Scalar value);
+    void Add(const Array& lhs, const Array& rhs, Array& out);
+    void Mul(const Array& lhs, const Array& rhs, Array& out);
+    std::shared_ptr<void> Allocate(const Device& device, size_t bytesize);
+    void MemoryCopy(void* dst_ptr, const void* src_ptr, size_t bytesize);
+    std::shared_ptr<void> FromBuffer(const Device& device, const std::shared_ptr<void>& src_ptr, size_t bytesize);
     void Synchronize();
 };
 
