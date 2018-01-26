0b03bd2b96541b67b23c9d5cf8fd2b579d326a2a
Naotoshi Seo <sonots@gmail.com>
2018-01-26 15:34:25 +0900

    Add empty implementation to avoid undefined reference to vtable

--- xchainer/cuda/cuda_backend.cc
+++ xchainer/cuda/cuda_backend.cc
@@ -4,6 +4,42 @@
 namespace xchainer {
 namespace cuda {
 
+void CudaBackend::Fill(Array& out, Scalar value) {
+    (void)out;    // unused
+    (void)value;  // unused
+}
+
+void CudaBackend::Add(const Array& lhs, const Array& rhs, Array& out) {
+    (void)lhs;  // unused
+    (void)rhs;  // unused
+    (void)out;  // unused
+}
+
+void CudaBackend::Mul(const Array& lhs, const Array& rhs, Array& out) {
+    (void)lhs;  // unused
+    (void)rhs;  // unused
+    (void)out;  // unused
+}
+
+std::shared_ptr<void> CudaBackend::Allocate(const Device& device, size_t bytesize) {
+    (void)device;    // unused
+    (void)bytesize;  // unused
+    return nullptr;
+}
+
+void CudaBackend::MemoryCopy(void* dst_ptr, const void* src_ptr, size_t bytesize) {
+    (void)dst_ptr;   // unused
+    (void)src_ptr;   // unused
+    (void)bytesize;  // unused
+}
+
+std::shared_ptr<void> CudaBackend::FromBuffer(const Device& device, const std::shared_ptr<void>& src_ptr, size_t bytesize) {
+    (void)device;    // unused
+    (void)src_ptr;   // unused
+    (void)bytesize;  // unused
+    return nullptr;
+}
+
 void CudaBackend::Synchronize() { CheckError(cudaDeviceSynchronize()); }
 
 }  // namespace cuda
--- xchainer/native_backend.cc
+++ xchainer/native_backend.cc
@@ -2,6 +2,42 @@
 
 namespace xchainer {
 
+void NativeBackend::Fill(Array& out, Scalar value) {
+    (void)out;    // unused
+    (void)value;  // unused
+}
+
+void NativeBackend::Add(const Array& lhs, const Array& rhs, Array& out) {
+    (void)lhs;  // unused
+    (void)rhs;  // unused
+    (void)out;  // unused
+}
+
+void NativeBackend::Mul(const Array& lhs, const Array& rhs, Array& out) {
+    (void)lhs;  // unused
+    (void)rhs;  // unused
+    (void)out;  // unused
+}
+
+std::shared_ptr<void> NativeBackend::Allocate(const Device& device, size_t bytesize) {
+    (void)device;    // unused
+    (void)bytesize;  // unused
+    return nullptr;
+}
+
+void NativeBackend::MemoryCopy(void* dst_ptr, const void* src_ptr, size_t bytesize) {
+    (void)dst_ptr;   // unused
+    (void)src_ptr;   // unused
+    (void)bytesize;  // unused
+}
+
+std::shared_ptr<void> NativeBackend::FromBuffer(const Device& device, const std::shared_ptr<void>& src_ptr, size_t bytesize) {
+    (void)device;    // unused
+    (void)src_ptr;   // unused
+    (void)bytesize;  // unused
+    return nullptr;
+}
+
 void NativeBackend::Synchronize() {}
 
 }  // namespace xchainer
