fbff541816f9a3a2038a7df308fa9638a574481b
Akira Naruse <anaruse@nvidia.com>
2018-01-30 08:41:14 +0900

    clean-up codes

--- chainer/functions/math/tensordot.py
+++ chainer/functions/math/tensordot.py
@@ -13,21 +13,21 @@ def _tensordot(a, b, a_axes, b_axes, c_axes):
     axes = (a_axes[1], b_axes[0])
     y = xp.tensordot(a, b, axes)
 
-    trans = [None for i in range(y.ndim)]
+    trans = [None for i in six.moves.range(y.ndim)]
 
-    table_a = [1 if i in a_axes[0] else 0 for i in range(a.ndim)]
+    table_a = [1 if i in a_axes[0] else 0 for i in six.moves.range(a.ndim)]
     table_a = numpy.cumsum(table_a) - 1
     for i, dst in enumerate(c_axes[0]):
         trans[dst] = table_a[a_axes[0][i]]
 
-    table_b = [1 if i in b_axes[1] else 0 for i in range(b.ndim)]
+    table_b = [1 if i in b_axes[1] else 0 for i in six.moves.range(b.ndim)]
     table_b = numpy.cumsum(table_b) - 1
     for i, dst in enumerate(c_axes[1]):
         trans[dst] = table_b[b_axes[1][i]]
         trans[dst] += len(a_axes[0])
 
     do_transpose = False
-    for i in range(y.ndim):
+    for i in six.moves.range(y.ndim):
         if trans[i] != i:
             do_transpose = True
     if do_transpose:
@@ -49,8 +49,8 @@ class TensorDot(function_node.FunctionNode):
         a, b = inputs
 
         if self.a_axes is None or self.b_axes is None:
-            a_axes = [None, None]
-            b_axes = [None, None]
+            a_axes = [[], []]  # 0:row, 1:col
+            b_axes = [[], []]  # 0:row, 1:col
             axes = self.axes
             if isinstance(axes, collections.Sequence):
                 if len(axes) != 2:
@@ -61,18 +61,20 @@ class TensorDot(function_node.FunctionNode):
                 if numpy.isscalar(b_axes[0]):
                     b_axes[0] = b_axes[0],
             else:
-                a_axes[1] = list(six.moves.range(a.ndim - axes, a.ndim))
-                b_axes[0] = list(six.moves.range(axes))
-            a_axes[0] = [i for i in six.moves.range(a.ndim) if i not in a_axes[1]]
-            b_axes[1] = [i for i in six.moves.range(b.ndim) if i not in b_axes[0]]
+                a_axes[1] = six.moves.range(a.ndim - axes, a.ndim)
+                b_axes[0] = six.moves.range(axes)
+            a_axes[0] = [i for i in six.moves.range(
+                a.ndim) if i not in a_axes[1]]
+            b_axes[1] = [i for i in six.moves.range(
+                b.ndim) if i not in b_axes[0]]
             self.a_axes = a_axes
             self.b_axes = b_axes
 
         if self.c_axes is None:
-            c_axes = [None, None]
-            c_axes[0] = list(six.moves.range(len(a_axes[0])))
-            c_axes[1] = list(six.moves.range(len(a_axes[0]),
-                                             len(a_axes[0]) + len(b_axes[1])))
+            c_axes = [[], []]  # 0:row, 1:col
+            c_axes[0] = six.moves.range(len(a_axes[0]))
+            c_axes[1] = six.moves.range(len(a_axes[0]),
+                                        len(a_axes[0]) + len(b_axes[1]))
             self.c_axes = c_axes
 
         y = _tensordot(a, b, self.a_axes, self.b_axes, self.c_axes)
