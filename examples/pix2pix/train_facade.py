97c5c72c1624d0e77356617e12448d7e8f90d9a2
taizan <taizan@preferred.jp>
2018-02-02 18:28:06 +0900

    fix code styles

--- examples/pix2pix/train_facade.py
+++ examples/pix2pix/train_facade.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# python train_facade.py -g 0 -i ./facade/base --out result_facade --snapshot_interval 10000
-
 from __future__ import print_function
 import argparse
 import os
@@ -19,8 +17,10 @@ from updater import FacadeUpdater
 from facade_dataset import FacadeDataset
 from facade_visualizer import out_image
 
+
 def main():
-    parser = argparse.ArgumentParser(description='chainer implementation of pix2pix')
+    parser = argparse.ArgumentParser(
+        description='chainer implementation of pix2pix')
     parser.add_argument('--batchsize', '-b', type=int, default=1,
                         help='Number of images in each mini-batch')
     parser.add_argument('--epoch', '-e', type=int, default=200,
@@ -50,7 +50,7 @@ def main():
     enc = Encoder(in_ch=12)
     dec = Decoder(out_ch=3)
     dis = Discriminator(in_ch=12, out_ch=3)
-    
+
     if args.gpu >= 0:
         chainer.cuda.get_device(args.gpu).use()  # Make a specified GPU current
         enc.to_gpu()  # Copy the model to the GPU
@@ -67,10 +67,8 @@ def main():
     opt_dec = make_optimizer(dec)
     opt_dis = make_optimizer(dis)
 
-    train_d = FacadeDataset(args.dataset, data_range=(1,300))
-    test_d = FacadeDataset(args.dataset, data_range=(300,379))
-    #train_iter = chainer.iterators.MultiprocessIterator(train_d, args.batchsize, n_processes=4)
-    #test_iter = chainer.iterators.MultiprocessIterator(test_d, args.batchsize, n_processes=4)
+    train_d = FacadeDataset(args.dataset, data_range=(1, 300))
+    test_d = FacadeDataset(args.dataset, data_range=(300, 379))
     train_iter = chainer.iterators.SerialIterator(train_d, args.batchsize)
     test_iter = chainer.iterators.SerialIterator(test_d, args.batchsize)
 
@@ -81,7 +79,7 @@ def main():
             'main': train_iter,
             'test': test_iter},
         optimizer={
-            'enc': opt_enc, 'dec': opt_dec, 
+            'enc': opt_enc, 'dec': opt_dec,
             'dis': opt_dis},
         device=args.gpu)
     trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
@@ -90,7 +88,7 @@ def main():
     display_interval = (args.display_interval, 'iteration')
     trainer.extend(extensions.snapshot(
         filename='snapshot_iter_{.updater.iteration}.npz'),
-                   trigger=snapshot_interval)
+        trigger=snapshot_interval)
     trainer.extend(extensions.snapshot_object(
         enc, 'enc_iter_{.updater.iteration}.npz'), trigger=snapshot_interval)
     trainer.extend(extensions.snapshot_object(
@@ -114,6 +112,7 @@ def main():
 
     # Run the training
     trainer.run()
+
 
 if __name__ == '__main__':
     main()
