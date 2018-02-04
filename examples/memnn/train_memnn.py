aa91acc984c0380ca16fe5800849117840468699
Yuya Unno <unnonouno@gmail.com>
2018-02-04 13:01:47 +0900

    Extract a method

--- examples/memnn/train_memnn.py
+++ examples/memnn/train_memnn.py
@@ -13,6 +13,52 @@ import babi
 import memnn
 
 
+def train(train_data_path, test_data_path, args):
+    vocab = collections.defaultdict(lambda: len(vocab))
+    vocab['<unk>'] = 0
+
+    train_data = babi.read_data(vocab, train_data_path)
+    test_data = babi.read_data(vocab, test_data_path)
+    print('Training data: %s: %d' % (train_data_path, len(train_data)))
+    print('Test data: %s: %d' % (test_data_path, len(test_data)))
+
+    train_data = memnn.convert_data(train_data, args.max_memory)
+    test_data = memnn.convert_data(test_data, args.max_memory)
+
+    encoder = memnn.make_encoder(args.sentence_repr)
+    network = memnn.MemNN(
+        args.unit, len(vocab), encoder, args.max_memory, args.hop)
+    model = chainer.links.Classifier(network, label_key='answer')
+    opt = chainer.optimizers.Adam()
+
+    if args.gpu >= 0:
+        chainer.cuda.get_device(args.gpu).use()
+        model.to_gpu()
+
+    opt.setup(model)
+
+    train_iter = chainer.iterators.SerialIterator(
+        train_data, args.batchsize)
+    test_iter = chainer.iterators.SerialIterator(
+        test_data, args.batchsize, repeat=False, shuffle=False)
+    updater = chainer.training.StandardUpdater(
+        train_iter, opt, device=args.gpu)
+    trainer = chainer.training.Trainer(updater, (args.epoch, 'epoch'))
+
+    @chainer.training.make_extension()
+    def fix_ignore_label(trainer):
+        network.fix_ignore_label()
+
+    trainer.extend(fix_ignore_label)
+    trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
+    trainer.extend(extensions.LogReport())
+    trainer.extend(extensions.PrintReport(
+        ['epoch', 'main/loss', 'validation/main/loss',
+         'main/accuracy', 'validation/main/accuracy']))
+    trainer.extend(extensions.ProgressBar(update_interval=10))
+    trainer.run()
+
+
 def main():
     parser = argparse.ArgumentParser(
         description='Chainer example: End-to-end memory networks')
@@ -35,54 +81,12 @@ def main():
                         'Select from BoW ("bow") or position encoding ("pe")')
     args = parser.parse_args()
 
-    vocab = collections.defaultdict(lambda: len(vocab))
-    vocab['<unk>'] = 0
-
-    for data_id in six.moves.range(1, 21):
-
-        train_data = babi.read_data(
-            vocab,
-            glob.glob('%s/qa%d_*train.txt' % (args.data, data_id))[0])
-        test_data = babi.read_data(
-            vocab,
-            glob.glob('%s/qa%d_*test.txt' % (args.data, data_id))[0])
-        print('Training data: %d' % len(train_data))
-
-        train_data = memnn.convert_data(train_data, args.max_memory)
-        test_data = memnn.convert_data(test_data, args.max_memory)
-
-        encoder = memnn.make_encoder(args.sentence_repr)
-        network = memnn.MemNN(
-            args.unit, len(vocab), encoder, args.max_memory, args.hop)
-        model = chainer.links.Classifier(network, label_key='answer')
-        opt = chainer.optimizers.Adam()
-
-        if args.gpu >= 0:
-            chainer.cuda.get_device(args.gpu).use()
-            model.to_gpu()
-
-        opt.setup(model)
-
-        train_iter = chainer.iterators.SerialIterator(
-            train_data, args.batchsize)
-        test_iter = chainer.iterators.SerialIterator(
-            test_data, args.batchsize, repeat=False, shuffle=False)
-        updater = chainer.training.StandardUpdater(
-            train_iter, opt, device=args.gpu)
-        trainer = chainer.training.Trainer(updater, (args.epoch, 'epoch'))
-
-        @chainer.training.make_extension()
-        def fix_ignore_label(trainer):
-            network.fix_ignore_label()
-
-        trainer.extend(fix_ignore_label)
-        trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
-        trainer.extend(extensions.LogReport())
-        trainer.extend(extensions.PrintReport(
-            ['epoch', 'main/loss', 'validation/main/loss',
-             'main/accuracy', 'validation/main/accuracy']))
-        trainer.extend(extensions.ProgressBar(update_interval=10))
-        trainer.run()
+    for data_id in six.moves.range(1, 2):
+        train_data_path = glob.glob(
+            '%s/qa%d_*train.txt' % (args.data, data_id))[0]
+        test_data_path = glob.glob(
+            '%s/qa%d_*test.txt' % (args.data, data_id))[0]
+        train(train_data_path, test_data_path, args)
 
 
 if __name__ == '__main__':
