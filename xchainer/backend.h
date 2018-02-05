22916a130b8fe23819abec87ef950dc6af014f30
Masayuki Takagi <kamonama@gmail.com>
2018-02-05 14:27:31 +0900

    Remove concept of current backend

--- xchainer/backend.h
+++ xchainer/backend.h
@@ -24,38 +24,4 @@ public:
     virtual void Synchronize() = 0;
 };
 
-namespace internal {
-
-Backend* GetCurrentBackendNoExcept() noexcept;
-
-}  // namespace internal
-
-Backend* GetCurrentBackend();
-void SetCurrentBackend(Backend* backend) noexcept;
-
-// Scope object that switches the current backend by RAII.
-class BackendScope {
-public:
-    BackendScope() : orig_(internal::GetCurrentBackendNoExcept()) {}
-    explicit BackendScope(Backend* backend) : BackendScope() { SetCurrentBackend(backend); }
-
-    BackendScope(const BackendScope&) = delete;
-    BackendScope(BackendScope&&) = delete;
-    BackendScope& operator=(const BackendScope&) = delete;
-    BackendScope& operator=(BackendScope&&) = delete;
-
-    ~BackendScope() { Exit(); }
-
-    // Explicitly recovers the original backend. It will invalidate the scope object so that dtor will do nothing.
-    void Exit() noexcept {
-        if (orig_ != nullptr) {
-            SetCurrentBackend(orig_);
-        }
-        orig_ = nullptr;
-    }
-
-private:
-    Backend* orig_;
-};
-
 }  // namespace xchainer
