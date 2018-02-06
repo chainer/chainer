24fb0bc7f0670633e35d10cec329e3f518b835a2
niboshi <niboshi000@gmail.com>
2018-02-06 16:04:03 +0900

    iDeep: F.linear

--- chainer/functions/connection/linear.py
+++ chainer/functions/connection/linear.py
@@ -1,11 +1,14 @@
 import numpy
 
+from chainer.backends import intel64
 from chainer import function_node
 import chainer.functions
 from chainer.utils import type_check
 
 
 class LinearFunction(function_node.FunctionNode):
+
+    _use_ideep = False
 
     def check_type_forward(self, in_types):
         n_in = in_types.size()
@@ -28,13 +31,18 @@ class LinearFunction(function_node.FunctionNode):
             )
 
     def forward(self, inputs):
-        x = inputs[0]
-        W = inputs[1]
+        if (intel64.should_use_ideep('>=auto')
+                and intel64.inputs_all_ready(inputs)):
 
-        if not type_check.same_types(*inputs):
-            raise ValueError('numpy and cupy must not be used together\n'
-                             'type(W): {0}, type(x): {1}'
-                             .format(type(W), type(x)))
+            # iDeep implementation
+            self._use_ideep = True
+            return self._forward_ideep(inputs)
+
+        # Generic implementation
+        if len(inputs) == 3:
+            x, W, b = inputs
+        else:
+            (x, W), b = inputs, None
 
         # NumPy raises an error when the array is not contiguous.
         # See: https://github.com/chainer/chainer/issues/2744
@@ -45,26 +53,114 @@ class LinearFunction(function_node.FunctionNode):
             x = numpy.ascontiguousarray(x)
 
         y = x.dot(W.T).astype(x.dtype, copy=False)
-        if len(inputs) == 3:
-            b = inputs[2]
+        if b is not None:
             y += b
         self.retain_inputs((0, 1))  # b is not retained
         return y,
 
+    def _forward_ideep(self, inputs):
+        if len(inputs) == 3:
+            x, W, b = inputs
+        else:
+            (x, W), b = inputs, None
+
+        y = intel64.ideep.linear.Forward(
+            intel64.ideep.array(x),
+            intel64.ideep.array(W),
+            intel64.ideep.array(b) if b is not None else None)
+
+        self.retain_inputs((0, 1))
+        return y,
+
     def backward(self, indexes, grad_outputs):
-        x, W = self.get_retained_inputs()
+        ret = []
         gy, = grad_outputs
 
+        x, W = self.get_retained_inputs()
+
+        if self._use_ideep:
+
+            # iDeep implementation
+            if 0 in indexes:  # grad_x
+                gx = LinearGradDIdeep().apply((W, gy))
+                ret.append(gx[0])
+            if 1 in indexes:  # grad_W
+                gW = LinearGradWIdeep().apply((x, gy))
+                ret.append(gW[0])
+            if 2 in indexes:  # grad_b
+                gb = chainer.functions.sum(gy, axis=0)
+                ret.append(gb)
+        else:
+            # Generic implementation
+            if 0 in indexes:
+                gx, = LinearGradData().apply((W, gy))
+                ret.append(chainer.functions.cast(gx, x.dtype))
+            if 1 in indexes:
+                gW, = LinearGradWeight().apply((x, gy))
+                ret.append(chainer.functions.cast(gW, W.dtype))
+            if 2 in indexes:
+                gb = chainer.functions.sum(gy, axis=0)
+                ret.append(gb)
+
+        return ret
+
+
+class LinearGradDIdeep(function_node.FunctionNode):
+
+    def forward_cpu(self, inputs):
+        W, gy = inputs
+
+        gx = intel64.ideep.linear.BackwardData(
+            intel64.ideep.array(W),
+            intel64.ideep.array(gy))
+
+        self.retain_inputs((0, 1))
+        return gx,
+
+    def backward(self, indexes, grad_outputs):
+        inputs = self.get_retained_inputs()
+        W, gy = inputs
+        ggx, = grad_outputs
+
         ret = []
-        if 0 in indexes:
-            gx, = LinearGradData().apply((W, gy))
-            ret.append(chainer.functions.cast(gx, x.dtype))
-        if 1 in indexes:
-            gW, = LinearGradWeight().apply((x, gy))
-            ret.append(chainer.functions.cast(gW, W.dtype))
-        if 2 in indexes:
-            gb = chainer.functions.sum(gy, axis=0)
-            ret.append(gb)
+        if 0 in indexes:  # grad_W
+            gg = linear(gy.T, ggx.T)
+            ret.append(gg)
+        if 1 in indexes:  # grad_gy
+            gg = linear(ggx, W)
+            ret.append(gg)
+
+        return ret
+
+
+class LinearGradWIdeep(function_node.FunctionNode):
+
+    def forward_cpu(self, inputs):
+        x, gy = inputs
+        gW = intel64.ideep.linear.BackwardWeights(
+            intel64.ideep.array(x),
+            intel64.ideep.array(gy))
+        self.retain_inputs((0, 1))
+        return gW,
+
+    def backward(self, indexes, grad_outputs):
+        inputs = self.get_retained_inputs()
+        x, gy = inputs
+        if len(grad_outputs) == 2:
+            ggW, ggb = grad_outputs
+        else:
+            ggW, = grad_outputs
+            ggb = None
+
+        ret = []
+        if 0 in indexes:  # grad_x
+            gg = linear(gy, ggW.T)
+            ret.append(gg)
+        if 1 in indexes:  # grad_gy
+            gg = linear(x, ggW)
+            if ggb is not None:
+                gg += chainer.functions.broadcast_to(ggb, gg.shape)
+            ret.append(gg)
 
         return ret
 
