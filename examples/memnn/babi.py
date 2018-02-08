446e03ca03327b2fff1a3c4d65e03c4c6d8af61f
Yuya Unno <unnonouno@gmail.com>
2018-02-08 18:46:00 +0900

    Add comments

--- examples/memnn/babi.py
+++ examples/memnn/babi.py
@@ -5,17 +5,44 @@ Query = collections.namedtuple('Query', ['sentence', 'answer', 'fact'])
 Sentence = collections.namedtuple('Sentence', ['sentence'])
 
 
-
-
 def split(sentence):
+    """Splits a sentence into words.
+
+    Args:
+        sentence (str): A sentence to split.
+
+    Returns:
+        list of str: A list of words. Punctuations are removed.
+
+    """
     return sentence.lower().replace('.', '').replace('?', '').split()
 
 
 def convert(vocab, words):
+    """Converts a word list into a word ID list.
+
+    Args:
+        vocab (collections.defaultdict): A dictionary storing word IDs.
+        words (list of str): A list of wards to convert.
+
+    Returns:
+        list of int: A list of word IDs.
+
+    """
     return [vocab[w] for w in words]
 
 
 def parse_line(vocab, line):
+    """Parses each line and make a named tuple.
+
+    Args:
+        vocab (collections.defaultdict): A dictionary storing word IDs.
+        line (str): A line to parse in bAbI dataset.
+
+    Returns:
+        Query or Sentence: Parsed tuple.
+
+    """
     if '\t' in line:
         # question line
         question, answer, fact_id = line.split('\t')
@@ -32,24 +59,30 @@ def parse_line(vocab, line):
         return Sentence(wid)
 
 
-def parse_data(vocab, lines):
+def read_data(vocab, path):
+    """Reads a bAbI dataset.
+
+    Args:
+        vocab (collections.defaultdict): A dictionary storing word IDs.
+        path (str): Path to bAbI data file.
+
+    Returns:
+        list of Query of Sentence: Parsed lines.
+
+    """
     data = []
     all_data = []
-    for line in lines:
-        sid, content = line.strip().split(' ', 1)
-        if sid == '1':
-            if len(data) > 0:
-                all_data.append(data)
-                data = []
-
-        data.append(parse_line(vocab, content))
-
-    if len(data) > 0:
-        all_data.append(data)
-
-    return all_data
-
-
-def read_data(vocab, path):
     with open(path) as f:
-        return parse_data(vocab, f)
+        for line in f:
+            sid, content = line.strip().split(' ', 1)
+            if sid == '1':
+                if len(data) > 0:
+                    all_data.append(data)
+                    data = []
+
+            data.append(parse_line(vocab, content))
+
+        if len(data) > 0:
+            all_data.append(data)
+
+        return all_data
