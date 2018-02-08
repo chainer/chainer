00f536dc1c7711d510e5e88943a39b619ec597f1
niboshi <niboshi000@gmail.com>
2018-02-08 18:07:02 +0900

    Comment on Backend member functions

--- xchainer/backend.h
+++ xchainer/backend.h
@@ -12,8 +12,16 @@ class Backend {
 public:
     virtual ~Backend() = default;
 
+    // Allocates a memory chunk on the specified device.
     virtual std::shared_ptr<void> Allocate(const Device& device, size_t bytesize) = 0;
+
+    // Copies the data between two memory chunks.
     virtual void MemoryCopy(void* dst_ptr, const void* src_ptr, size_t bytesize) = 0;
+
+    // Creates a data buffer filled with the specified data on the specified device.
+    //
+    // It may allocate a new memory or return an alias.
+    // src_ptr is guaranteed to reside in the main RAM.
     virtual std::shared_ptr<void> FromBuffer(const Device& device, const std::shared_ptr<void>& src_ptr, size_t bytesize) = 0;
 
     virtual void Fill(Array& out, Scalar value) = 0;
