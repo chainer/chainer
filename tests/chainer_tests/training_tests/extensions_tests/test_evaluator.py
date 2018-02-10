34f659cbf5e1bd0e88673837cfff734895459d37
Kenta OONO <oono@preferred.jp>
2018-02-10 16:03:00 +0900

    Use assert instead of unittest.TestCase.assertEqual

--- tests/chainer_tests/training_tests/extensions_tests/test_evaluator.py
+++ tests/chainer_tests/training_tests/extensions_tests/test_evaluator.py
@@ -253,7 +253,7 @@ class TestEvaluatorRepeat(unittest.TestCase):
         else:
             expect = 0
 
-        self.assertEqual(len(w), expect)
+        assert len(w) == expect
 
 
 testing.run_module(__name__, __file__)
