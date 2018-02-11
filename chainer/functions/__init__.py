1fc2dc9409b15d7f449482522e30b7a9cd6a8d4e
muupan <muupan@gmail.com>
2018-02-11 13:09:39 +0900

    Stop exposing Erf class

--- chainer/functions/__init__.py
+++ chainer/functions/__init__.py
@@ -185,7 +185,6 @@ from chainer.functions.math.det import batch_det  # NOQA
 from chainer.functions.math.det import BatchDet  # NOQA
 from chainer.functions.math.det import det  # NOQA
 from chainer.functions.math.erf import erf  # NOQA
-from chainer.functions.math.erf import Erf  # NOQA
 from chainer.functions.math.exponential import exp  # NOQA
 from chainer.functions.math.exponential import Exp  # NOQA
 from chainer.functions.math.exponential import log  # NOQA
