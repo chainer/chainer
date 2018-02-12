caed64d58da3fd8e74b34e83d370e36d18659def
Akira Naruse <anaruse@nvidia.com>
2018-02-13 08:37:23 +0900

    skip processings for transpose when it is not necessary

--- chainer/functions/math/tensordot.py
+++ chainer/functions/math/tensordot.py
@@ -8,19 +8,11 @@ from chainer import utils
 from chainer.utils import type_check
 
 
-def _tensordot(a, b, a_axes, b_axes, c_axes):
+def _tensordot(a, b, a_axes, b_axes, c_axes=None):
 
-    a_row_ndim = len(a_axes[0])
     a_col_ndim = len(a_axes[1])
     b_row_ndim = len(b_axes[0])
-    b_col_ndim = len(b_axes[1])
-    c_row_ndim = len(c_axes[0])
-    c_col_ndim = len(c_axes[1])
     if a_col_ndim != b_row_ndim:
-        raise ValueError('axes count mismatch')
-    if a_row_ndim != c_row_ndim:
-        raise ValueError('axes count mismatch')
-    if b_col_ndim != c_col_ndim:
         raise ValueError('axes count mismatch')
     if a.ndim < a_col_ndim or b.ndim < b_row_ndim:
         raise ValueError('dimension of input tensors must be '
@@ -36,19 +28,29 @@ def _tensordot(a, b, a_axes, b_axes, c_axes):
         xp = cuda.get_array_module(a)
         y = xp.tensordot(a, b, axes=(a_axes[1], b_axes[0]))
 
-    trans = [None for i in six.moves.range(y.ndim)]
-    table_a = [1 if i in a_axes[0] else 0 for i in six.moves.range(a.ndim)]
-    table_a = numpy.cumsum(table_a) - 1
-    for i, c_axis in enumerate(c_axes[0]):
-        trans[c_axis] = table_a[a_axes[0][i]]
-    table_b = [1 if i in b_axes[1] else 0 for i in six.moves.range(b.ndim)]
-    table_b = numpy.cumsum(table_b) - 1
-    for i, c_axis in enumerate(c_axes[1]):
-        trans[c_axis] = table_b[b_axes[1][i]] + len(a_axes[0])
-    for i, c_axis in enumerate(trans):
-        if i != c_axis:
-            y = xp.transpose(y, trans)
-            break
+    if c_axes is not None:
+        a_row_ndim = len(a_axes[0])
+        b_col_ndim = len(b_axes[1])
+        c_row_ndim = len(c_axes[0])
+        c_col_ndim = len(c_axes[1])
+        if a_row_ndim != c_row_ndim:
+            raise ValueError('axes count mismatch')
+        if b_col_ndim != c_col_ndim:
+            raise ValueError('axes count mismatch')
+
+        trans = [None for i in six.moves.range(y.ndim)]
+        table_a = [1 if i in a_axes[0] else 0 for i in six.moves.range(a.ndim)]
+        table_a = numpy.cumsum(table_a) - 1
+        for i, c_axis in enumerate(c_axes[0]):
+            trans[c_axis] = table_a[a_axes[0][i]]
+        table_b = [1 if i in b_axes[1] else 0 for i in six.moves.range(b.ndim)]
+        table_b = numpy.cumsum(table_b) - 1
+        for i, c_axis in enumerate(c_axes[1]):
+            trans[c_axis] = table_b[b_axes[1][i]] + len(a_axes[0])
+        for i, c_axis in enumerate(trans):
+            if i != c_axis:
+                y = xp.transpose(y, trans)
+                break
 
     return y
 
@@ -106,6 +108,8 @@ class TensorDot(function_node.FunctionNode):
             self.a_axes = a_axes
             self.b_axes = b_axes
 
+        c = _tensordot(a, b, self.a_axes, self.b_axes, self.c_axes)
+
         if self.c_axes is None:
             c_axes = [[], []]  # 0:row axes, 1:col axes
             c_row_ndim = len(self.a_axes[0])
@@ -114,7 +118,6 @@ class TensorDot(function_node.FunctionNode):
             c_axes[1] = six.moves.range(c_row_ndim, c_row_ndim + c_col_ndim)
             self.c_axes = c_axes
 
-        c = _tensordot(a, b, self.a_axes, self.b_axes, self.c_axes)
         return utils.force_array(c, self.dtype),
 
     def backward(self, indexes, grad_outputs):
