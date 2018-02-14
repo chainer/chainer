661bf3a6972f1c3c508f160df9ea0e22b30bc12b
niboshi <niboshi000@gmail.com>
2018-02-15 03:13:33 +0900

    Update F.linear implementation for iDeep

--- chainer/functions/connection/linear.py
+++ chainer/functions/connection/linear.py
@@ -8,7 +8,7 @@ from chainer.utils import type_check
 
 class LinearFunction(function_node.FunctionNode):
 
-    _use_ideep = False
+    _use_ideep_config = None
 
     def check_type_forward(self, in_types):
         n_in = in_types.size()
@@ -31,10 +31,10 @@ class LinearFunction(function_node.FunctionNode):
             )
 
     def forward(self, inputs):
+        self._use_ideep_config = chainer.config.use_ideep
         if (intel64.should_use_ideep('>=auto')
                 and intel64.inputs_all_ready(inputs)):
             # iDeep implementation
-            self._use_ideep = True
             return self._forward_ideep(inputs)
 
         # Generic implementation
@@ -72,29 +72,15 @@ class LinearFunction(function_node.FunctionNode):
         return y,
 
     def backward(self, indexes, grad_outputs):
-        ret = []
-        gy, = grad_outputs
-
         x, W = self.get_retained_inputs()
-
-        if self._use_ideep:
-            # iDeep implementation
-            if 0 in indexes:  # grad_x
-                gx = LinearGradDIdeep().apply((W, gy))
-                ret.append(gx[0])
-            if 1 in indexes:  # grad_W
-                gW = LinearGradWIdeep().apply((x, gy))
-                ret.append(gW[0])
-            if 2 in indexes:  # grad_b
-                gb = chainer.functions.sum(gy, axis=0)
-                ret.append(gb)
-        else:
-            # Generic implementation
+        gy, = grad_outputs
+        ret = []
+        with chainer.using_config('use_ideep', self._use_ideep_config):
             if 0 in indexes:
                 gx, = LinearGradData().apply((W, gy))
                 ret.append(chainer.functions.cast(gx, x.dtype))
             if 1 in indexes:
-                gW, = LinearGradWeight().apply((x, gy))
+                gW, = LinearGradWeight(W.dtype).apply((x, gy))
                 ret.append(chainer.functions.cast(gW, W.dtype))
             if 2 in indexes:
                 gb = chainer.functions.sum(gy, axis=0)
@@ -103,69 +89,18 @@ class LinearFunction(function_node.FunctionNode):
         return ret
 
 
-class LinearGradDIdeep(function_node.FunctionNode):
-
-    def forward_cpu(self, inputs):
-        W, gy = inputs
-
-        gx = intel64.ideep.linear.BackwardData(
-            intel64.ideep.array(W),
-            intel64.ideep.array(gy))
-
-        self.retain_inputs((0, 1))
-        return gx,
-
-    def backward(self, indexes, grad_outputs):
-        inputs = self.get_retained_inputs()
-        W, gy = inputs
-        ggx, = grad_outputs
-
-        ret = []
-        if 0 in indexes:  # grad_W
-            gg = linear(gy.T, ggx.T)
-            ret.append(gg)
-        if 1 in indexes:  # grad_gy
-            gg = linear(ggx, W)
-            ret.append(gg)
-
-        return ret
-
-
-class LinearGradWIdeep(function_node.FunctionNode):
-
-    def forward_cpu(self, inputs):
-        x, gy = inputs
-        gW = intel64.ideep.linear.BackwardWeights(
-            intel64.ideep.array(x),
-            intel64.ideep.array(gy))
-        self.retain_inputs((0, 1))
-        return gW,
-
-    def backward(self, indexes, grad_outputs):
-        inputs = self.get_retained_inputs()
-        x, gy = inputs
-        if len(grad_outputs) == 2:
-            ggW, ggb = grad_outputs
-        else:
-            ggW, = grad_outputs
-            ggb = None
-
-        ret = []
-        if 0 in indexes:  # grad_x
-            gg = linear(gy, ggW.T)
-            ret.append(gg)
-        if 1 in indexes:  # grad_gy
-            gg = linear(x, ggW)
-            if ggb is not None:
-                gg += chainer.functions.broadcast_to(ggb, gg.shape)
-            ret.append(gg)
-
-        return ret
-
-
 class LinearGradData(function_node.FunctionNode):
 
+    _use_ideep_config = None
+
     def forward(self, inputs):
+        self._use_ideep_config = chainer.config.use_ideep
+        if (intel64.should_use_ideep('>=auto')
+                and intel64.inputs_all_ready(inputs)):
+            # iDeep implementation
+            return self._forward_ideep(inputs)
+
+        # Generic implementation
         self.retain_inputs((0, 1))
         W, gy = inputs
 
@@ -177,24 +112,45 @@ class LinearGradData(function_node.FunctionNode):
         gx = gy.dot(W).astype(gy.dtype, copy=False)
         return gx,
 
+    def _forward_ideep(self, inputs):
+        self.retain_inputs((0, 1))
+        W, gy = inputs
+        gx = intel64.ideep.linear.BackwardData(
+            intel64.ideep.array(W),
+            intel64.ideep.array(gy))
+        return gx,
+
     def backward(self, indexes, grad_outputs):
         W, gy = self.get_retained_inputs()
         ggx, = grad_outputs
 
         ret = []
-
-        if 0 in indexes:
-            gw, = LinearGradWeight().apply((ggx, gy))
-            ret.append(chainer.functions.cast(gw, W.dtype))
-        if 1 in indexes:
-            ggy = linear(ggx, W)
-            ret.append(chainer.functions.cast(ggy, gy.dtype))
+        with chainer.using_config('use_ideep', self._use_ideep_config):
+            if 0 in indexes:
+                gw, = LinearGradWeight(W.dtype).apply((ggx, gy))
+                ret.append(chainer.functions.cast(gw, W.dtype))
+            if 1 in indexes:
+                ggy = linear(ggx, W)
+                ret.append(chainer.functions.cast(ggy, gy.dtype))
         return ret
 
 
 class LinearGradWeight(function_node.FunctionNode):
 
+    _use_ideep_config = None
+
+    def __init__(self, w_dtype):
+        self._w_dtype = w_dtype
+
     def forward(self, inputs):
+        self._use_ideep_config = chainer.config.use_ideep
+        if (intel64.should_use_ideep('>=auto')
+                and self._w_dtype == numpy.float32
+                and intel64.inputs_all_ready(inputs)):
+            # iDeep implementation
+            return self._forward_ideep(inputs)
+
+        # Generic implementation
         self.retain_inputs((0, 1))
         x, gy = inputs
 
@@ -203,7 +159,15 @@ class LinearGradWeight(function_node.FunctionNode):
                 1 in gy.shape):
             gy = numpy.ascontiguousarray(gy)
 
-        gW = gy.T.dot(x).astype(gy.dtype, copy=False)
+        gW = gy.T.dot(x).astype(self._w_dtype, copy=False)
+        return gW,
+
+    def _forward_ideep(self, inputs):
+        self.retain_inputs((0, 1))
+        x, gy = inputs
+        gW = intel64.ideep.linear.BackwardWeights(
+            intel64.ideep.array(x),
+            intel64.ideep.array(gy))
         return gW,
 
     def backward(self, indexes, grad_outputs):
@@ -211,12 +175,13 @@ class LinearGradWeight(function_node.FunctionNode):
         ggW, = grad_outputs
 
         ret = []
-        if 0 in indexes:
-            gx, = LinearGradData().apply((ggW, gy))
-            ret.append(chainer.functions.cast(gx, x.dtype))
-        if 1 in indexes:
-            ggy = linear(x, ggW)
-            ret.append(chainer.functions.cast(ggy, gy.dtype))
+        with chainer.using_config('use_ideep', self._use_ideep_config):
+            if 0 in indexes:
+                gx, = LinearGradData().apply((ggW, gy))
+                ret.append(chainer.functions.cast(gx, x.dtype))
+            if 1 in indexes:
+                ggy = linear(x, ggW)
+                ret.append(chainer.functions.cast(ggy, gy.dtype))
         return ret
 
 
