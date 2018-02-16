759ad71d4e57d73eab03ce059b6c14a81c752d3b
Dr. Kashif Rasul <kashif.rasul@gmail.com>
2018-02-16 16:27:31 +0100

    fixed WS

--- chainer/optimizers/adam.py
+++ chainer/optimizers/adam.py
@@ -169,7 +169,7 @@ class Adam(optimizer.GradientMethod):
     See: `Fixing Weight Decay Regularization in Adam \
           <https://openreview.net/forum?id=rk6qdGgCZ>`_
 
-    A flag ``amsgrad`` to use the AMSGrad variant of Adam from 
+    A flag ``amsgrad`` to use the AMSGrad variant of Adam from
     the paper:
         `On the Convergence of Adam and Beyond \
         <https://openreview.net/forum?id=ryQu7f-RZ>`_
