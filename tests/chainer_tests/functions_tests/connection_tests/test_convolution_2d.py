0de79e12fbebd73015c8aec0317cd6fcc8b6e388
niboshi <niboshi000@gmail.com>
2018-02-19 20:32:49 +0900

    Fix style for nested with

--- tests/chainer_tests/functions_tests/connection_tests/test_convolution_2d.py
+++ tests/chainer_tests/functions_tests/connection_tests/test_convolution_2d.py
@@ -272,37 +272,31 @@ class TestConvolution2DCudnnCall(unittest.TestCase):
         name = 'cupy.cuda.cudnn.convolutionForward'
         name2 = 'chainer.functions.connection.convolution_2d' \
             '.Convolution2DFunction._tensor_core_adjust_algo'
-        with chainer.using_config('use_cudnn', self.use_cudnn):
-            with chainer.using_config('cudnn_deterministic',
-                                      self.cudnn_deterministic):
-                with testing.patch(
-                    name
-                ) as func, testing.patch(
-                    name2
-                ) as tensor_core_adjust_algo:
-                    self.forward()
-                    self.assertEqual(func.called, self.should_call_cudnn)
-                    if not self.can_use_tensor_core:
-                        self.assertEqual(tensor_core_adjust_algo.called, False)
+        with chainer.using_config('use_cudnn', self.use_cudnn), \
+                chainer.using_config('cudnn_deterministic',
+                                     self.cudnn_deterministic), \
+                testing.patch(name) as func, \
+                testing.patch(name2) as tensor_core_adjust_algo:
+            self.forward()
+            self.assertEqual(func.called, self.should_call_cudnn)
+            if not self.can_use_tensor_core:
+                self.assertEqual(tensor_core_adjust_algo.called, False)
 
     def test_call_cudnn_backward(self):
         name = 'cupy.cuda.cudnn.convolutionBackwardData_v3'
         name2 = 'chainer.functions.connection.convolution_2d' \
             '.Convolution2DGradW._tensor_core_adjust_algo'
-        with chainer.using_config('use_cudnn', self.use_cudnn):
-            with chainer.using_config('cudnn_deterministic',
-                                      self.cudnn_deterministic):
-                y = self.forward()
-                y.grad = self.gy
-                with testing.patch(
-                    name
-                ) as func, testing.patch(
-                    name2
-                ) as tensor_core_adjust_algo:
-                    y.backward()
-                    self.assertEqual(func.called, self.should_call_cudnn)
-                    if not self.can_use_tensor_core:
-                        self.assertEqual(tensor_core_adjust_algo.called, False)
+        with chainer.using_config('use_cudnn', self.use_cudnn), \
+                chainer.using_config('cudnn_deterministic',
+                                     self.cudnn_deterministic):
+            y = self.forward()
+            y.grad = self.gy
+            with testing.patch(name) as func, \
+                    testing.patch(name2) as tensor_core_adjust_algo:
+                y.backward()
+                self.assertEqual(func.called, self.should_call_cudnn)
+                if not self.can_use_tensor_core:
+                    self.assertEqual(tensor_core_adjust_algo.called, False)
 
 
 @testing.parameterize(*testing.product({
