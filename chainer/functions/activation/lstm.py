ca359b26d955a192a8415538d2ee85c3da9245b5
Shunta Saito <shunta.saito@gmail.com>
2018-02-19 15:44:05 +0900

    Remove unnecessary part after #4186 is merged

--- chainer/functions/activation/lstm.py
+++ chainer/functions/activation/lstm.py
@@ -109,8 +109,6 @@ class LSTM(function_node.FunctionNode):
         return c_next, h
 
     def backward(self, indexes, grads):
-        if not isinstance(grads, tuple):
-            grads = tuple(grads)
         grad_inputs = (
             self.get_retained_inputs() + self.get_retained_outputs() + grads)
         return LSTMGrad()(*grad_inputs)
