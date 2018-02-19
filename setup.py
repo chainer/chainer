81c4755b816588e356deb6d3f15a156f7951488d
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-02-19 12:59:37 +0900

    fix order of cupy packages

--- setup.py
+++ setup.py
@@ -104,11 +104,11 @@ def find_any_distribution(pkgs):
 # Currently cupy provides source package (cupy) and binary wheel packages
 # (cupy-cudaXX). Chainer can use any one of these packages.
 cupy_pkg = find_any_distribution([
-    'cupy',
-    'cupy-cuda70',
-    'cupy-cuda75',
-    'cupy-cuda80',
     'cupy-cuda90',
+    'cupy-cuda80',
+    'cupy-cuda75',
+    'cupy-cuda70',
+    'cupy',
 ])
 if cupy_pkg is not None:
     req = cupy_requirement(cupy_pkg.project_name)
