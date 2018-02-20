5d2150321eae083c718a72f57b10e3d79150ca3b
Naotoshi Seo <sonots@gmail.com>
2018-02-20 17:41:03 +0900

    Add device_list_test.cc

--- xchainer/device_list_test.cc
+++ xchainer/device_list_test.cc
@@ -2,11 +2,23 @@
 
 #include <gtest/gtest.h>
 #include "xchainer/device.h"
+#include "xchainer/native_backend.h"
+#include "xchainer/native_device.h"
 
 namespace xchainer {
 namespace {
 
-TEST(DeviceList, Add) { DeviceList device_list; }
+TEST(DeviceList, AddGet) {
+    NativeBackend backend;
+    NativeDevice device{&backend, 0};
+    DeviceList device_list;
+
+    auto device = std::make_unique<NativeDevice>(&backend, 0);
+    device_list.AddDevice(device);
+    EXPECT_TRUE(devlice_list.HasDevice(0));
+    EXPECT_FALSE(devlice_list.HasDevice(1));
+    EXPECT_EQ(*device.get(), devlice_list.GetDevice(0));
+}
 
 }  // namespace
 }  // namespace xchainer
