610bf3f2a3b6d2cd4f6238467867a111eb7d9dad
Naotoshi Seo <sonots@gmail.com>
2018-02-20 18:02:45 +0900

    Add native_device_test and cuda_device_test

---
+++ xchainer/native_device_test.cc
@@ -0,0 +1,26 @@
+#include "xchainer/native_device.h"
+
+#include <gtest/gtest.h>
+
+#include "xchainer/native_backend.h"
+
+namespace xchainer {
+namespace {
+
+TEST(NativeDeviceTest, Ctor) {
+    NativeBackend backend;
+
+    {
+        NativeDevice device{backend, 0};
+        EXPECT_EQ(&backend, &deivce.backend());
+        EXPECT_EQ(0, device.index());
+    }
+    {
+        NativeDevice device{backend, 1};
+        EXPECT_EQ(&backend, &deivce.backend());
+        EXPECT_EQ(1, device.index());
+    }
+}
+
+}  // namespace
+}  // namespace xchainer
