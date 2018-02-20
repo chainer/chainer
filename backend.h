6bc532bb65d0153f0d53c74ff70ffef64bffa0a9
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-02-20 12:42:33 +0900

    Pure abstract new backend

---
+++ backend.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+
+namespace xchainer {
+
+class Device;
+
+class Backend {
+public:
+    virtual ~Backend() = default;
+
+    // Returns the number of available devices.
+    //
+    // This count is usually configurable by backend specific ways.
+    virtual int GetDeviceCount() const = 0;
+
+    // Returns the device for the given index.
+    //
+    // Throws out-of-index exception if index >= GetDeviceCount().
+    virtual Device& GetDevice(int index) = 0;
+
+    // Returns the name of this backend. This name should be unique within the context.
+    virtual std::string GetName() const = 0;
+};
+
+}  // namespace xchainer
