6c1b0730681b92779a49b7202a7340f56f57f99c
Masayuki Takagi <kamonama@gmail.com>
2018-02-22 11:42:52 +0900

    Replace DeviceId with Device

--- xchainer/array_device_id_test.cc
+++ xchainer/array_device_id_test.cc
@@ -11,211 +11,213 @@
 #include "xchainer/backend.h"
 #ifdef XCHAINER_ENABLE_CUDA
 #include "xchainer/cuda/cuda_backend.h"
+#include "xchainer/cuda/cuda_device.h"
 #include "xchainer/cuda/cuda_runtime.h"
 #endif  // XCHAINER_ENABLE_CUDA
-#include "xchainer/device_id.h"
+#include "xchainer/device.h"
 #include "xchainer/memory.h"
 #include "xchainer/native_backend.h"
+#include "xchainer/native_device.h"
 
 namespace xchainer {
 namespace {
 
-class ArrayDeviceIdTest : public ::testing::Test {
+class ArrayDeviceTest : public ::testing::Test {
 protected:
     void SetUp() override {
-        orig_ = internal::GetDefaultDeviceIdNoExcept();
-        SetDefaultDeviceId(internal::kNullDeviceId);
+        orig_ = internal::GetDefaultDeviceNoExcept();
+        SetDefaultDevice(nullptr);
     }
 
-    void TearDown() override { SetDefaultDeviceId(orig_); }
+    void TearDown() override { SetDefaultDevice(orig_); }
 
 private:
-    DeviceId orig_;
+    Device* orig_;
 };
 
-// Check that Array data exists on the specified device_id
-void ExpectDataExistsOnDeviceId(const DeviceId& expected_device_id, const Array& array) {
-    // Check device_id member of the Array
-    EXPECT_EQ(expected_device_id, array.device_id());
+// Check that Array data exists on the specified device
+void ExpectDataExistsOnDevice(const Device& expected_device, const Array& array) {
+    // Check device member of the Array
+    EXPECT_EQ(expected_device, array.device());
 
-    // Check device_id of data pointee
-    if (expected_device_id.backend()->GetName() == "native") {
+    // Check device of data pointee
+    if (expected_device.backend().GetName() == "native") {
         EXPECT_FALSE(internal::IsPointerCudaMemory(array.data().get()));
-    } else if (expected_device_id.backend()->GetName() == "cuda") {
+    } else if (expected_device.backend().GetName() == "cuda") {
         EXPECT_TRUE(internal::IsPointerCudaMemory(array.data().get()));
     } else {
-        FAIL() << "invalid device_id";
+        FAIL() << "invalid device";
     }
 }
 
-// Check that Arrays are created on the default device_id if no other device_ids are specified
-void CheckDeviceIdFallback(const std::function<Array()>& create_array_func) {
-    // Fallback to default device_id which is CPU
+// Check that Arrays are created on the default device if no other devices are specified
+void CheckDeviceFallback(const std::function<Array()>& create_array_func) {
+    // Fallback to default device which is CPU
     {
         NativeBackend native_backend;
-        DeviceId cpu_device_id{&native_backend};
-        auto scope = std::make_unique<DeviceScope>(cpu_device_id);
+        NativeDevice cpu_device{native_backend, 0};
+        auto scope = std::make_unique<DeviceScope>(cpu_device);
         Array array = create_array_func();
-        ExpectDataExistsOnDeviceId(cpu_device_id, array);
+        ExpectDataExistsOnDevice(cpu_device, array);
     }
 #ifdef XCHAINER_ENABLE_CUDA
-    // Fallback to default device_id which is GPU
+    // Fallback to default device which is GPU
     {
         cuda::CudaBackend cuda_backend;
-        DeviceId cuda_device_id{&cuda_backend};
-        auto scope = std::make_unique<DeviceScope>(cuda_device_id);
+        cuda::CudaDevice cuda_device{cuda_backend, 0};
+        auto scope = std::make_unique<DeviceScope>(cuda_device);
         Array array = create_array_func();
-        ExpectDataExistsOnDeviceId(cuda_device_id, array);
+        ExpectDataExistsOnDevice(cuda_device, array);
     }
 #endif
 }
 
-// Check that Arrays are created on the specified device_id, if specified, without taking into account the default device_id
-void CheckDeviceIdExplicit(const std::function<Array(const DeviceId& device_id)>& create_array_func) {
+// Check that Arrays are created on the specified device, if specified, without taking into account the default device
+void CheckDeviceExplicit(const std::function<Array(Device& device)>& create_array_func) {
     NativeBackend native_backend;
-    DeviceId cpu_device_id{&native_backend};
+    NativeDevice cpu_device{native_backend, 0};
 
     // Explicitly create on CPU
     {
-        Array array = create_array_func(cpu_device_id);
-        ExpectDataExistsOnDeviceId(cpu_device_id, array);
+        Array array = create_array_func(cpu_device);
+        ExpectDataExistsOnDevice(cpu_device, array);
     }
     {
-        auto scope = std::make_unique<DeviceScope>(cpu_device_id);
-        Array array = create_array_func(cpu_device_id);
-        ExpectDataExistsOnDeviceId(cpu_device_id, array);
+        auto scope = std::make_unique<DeviceScope>(cpu_device);
+        Array array = create_array_func(cpu_device);
+        ExpectDataExistsOnDevice(cpu_device, array);
     }
 #ifdef XCHAINER_ENABLE_CUDA
     cuda::CudaBackend cuda_backend;
-    DeviceId cuda_device_id{&cuda_backend};
+    cuda::CudaDevice cuda_device{cuda_backend, 0};
 
     {
-        auto scope = std::make_unique<DeviceScope>(cuda_device_id);
-        Array array = create_array_func(cpu_device_id);
-        ExpectDataExistsOnDeviceId(cpu_device_id, array);
+        auto scope = std::make_unique<DeviceScope>(cuda_device);
+        Array array = create_array_func(cpu_device);
+        ExpectDataExistsOnDevice(cpu_device, array);
     }
     // Explicitly create on GPU
     {
-        Array array = create_array_func(cuda_device_id);
-        ExpectDataExistsOnDeviceId(cuda_device_id, array);
+        Array array = create_array_func(cuda_device);
+        ExpectDataExistsOnDevice(cuda_device, array);
     }
     {
-        auto scope = std::make_unique<DeviceScope>(cpu_device_id);
-        Array array = create_array_func(cuda_device_id);
-        ExpectDataExistsOnDeviceId(cuda_device_id, array);
+        auto scope = std::make_unique<DeviceScope>(cpu_device);
+        Array array = create_array_func(cuda_device);
+        ExpectDataExistsOnDevice(cuda_device, array);
     }
     {
-        auto scope = std::make_unique<DeviceScope>(cuda_device_id);
-        Array array = create_array_func(cuda_device_id);
-        ExpectDataExistsOnDeviceId(cuda_device_id, array);
+        auto scope = std::make_unique<DeviceScope>(cuda_device);
+        Array array = create_array_func(cuda_device);
+        ExpectDataExistsOnDevice(cuda_device, array);
     }
 #endif  // XCHAINER_ENABLE_CUDA
 }
 
-TEST_F(ArrayDeviceIdTest, FromBuffer) {
+TEST_F(ArrayDeviceTest, FromBuffer) {
     Shape shape({2, 3});
     Dtype dtype = Dtype::kFloat32;
     float raw_data[] = {0.f, 1.f, 2.f, 3.f, 4.f, 5.f};
     std::shared_ptr<void> data(raw_data, [](float* ptr) {
         (void)ptr;  // unused
     });
-    CheckDeviceIdFallback([&]() { return Array::FromBuffer(shape, dtype, data); });
-    CheckDeviceIdExplicit([&](const DeviceId& device_id) { return Array::FromBuffer(shape, dtype, data, device_id); });
+    CheckDeviceFallback([&]() { return Array::FromBuffer(shape, dtype, data); });
+    CheckDeviceExplicit([&](Device& device) { return Array::FromBuffer(shape, dtype, data, device); });
 }
 
-TEST_F(ArrayDeviceIdTest, Empty) {
+TEST_F(ArrayDeviceTest, Empty) {
     Shape shape({2, 3});
     Dtype dtype = Dtype::kFloat32;
-    CheckDeviceIdFallback([&]() { return Array::Empty(shape, dtype); });
-    CheckDeviceIdExplicit([&](const DeviceId& device_id) { return Array::Empty(shape, dtype, device_id); });
+    CheckDeviceFallback([&]() { return Array::Empty(shape, dtype); });
+    CheckDeviceExplicit([&](Device& device) { return Array::Empty(shape, dtype, device); });
 }
 
-TEST_F(ArrayDeviceIdTest, Full) {
+TEST_F(ArrayDeviceTest, Full) {
     Shape shape({2, 3});
     Scalar scalar{2.f};
     Dtype dtype = Dtype::kFloat32;
-    CheckDeviceIdFallback([&]() { return Array::Full(shape, scalar, dtype); });
-    CheckDeviceIdFallback([&]() { return Array::Full(shape, scalar); });
-    CheckDeviceIdExplicit([&](const DeviceId& device_id) { return Array::Full(shape, scalar, dtype, device_id); });
-    CheckDeviceIdExplicit([&](const DeviceId& device_id) { return Array::Full(shape, scalar, device_id); });
+    CheckDeviceFallback([&]() { return Array::Full(shape, scalar, dtype); });
+    CheckDeviceFallback([&]() { return Array::Full(shape, scalar); });
+    CheckDeviceExplicit([&](Device& device) { return Array::Full(shape, scalar, dtype, device); });
+    CheckDeviceExplicit([&](Device& device) { return Array::Full(shape, scalar, device); });
 }
 
-TEST_F(ArrayDeviceIdTest, Zeros) {
+TEST_F(ArrayDeviceTest, Zeros) {
     Shape shape({2, 3});
     Dtype dtype = Dtype::kFloat32;
-    CheckDeviceIdFallback([&]() { return Array::Zeros(shape, dtype); });
-    CheckDeviceIdExplicit([&](const DeviceId& device_id) { return Array::Zeros(shape, dtype, device_id); });
+    CheckDeviceFallback([&]() { return Array::Zeros(shape, dtype); });
+    CheckDeviceExplicit([&](Device& device) { return Array::Zeros(shape, dtype, device); });
 }
 
-TEST_F(ArrayDeviceIdTest, Ones) {
+TEST_F(ArrayDeviceTest, Ones) {
     Shape shape({2, 3});
     Dtype dtype = Dtype::kFloat32;
-    CheckDeviceIdFallback([&]() { return Array::Ones(shape, dtype); });
-    CheckDeviceIdExplicit([&](const DeviceId& device_id) { return Array::Ones(shape, dtype, device_id); });
+    CheckDeviceFallback([&]() { return Array::Ones(shape, dtype); });
+    CheckDeviceExplicit([&](Device& device) { return Array::Ones(shape, dtype, device); });
 }
 
-TEST_F(ArrayDeviceIdTest, EmptyLike) {
+TEST_F(ArrayDeviceTest, EmptyLike) {
     Shape shape({2, 3});
     Dtype dtype = Dtype::kFloat32;
 
-    CheckDeviceIdFallback([&]() {
+    CheckDeviceFallback([&]() {
         Array array_orig = Array::Empty(shape, dtype);
         return Array::EmptyLike(array_orig);
     });
-    CheckDeviceIdExplicit([&](const DeviceId& device_id) {
+    CheckDeviceExplicit([&](Device& device) {
         NativeBackend native_backend;
-        DeviceId cpu_device_id{&native_backend};
-        Array array_orig = Array::Empty(shape, dtype, cpu_device_id);
-        return Array::EmptyLike(array_orig, device_id);
+        NativeDevice cpu_device{native_backend, 0};
+        Array array_orig = Array::Empty(shape, dtype, cpu_device);
+        return Array::EmptyLike(array_orig, device);
     });
 }
 
-TEST_F(ArrayDeviceIdTest, FullLike) {
+TEST_F(ArrayDeviceTest, FullLike) {
     Shape shape({2, 3});
     Scalar scalar{2.f};
     Dtype dtype = Dtype::kFloat32;
 
-    CheckDeviceIdFallback([&]() {
+    CheckDeviceFallback([&]() {
         Array array_orig = Array::Empty(shape, dtype);
         return Array::FullLike(array_orig, scalar);
     });
-    CheckDeviceIdExplicit([&](const DeviceId& device_id) {
+    CheckDeviceExplicit([&](Device& device) {
         NativeBackend native_backend;
-        DeviceId cpu_device_id{&native_backend};
-        Array array_orig = Array::Empty(shape, dtype, cpu_device_id);
-        return Array::FullLike(array_orig, scalar, device_id);
+        NativeDevice cpu_device{native_backend, 0};
+        Array array_orig = Array::Empty(shape, dtype, cpu_device);
+        return Array::FullLike(array_orig, scalar, device);
     });
 }
 
-TEST_F(ArrayDeviceIdTest, ZerosLike) {
+TEST_F(ArrayDeviceTest, ZerosLike) {
     Shape shape({2, 3});
     Dtype dtype = Dtype::kFloat32;
 
-    CheckDeviceIdFallback([&]() {
+    CheckDeviceFallback([&]() {
         Array array_orig = Array::Empty(shape, dtype);
         return Array::ZerosLike(array_orig);
     });
-    CheckDeviceIdExplicit([&](const DeviceId& device_id) {
+    CheckDeviceExplicit([&](Device& device) {
         NativeBackend native_backend;
-        DeviceId cpu_device_id{&native_backend};
-        Array array_orig = Array::Empty(shape, dtype, cpu_device_id);
-        return Array::ZerosLike(array_orig, device_id);
+        NativeDevice cpu_device{native_backend, 0};
+        Array array_orig = Array::Empty(shape, dtype, cpu_device);
+        return Array::ZerosLike(array_orig, device);
     });
 }
 
-TEST_F(ArrayDeviceIdTest, OnesLike) {
+TEST_F(ArrayDeviceTest, OnesLike) {
     Shape shape({2, 3});
     Dtype dtype = Dtype::kFloat32;
 
-    CheckDeviceIdFallback([&]() {
+    CheckDeviceFallback([&]() {
         Array array_orig = Array::Empty(shape, dtype);
         return Array::OnesLike(array_orig);
     });
-    CheckDeviceIdExplicit([&](const DeviceId& device_id) {
+    CheckDeviceExplicit([&](Device& device) {
         NativeBackend native_backend;
-        DeviceId cpu_device_id{&native_backend};
-        Array array_orig = Array::Empty(shape, dtype, cpu_device_id);
-        return Array::OnesLike(array_orig, device_id);
+        NativeDevice cpu_device{native_backend, 0};
+        Array array_orig = Array::Empty(shape, dtype, cpu_device);
+        return Array::OnesLike(array_orig, device);
     });
 }
 
