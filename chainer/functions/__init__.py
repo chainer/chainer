59b3841bc3b5039cf4a753db1c0fcca83310a491
Akira Naruse <anaruse@nvidia.com>
2018-02-22 22:48:03 +0900

    Support Sparse MatMul

--- chainer/functions/__init__.py
+++ chainer/functions/__init__.py
@@ -235,6 +235,9 @@ from chainer.functions.math.prod import prod  # NOQA
 from chainer.functions.math.prod import Prod  # NOQA
 from chainer.functions.math.scale import scale  # NOQA
 from chainer.functions.math.sign import sign  # NOQA
+from chainer.functions.math.sparse_matmul import sparse_coo_matrix  # NOQA
+from chainer.functions.math.sparse_matmul import sparse_dense2coo  # NOQA
+from chainer.functions.math.sparse_matmul import sparse_matmul  # NOQA
 from chainer.functions.math.sqrt import rsqrt  # NOQA
 from chainer.functions.math.sqrt import sqrt  # NOQA
 from chainer.functions.math.sqrt import Sqrt  # NOQA
