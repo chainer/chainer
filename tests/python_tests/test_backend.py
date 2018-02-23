53cf78b4a1da169e23543a4ff8e373b57befa1d9
Naotoshi Seo <sonots@gmail.com>
2018-02-23 17:58:35 +0900

    autopep8

--- tests/python_tests/test_backend.py
+++ tests/python_tests/test_backend.py
@@ -4,6 +4,7 @@ import xchainer
 def test_name():
     assert 'native' == xchainer.get_global_default_context().get_backend('native').name
 
+
 def test_get_device():
     device = xchainer.get_global_default_context().get_backend('native').get_device(0)
     assert 0 == device.index
