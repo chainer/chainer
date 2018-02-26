87c85adb6e228953251f43f1dbff42e980ee718f
vilyaair <vilyaair@hotmail.com>
2018-02-26 04:23:02 -0800

    Add swish function and link to the corresponding `__init__.py`

--- chainer/links/activation/__init__.py
+++ chainer/links/activation/__init__.py
@@ -1,3 +1,4 @@
 from chainer.links.activation import maxout  # NOQA
 from chainer.links.activation import prelu  # NOQA
 from chainer.links.activation import simplified_dropconnect  # NOQA
+from chainer.links.activation import swish  # NOQA
