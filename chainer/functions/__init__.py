b54c4a30033c14be101d3c8be93918c5cf43252b
vilyaair <vilyaair@hotmail.com>
2018-02-26 04:37:13 -0800

    Make import style more consistent

--- chainer/functions/__init__.py
+++ chainer/functions/__init__.py
@@ -324,4 +324,4 @@ from chainer.functions.util.forget import forget  # NOQA
 from chainer.functions.util.forget import Forget  # NOQA
 
 # Aliases
-mean = average
+from chainer.functions.math.average import average as mean  # NOQA
