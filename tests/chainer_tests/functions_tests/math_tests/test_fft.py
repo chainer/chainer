37f82b58a9f55fe3061b2f10b1c9a07931c5071a
Yuya Unno <unnonouno@gmail.com>
2018-02-26 11:04:30 +0900

    Add test for double backprop

--- tests/chainer_tests/functions_tests/math_tests/test_fft.py
+++ tests/chainer_tests/functions_tests/math_tests/test_fft.py
@@ -21,6 +21,8 @@ class TestFFT(unittest.TestCase):
         self.ix = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
         self.rg = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
         self.ig = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        self.grx = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        self.gix = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
 
     def check_forward(self, rx_data, ix_data):
         ry, iy = getattr(chainer.functions, self.method)((rx_data, ix_data))
@@ -54,6 +56,25 @@ class TestFFT(unittest.TestCase):
         self.check_backward(
             cuda.to_gpu(self.rx), cuda.to_gpu(self.ix),
             cuda.to_gpu(self.rg), cuda.to_gpu(self.ig))
+
+    def check_double_backward(self, rx, ix, rg, ig, grx, gix):
+        def f(rx, ix):
+            ry, iy = chainer.functions.fft((rx, ix))
+            return ry * ry, iy * iy
+
+        gradient_check.check_double_backward(
+            f, (rx, ix), (rg, ig), (grx, gix), dtype='d', atol=1e-2, rtol=1e-3)
+
+    def test_double_backward_cpu(self):
+        self.check_double_backward(
+            self.rx, self.ix, self.rg, self.ig, self.grx, self.gix)
+
+    @attr.gpu
+    def test_double_backward_gpu(self):
+        self.check_double_backward(
+            cuda.to_gpu(self.rx), cuda.to_gpu(self.ix),
+            cuda.to_gpu(self.rg), cuda.to_gpu(self.ig),
+            cuda.to_gpu(self.grx), cuda.to_gpu(self.gix))
 
 
 testing.run_module(__name__, __file__)
