071625e4d089550349753fb1b8e6c7d7c0a938ee
Naotoshi Seo <sonots@gmail.com>
2018-02-27 16:29:49 +0900

    cleanup test_backend.py

--- tests/python_tests/test_backend.py
+++ tests/python_tests/test_backend.py
@@ -2,10 +2,13 @@ import xchainer
 
 
 def test_name():
-    assert 'native' == xchainer.get_global_default_context().get_backend('native').name
+    backend = xchainer.get_global_default_context().get_backend('native')
+    assert 'native' == backend.name
 
 
 def test_get_device():
-    device = xchainer.get_global_default_context().get_backend('native').get_device(0)
+    backend = xchainer.get_global_default_context().get_backend('native')
+    device = backend.get_device(0)
     assert 0 == device.index
     assert 'native:0' == device.name
+    assert device is backend.get_device(0)
