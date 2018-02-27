16dba681598556ca9f1d46c5f624c6d789addb9b
Naotoshi Seo <sonots@gmail.com>
2018-02-27 17:43:08 +0900

    Fix to accept creating zero byte sized array

--- xchainer/cuda/cuda_device.cu
+++ xchainer/cuda/cuda_device.cu
@@ -37,6 +37,9 @@ __global__ void MulKernel(const T* ldata, const T* rdata, T* odata, int64_t tota
 }  // namespace
 
 std::shared_ptr<void> CudaDevice::Allocate(size_t bytesize) {
+    if (bytesize == 0) {
+        return std::make_unique<uint8_t[]>(0);
+    }
     CheckError(cudaSetDevice(index()));
     void* raw_ptr = nullptr;
     // Be careful to be exception-safe, i.e.,
