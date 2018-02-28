2c874e34b766cb79f6cbac5ec888a10bdf97f69e
niboshi <niboshi000@gmail.com>
2018-03-01 05:30:23 +0900

    Remove unused overload

--- xchainer/testing/array.h
+++ xchainer/testing/array.h
@@ -6,30 +6,12 @@
 #include <memory>
 #include <utility>
 
-#include <gsl/gsl>
-
 #include "xchainer/array.h"
 #include "xchainer/dtype.h"
 #include "xchainer/shape.h"
 
 namespace xchainer {
 namespace testing {
-
-template <typename T, size_t N>
-Array MakeArray(const Shape& shape, const T (&data)[N]) {
-    assert(static_cast<size_t>(shape.GetTotalSize()) == N);
-    auto a = std::make_unique<T[]>(N);
-    std::copy(std::begin(data), std::end(data), a.get());
-    return Array::FromBuffer(shape, TypeToDtype<T>, std::move(a));
-}
-
-template <typename T>
-Array MakeArray(const Shape& shape, gsl::span<const T> data) {
-    assert(static_cast<size_t>(shape.GetTotalSize()) == data.size());
-    auto a = std::make_unique<T[]>(data.size());
-    std::copy(data.begin(), data.end(), a.get());
-    return Array::FromBuffer(shape, TypeToDtype<T>, std::move(a));
-}
 
 template <typename T, size_t N>
 Array MakeArray(const Shape& shape, const std::array<T, N>& data) {
