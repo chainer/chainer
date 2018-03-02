e2795de3c213202b96fe5472dbdfe2d7971288f6
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-03-02 15:54:54 +0900

    eliminate division

--- chainer/functions/math/sqrt.py
+++ chainer/functions/math/sqrt.py
@@ -56,7 +56,7 @@ class Rsqrt(function_node.FunctionNode):
     def backward(self, indexes, grad_outputs):
         x, = self.get_retained_inputs()
         gy, = grad_outputs
-        return - gy * (x ** -1.5) / 2.0,
+        return gy * (x ** -1.5) * -0.5,
 
 
 def sqrt(x):
