363c9a98e3ff154bbefc7e39b4b5b0f02fb7d2ce
Masayuki Takagi <kamonama@gmail.com>
2018-03-05 16:30:55 +0900

    Fix test data to realistic values

--- xchainer/strides_test.cc
+++ xchainer/strides_test.cc
@@ -11,16 +11,16 @@ void CheckSpanEqual(std::initializer_list<int64_t> expect, gsl::span<const int64
 
 TEST(StridesTest, Ctor) {
     {
-        const Strides strides = {2, 3, 4};
+        const Strides strides = {48, 16, 4};
         EXPECT_EQ(3, strides.ndim());
         EXPECT_EQ(size_t{3}, strides.size());
-        CheckSpanEqual({2, 3, 4}, strides.span());
+        CheckSpanEqual({48, 16, 4}, strides.span());
     }
     {
-        const std::array<int64_t, 3> dims = {2, 3, 4};
+        const std::array<int64_t, 3> dims = {48, 16, 4};
         const Strides strides(gsl::make_span(dims));
         EXPECT_EQ(3, strides.ndim());
-        CheckSpanEqual({2, 3, 4}, strides.span());
+        CheckSpanEqual({48, 16, 4}, strides.span());
     }
     {
         const std::array<int64_t, kMaxNdim + 1> too_long = {1};
@@ -29,9 +29,9 @@ TEST(StridesTest, Ctor) {
 }
 
 TEST(StridesTest, Subscript) {
-    const Strides strides = {2, 3, 4};
-    EXPECT_EQ(2, strides[0]);
-    EXPECT_EQ(3, strides[1]);
+    const Strides strides = {48, 16, 4};
+    EXPECT_EQ(48, strides[0]);
+    EXPECT_EQ(16, strides[1]);
     EXPECT_EQ(4, strides[2]);
     EXPECT_THROW(strides[-1], DimensionError);
     EXPECT_THROW(strides[3], DimensionError);
@@ -39,39 +39,39 @@ TEST(StridesTest, Subscript) {
 
 TEST(StridesTest, Compare) {
     {
-        const Strides strides = {2, 3, 4};
-        const Strides strides2 = {2, 3, 4};
+        const Strides strides = {48, 16, 4};
+        const Strides strides2 = {48, 16, 4};
         EXPECT_TRUE(strides == strides2);
     }
     {
-        const Strides strides = {2, 3, 4};
-        const Strides strides2 = {2, 3};
+        const Strides strides = {48, 16, 4};
+        const Strides strides2 = {48, 16};
         EXPECT_TRUE(strides != strides2);
     }
     {
-        const Strides strides = {2, 3, 4};
-        const Strides strides2 = {1, 2, 3};
+        const Strides strides = {48, 16, 4};
+        const Strides strides2 = {4, 8, 24};
         EXPECT_TRUE(strides != strides2);
     }
 }
 
 TEST(StridesTest, CheckEqual) {
     {
-        const Strides strides = {2, 3, 4};
-        const Strides strides2 = {2, 3, 4};
+        const Strides strides = {48, 16, 4};
+        const Strides strides2 = {48, 16, 4};
         EXPECT_NO_THROW(CheckEqual(strides, strides2));
     }
     {
-        const Strides strides = {2, 3, 4};
+        const Strides strides = {48, 16, 4};
         const Strides strides2 = {};
         EXPECT_THROW(CheckEqual(strides, strides2), DimensionError);
     }
 }
 
 TEST(StridesTest, Iterator) {
-    const Strides strides = {2, 3, 4};
-    CheckSpanEqual({2, 3, 4}, gsl::make_span(strides.begin(), strides.end()));
-    CheckSpanEqual({4, 3, 2}, gsl::make_span(std::vector<int64_t>{strides.rbegin(), strides.rend()}));
+    const Strides strides = {48, 16, 4};
+    CheckSpanEqual({48, 16, 4}, gsl::make_span(strides.begin(), strides.end()));
+    CheckSpanEqual({4, 16, 48}, gsl::make_span(std::vector<int64_t>{strides.rbegin(), strides.rend()}));
 }
 
 TEST(StridesTest, ToString) {
@@ -80,12 +80,12 @@ TEST(StridesTest, ToString) {
         EXPECT_EQ(strides.ToString(), "()");
     }
     {
-        const Strides strides = {1};
-        EXPECT_EQ(strides.ToString(), "(1,)");
+        const Strides strides = {4};
+        EXPECT_EQ(strides.ToString(), "(4,)");
     }
     {
-        const Strides strides = {2, 3, 4};
-        EXPECT_EQ(strides.ToString(), "(2, 3, 4)");
+        const Strides strides = {48, 16, 4};
+        EXPECT_EQ(strides.ToString(), "(48, 16, 4)");
     }
 }
 
