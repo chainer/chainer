85ed423121743308115b8947c07dc69fd50a460b
Naotoshi Seo <sonots@gmail.com>
2018-03-06 17:52:08 +0900

    pytest: cache cuda_limit not to call get_device_count many times

--- conftest.py
+++ conftest.py
@@ -29,13 +29,22 @@ def _setup_cuda_marker(item):
     cuda_marker = item.get_marker('cuda')
     if cuda_marker is not None:
         required_num = cuda_marker.args[0] if cuda_marker.args else 1
-        if _cuda_limit() < required_num:
+        if _get_cuda_limit() < required_num:
             pytest.skip('{} NVIDIA GPUs required'.format(required_num))
 
 
-def _cuda_limit():
+_cuda_limit = None
+
+def _get_cuda_limit():
+    global _cuda_limit
+    if _cuda_limit is not None:
+        return _cuda_limit
     if os.getenv('XCHAINER_TEST_CUDA_LIMIT') is None:
-        backend = xchainer.get_global_default_context().get_backend('cuda')
-        return backend.get_device_count()
+        try:
+            backend = xchainer.get_global_default_context().get_backend('cuda')
+            _cuda_limit = backend.get_device_count()
+        except xchainer.BackendError:
+            _cuda_limit = 0
     else:
-        return int(os.getenv('XCHAINER_TEST_CUDA_LIMIT'))
+        _cuda_limit = int(os.getenv('XCHAINER_TEST_CUDA_LIMIT'))
+    return _cuda_limit
