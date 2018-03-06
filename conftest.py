2e9b11d3dc6dcb8301870f9219878d18e5fafa71
Naotoshi Seo <sonots@gmail.com>
2018-03-06 12:19:36 +0900

    Use XCHAINER_TEST_CUDA_LIMIT env int pytest

--- conftest.py
+++ conftest.py
@@ -1,9 +1,5 @@
 import pytest
-
-
-def pytest_addoption(parser):
-    _add_cuda_option(parser)
-
+import os
 
 def pytest_configure(config):
     _register_cuda_marker(config)
@@ -11,11 +7,6 @@ def pytest_configure(config):
 
 def pytest_runtest_setup(item):
     _setup_cuda_marker(item)
-
-
-def _add_cuda_option(parser):
-    parser.addoption("--cuda", action="store", metavar="LIMIT_NUM", default=-1,
-                     help="only run tests that require NVIDIA GPUs less than or equal to the limit. Default: test all")
 
 
 def _register_cuda_marker(config):
@@ -29,14 +20,14 @@ def _setup_cuda_marker(item):
     declare that one NVIDIA GPU is required to run.
 
     Tests can also be annotated as ``@pytest.mark.cuda(2)`` to declare number of
-    NVIDIA GPUs required to run. When running tests, if ``--cuda LIMIT_NUM``
-    pytest custom option is set to value greater than or equals to 0, test cases
+    NVIDIA GPUs required to run. When running tests, if ``XCHAINER_TEST_CUDA_LIMIT``
+    environment variable is set to value greater than or equals to 0, test cases
     that require GPUs more than the limit will be skipped.
     """
 
     cuda_marker = item.get_marker('cuda')
     if cuda_marker is not None:
         required_num = cuda_marker.args[0] if cuda_marker.args else 1
-        minimum_num = int(item.config.getoption('--cuda'))
+        minimum_num = int(os.getenv('XCHAINER_TEST_CUDA_LIMIT', '-1'))
         if 0 <= minimum_num and minimum_num < required_num:
             pytest.skip('{} NVIDIA GPUs required'.format(required_num))
