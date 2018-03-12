b28ec95d5bc02fe0d6b2d627c62aa2cca2f9d6db
vilyaair <vilyaair@hotmail.com>
2018-03-12 07:43:49 -0700

    Add negligent modules in the `__init__.py`

--- chainer/functions/connection/__init__.py
+++ chainer/functions/connection/__init__.py
@@ -7,6 +7,7 @@ from chainer.functions.connection import depthwise_convolution_2d  # NOQA
 from chainer.functions.connection import dilated_convolution_2d  # NOQA
 from chainer.functions.connection import embed_id  # NOQA
 from chainer.functions.connection import linear  # NOQA
+from chainer.functions.connection import local_convolution_2d  # NOQA
 from chainer.functions.connection import n_step_gru  # NOQA
 from chainer.functions.connection import n_step_lstm  # NOQA
 from chainer.functions.connection import n_step_rnn  # NOQA
