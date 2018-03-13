47c7d9ae602f76eb9fe00841a484e7abc990e9e8
sonots <sonots@gmail.com>
2018-03-14 01:03:19 +0900

    scalar_test.cc: Using deprecated casting style.  Use static_cast<bool>(...) instead  [readability/casting] [4]

--- xchainer/scalar_test.cc
+++ xchainer/scalar_test.cc
@@ -61,76 +61,76 @@ TEST(ScalarTest, Inequality) {
 }
 
 TEST(ScalarTest, Cast) {
-    EXPECT_TRUE(bool(Scalar(true)));
-    EXPECT_TRUE(bool(Scalar(1)));
-    EXPECT_TRUE(bool(Scalar(-3.2)));
+    EXPECT_TRUE(static_cast<bool>(Scalar(true)));
+    EXPECT_TRUE(static_cast<bool>(Scalar(1)));
+    EXPECT_TRUE(static_cast<bool>(Scalar(-3.2)));
 
-    EXPECT_FALSE(bool(Scalar(false)));
-    EXPECT_FALSE(bool(Scalar(0)));
-    EXPECT_FALSE(bool(Scalar(0.0f)));
+    EXPECT_FALSE(static_cast<bool>(Scalar(false)));
+    EXPECT_FALSE(static_cast<bool>(Scalar(0)));
+    EXPECT_FALSE(static_cast<bool>(Scalar(0.0f)));
 
-    EXPECT_EQ(int8_t(Scalar(1)), 1);
-    EXPECT_EQ(int8_t(Scalar(-1.1f)), -1);
-    EXPECT_EQ(int8_t(Scalar(1.1)), 1);
+    EXPECT_EQ(static_cast<int8_t>(Scalar(1)), 1);
+    EXPECT_EQ(static_cast<int8_t>(Scalar(-1.1f)), -1);
+    EXPECT_EQ(static_cast<int8_t>(Scalar(1.1)), 1);
 
-    EXPECT_EQ(int16_t(Scalar(-2)), -2);
-    EXPECT_EQ(int16_t(Scalar(2.2f)), 2);
-    EXPECT_EQ(int16_t(Scalar(2.2)), 2);
+    EXPECT_EQ(static_cast<int16_t>(Scalar(-2)), -2);
+    EXPECT_EQ(static_cast<int16_t>(Scalar(2.2f)), 2);
+    EXPECT_EQ(static_cast<int16_t>(Scalar(2.2)), 2);
 
-    EXPECT_EQ(int32_t(Scalar(3)), 3);
-    EXPECT_EQ(int32_t(Scalar(3.3f)), 3);
-    EXPECT_EQ(int32_t(Scalar(-3.3)), -3);
+    EXPECT_EQ(static_cast<int32_t>(Scalar(3)), 3);
+    EXPECT_EQ(static_cast<int32_t>(Scalar(3.3f)), 3);
+    EXPECT_EQ(static_cast<int32_t>(Scalar(-3.3)), -3);
 
-    EXPECT_EQ(int64_t(Scalar(4)), 4);
-    EXPECT_EQ(int64_t(Scalar(4.4f)), 4);
-    EXPECT_EQ(int64_t(Scalar(-4.4)), -4);
+    EXPECT_EQ(static_cast<int64_t>(Scalar(4)), 4);
+    EXPECT_EQ(static_cast<int64_t>(Scalar(4.4f)), 4);
+    EXPECT_EQ(static_cast<int64_t>(Scalar(-4.4)), -4);
 
-    EXPECT_EQ(uint8_t(Scalar(5)), 5);
-    EXPECT_EQ(uint8_t(Scalar(5.5f)), 5);
-    EXPECT_EQ(uint8_t(Scalar(5.0)), 5);
+    EXPECT_EQ(static_cast<uint8_t>(Scalar(5)), 5);
+    EXPECT_EQ(static_cast<uint8_t>(Scalar(5.5f)), 5);
+    EXPECT_EQ(static_cast<uint8_t>(Scalar(5.0)), 5);
 
-    EXPECT_FLOAT_EQ(float(Scalar(-6)), -6.0f);
-    EXPECT_FLOAT_EQ(float(Scalar(6.7f)), 6.7f);
-    EXPECT_FLOAT_EQ(float(Scalar(6.7)), 6.7f);
+    EXPECT_FLOAT_EQ(static_cast<float>(Scalar(-6)), -6.0f);
+    EXPECT_FLOAT_EQ(static_cast<float>(Scalar(6.7f)), 6.7f);
+    EXPECT_FLOAT_EQ(static_cast<float>(Scalar(6.7)), 6.7f);
 
-    EXPECT_DOUBLE_EQ(double(Scalar(8)), 8.0);
-    EXPECT_DOUBLE_EQ(double(Scalar(-8.9f)), double(-8.9f));
-    EXPECT_DOUBLE_EQ(double(Scalar(8.9)), 8.9);
+    EXPECT_DOUBLE_EQ(static_cast<double>(Scalar(8)), 8.0);
+    EXPECT_DOUBLE_EQ(static_cast<double>(Scalar(-8.9f)), double{-8.9f});
+    EXPECT_DOUBLE_EQ(static_cast<double>(Scalar(8.9)), 8.9);
 }
 
 TEST(DtypeTest, UnaryOps) {
     EXPECT_THROW(-Scalar(true), DtypeError);
     EXPECT_THROW(-Scalar(false), DtypeError);
-    EXPECT_EQ(int8_t(-Scalar(1)), -1);
-    EXPECT_EQ(int16_t(-Scalar(2)), -2);
-    EXPECT_EQ(int32_t(-Scalar(3)), -3);
-    EXPECT_EQ(int64_t(-Scalar(4)), -4);
-    EXPECT_EQ(uint8_t(-Scalar(5)), uint8_t(-5));
-    EXPECT_FLOAT_EQ(float(-Scalar(6)), -6.0f);
-    EXPECT_FLOAT_EQ(float(-Scalar(6.7)), -6.7f);
-    EXPECT_DOUBLE_EQ(double(-Scalar(8)), -8.0);
-    EXPECT_DOUBLE_EQ(double(-Scalar(8.9)), -8.9);
+    EXPECT_EQ(static_cast<int8_t>(-Scalar(1)), -1);
+    EXPECT_EQ(static_cast<int16_t>(-Scalar(2)), -2);
+    EXPECT_EQ(static_cast<int32_t>(-Scalar(3)), -3);
+    EXPECT_EQ(static_cast<int64_t>(-Scalar(4)), -4);
+    EXPECT_EQ(static_cast<uint8_t>(-Scalar(5)), static_cast<uint8_t>(-5));
+    EXPECT_FLOAT_EQ(static_cast<float>(-Scalar(6)), -6.0f);
+    EXPECT_FLOAT_EQ(static_cast<float>(-Scalar(6.7)), -6.7f);
+    EXPECT_DOUBLE_EQ(static_cast<double>(-Scalar(8)), -8.0);
+    EXPECT_DOUBLE_EQ(static_cast<double>(-Scalar(8.9)), -8.9);
 
-    EXPECT_EQ(int8_t(+Scalar(1)), 1);
-    EXPECT_EQ(int16_t(+Scalar(2)), 2);
-    EXPECT_EQ(int32_t(+Scalar(3)), 3);
-    EXPECT_EQ(int64_t(+Scalar(4)), 4);
-    EXPECT_EQ(uint8_t(+Scalar(5)), 5);
-    EXPECT_EQ(float(+Scalar(5)), 5);
-    EXPECT_FLOAT_EQ(float(+Scalar(6)), 6.0f);
-    EXPECT_FLOAT_EQ(float(+Scalar(6.7)), 6.7f);
-    EXPECT_DOUBLE_EQ(double(+Scalar(8)), 8.0);
-    EXPECT_DOUBLE_EQ(double(+Scalar(8.9)), 8.9);
+    EXPECT_EQ(static_cast<int8_t>(+Scalar(1)), 1);
+    EXPECT_EQ(static_cast<int16_t>(+Scalar(2)), 2);
+    EXPECT_EQ(static_cast<int32_t>(+Scalar(3)), 3);
+    EXPECT_EQ(static_cast<int64_t>(+Scalar(4)), 4);
+    EXPECT_EQ(static_cast<uint8_t>(+Scalar(5)), 5);
+    EXPECT_EQ(static_cast<float>(+Scalar(5)), 5);
+    EXPECT_FLOAT_EQ(static_cast<float>(+Scalar(6)), 6.0f);
+    EXPECT_FLOAT_EQ(static_cast<float>(+Scalar(6.7)), 6.7f);
+    EXPECT_DOUBLE_EQ(static_cast<double>(+Scalar(8)), 8.0);
+    EXPECT_DOUBLE_EQ(static_cast<double>(+Scalar(8.9)), 8.9);
 }
 
 TEST(ScalarTest, ToString) {
     EXPECT_EQ(Scalar(true).ToString(), "True");
     EXPECT_EQ(Scalar(false).ToString(), "False");
-    EXPECT_EQ(Scalar(int8_t(1)).ToString(), std::to_string(int8_t(1)));
-    EXPECT_EQ(Scalar(int16_t(2)).ToString(), std::to_string(int16_t(2)));
-    EXPECT_EQ(Scalar(int32_t(3)).ToString(), std::to_string(int32_t(3)));
-    EXPECT_EQ(Scalar(int64_t(4)).ToString(), std::to_string(int64_t(4)));
-    EXPECT_EQ(Scalar(uint8_t(5)).ToString(), std::to_string(uint8_t(5)));
+    EXPECT_EQ(Scalar(int8_t{1}).ToString(), std::to_string(int8_t{1}));
+    EXPECT_EQ(Scalar(int16_t{2}).ToString(), std::to_string(int16_t{2}));
+    EXPECT_EQ(Scalar(int32_t{3}).ToString(), std::to_string(int32_t{3}));
+    EXPECT_EQ(Scalar(int64_t{4}).ToString(), std::to_string(int64_t{4}));
+    EXPECT_EQ(Scalar(uint8_t{5}).ToString(), std::to_string(uint8_t{5}));
 }
 
 }  // namespace
