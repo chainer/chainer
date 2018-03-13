25dc3b3c884e1b22b3bc76aa6c827a63518d5871
sonots <sonots@gmail.com>
2018-03-14 01:42:58 +0900

    array_index.h: NOLINT runtime/explicit
    ArrayIndex intentionally accepts implicit conversion so that
    we can write std::vector<ArrayIndex> as `{1, Slice(0, 2), NewAxis{}}`

--- xchainer/array_index.h
+++ xchainer/array_index.h
@@ -16,10 +16,10 @@ class NewAxis {};
 
 class ArrayIndex {
 public:
-    ArrayIndex(int64_t index) : tag_(ArrayIndexTag::kSingleElement), index_(index) {}
-    ArrayIndex(Slice slice) : tag_(ArrayIndexTag::kSlice), slice_(slice) {}
-    ArrayIndex(NewAxis new_axis) : tag_(ArrayIndexTag::kNewAxis) {
-        (void)new_axis;  // unused
+    ArrayIndex(int64_t index) : tag_(ArrayIndexTag::kSingleElement), index_(index) {}  // NOLINT: runtime/explicit
+    ArrayIndex(Slice slice) : tag_(ArrayIndexTag::kSlice), slice_(slice) {}            // NOLINT: runtime/explicit
+    ArrayIndex(NewAxis new_axis) : tag_(ArrayIndexTag::kNewAxis) {                     // NOLINT: runtime/explicit
+        (void)new_axis;                                                                // unused
     }
 
     ArrayIndexTag tag() const { return tag_; }
