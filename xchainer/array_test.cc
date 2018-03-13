b82982a617a99e00f13fc957f20b531c076e99be
niboshi <niboshi000@gmail.com>
2018-03-13 16:32:54 +0900

    Remove needless // comment

--- xchainer/array_test.cc
+++ xchainer/array_test.cc
@@ -1419,24 +1419,23 @@ INSTANTIATE_TEST_CASE_P(
         ForEachInputs,
         ArrayReshapeTest,
         ::testing::Values(
-                ArrayReshapeTestParam{{}, {}},                //
-                ArrayReshapeTestParam{{0}, {0}},              //
-                ArrayReshapeTestParam{{1}, {1}},              //
-                ArrayReshapeTestParam{{5}, {5}},              //
-                ArrayReshapeTestParam{{2, 3}, {2, 3}},        //
-                ArrayReshapeTestParam{{1}, {}},               //
-                ArrayReshapeTestParam{{1}, {}},               //
-                ArrayReshapeTestParam{{}, {1}},               //
-                ArrayReshapeTestParam{{1, 1}, {}},            //
-                ArrayReshapeTestParam{{}, {1, 1}},            //
-                ArrayReshapeTestParam{{6}, {2, 3}},           //
-                ArrayReshapeTestParam{{2, 3}, {6}},           //
-                ArrayReshapeTestParam{{2, 0, 3}, {5, 0, 7}},  //
-                ArrayReshapeTestParam{{5}, {1, 1, 5, 1, 1}},  //
-                ArrayReshapeTestParam{{1, 1, 5, 1, 1}, {5}},  //
-                ArrayReshapeTestParam{{2, 3}, {3, 2}},        //
-                ArrayReshapeTestParam{{2, 3, 4}, {3, 4, 2}}   //
-                ));
+                ArrayReshapeTestParam{{}, {}},
+                ArrayReshapeTestParam{{0}, {0}},
+                ArrayReshapeTestParam{{1}, {1}},
+                ArrayReshapeTestParam{{5}, {5}},
+                ArrayReshapeTestParam{{2, 3}, {2, 3}},
+                ArrayReshapeTestParam{{1}, {}},
+                ArrayReshapeTestParam{{1}, {}},
+                ArrayReshapeTestParam{{}, {1}},
+                ArrayReshapeTestParam{{1, 1}, {}},
+                ArrayReshapeTestParam{{}, {1, 1}},
+                ArrayReshapeTestParam{{6}, {2, 3}},
+                ArrayReshapeTestParam{{2, 3}, {6}},
+                ArrayReshapeTestParam{{2, 0, 3}, {5, 0, 7}},
+                ArrayReshapeTestParam{{5}, {1, 1, 5, 1, 1}},
+                ArrayReshapeTestParam{{1, 1, 5, 1, 1}, {5}},
+                ArrayReshapeTestParam{{2, 3}, {3, 2}},
+                ArrayReshapeTestParam{{2, 3, 4}, {3, 4, 2}}));
 
 }  // namespace
 }  // namespace xchainer
