7311d9ddb8ea51cc348207e44209abd237cff84c
akitotakeki <takeki@hal.t.u-tokyo.ac.jp>
2018-03-16 18:04:08 +0900

    fix resnext

--- examples/imagenet/resnext50.py
+++ examples/imagenet/resnext50.py
@@ -15,10 +15,10 @@ class BottleNeckA(chainer.Chain):
 
         with self.init_scope():
             self.conv1 = L.Convolution2D(
-                in_size, ch, 1, stride, 0, initialW=initialW, nobias=True)
+                in_size, ch, 1, 1, 0, initialW=initialW, nobias=True)
             self.bn1 = L.BatchNormalization(ch)
             self.conv2 = L.Convolution2D(
-                ch, ch, 3, 1, 1, initialW=initialW, nobias=True,
+                ch, ch, 3, stride, 1, initialW=initialW, nobias=True,
                 groups=groups)
             self.bn2 = L.BatchNormalization(ch)
             self.conv3 = L.Convolution2D(
@@ -79,20 +79,20 @@ class Block(chainer.ChainList):
         return x
 
 
-class ResNet50(chainer.Chain):
+class ResNeXt50(chainer.Chain):
 
     insize = 224
 
     def __init__(self):
-        super(ResNet50, self).__init__()
+        super(ResNeXt50, self).__init__()
         with self.init_scope():
             self.conv1 = L.Convolution2D(
                 3, 64, 7, 2, 3, initialW=initializers.HeNormal())
             self.bn1 = L.BatchNormalization(64)
-            self.res2 = Block(3, 64, 64, 256, 1)
-            self.res3 = Block(4, 256, 128, 512)
-            self.res4 = Block(6, 512, 256, 1024)
-            self.res5 = Block(3, 1024, 512, 2048)
+            self.res2 = Block(3, 64, 128, 256, 1, groups=32)
+            self.res3 = Block(4, 256, 256, 512, groups=32)
+            self.res4 = Block(6, 512, 512, 1024, groups=32)
+            self.res5 = Block(3, 1024, 1024, 2048, groups=32)
             self.fc = L.Linear(2048, 1000)
 
     def __call__(self, x, t):
