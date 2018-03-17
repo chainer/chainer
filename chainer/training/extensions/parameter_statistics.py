e074caf605ee4121f08c0c13647522d3c8a1b04a
vilyaair <vilyaair@hotmail.com>
2018-03-17 05:58:44 -0700

    Attempt to use tuple of tuple for Python2

--- chainer/training/extensions/parameter_statistics.py
+++ chainer/training/extensions/parameter_statistics.py
@@ -61,10 +61,11 @@ class ParameterStatistics(extension.Extension):
         'percentile': lambda x: cuda.get_array_module(x).percentile(
             x, (0.13, 2.28, 15.87, 50, 84.13, 97.72, 99.87))
     }
+    # make variable immutable
     try:
         from types import MappingProxyType
     except ImportError:
-        pass
+        default_statistics = tuple(six.viewitems(default_statistics))
     else:
         default_statistics = MappingProxyType(default_statistics)
 
