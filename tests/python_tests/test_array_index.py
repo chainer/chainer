4088b0b3f4c5bf9887bdbe7fd43c5944dc1bceb7
sonots <sonots@gmail.com>
2018-03-19 18:08:50 +0900

    Add simple tests of newaxis and broadcastable python binding

---
+++ tests/python_tests/test_array_index.py
@@ -0,0 +1,11 @@
+import pytest
+
+import xchainer
+
+
+def test_newaxis():
+    assert xchainer.newaxis is None
+
+
+def test_broadcastable():
+    assert xchainer.broadcastable is xchainer.broadcastable
