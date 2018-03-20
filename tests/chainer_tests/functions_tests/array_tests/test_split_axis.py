40aaed0b8f3305edd65b82748f3a5045f7cf3e03
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-03-20 12:59:56 +0900

    avoid running tests on NumPy 1.10

--- tests/chainer_tests/functions_tests/array_tests/test_split_axis.py
+++ tests/chainer_tests/functions_tests/array_tests/test_split_axis.py
@@ -109,7 +109,7 @@ def inject_backend_tests(method_names):
         {'dtype': numpy.float64},
     ],
 ))
-@testing.with_requires('numpy>=1.10')
+@testing.with_requires('numpy>=1.11')
 @inject_backend_tests(['test_forward', 'test_backward'])
 class TestSplitAxis(unittest.TestCase):
 
