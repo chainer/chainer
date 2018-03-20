7be0b956cda340f3f1e8e64a5c226b591c1cb3ce
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-03-20 11:48:49 +0900

    Call function hooks in grad

--- tests/chainer_tests/test_function_hook.py
+++ tests/chainer_tests/test_function_hook.py
@@ -1,5 +1,8 @@
 import unittest
 
+import mock
+import numpy
+
 import chainer
 from chainer import testing
 
@@ -23,6 +26,28 @@ class TestFunctionHook(unittest.TestCase):
 
     def test_backward_postprocess(self):
         self.assertTrue(hasattr(self.h, 'backward_postprocess'))
+
+    def check_hook_methods_called(self, func):
+        def check_method_called(name):
+            with mock.patch.object(self.h, name) as patched:
+                with self.h:
+                    func()
+                patched.assert_called()
+
+        check_method_called('forward_preprocess')
+        check_method_called('forward_postprocess')
+        check_method_called('backward_preprocess')
+        check_method_called('backward_postprocess')
+
+    def test_all_called_with_backward(self):
+        x = chainer.Variable(numpy.random.rand(2, 3).astype(numpy.float32))
+        y = chainer.functions.sum(x * x)
+        self.check_hook_methods_called(y.backward)
+
+    def test_all_called_with_grad(self):
+        x = chainer.Variable(numpy.random.rand(2, 3).astype(numpy.float32))
+        y = chainer.functions.sum(x * x)
+        self.check_hook_methods_called(lambda: chainer.grad([y], [x]))
 
 
 testing.run_module(__name__, __file__)
