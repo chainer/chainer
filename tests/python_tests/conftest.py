3bb5c1e920ae34ae030f6a090f15695ddb68a515
niboshi <niboshi000@gmail.com>
2018-03-26 11:37:39 +0900

    Add comments

--- tests/python_tests/conftest.py
+++ tests/python_tests/conftest.py
@@ -48,6 +48,8 @@ def scope_session():
 
 
 def _get_required_cuda_devices_from_device_name(device_name):
+    # Returns the number of required CUDA devices to run a test, given a device name.
+    # If the device is non-CUDA device, 0 is returned.
     s = device_name.split(':')
     assert len(s) == 2
     if s[0] != 'cuda':
@@ -57,10 +59,16 @@ def _get_required_cuda_devices_from_device_name(device_name):
 
 @pytest.fixture
 def device(request):
+    # A fixture to wrap a test with a device scope, given a device name.
+    # Device instance is passed to the test.
+
     device_name = request.param
+
+    # Skip if the device is CUDA device and there's no sufficient CUDA devices.
     cuda_device_count = _get_required_cuda_devices_from_device_name(device_name)
     if cuda_device_count > cuda_utils.get_cuda_limit():
         pytest.skip()
+
     device = xchainer.get_device(device_name)
     device_scope = xchainer.device_scope(device)
 
