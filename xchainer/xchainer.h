d68d193daf3b8379e76f146b371fcdebb4d391dc
sonots <sonots@gmail.com>
2018-03-27 17:47:34 +0900

    Revert "Add using routines in xchainer.h"
    We do this later with creating python binding and tests
    
    This reverts commit b0706be76f699b200bc84530d2664c51535d8aa6.

--- xchainer/xchainer.h
+++ xchainer/xchainer.h
@@ -2,35 +2,3 @@
 
 #include "xchainer/array.h"
 #include "xchainer/dtype.h"
-#include "xchainer/routines/creation.h"
-#include "xchainer/routines/indexing.h"
-#include "xchainer/routines/manipulation.h"
-#include "xchainer/routines/math.h"
-
-namespace xchainer {
-// TODO(sonots): Create python binding and do tests
-
-// creation
-using routines::Copy;
-using routines::Empty;
-using routines::EmptyLike;
-using routines::FromBuffer;
-using routines::Full;
-using routines::FullLike;
-using routines::Ones;
-using routines::OnesLike;
-using routines::Zeros;
-using routines::ZerosLike;
-
-// manipulation
-using routines::BroadcastTo;
-using routines::Reshape;
-using routines::Squeeze;
-using routines::Transpose;
-
-// math
-using routines::Add;
-using routines::Mul;
-using routines::Sum;
-
-}  // namespace xchainer
