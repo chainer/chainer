b0706be76f699b200bc84530d2664c51535d8aa6
sonots <sonots@gmail.com>
2018-03-27 16:35:59 +0900

    Add using routines in xchainer.h

--- xchainer/xchainer.h
+++ xchainer/xchainer.h
@@ -2,3 +2,33 @@
 
 #include "xchainer/array.h"
 #include "xchainer/dtype.h"
+#include "xchainer/routines/creation.h"
+#include "xchainer/routines/indexing.h"
+#include "xchainer/routines/manipulation.h"
+#include "xchainer/routines/math.h"
+
+namespace xchainer {
+// creation
+using routines::Copy;
+using routines::Empty;
+using routines::EmptyLike;
+using routines::FromBuffer;
+using routines::Full;
+using routines::FullLike;
+using routines::Ones;
+using routines::OnesLike;
+using routines::Zeros;
+using routines::ZerosLike;
+
+// manipulation
+using routines::BroadcastTo;
+using routines::Reshape;
+using routines::Squeeze;
+using routines::Transpose;
+
+// math
+using routines::Add;
+using routines::Mul;
+using routines::Sum;
+
+}  // namespace xchainer
