e67fb342672fb4aa7fb49eddd236e504835b5fa2
niboshi <niboshi000@gmail.com>
2018-03-29 13:52:06 +0900

    Add test_routines.py as placeholder

---
+++ tests/python_tests/test_routines.py
@@ -0,0 +1,2 @@
+# TODO(niboshi): Currently this file is just a placeholder for future migration of routine tests from test_array.py.
+#   In order to do that, we need to somehow share check logics/test parameters with test_array.py.
