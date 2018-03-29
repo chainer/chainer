1b344b78f1d2316365df205c358f937d122d24fd
niboshi <niboshi000@gmail.com>
2018-03-29 10:52:50 +0900

    No need of reinterpret_cast

--- xchainer/routines/manipulation.cc
+++ xchainer/routines/manipulation.cc
@@ -36,10 +36,9 @@ Scalar AsScalar(const Array& a) {
     native_device.Synchronize();
     return VisitDtype(a.dtype(), [&native_copy](auto pt) -> Scalar {
         using T = typename decltype(pt)::type;
-        const void* ptr = static_cast<const uint8_t*>(native_copy.data().get()) + native_copy.offset();
-        auto typed_ptr = reinterpret_cast<const T*>(ptr);  // NOLINT: reinterpret_cast
-        T value = *typed_ptr;
-        return Scalar{value};
+        auto ptr = static_cast<const T*>(
+                static_cast<const void*>(static_cast<const uint8_t*>(native_copy.data().get()) + native_copy.offset()));
+        return Scalar{*ptr};
     });
 }
 
