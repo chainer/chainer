c00f45a66fcb1ad4c19fa9e6da51f416e5168dbe
niboshi <niboshi000@gmail.com>
2018-03-29 18:33:48 +0900

    Make ignore_errors False by default

--- chainer/utils/__init__.py
+++ chainer/utils/__init__.py
@@ -40,8 +40,10 @@ def force_type(dtype, value):
 @contextlib.contextmanager
 def tempdir(**kwargs):
     # A context manager that defines a lifetime of a temporary directory.
+    ignore_errors = kwargs.pop('ignore_errors', False)
+
     temp_dir = tempfile.mkdtemp(**kwargs)
     try:
         yield temp_dir
     finally:
-        shutil.rmtree(temp_dir, ignore_errors=True)
+        shutil.rmtree(temp_dir, ignore_errors=ignore_errors)
