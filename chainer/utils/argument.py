570c46e22e1788d20fa96eabf362b862db996e44
vilyaair <vilyaair@hotmail.com>
2018-03-30 06:20:23 -0700

    Improve chainer.utils.argument exception message

--- chainer/utils/argument.py
+++ chainer/utils/argument.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 def check_unexpected_kwargs(kwargs, **unexpected):
     for key, message in unexpected.items():
         if key in kwargs:
@@ -8,8 +11,11 @@ def parse_kwargs(kwargs, *name_and_values):
     values = [kwargs.pop(name, default_value)
               for name, default_value in name_and_values]
     if kwargs:
-        args = ', '.join(["'%s'" % arg for arg in kwargs.keys()])
-        raise TypeError('got unexpected keyword argument(s) %s' % args)
+        caller = inspect.stack()[1]
+        args = ', '.join(repr(arg) for arg in kwargs.keys())
+        message = caller.function + \
+            '() got unexpected keyword argument(s) {}'.format(args)
+        raise TypeError(message)
     return tuple(values)
 
 
