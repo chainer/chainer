0d88be74d3199ec4d58e3f642e93be77f5efbf06
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-04-04 10:57:20 +0900

    Fix rebase error

--- xchainer/numerical_gradient.cc
+++ xchainer/numerical_gradient.cc
@@ -25,26 +25,6 @@ namespace numerical_gradient_internal {
 // (e.g. with unified memory). In order for numerical gradient calculation to work corretly on general devices, They should be replaced with
 // full-featured operations.
 
-Array& Subtract(const Array& lhs, const Array& rhs, Array& out) {
-    lhs.device().Synchronize();
-    rhs.device().Synchronize();
-    out.device().Synchronize();
-
-    VisitDtype(lhs.dtype(), [&](auto pt) {
-        using T = typename decltype(pt)::type;
-        IndexableArray<const T> lhs_iarray{lhs};
-        IndexableArray<const T> rhs_iarray{rhs};
-        IndexableArray<T> out_iarray{out};
-        Indexer indexer{out.shape()};
-
-        for (int64_t i = 0; i < indexer.total_size(); ++i) {
-            indexer.Set(i);
-            out_iarray[indexer] = lhs_iarray[indexer] - rhs_iarray[indexer];
-        }
-    });
-    return out;
-}
-
 Array& Divide(const Array& lhs, const Array& rhs, Array& out) {
     lhs.device().Synchronize();
     rhs.device().Synchronize();
