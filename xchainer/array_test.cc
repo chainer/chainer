94650cd1c106612351c67f102a1e130bb2212dec
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-04-05 17:07:08 +0900

    Rename device method arguments similar to numpy and rename Mul to Multiply

--- xchainer/array_test.cc
+++ xchainer/array_test.cc
@@ -498,7 +498,7 @@ TEST_P(ArrayTest, ISubtract) {
     testing::ExpectEqual<float>(e, a);
 }
 
-TEST_P(ArrayTest, IMul) {
+TEST_P(ArrayTest, IMultiply) {
     Array a = testing::BuildArray<float>({3, 1}, {1, 2, 3});
     Array b = testing::BuildArray<float>({3, 1}, {1, 2, 3});
     Array e = testing::BuildArray<float>({3, 1}, {1, 4, 9});
@@ -522,7 +522,7 @@ TEST_P(ArrayTest, Subtract) {
     testing::ExpectEqual<float>(e, o);
 }
 
-TEST_P(ArrayTest, Mul) {
+TEST_P(ArrayTest, Multiply) {
     Array a = testing::BuildArray<float>({3, 1}, {1, 2, 3});
     Array b = testing::BuildArray<float>({3, 1}, {1, 2, 3});
     Array e = testing::BuildArray<float>({3, 1}, {1, 4, 9});
@@ -531,7 +531,7 @@ TEST_P(ArrayTest, Mul) {
 }
 
 // TODO(hvy): Also test CUDA using ArrayTest.
-TEST(ArrayNativeTest, MulScalar) {
+TEST(ArrayNativeTest, MultiplyScalar) {
     testing::ContextSession context_session;
 
     Array a = testing::BuildArray<float>({3, 1}, {1, 2, 3});
@@ -817,7 +817,7 @@ TEST_P(ArrayTest, AddBackward) {
     testing::ExpectEqual<bool>(gb, go);
 }
 
-TEST_P(ArrayTest, MulBackward) {
+TEST_P(ArrayTest, MultiplyBackward) {
     Array a = testing::BuildArray<bool>({4, 1}, {true, true, false, false});
     Array b = testing::BuildArray<bool>({4, 1}, {true, false, true, false});
 
@@ -838,7 +838,7 @@ TEST_P(ArrayTest, MulBackward) {
     EXPECT_FALSE(gb.IsGradRequired());
 }
 
-TEST_P(ArrayTest, MulBackwardCapture) {
+TEST_P(ArrayTest, MultiplyBackwardCapture) {
     Array y = [this]() {
         Array x1 = testing::BuildArray<float>({1}, {2.0f});
         Array x2 = testing::BuildArray<float>({1}, {3.0f});
@@ -862,7 +862,7 @@ TEST_P(ArrayTest, MulBackwardCapture) {
     EXPECT_FALSE(gx2.IsGradRequired());
 }
 
-TEST_P(ArrayTest, MulBackwardMultipleGraphs) {
+TEST_P(ArrayTest, MultiplyBackwardMultipleGraphs) {
     GraphId graph_id1 = "graph_1";
     GraphId graph_id2 = "graph_2";
 
