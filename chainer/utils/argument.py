2dadfec8d45ec166804027bc09073aa7a505fcbe
vilyaair <vilyaair@hotmail.com>
2018-04-06 23:46:08 -0700

    Add test for chainer.utils.argument.parse_kwargs

--- chainer/utils/argument.py
+++ chainer/utils/argument.py
@@ -1,6 +1,3 @@
-import inspect
-
-
 def check_unexpected_kwargs(kwargs, **unexpected):
     for key, message in unexpected.items():
         if key in kwargs:
@@ -11,8 +8,9 @@ def parse_kwargs(kwargs, *name_and_values):
     values = [kwargs.pop(name, default_value)
               for name, default_value in name_and_values]
     if kwargs:
+        import inspect
         caller = inspect.stack()[1]
-        args = ', '.join(repr(arg) for arg in kwargs.keys())
+        args = ', '.join(repr(arg) for arg in sorted(kwargs.keys()))
         message = caller[3] + \
             '() got unexpected keyword argument(s) {}'.format(args)
         raise TypeError(message)
