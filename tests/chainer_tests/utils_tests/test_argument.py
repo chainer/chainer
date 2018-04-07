a8a27ef401868aa651eefa5ebef22f6e6cf3df3d
vilyaair <vilyaair@hotmail.com>
2018-04-06 23:46:08 -0700

    Add test for chainer.utils.argument.parse_kwargs

---
+++ tests/chainer_tests/utils_tests/test_argument.py
@@ -0,0 +1,23 @@
+import unittest
+
+from chainer import testing
+from chainer.utils.argument import parse_kwargs
+
+
+class TestArgument(unittest.TestCase):
+
+    def test_parse_kwargs(self):
+
+        def test(**kwargs):
+            return parse_kwargs(kwargs, ('foo', 1), ('bar', 2))
+
+        self.assertEqual(test(), (1, 2))
+        self.assertEqual(test(bar=1, foo=2), (2, 1))
+
+        with self.assertRaisesRegex(
+                TypeError, "test\(\) got unexpected keyword argument\(s\) "
+                           "'ham', 'spam'"):
+            test(spam=1, ham=2)
+
+
+testing.run_module(__name__, __file__)
