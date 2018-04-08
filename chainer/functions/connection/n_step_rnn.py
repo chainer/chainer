9a960cbde0227e372da83ce4582f9e8d94bdbcc3
Yuya Unno <unnonouno@gmail.com>
2018-04-08 18:29:51 +0900

    Removed redundant member variables

--- chainer/functions/connection/n_step_rnn.py
+++ chainer/functions/connection/n_step_rnn.py
@@ -380,7 +380,6 @@ class BaseNStepRNN(function.Function):
 
         w_desc = cudnn.create_filter_descriptor(w)
 
-        self.w = w
         self.w_desc = w_desc
 
         y_list = cuda.cupy.split(ys, self.sections[:-1])
@@ -415,14 +414,15 @@ class BaseNStepRNN(function.Function):
                 self.reserve_space.data.ptr, reserve_size)
 
         self.c_y_descs = c_y_descs
-        self.ys = ys
         self.c_x_descs = c_x_descs
 
         if self.use_cell:
             # LSTM
+            self.retain_outputs([2])
             return hy, cy, ys
         else:
             # GRU, RNN
+            self.retain_outputs([1])
             return hy, ys
 
     def backward(self, inputs, grads):
@@ -454,6 +454,8 @@ class BaseNStepRNN(function.Function):
             cx_data_ptr = dcy_data_ptr = dcx_data_ptr = 0
             cx_desc_value = dcx_desc_value = dcy_desc_value = 0
 
+        ys = self.output_data[-1]
+
         xs = cuda.cupy.ascontiguousarray(xs)
         hx = cuda.cupy.ascontiguousarray(hx)
 
@@ -461,7 +463,7 @@ class BaseNStepRNN(function.Function):
             dhy = cuda.cupy.zeros_like(hx)
 
         if dys is None:
-            dys = cuda.cupy.zeros_like(self.ys)
+            dys = cuda.cupy.zeros_like(ys)
 
         length = len(self.lengths)
 
@@ -487,20 +489,20 @@ class BaseNStepRNN(function.Function):
 
         libcudnn.RNNBackwardData(
             handle, rnn_desc.value, length,
-            self.c_y_descs.data, self.ys.data.ptr,
+            self.c_y_descs.data, ys.data.ptr,
             c_dy_descs.data, dys.data.ptr, dhy_desc.value, dhy.data.ptr,
-            dcy_desc_value, dcy_data_ptr, self.w_desc.value, self.w.data.ptr,
+            dcy_desc_value, dcy_data_ptr, self.w_desc.value, w.data.ptr,
             hx_desc.value, hx.data.ptr, cx_desc_value, cx_data_ptr,
             c_dx_descs.data, dxs.data.ptr, dhx_desc.value, dhx.data.ptr,
             dcx_desc_value, dcx_data_ptr, workspace.data.ptr, work_size,
             self.reserve_space.data.ptr, self.reserve_space.size)
 
-        dw = cuda.cupy.zeros_like(self.w)
+        dw = cuda.cupy.zeros_like(w)
         dw_desc = cudnn.create_filter_descriptor(dw)
         libcudnn.RNNBackwardWeights(
             handle, rnn_desc.value, length,
             self.c_x_descs.data, xs.data.ptr,
-            hx_desc.value, hx.data.ptr, self.c_y_descs.data, self.ys.data.ptr,
+            hx_desc.value, hx.data.ptr, self.c_y_descs.data, ys.data.ptr,
             workspace.data.ptr, work_size, dw_desc.value, dw.data.ptr,
             self.reserve_space.data.ptr, self.reserve_space.size)
 
