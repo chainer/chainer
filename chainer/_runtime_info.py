0052f4464b53485d18de59e8802829a45bb3c498
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-04-09 11:38:55 +0900

    fix to work if cupy is unavailable

--- chainer/_runtime_info.py
+++ chainer/_runtime_info.py
@@ -16,15 +16,21 @@ class _RuntimeInfo(object):
     def __init__(self):
         self.chainer_version = chainer.__version__
         self.numpy_version = numpy.__version__
-        self.cuda_info = cuda.cupyx.get_runtime_info()
+        if cuda.available:
+            self.cuda_info = cuda.cupyx.get_runtime_info()
+        else:
+            self.cuda_info = None
 
     def __str__(self):
         s = six.StringIO()
         s.write('''Chainer: {}\n'''.format(self.chainer_version))
         s.write('''NumPy: {}\n'''.format(self.numpy_version))
-        s.write('''CuPy:\n''')
-        for line in str(self.cuda_info).splitlines():
-            s.write('''  {}\n'''.format(line))
+        if self.cuda_info is None:
+            s.write('''CuPy: Not Available\n''')
+        else:
+            s.write('''CuPy:\n''')
+            for line in str(self.cuda_info).splitlines():
+                s.write('''  {}\n'''.format(line))
         return s.getvalue()
 
 
