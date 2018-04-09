86bdb1ed01affcea9878873c8af4e694c5a68d97
Seiya Tokui <beam.web@gmail.com>
2018-04-09 15:46:18 +0900

    Add document and tests for GetSortedAxes/OrAll

--- xchainer/routines/util.h
+++ xchainer/routines/util.h
@@ -8,6 +8,9 @@
 namespace xchainer {
 namespace internal {
 
+// Resolves the axis argument of many operations.
+// Negative axis value is first converted to non-negative one (by wrapping at ndim), and then the axis is sorted.
+// In GetSortedAxesOrAll, nullopt is converted to a vector of all axes.
 std::vector<int8_t> GetSortedAxes(const std::vector<int8_t>& axis, int8_t ndim);
 std::vector<int8_t> GetSortedAxesOrAll(const nonstd::optional<std::vector<int8_t>>& axis, int8_t ndim);
 
