8ae9a75307ead3057460d439f226ce20b6016f61
Seiya Tokui <beam.web@gmail.com>
2018-04-09 14:39:34 +0900

    Use exact-bit-width constants in numeric_limits.h

--- xchainer/numeric_limits.h
+++ xchainer/numeric_limits.h
@@ -21,31 +21,31 @@ struct NumericLimits<bool> {
 template <>
 struct NumericLimits<uint8_t> {
     XCHAINER_HOST_DEVICE static constexpr uint8_t LowestOrInf() noexcept { return 0; }
-    XCHAINER_HOST_DEVICE static constexpr uint8_t MaxOrInf() noexcept { return UCHAR_MAX; }
+    XCHAINER_HOST_DEVICE static constexpr uint8_t MaxOrInf() noexcept { return UINT8_MAX; }
 };
 
 template <>
 struct NumericLimits<int8_t> {
-    XCHAINER_HOST_DEVICE static constexpr int8_t LowestOrInf() noexcept { return SCHAR_MIN; }
-    XCHAINER_HOST_DEVICE static constexpr int8_t MaxOrInf() noexcept { return SCHAR_MAX; }
+    XCHAINER_HOST_DEVICE static constexpr int8_t LowestOrInf() noexcept { return INT8_MIN; }
+    XCHAINER_HOST_DEVICE static constexpr int8_t MaxOrInf() noexcept { return INT8_MAX; }
 };
 
 template <>
 struct NumericLimits<int16_t> {
-    XCHAINER_HOST_DEVICE static constexpr int16_t LowestOrInf() noexcept { return SHRT_MIN; }
-    XCHAINER_HOST_DEVICE static constexpr int16_t MaxOrInf() noexcept { return SHRT_MAX; }
+    XCHAINER_HOST_DEVICE static constexpr int16_t LowestOrInf() noexcept { return INT16_MIN; }
+    XCHAINER_HOST_DEVICE static constexpr int16_t MaxOrInf() noexcept { return INT16_MAX; }
 };
 
 template <>
 struct NumericLimits<int32_t> {
-    XCHAINER_HOST_DEVICE static constexpr int32_t LowestOrInf() noexcept { return INT_MIN; }
-    XCHAINER_HOST_DEVICE static constexpr int32_t MaxOrInf() noexcept { return INT_MAX; }
+    XCHAINER_HOST_DEVICE static constexpr int32_t LowestOrInf() noexcept { return INT32_MIN; }
+    XCHAINER_HOST_DEVICE static constexpr int32_t MaxOrInf() noexcept { return INT32_MAX; }
 };
 
 template <>
 struct NumericLimits<int64_t> {
-    XCHAINER_HOST_DEVICE static constexpr int64_t LowestOrInf() noexcept { return LLONG_MIN; }
-    XCHAINER_HOST_DEVICE static constexpr int64_t MaxOrInf() noexcept { return LLONG_MAX; }
+    XCHAINER_HOST_DEVICE static constexpr int64_t LowestOrInf() noexcept { return INT64_MIN; }
+    XCHAINER_HOST_DEVICE static constexpr int64_t MaxOrInf() noexcept { return INT64_MAX; }
 };
 
 template <>
