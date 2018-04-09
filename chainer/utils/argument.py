a2245d1677fd0bc70608c0a1955e11c7fcd0c356
vilyaair <35159051+vilyaair@users.noreply.github.com>
2018-04-09 16:29:04 -0700

    Put `import inspect` at the top of the file

--- chainer/utils/argument.py
+++ chainer/utils/argument.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 def check_unexpected_kwargs(kwargs, **unexpected):
     for key, message in unexpected.items():
         if key in kwargs:
@@ -8,7 +11,6 @@ def parse_kwargs(kwargs, *name_and_values):
     values = [kwargs.pop(name, default_value)
               for name, default_value in name_and_values]
     if kwargs:
-        import inspect
         caller = inspect.stack()[1]
         args = ', '.join(repr(arg) for arg in sorted(kwargs.keys()))
         message = caller[3] + \
