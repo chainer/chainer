7266c1e664c342bfd8bc5689b2067728abb4bef9
Masaki Saito <msaito@preferred.jp>
2018-04-10 20:37:08 +0900

    Support dilated convolution for ConvolutionND

--- chainer/utils/conv_nd_kernel.py
+++ chainer/utils/conv_nd_kernel.py
@@ -182,7 +182,8 @@ class Col2imNDKernel(object):
 
     def _in_params(self, ds, outs, ks, ss, ps, dilate):
         # 2D: raw T col, int32 d_0, int32 d_1, int32 out_0, int32 out_1,
-        #     int32 k_0, int32 k_1, int32 s_0, int32 s_1, int32 p_0, int32 p_1
+        #     int32 k_0, int32 k_1, int32 s_0, int32 s_1, int32 p_0, int32 p_1,
+        #     int32 di_0, int32 di_1
         def aux(x):
             return 'int32 {}'.format(x)
         return ', '.join(
@@ -195,62 +196,55 @@ class Col2imNDKernel(object):
         # 2D: int c0 = i / (d_0 * d_1);
         return ['int c0 = i / ({});'.format(mulexp(ds))]
 
-    def _compile_x(self, ndim, ds, ps):
-        # 2D: int x_0 = i / (d_1) % d_0 + p_0;
-        #     int x_1 = i % d_1 + p_1;
-        def aux(x, ds, p):
+    def _compile_x(self, ndim, ds):
+        # 2D: int x_0 = i / (d_1) % d_0;
+        #     int x_1 = i % d_1;
+        def aux(x, ds):
             head = ds[0]
             tail = ds[1:]
             if tail:
-                return 'int {} = i / ({}) % {} + {};'.format(
-                    x, mulexp(tail), head, p)
+                return 'int {} = i / ({}) % {};'.format(
+                    x, mulexp(tail), head)
             else:
-                return 'int {} = i % {} + {};'.format(x, head, p)
+                return 'int {} = i % {};'.format(x, head)
         xs = vars('x', ndim)
-        x_decls = map_(aux, xs, succ_sublists(ds), ps)
+        x_decls = map_(aux, xs, succ_sublists(ds))
         return x_decls, xs
 
-    def _compile_loop(self, ndim, outs, ks, ss, xs, dilate):
-        # 2D: int out_x0_0 = max(0,     (x_0 - k_0 + s_0) / s_0);
-        #     int out_x1_0 = min(out_0, (x_0       + s_0) / s_0);
-        #     int out_x0_1 = max(0,     (x_1 - k_1 + s_1) / s_1);
-        #     int out_x1_1 = min(out_1, (x_1       + s_1) / s_1);
-        #     ... Before-part here ...
-        #     for (int out_x_0 = out_x0_0; out_x_0 < out_x1_0; ++out_x_0) {
-        #       int kx_0 = x_0 - out_x_0 * s_0 + k_0 * c0;
-        #       for (int out_x_1 = out_x0_1; out_x_1 < out_x1_1; ++out_x_1) {
-        #         int kx_1 = x_1 - out_x_1 * s_1 + k_1 * kx_0;
-        #         ... Main-part here ...
-        #       }
+    def _compile_loop(self, ndim, outs, ks, ss, ps, xs, dilate):
+        # 2D: for (int kx_0 = 0; kx_0 < k_0; ++kx_0) {
+        #     int out_x_0 = x_0 + p_0 - kx_0 * di_0;
+        #     if (0 > out_x_0 || out_x_0 >= out_0 * s_0) continue;
+        #     if (out_x_0 % s_0 != 0) continue;
+        #     out_x_0 /= s_0;
+        #     for (int kx_1 = 0; kx_1 < k_1; ++kx_1) {
+        #       int out_x_1 = x_1 + p_1 - kx_1 * di_1;
+        #       if (0 > out_x_1 || out_x_1 >= out_1 * s_1) continue;
+        #       if (out_x_1 % s_1 != 0) continue;
+        #       out_x_1 /= s_1;
+        #       ... Main-part here ...
         #     }
-        #     ... After-part here ...
-        def aux(out_x0, out_x1, out, x, k, s, di):
-            return [
-                'int {} = max(0, ({} - {} * {} + {}) / {});'.format(
-                    out_x0, x, k, di, s, s),
-                'int {} = min({}, ({} + {}) / {});'.format(
-                    out_x1, out, x, s, s)]
-        out_x0s = vars('out_x0', ndim)
-        out_x1s = vars('out_x1', ndim)
-        bounds = sum(map_(aux, out_x0s, out_x1s, outs, xs, ks, ss, dilate), [])
-
+        #   }
+        #   ... After-part here ...
         def _loop_main(main, ndim, ks, ss):
             w = Writer()
 
             # Loop openings.
             out_xs = vars('out_x', ndim)
             kxs = vars('kx', ndim)
-            kxs1 = ['c0'] + kxs[:-1]
-            for out_x, out_x0, out_x1, kx, s, x, k, kx1, di in six.moves.zip(
-                    out_xs, out_x0s, out_x1s, kxs, ss, xs, ks, kxs1, dilate):
-                w.write('for (int {} = {}; {} < {}; ++{}) {{'.format(
-                    out_x, out_x0, out_x, out_x1, out_x), indent='inc')
-                w.write('int {} = ({} - {} * {} + {} * {}) / {};'.format(
-                    kx, x, out_x, s, k, kx1, di))
+            for out, out_x, kx, s, p, x, k, di in six.moves.zip(
+                    outs, out_xs, kxs, ss, ps, xs, ks, dilate):
+                w.write('for (int {} = 0; {} < {}; ++{}) {{'.format(
+                    kx, kx, k, kx), indent='inc')
+                w.write('int {} = {} + {} - {} * {};'.format(
+                    out_x, x, p, kx, di))
+                w.write('if (0 > {} || {} >= {} * {}) continue;'.format(
+                    out_x, out_x, out, s))
+                w.write('if ({} % {} != 0) continue;'.format(out_x, s))
+                w.write('{} /= {};'.format(out_x, s))
 
             # Main-part.
-            kx = kxs[-1]
-            for l in main(kx, out_xs).split('\n'):
+            for l in main(ks, kxs, out_xs).split('\n'):
                 w.write(l)
 
             # Loop closings.
@@ -259,12 +253,14 @@ class Col2imNDKernel(object):
 
             return [w.get()]
 
-        return bounds, _loop_main
+        return _loop_main
 
     def _compile_procedure(self, outs, xs):
-        # 2D: val = val + col[(out_x_1 + out_1 * (out_x_0 + out_0 * kx_1))];
-        def _main(kx, out_xs):
-            index = muladdexp(outs, out_xs, kx)
+        # 2D: val = val + col[
+        #     (out_x_1 + out_1 * (out_x_0 + out_0 *
+        #     (kx_1 + k_1 * (kx_0 + k_0 * c0))))];
+        def _main(ks, kxs, out_xs):
+            index = muladdexp(outs, out_xs, muladdexp(ks, kxs, 'c0'))
             return 'val = val + col[{}];'.format(index)
         before = ['T val = 0;']
         after = ['img = val;']
@@ -272,13 +268,11 @@ class Col2imNDKernel(object):
 
     def _operation(self, ndim, ds, outs, ks, ss, ps, dilate):
         c0 = self._compile_c0(ds)
-        x, xs = self._compile_x(ndim, ds, ps)
-        loop_bounds, loop_main = \
-            self._compile_loop(ndim, outs, ks, ss, xs, dilate)
+        x, xs = self._compile_x(ndim, ds)
+        loop_main = self._compile_loop(ndim, outs, ks, ss, ps, xs, dilate)
         before, main, after = self._compile_procedure(outs, xs)
         return '\n'.join(
-            c0 + x + loop_bounds + before + loop_main(
-                main, ndim, ks, ss) + after)
+            c0 + x + before + loop_main(main, ndim, ks, ss) + after)
 
     def _generate(self, ndim):
         ds = vars('d', ndim)
