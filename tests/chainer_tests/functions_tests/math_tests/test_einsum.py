85a983466e339af4c76cd16259e79119e571320a
Toshiki Kataoka <kataoka@preferred.jp>
2018-04-12 19:15:39 +0900

    add tests

--- tests/chainer_tests/functions_tests/math_tests/test_einsum.py
+++ tests/chainer_tests/functions_tests/math_tests/test_einsum.py
@@ -10,6 +10,9 @@ from chainer import testing
 from chainer.testing import attr
 
 
+def _tuple_to_gpu(xs):
+    return tuple(cuda.to_gpu(x) for x in xs)
+
 @testing.parameterize(*testing.product_dict(
     [
         {'subscripts': 'ij,jk->ik', 'shapes': ((2, 3), (3, 4))},
@@ -17,6 +20,8 @@ from chainer.testing import attr
         {'subscripts': 'ij,jk,kl->il', 'shapes': ((5, 2), (2, 3), (3, 4))},
         {'subscripts': 'ij,ij->i', 'shapes': ((2, 3), (2, 3))},
         {'subscripts': 'ij,jk', 'shapes': ((2, 3), (3, 4))},
+        {'subscripts': 'ii', 'shapes': ((2, 2),)},
+        {'subscripts': 'ii->i', 'shapes': ((2, 2),)},
     ],
     [
         {'dtype': numpy.float16},
@@ -33,33 +38,32 @@ class TestEinSum(unittest.TestCase):
         ]
         self.forward_answer = numpy.einsum(self.subscripts, *self.inputs)
         self.g = self._setup_tensor(-1, 1, self.forward_answer.shape, self.dtype)
+        self.gg_inputs = [
+            self._setup_tensor(-1, 1, shape, self.dtype)
+            for shape in self.shapes
+        ]
         self.op = lambda *xs: F.einsum(self.subscripts, *xs)
 
     def _setup_tensor(self, _min, _max, shape, dtype):
         return numpy.random.uniform(_min, _max, shape).astype(dtype)
 
-    """
-    def check_forward(self, a_data, b_data, atol=1e-4, rtol=1e-5):
-        a = chainer.Variable(a_data)
-        b = chainer.Variable(b_data)
-        c = self.op(a, b)
-        testing.assert_allclose(self.forward_answer, c.data, atol, rtol)
+    def check_forward(self, inputs_data, atol=1e-4, rtol=1e-5):
+        out = self.op(*[chainer.Variable(x) for x in self.inputs])
+        testing.assert_allclose(self.forward_answer, out.data, atol, rtol)
 
-    def test_tensordot_forward_cpu(self):
-        if self.a.dtype == numpy.float16 or self.b.dtype == numpy.float16:
-            self.check_forward(self.a, self.b, atol=1e-3, rtol=1e-3)
+    def test_einsum_forward_cpu(self):
+        if self.dtype == numpy.float16:
+            self.check_forward(self.inputs, atol=1e-3, rtol=1e-3)
         else:
-            self.check_forward(self.a, self.b)
+            self.check_forward(self.inputs)
 
     @attr.gpu
-    def test_tensordot_forward_gpu(self):
-        a = cuda.to_gpu(self.a)
-        b = cuda.to_gpu(self.b)
-        if self.a.dtype == numpy.float16 or self.b.dtype == numpy.float16:
-            self.check_forward(a, b, atol=1e-3, rtol=1e-3)
+    def test_einsum_forward_gpu(self):
+        inputs = [cuda.to_gpu(x) for x in self.inputs]
+        if self.dtype == numpy.float16:
+            self.check_forward(self.inputs, atol=1e-3, rtol=1e-3)
         else:
-            self.check_forward(a, b)
-    """
+            self.check_forward(self.inputs)
 
     def check_backward(self, inputs_data, output_grad, atol, rtol):
         gradient_check.check_backward(
@@ -72,29 +76,26 @@ class TestEinSum(unittest.TestCase):
     @attr.gpu
     def test_einsum_backward_gpu(self):
         self.check_backward(
-            tuple(cuda.to_gpu(x) for x in self.inputs),
+            _tuple_to_gpu(self.inputs),
             self.g, atol=1e-2, rtol=5e-2)
 
-    """
     def check_double_backward(
-            self, a_data, b_data, y_grad, a_grad_grad, b_grad_grad,
+            self, inputs_data, y_grad, inputs_grad_grad,
             atol, rtol):
         gradient_check.check_double_backward(
-            self.op, (a_data, b_data), y_grad, (a_grad_grad, b_grad_grad),
+            self.op, inputs_data, y_grad, inputs_grad_grad,
             atol=atol, rtol=rtol, dtype=numpy.float32)
 
-    def test_tensordot_double_backward_cpu(self):
+    def test_einsum_double_backward_cpu(self):
         self.check_double_backward(
-            self.a, self.b, self.gc, self.gga, self.ggb,
+            self.inputs, self.g, self.gg_inputs,
             atol=1e-2, rtol=5e-2)
 
     @attr.gpu
-    def test_tensordot_double_backward_gpu(self):
+    def test_einsum_double_backward_gpu(self):
         self.check_double_backward(
-            cuda.to_gpu(self.a), cuda.to_gpu(self.b),
-            cuda.to_gpu(self.gc), cuda.to_gpu(self.gga),
-            cuda.to_gpu(self.ggb), atol=1e-2, rtol=1e-2)
-    """
+            _tuple_to_gpu(self.inputs), cuda.to_gpu(self.g),
+            _tuple_to_gpu(self.gg_inputs), atol=1e-2, rtol=1e-2)
 
 
 """
