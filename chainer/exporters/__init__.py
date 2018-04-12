4d22c17f1dbe74b7c33763c37f3f16cc39c8e59b
Ryosuke Okuta <okuta@preferred.jp>
2018-04-13 08:26:25 +0900

    Revert "[backport] Implement caffe export"

--- chainer/exporters/__init__.py
+++
@@ -1,1 +0,0 @@
-from chainer.exporters import caffe  # NOQA
--- setup.py
+++ setup.py
@@ -116,7 +116,6 @@ setup(
               'chainer.backends',
               'chainer.dataset',
               'chainer.datasets',
-              'chainer.exporters',
               'chainer.functions',
               'chainer.functions.activation',
               'chainer.functions.array',
