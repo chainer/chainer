b7ea9949ab92676f91a4820e5984b1156d35119d
Yusuke Niitani <yuyuniitani@gmail.com>
2018-04-16 15:44:17 +0900

    simplify seed selection

--- chainer/iterators/order_samplers.py
+++ chainer/iterators/order_samplers.py
@@ -1,21 +1,32 @@
 import numpy
 
 
-def shuffle_order_sampler(current_order, current_position):
-    """The order sampler of an iterator when shuffle option is True.
+class ShuffleOrderSampler(object):
 
-    Args:
-        current_order (numpy.ndarray): 1-D array of indices.
-            The length should be the same as the dataset to sample
-            data from.
-        current_position (int): The current position of an iterator.
+    def __init__(self, seed=None):
+        # Use a distinct RandomState in the thread
+        # for deterministic random number generation.
+        # To support 32-bit platform and numpy < 1.11,
+        # the seed is taken in a verbose manner.
+        if seed is None:
+            seed = numpy.random.randint(2**31)
+        self._random = numpy.random.RandomState(seed)
 
-    Returns:
-        numpy.ndarray: 1-D array of indices. This is the order in which
-            examples are sampled from a dataset in the next epoch.
+    def __call__(self, current_order, current_position):
+        """The order sampler of an iterator when shuffle option is True.
 
-    """
-    return numpy.random.permutation(len(current_order))
+        Args:
+            current_order (numpy.ndarray): 1-D array of indices.
+                The length should be the same as the dataset to sample
+                data from.
+            current_position (int): The current position of an iterator.
+
+        Returns:
+            numpy.ndarray: 1-D array of indices. This is the order in which
+                examples are sampled from a dataset in the next epoch.
+
+        """
+        return self._random.permutation(len(current_order))
 
 
 def no_shuffle_order_sampler(current_order, current_position):
