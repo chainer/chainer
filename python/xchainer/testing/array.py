672c7c1bfedfb29cdbeb1721f81f8fbb4700908c
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-04-17 11:23:26 +0900

    Improve testing array utilities

--- python/xchainer/testing/array.py
+++ python/xchainer/testing/array.py
@@ -5,16 +5,16 @@ import xchainer
 # NumPy-like assertion functions that accept both NumPy and xChainer arrays
 
 
-def _check_xchainer_attributes(x):
-    '''Check basic conditions that are assumed to hold true for any given xChainer array.'''
-    if not isinstance(x, xchainer.Array):
-        return
+def _check_xchainer_array(x):
+    # Checks basic conditions that are assumed to hold true for any given xChainer array passed to assert_array_close and
+    # assert_array_equal.
+    assert isinstance(x, xchainer.Array)
     assert not x.is_grad_required()
 
 
-# TODO(hvy): Remove the following function if conversion from xchainer.Array to numpy.ndarray via buffer protocol supports device transfer.
-def _as_native(x):
+def _as_numpy(x):
     if isinstance(x, xchainer.Array):
+        # TODO(hvy): Use a function that convers an xChainer array to a NumPy array.
         return x.to_device('native:0')
     assert isinstance(x, numpy.ndarray) or numpy.isscalar(x)
     return x
@@ -34,14 +34,18 @@ def assert_allclose(x, y, rtol=1e-7, atol=0, equal_nan=True, err_msg='', verbose
              are appended to the error message.
     .. seealso:: :func:`numpy.testing.assert_allclose`
     """
-    _check_xchainer_attributes(x)
-    _check_xchainer_attributes(y)
+    def check_array(array):
+        if isinstance(array, xchainer.Array):
+            _check_xchainer_array(array)
+
+    check_array(x)
+    check_array(y)
 
     # TODO(sonots): Uncomment after strides compatibility between xChainer and NumPy is implemented.
     # assert x.strides == y.strides
 
     numpy.testing.assert_allclose(
-        _as_native(x), _as_native(y), rtol=rtol, atol=atol, equal_nan=equal_nan, err_msg=err_msg, verbose=verbose)
+        _as_numpy(x), _as_numpy(y), rtol=rtol, atol=atol, equal_nan=equal_nan, err_msg=err_msg, verbose=verbose)
 
 
 def assert_array_equal(x, y, err_msg='', verbose=True):
@@ -55,10 +59,14 @@ def assert_array_equal(x, y, err_msg='', verbose=True):
              are appended to the error message.
     .. seealso:: :func:`numpy.testing.assert_array_equal`
     """
-    _check_xchainer_attributes(x)
-    _check_xchainer_attributes(y)
+    def check_array(array):
+        if isinstance(array, xchainer.Array):
+            _check_xchainer_array(array)
+
+    check_array(x)
+    check_array(y)
 
     # TODO(sonots): Uncomment after strides compatibility between xChainer and NumPy is implemented.
     # assert x.strides == y.strides
 
-    numpy.testing.assert_array_equal(_as_native(x), _as_native(y), err_msg=err_msg, verbose=verbose)
+    numpy.testing.assert_array_equal(_as_numpy(x), _as_numpy(y), err_msg=err_msg, verbose=verbose)
