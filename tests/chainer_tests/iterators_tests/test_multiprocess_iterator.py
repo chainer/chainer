fafc2a67a89f9f219a939c82a8a628d897aca6c5
Yusuke Niitani <yuyuniitani@gmail.com>
2018-04-23 17:06:56 +0900

    add a test that uses two arguments passed to order_sampler

--- tests/chainer_tests/iterators_tests/test_multiprocess_iterator.py
+++ tests/chainer_tests/iterators_tests/test_multiprocess_iterator.py
@@ -450,6 +450,33 @@ class TestMultiprocessIteratorOrderSamplerEpochSize(unittest.TestCase):
                 sorted(batch1 + batch2 + batch3), [1, 1, 2, 2, 3, 3])
 
 
+class _NoSameIndicesOrderSampler(object):
+
+    def __init__(self, batchsize):
+        self.n_call = 0
+
+    def __call__(self, current_order, current_pos):
+        # all batches contain unique indices
+        remaining = current_order[current_pos:]
+        first = numpy.setdiff1d(numpy.arange(len(current_order)), remaining)
+        second = numpy.setdiff1d(numpy.arange(len(current_order)), first)
+        return numpy.concatenate((first, second))
+
+
+class TestNoSameIndicesOrderSampler(unittest.TestCase):
+
+    def test_no_same_indices_order_sampler(self):
+        dataset = [1, 2, 3, 4, 5, 6]
+        batchsize = 5
+
+        it = iterators.MultiprocessIterator(
+            dataset, batchsize,
+            order_sampler=_NoSameIndicesOrderSampler(batchsize))
+        for _ in range(5):
+            batch = it.next()
+            self.assertEqual(len(numpy.unique(batch)), batchsize)
+
+
 class _InvalidOrderSampler(object):
 
     def __init__(self):
