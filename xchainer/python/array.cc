587ee4c4b1a87764fc12cf233ad541a553aa2ca7
sonots <sonots@gmail.com>
2018-04-23 12:10:11 +0900

    list initialization is preferred

--- xchainer/python/array.cc
+++ xchainer/python/array.cc
@@ -92,21 +92,21 @@ py::array MakeNumpyArrayFromArray(const ArrayBodyPtr& self) {
     // TODO(sonots): Remove following workaround if pybind11's segv problem with zero-sized shape is fixed.
     // See https://github.com/pybind/pybind11/issues/1370
     if (array.shape().ndim() == 0) {
-        py::array py_array = py::array(
-                py::dtype{std::string(1, GetCharCode(array.dtype()))},
+        py::array py_array = py::array{
+                py::dtype{std::string{1, GetCharCode(array.dtype())}},
                 {1},
-                reinterpret_cast<uint8_t*>(array.raw_data()) + array.offset());  // NOLINT: reinterpret_cast
+                reinterpret_cast<uint8_t*>(array.raw_data()) + array.offset()};  // NOLINT: reinterpret_cast
         py_array.resize({});
         return py_array;
     }
 
-    return py::array(py::buffer_info(
+    return py::array{py::buffer_info{
             reinterpret_cast<uint8_t*>(array.raw_data()) + array.offset(),  // NOLINT: reinterpret_cast
             array.element_bytes(),
-            std::string(1, GetCharCode(array.dtype())),
+            std::string{1, GetCharCode(array.dtype())},
             array.ndim(),
             array.shape(),
-            array.strides()));
+            array.strides()}};
 }
 
 }  // namespace
