f36dc0155aca97deb27eef7c80a799c7b41ff4fa
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-04-27 12:02:08 +0900

    Remove unnecessary argument check

--- python/xchainer/creation/from_data.py
+++ python/xchainer/creation/from_data.py
@@ -4,8 +4,6 @@ import xchainer
 
 
 def fromfile(file, dtype=float, count=-1, sep='', device=None):
-    if device is None:
-        device = xchainer.get_default_device()
     if isinstance(dtype, xchainer.dtype):
         dtype = dtype.name
     return xchainer.array(numpy.fromfile(file, dtype, count, sep), device=device)
