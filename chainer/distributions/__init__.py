d6850cac645e1055184bf5188d23cd1f0f921004
yoshikawa <yoshikawa@preferred.jp>
2018-04-27 14:44:26 +0900

    [add] Laplace Distribution

--- chainer/distributions/__init__.py
+++ chainer/distributions/__init__.py
@@ -3,4 +3,5 @@
 from chainer.distributions.Bernoulli import Bernoulli  # NOQA
 from chainer.distributions.Beta import Beta  # NOQA
 from chainer.distributions.Gamma import Gamma  # NOQA
+from chainer.distributions.Laplace import Laplace  # NOQA
 from chainer.distributions.Normal import Normal  # NOQA
