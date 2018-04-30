dd0014b74d178a384682c3e0b6e2fc0eba0f6055
yoshikawa <yoshikawa@preferred.jp>
2018-05-01 07:56:41 +0900

    [add] Cauchy Distribution

---
+++ chainer/distributions/Cauchy.py
@@ -0,0 +1,149 @@
+from chainer.backends import cuda
+from chainer import Distribution
+from chainer.functions.array import expand_dims
+from chainer.functions.array import repeat
+from chainer.functions.math import exponential
+from chainer.functions.math import trigonometric
+import numpy
+
+
+class Cauchy(Distribution):
+
+    """Cauchy Distribution.
+
+    Args:
+        loc(:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
+        :class:`cupy.ndarray`): Parameter of distribution representing the \
+        location :math:`\\mu`.
+        scale(:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
+        :class:`cupy.ndarray`): Parameter of distribution representing the \
+        scale :math:`\\log(\\sigma^2)`.
+
+    """
+
+    def __init__(self, loc, scale):
+        self.loc, self.scale = loc, scale
+
+    def __copy__(self):
+        return self._copy_to(Cauchy(self.loc, self.scale))
+
+    @property
+    def batch_shape(self):
+        return self.loc.shape
+
+    def cdf(self, x):
+        """Returns Cumulative Distribution Function for a input variable.
+
+        Args:
+            x(:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
+            :class:`cupy.ndarray`): Input variable representing a random
+            variable.
+
+        Returns:
+            ~chainer.Variable: Output variable representing Cumulative
+            Distribution Function.
+
+        """
+        return 1 / numpy.pi \
+            * trigonometric.arctan((x - self.loc) / self.scale) + 0.5
+
+    @property
+    def entropy(self):
+        """Returns entropy.
+
+        Returns:
+            Output Variable representing entropy.
+
+        """
+        return exponential.log(4 * numpy.pi * self.scale)
+
+    @property
+    def event_shape(self):
+        return ()
+
+    def icdf(self, x):
+        """Returns Inverse Cumulative Distribution Function for a input Variable.
+
+        Args:
+            x(:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
+            :class:`cupy.ndarray`): Input variable representing a random
+            variable.
+
+        Returns:
+            ~chainer.Variable: Output variable representing Inverse Cumulative
+            Distribution Function.
+
+        """
+        return self.loc + self.scale * trigonometric.tan((x - 0.5) * numpy.pi)
+
+    @property
+    def _is_gpu(self):
+        return isinstance(self.loc, cuda.ndarray)
+
+    def log_prob(self, x):
+        """Returns logarithm logarithm of probability for a input variable.
+
+        Args:
+            x: Input variable representing a random variable.
+
+        Returns:
+            Output variable representing logarithm of probability.
+
+        """
+        return - numpy.log(numpy.pi) + exponential.log(self.scale) \
+            - exponential.log((x - self.loc)**2 + self.scale**2)
+
+    @property
+    def mean(self):
+        """Returns mean value.
+
+        Returns:
+            ~chainer.Variable: Output variable representing mean value.
+
+        """
+        return self.loc * numpy.nan
+
+    def _sample_n(self, n):
+        """Samples from this distribution.
+
+        Args:
+            n(`int`): Sampling size.
+
+        Returns:
+            ~chainer.Variable: Output variable representing sampled random
+            variable.
+        """
+        if self._is_gpu:
+            eps = numpy.random.standard_cauchy(
+                (n,)+self.loc.shape).astype(numpy.float32)
+            eps = cuda.to_gpu(eps, cuda.get_device_from_array(self.loc).id)
+        else:
+            eps = numpy.random.standard_cauchy(
+                (n,)+self.loc.shape).astype(numpy.float32)
+
+        noise = repeat.repeat(
+            expand_dims.expand_dims(self.scale, axis=0), n, axis=0) * eps
+        noise += repeat.repeat(expand_dims.expand_dims(
+            self.loc, axis=0), n, axis=0)
+
+        return noise
+
+    @property
+    def support(self):
+        """Returns support.
+
+        Returns:
+            string: Output string that means support of this distribution.
+
+        """
+        return 'real'
+
+    @property
+    def variance(self):
+        """Returns variance.
+
+        Returns:
+            ~chainer.Variable: Output variable representing variance.
+
+        """
+        return self.loc * numpy.nan
--- chainer/distributions/__init__.py
+++ chainer/distributions/__init__.py
@@ -2,6 +2,7 @@
 
 from chainer.distributions.Bernoulli import Bernoulli  # NOQA
 from chainer.distributions.Beta import Beta  # NOQA
+from chainer.distributions.Cauchy import Cauchy   # NOQA
 from chainer.distributions.Gamma import Gamma  # NOQA
 from chainer.distributions.Laplace import Laplace  # NOQA
 from chainer.distributions.Normal import Normal  # NOQA
