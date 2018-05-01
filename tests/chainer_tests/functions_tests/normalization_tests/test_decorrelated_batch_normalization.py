87640e9cadeac6f1ef406bdcf83060c3e63b7c73
Tommi Kerola <tommi@preferred.jp>
2018-05-01 17:57:47 +0900

    Add group whitening

--- tests/chainer_tests/functions_tests/normalization_tests/test_decorrelated_batch_normalization.py
+++ tests/chainer_tests/functions_tests/normalization_tests/test_decorrelated_batch_normalization.py
@@ -1,7 +1,6 @@
 import unittest
 
 import numpy
-import six
 
 from chainer.backends import cuda
 from chainer import functions
@@ -16,45 +15,57 @@ def _to_fcontiguous(arrays):
 
 
 def _decorrelated_batch_normalization(args):
-    x, mean, projection = args
+    x, mean, projection, groups = args
     spatial_ndim = len(x.shape[2:])
     spatial_axis = tuple(range(2, 2 + spatial_ndim))
     b, c = x.shape[:2]
+    g = groups
+    C = c // g
 
-    x_hat = x.transpose((1, 0) + spatial_axis).reshape((c, -1))
+    x = x.reshape((b * g, C, ) + x.shape[2:])
+    x_hat = x.transpose((1, 0) + spatial_axis).reshape((C, -1))
 
     y_hat = projection.dot(x_hat - mean[:, None])
 
-    y = y_hat.reshape((c, b,) + x.shape[2:]).transpose((1, 0) + spatial_axis)
+    y = y_hat.reshape((C, b * g,) + x.shape[2:]).transpose(
+        (1, 0) + spatial_axis)
+    y = y.reshape((-1, c, ) + x.shape[2:])
     return y
 
 
-def _calc_projection(x, mean, eps):
+def _calc_projection(x, mean, eps, groups):
     spatial_ndim = len(x.shape[2:])
     spatial_axis = tuple(range(2, 2 + spatial_ndim))
     b, c = x.shape[:2]
-    m = b * numpy.prod(numpy.array([x.shape[i] for i in spatial_axis]))
+    g = groups
+    C = c // g
+    m = b * g
+    for i in spatial_axis:
+        m *= x.shape[i]
 
-    x_hat = x.transpose((1, 0) + spatial_axis).reshape((c, -1))
+    x = x.reshape((b * g, C, ) + x.shape[2:])
+    x_hat = x.transpose((1, 0) + spatial_axis).reshape((C, -1))
 
     mean = x_hat.mean(axis=1)
     x_hat = x_hat - mean[:, None]
 
-    cov = x_hat.dot(x_hat.T) / m + eps * numpy.eye(c, dtype=x.dtype)
+    cov = x_hat.dot(x_hat.T) / m + eps * numpy.eye(C, dtype=x.dtype)
     eigvals, eigvectors = numpy.linalg.eigh(cov)
     projection = eigvectors.dot(numpy.diag(eigvals ** -0.5)).dot(eigvectors.T)
     return projection
 
 
 @testing.parameterize(*(testing.product({
-    'n_channels': [1, 3],
+    'n_channels': [8],
     'ndim': [0, 2],
+    'groups': [1, 2],
     'eps': [2e-5, 5e-1],
     'dtype': [numpy.float32],
     'c_contiguous': [True, False],
 }) + testing.product({
-    'n_channels': [3],
+    'n_channels': [8],
     'ndim': [1],
+    'groups': [1, 2],
     'eps': [2e-5, 5e-1],
     # NOTE(tommi): xp.linalg.eigh does not support float16
     'dtype': [numpy.float32, numpy.float64],
@@ -70,18 +81,20 @@ def _calc_projection(x, mean, eps):
 class TestDecorrelatedBatchNormalization(unittest.TestCase):
 
     def setUp(self):
-        n_channels = self.n_channels
+        C = self.n_channels // self.groups
         dtype = self.dtype
         ndim = self.ndim
 
         head_ndim = 2
-        shape = (5, n_channels) + (2,) * ndim
+        shape = (5, self.n_channels) + (2,) * ndim
         x = numpy.random.uniform(-1, 1, shape).astype(dtype)
         gy = numpy.random.uniform(-1, 1, shape).astype(dtype)
 
-        aggr_axes = (0,) + tuple(six.moves.range(head_ndim, x.ndim))
-        mean = x.mean(axis=aggr_axes)
-        projection = _calc_projection(x, mean, self.eps)
+        spatial_axis = tuple(range(head_ndim, x.ndim))
+        x_hat = x.reshape((5 * self.groups, C, ) + x.shape[2:])
+        x_hat = x_hat.transpose((1, 0) + spatial_axis).reshape((C, -1))
+        mean = x_hat.mean(axis=1)
+        projection = _calc_projection(x, mean, self.eps, self.groups)
 
         self.decay = 0.9
         self.mean = mean
@@ -96,10 +109,13 @@ class TestDecorrelatedBatchNormalization(unittest.TestCase):
             self.check_forward_options = {'atol': 1e-2, 'rtol': 1e-2}
             self.check_backward_options = {
                 'dtype': numpy.float64, 'atol': 1e-2, 'rtol': 1e-2}
+        elif self.dtype == numpy.float32:
+            self.check_backward_options = {
+                'dtype': numpy.float64, 'atol': 1e-2, 'rtol': 1e-2}
 
     def forward_cpu(self, inputs):
         y_expect = _decorrelated_batch_normalization(
-            inputs + [self.mean, self.projection])
+            inputs + [self.mean, self.projection, self.groups])
         return y_expect,
 
     def check_forward(self, inputs, backend_config):
@@ -112,7 +128,8 @@ class TestDecorrelatedBatchNormalization(unittest.TestCase):
 
         with backend_config:
             y = functions.decorrelated_batch_normalization(
-                *inputs, decay=self.decay, eps=self.eps)
+                *inputs, groups=self.groups, decay=self.decay,
+                eps=self.eps)
         assert y.data.dtype == self.dtype
 
         testing.assert_allclose(
@@ -131,7 +148,8 @@ class TestDecorrelatedBatchNormalization(unittest.TestCase):
 
         def f(*inputs):
             y = functions.decorrelated_batch_normalization(
-                *inputs, decay=self.decay, eps=self.eps)
+                *inputs, groups=self.groups, decay=self.decay,
+                eps=self.eps)
             return y,
 
         with backend_config:
@@ -144,14 +162,16 @@ class TestDecorrelatedBatchNormalization(unittest.TestCase):
 
 
 @testing.parameterize(*(testing.product({
-    'n_channels': [1, 3],
+    'n_channels': [8],
     'ndim': [0, 1, 2],
+    'groups': [1, 2],
     'eps': [2e-5, 5e-1],
     'dtype': [numpy.float32],
     'c_contiguous': [True, False],
 }) + testing.product({
-    'n_channels': [3],
+    'n_channels': [8],
     'ndim': [1],
+    'groups': [1, 2],
     'eps': [2e-5, 5e-1],
     'dtype': [numpy.float16, numpy.float32, numpy.float64],
     'c_contiguous': [True, False],
@@ -166,14 +186,14 @@ class TestDecorrelatedBatchNormalization(unittest.TestCase):
 class TestFixedDecorrelatedBatchNormalization(unittest.TestCase):
 
     def setUp(self):
-        c = self.n_channels
+        C = self.n_channels // self.groups
         dtype = self.dtype
         ndim = self.ndim
 
-        shape = (5, c) + (2,) * ndim
+        shape = (5, self.n_channels) + (2,) * ndim
         x = numpy.random.uniform(-1, 1, shape).astype(dtype)
-        mean = numpy.random.uniform(-1, 1, c).astype(dtype)
-        projection = numpy.random.uniform(0.5, 1, (c, c)).astype(dtype)
+        mean = numpy.random.uniform(-1, 1, C).astype(dtype)
+        projection = numpy.random.uniform(0.5, 1, (C, C)).astype(dtype)
 
         self.inputs = [x, mean, projection]
 
@@ -182,7 +202,7 @@ class TestFixedDecorrelatedBatchNormalization(unittest.TestCase):
             self.check_forward_options = {'atol': 1e-2, 'rtol': 1e-2}
 
     def forward_cpu(self, inputs):
-        y_expect = _decorrelated_batch_normalization(inputs)
+        y_expect = _decorrelated_batch_normalization(inputs + [self.groups, ])
         return y_expect,
 
     def check_forward(self, inputs, backend_config):
@@ -194,7 +214,8 @@ class TestFixedDecorrelatedBatchNormalization(unittest.TestCase):
             inputs = _to_fcontiguous(inputs)
 
         with backend_config:
-            y = functions.fixed_decorrelated_batch_normalization(*inputs)
+            y = functions.fixed_decorrelated_batch_normalization(
+                *inputs, groups=self.groups)
         assert y.data.dtype == self.dtype
 
         testing.assert_allclose(
