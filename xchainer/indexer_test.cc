5e629d74bcab508c394589df19f42d0c7c8ed90f
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-05-03 01:18:33 +0900

    Add tests

--- xchainer/indexer_test.cc
+++ xchainer/indexer_test.cc
@@ -17,14 +17,14 @@ std::string ToString(const Indexer<kNdim>& indexer) {
 }
 
 TEST(IndexerTest, Rank0) {
-    Indexer<> indexer({});
+    Indexer<0> indexer({});
     EXPECT_EQ(0, indexer.ndim());
     EXPECT_EQ(1, indexer.total_size());
     EXPECT_EQ("Indexer(shape=())", ToString(indexer));
 }
 
 TEST(IndexerTest, Rank1) {
-    Indexer<> indexer({3});
+    Indexer<1> indexer({3});
     EXPECT_EQ(1, indexer.ndim());
     EXPECT_EQ(3, indexer.total_size());
     EXPECT_EQ(3, indexer.shape()[0]);
@@ -32,6 +32,33 @@ TEST(IndexerTest, Rank1) {
 }
 
 TEST(IndexerTest, Rank3) {
+    Indexer<3> indexer({2, 3, 4});
+    EXPECT_EQ(3, indexer.ndim());
+    EXPECT_EQ(2 * 3 * 4, indexer.total_size());
+
+    EXPECT_EQ(2, indexer.shape()[0]);
+    EXPECT_EQ(3, indexer.shape()[1]);
+    EXPECT_EQ(4, indexer.shape()[2]);
+
+    EXPECT_EQ("Indexer(shape=(2, 3, 4))", ToString(indexer));
+}
+
+TEST(DynamicIndexerTest, Rank0) {
+    Indexer<> indexer({});
+    EXPECT_EQ(0, indexer.ndim());
+    EXPECT_EQ(1, indexer.total_size());
+    EXPECT_EQ("Indexer(shape=())", ToString(indexer));
+}
+
+TEST(DynamicIndexerTest, Rank1) {
+    Indexer<> indexer({3});
+    EXPECT_EQ(1, indexer.ndim());
+    EXPECT_EQ(3, indexer.total_size());
+    EXPECT_EQ(3, indexer.shape()[0]);
+    EXPECT_EQ("Indexer(shape=(3,))", ToString(indexer));
+}
+
+TEST(DynamicIndexerTest, Rank3) {
     Indexer<> indexer({2, 3, 4});
     EXPECT_EQ(3, indexer.ndim());
     EXPECT_EQ(2 * 3 * 4, indexer.total_size());
