ea8c6cfdce889bbc92edf317e0818ced018881ab
Akira Naruse <anaruse@nvidia.com>
2018-05-07 11:04:59 +0900

    Add documents, checks, etc.

--- tests/chainer_tests/utils_tests/test_sparse.py
+++ tests/chainer_tests/utils_tests/test_sparse.py
@@ -27,6 +27,21 @@ class TestCooMatrix(unittest.TestCase):
             x_shape = self.shape
         x0 = _setup_tensor(.5, 1, x_shape, self.dtype, .75)
         sp_x = utils.to_coo(x0)
+        if not sp_x.data.shape == sp_x.row.shape == sp_x.col.shape:
+            raise ValueError('shape of data/row/col must be identical.')
+        max_nnz = self.shape[0] * self.shape[1]
+        if self.nbatch > 0:
+            if sp_x.data.ndim != 2:
+                raise ValueError('ndim of data/row/col must be two.')
+            if sp_x.data.shape[0] != self.nbatch:
+                raise ValueError('batch size is wrong.')
+            if not 0 <= sp_x.data.shape[1] <= max_nnz:
+                raise ValueError('num non-zero element is wrong.')
+        else:
+            if sp_x.data.ndim != 1:
+                raise ValueError('ndim of data/row/col must be one.')
+            if not 0 <= sp_x.data.shape[0] <= max_nnz:
+                raise ValueError('num non-zero element is wrong.')
         x1 = sp_x.to_dense()
         numpy.testing.assert_array_equal(x0, x1)
 
