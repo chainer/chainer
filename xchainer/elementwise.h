079b148a3f749110385ae4162940cc74d777b838
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-05-09 02:46:04 +0900

    Elementwise dimension reduction

---
+++ xchainer/elementwise.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <algorithm>
+#include <cstdint>
+#include <tuple>
+
+#include "xchainer/array.h"
+#include "xchainer/axes.h"
+#include "xchainer/shape.h"
+#include "xchainer/strides.h"
+
+namespace xchainer {
+namespace elementwise_detail {
+
+// Returns true if dimension i can be compressed for all strides.
+template <typename... PackedStrides>
+inline bool IsCompressibleDimension(size_t i, const Shape& shape, const PackedStrides&... strides) {
+    // If strides[i] * shape[i] != strides[i - 1] for any i for any strides, return false.
+    // std::max seems to be faster than variadic function recursions.
+    return !static_cast<bool>(std::max({(strides[i] * shape[i] != strides[i - 1])...}));
+}
+
+}  // namespace elementwise_detail
+
+// Returns a subset of strides with elements corresponding to given axes.
+// It is used in conjunction with ReducedShape to obtain the reduced strides.
+inline Strides Reduce(const Strides& strides, const Axes& keep) {
+    Strides reduced{};
+    std::transform(keep.begin(), keep.end(), std::back_inserter(reduced), [&strides](int8_t axis) { return strides[axis]; });
+    return reduced;
+}
+
+// Returns a reduced shape with indices of the axes that were kept.
+template <typename... Arrays>
+std::pair<Shape, Axes> ReducedShape(const Array& array, Arrays&&... arrays) {
+    Shape reduced{};
+    Axes keep{};
+
+    const Shape& shape = array.shape();
+    switch (int8_t ndim = shape.ndim()) {
+        case 0:
+            reduced = shape;
+            break;
+        case 1:
+            reduced = shape;
+            keep.emplace_back(0);
+            break;
+        default:
+            Shape compressed = shape;
+            for (int8_t i = 1; i < ndim; ++i) {
+                if (compressed[i - 1] == 1) {
+                    continue;
+                } else if (elementwise_detail::IsCompressibleDimension(i, compressed, array.strides(), arrays.strides()...)) {
+                    compressed[i] *= compressed[i - 1];
+                    compressed[i - 1] = 1;
+                    continue;
+                }
+                keep.emplace_back(i - 1);
+            }
+            if (compressed.back() != 1) {
+                keep.emplace_back(ndim - 1);
+            }
+
+            if (keep.ndim() == ndim) {
+                // No dimensions could be reduced.
+                reduced = compressed;
+                break;
+            }
+            // Reduce compressed dimensions.
+            std::copy_if(compressed.begin(), compressed.end(), std::back_inserter(reduced), [](int64_t dim) { return dim != 1; });
+            break;
+    }
+    assert(reduced.ndim() == keep.ndim());
+    return std::make_pair(reduced, keep);
+}
+
+}  // namespace xchainer
---
+++ xchainer/elementwise_test.cc
@@ -0,0 +1,114 @@
+#include "xchainer/elementwise.h"
+
+#include <tuple>
+
+#include <gtest/gtest.h>
+
+#include "xchainer/array.h"
+#include "xchainer/axes.h"
+#include "xchainer/shape.h"
+#include "xchainer/strides.h"
+#include "xchainer/testing/array.h"
+#include "xchainer/testing/context_session.h"
+
+namespace xchainer {
+namespace {
+
+TEST(ElementwiseTest, Reduce) {
+    {
+        Strides reduced = Reduce({3, 2, 5, 4}, {0, 1, 2, 3});
+        EXPECT_EQ(4, reduced.ndim());
+        EXPECT_EQ(3, reduced[0]);
+        EXPECT_EQ(2, reduced[1]);
+        EXPECT_EQ(5, reduced[2]);
+        EXPECT_EQ(4, reduced[3]);
+    }
+    {
+        Strides reduced = Reduce({3, 2, 5, 4}, {1, 2});
+        EXPECT_EQ(2, reduced.ndim());
+        EXPECT_EQ(2, reduced[0]);
+        EXPECT_EQ(5, reduced[1]);
+    }
+    {
+        Strides reduced = Reduce({3, 2, 5, 4}, {});
+        EXPECT_EQ(0, reduced.ndim());
+    }
+    {
+        Strides reduced = Reduce({}, {});
+        EXPECT_EQ(0, reduced.ndim());
+    }
+}
+
+TEST(ElementwiseTest, CompressAllDims) {
+    testing::ContextSession context_session;
+    Shape shape{3, 2, 5, 4};
+    Array a = testing::BuildArray(shape).WithLinearData<float>();
+
+    Shape reduced{};
+    Axes keep{};
+    std::tie(reduced, keep) = ReducedShape(a);
+
+    EXPECT_EQ(1, reduced.ndim());
+    EXPECT_EQ(shape[0] * shape[1] * shape[2] * shape[3], reduced[0]);
+    EXPECT_EQ(1, keep.ndim());
+    EXPECT_EQ(3, keep[0]);
+}
+
+TEST(ElementwiseTest, CompressSomeDims) {
+    testing::ContextSession context_session;
+    Shape shape{3, 2, 5, 4};
+    Array a = testing::BuildArray(shape).WithLinearData<float>().WithPadding({0, 2, 0, 0});
+
+    Shape reduced{};
+    Axes keep{};
+    std::tie(reduced, keep) = ReducedShape(a);
+
+    EXPECT_EQ(2, reduced.ndim());
+    EXPECT_EQ(shape[0] * shape[1], reduced[0]);
+    EXPECT_EQ(shape[2] * shape[3], reduced[1]);
+    EXPECT_EQ(2, keep.ndim());
+    EXPECT_EQ(1, keep[0]);
+    EXPECT_EQ(3, keep[1]);
+}
+
+TEST(ElementwiseTest, CompressUnitLengthDim) {
+    testing::ContextSession context_session;
+    Shape shape{3, 2, 1, 4};
+    Array a = testing::BuildArray(shape).WithLinearData<float>().WithPadding(1);
+
+    Shape reduced{};
+    Axes keep{};
+    std::tie(reduced, keep) = ReducedShape(a);
+
+    EXPECT_EQ(3, reduced.ndim());
+    EXPECT_EQ(shape[0], reduced[0]);
+    EXPECT_EQ(shape[1], reduced[1]);
+    EXPECT_EQ(shape[3], reduced[2]);
+    EXPECT_EQ(3, keep.ndim());
+    EXPECT_EQ(0, keep[0]);
+    EXPECT_EQ(1, keep[1]);
+    EXPECT_EQ(3, keep[2]);
+}
+
+TEST(ElementwiseTest, CompressMultipleArrayDims) {
+    testing::ContextSession context_session;
+    Shape shape{3, 2, 5, 4};
+    Array a = testing::BuildArray(shape).WithLinearData<float>().WithPadding({0, 2, 0, 0});
+    Array b = testing::BuildArray(shape).WithLinearData<float>().WithPadding({0, 0, 1, 0});
+
+    Shape reduced{};
+    Axes keep{};
+    std::tie(reduced, keep) = ReducedShape(a, b);
+
+    EXPECT_EQ(3, reduced.ndim());
+    EXPECT_EQ(shape[0] * shape[1], reduced[0]);
+    EXPECT_EQ(shape[2], reduced[1]);
+    EXPECT_EQ(shape[3], reduced[2]);
+    EXPECT_EQ(3, keep.ndim());
+    EXPECT_EQ(1, keep[0]);
+    EXPECT_EQ(2, keep[1]);
+    EXPECT_EQ(3, keep[2]);
+}
+
+}  // namespace
+}  // namespace xchainer
