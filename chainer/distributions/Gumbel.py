2739f01dca5351f480829f3d69c7c30196648afd
yoshikawa <yoshikawa@preferred.jp>
2018-05-09 09:24:52 +0900

    [change] refactor test_gumbel

--- chainer/distributions/Gumbel.py
+++ chainer/distributions/Gumbel.py
@@ -1,8 +1,7 @@
 import chainer
 from chainer.backends import cuda
 from chainer import Distribution
-from chainer.functions.array import expand_dims
-from chainer.functions.array import repeat
+from chainer.functions.array import broadcast
 from chainer.functions.math import exponential
 import numpy
 
@@ -53,7 +52,7 @@ class Gumbel(Distribution):
 
     @property
     def _is_gpu(self):
-        return isinstance(self.loc, cuda.ndarray)
+        return isinstance(self.loc.data, cuda.ndarray)
 
     def log_prob(self, x):
         """Returns logarithm logarithm of probability for a input variable.
@@ -65,8 +64,10 @@ class Gumbel(Distribution):
             Output variable representing logarithm of probability.
 
         """
-        y = (x - self.loc) / self.scale
-        return - exponential.log(self.scale) - y - exponential.exp(-y)
+        bl = broadcast.broadcast_to(self.loc, x.shape)
+        bs = broadcast.broadcast_to(self.scale, x.shape)
+        y = (x - bl) / bs
+        return - exponential.log(bs) - y - exponential.exp(-y)
 
     @property
     def mean(self):
@@ -91,15 +92,14 @@ class Gumbel(Distribution):
         if self._is_gpu:
             eps = numpy.random.gumbel(
                 size=(n,)+self.loc.shape).astype(numpy.float32)
-            eps = cuda.to_gpu(eps, cuda.get_device_from_array(self.k).id)
+            eps = cuda.to_gpu(
+                eps, cuda.get_device_from_array(self.loc.data).id)
         else:
             eps = numpy.random.gumbel(
                 size=(n,)+self.loc.shape).astype(numpy.float32)
 
-        noise = repeat.repeat(
-            expand_dims.expand_dims(self.scale, axis=0), n, axis=0) * eps
-        noise += repeat.repeat(expand_dims.expand_dims(
-            self.loc, axis=0), n, axis=0)
+        noise = broadcast.broadcast_to(self.scale, eps.shape) * eps
+        noise += broadcast.broadcast_to(self.loc, eps.shape)
 
         return noise
 
--- tests/chainer_tests/distributions_tests/test_gumbel.py
+++ tests/chainer_tests/distributions_tests/test_gumbel.py
@@ -6,52 +6,24 @@ import numpy
 from scipy import stats
 
 
-@testing.parameterize(*testing.product({
-    'shape': [(3, 2), (1,)],
-}))
+def params_init(shape):
+    loc = numpy.random.uniform(-1, 1, shape).astype(numpy.float32)
+    scale = numpy.exp(numpy.random.uniform(-1, 1, shape)).astype(numpy.float32)
+    params = {"loc": loc, "scale": scale}
+    sp_params = {"loc": loc, "scale": scale}
+    return params, sp_params
+
+
+def sample_for_test(shape):
+    smp = numpy.random.normal(size=shape).astype(numpy.float32)
+    return smp
+
+tests = set(["batch_shape", "entropy", "event_shape", "log_prob",
+             "mean", "sample", "support", "variance"])
+
+
+@testing.distribution_unittest(distributions.Gumbel, stats.gumbel_r,
+                               params_init, sample_for_test,
+                               tests=tests)
 class TestGumbel(unittest.TestCase):
-    def setUp(self):
-        self.loc = numpy.random.uniform(
-            -1, 1, self.shape).astype(numpy.float32)
-        self.scale = numpy.exp(numpy.random.uniform(
-            -1, 1, self.shape)).astype(numpy.float32)
-        self.dist = distributions.Gumbel(self.loc, self.scale)
-        self.sp_dist = stats.gumbel_r
-
-    def test_batch_shape(self):
-        self.assertEqual(self.dist.batch_shape, self.shape)
-
-    def test_entropy(self):
-        ent1 = self.dist.entropy.data
-        ent2 = self.sp_dist.entropy(self.loc, self.scale)
-        testing.assert_allclose(ent1, ent2,
-                                atol=1e-2, rtol=1e-2)
-
-    def test_event_shape(self):
-        self.assertEqual(self.dist.event_shape, ())
-
-    def test_log_prob(self):
-        smp = numpy.random.normal(size=self.shape).astype(numpy.float32)
-        log_prob1 = self.dist.log_prob(smp).data
-        log_prob2 = self.sp_dist.logpdf(smp, self.loc, self.scale)
-        testing.assert_allclose(log_prob1, log_prob2)
-
-    def test_mean(self):
-        mean1 = self.dist.mean.data
-        mean2 = self.sp_dist.mean(self.loc, self.scale)
-        testing.assert_allclose(mean1, mean2)
-
-    def test_sample(self):
-        smp1 = self.dist.sample(shape=(1000000)).data
-        smp2 = self.sp_dist.rvs(self.loc, self.scale,
-                                size=(1000000,)+self.shape)
-        testing.assert_allclose(smp1.mean(axis=0), smp2.mean(axis=0),
-                                atol=1e-2, rtol=1e-2)
-
-    def test_support(self):
-        self.assertEqual(self.dist.support, "real")
-
-    def test_variance(self):
-        variance1 = self.dist.variance.data
-        variance2 = self.sp_dist.var(self.loc, self.scale)
-        testing.assert_allclose(variance1, variance2)
+    pass
