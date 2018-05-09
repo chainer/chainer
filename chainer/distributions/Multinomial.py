36487401a3b5d3fdcaee5b249e87bcf47163405e
yoshikawa <yoshikawa@preferred.jp>
2018-05-09 10:07:52 +0900

    [change] change test_multinomial

--- chainer/distributions/Multinomial.py
+++ chainer/distributions/Multinomial.py
@@ -42,7 +42,7 @@ class Multinomial(Distribution):
 
     @property
     def _is_gpu(self):
-        return isinstance(self.p, cuda.ndarray)
+        return isinstance(self.p.data, cuda.ndarray)
 
     def log_prob(self, x):
         """Returns logarithm logarithm of probability for a input variable.
@@ -83,17 +83,21 @@ class Multinomial(Distribution):
             ~chainer.Variable: Output variable representing sampled random
             variable.
         """
-        obo_p = self.p.data.reshape(-1, self.p.shape[-1])
-        obo_n = numpy.broadcast_to(self.n.data, self.batch_shape).reshape(-1)
         if self._is_gpu:
+            obo_p = cuda.to_cpu(self.p.data).reshape(-1, self.p.shape[-1])
+            obo_n = numpy.broadcast_to(
+                cuda.to_cpu(self.n.data), self.batch_shape).reshape(-1)
             eps = [numpy.random.multinomial(
                 one_n, one_p, size=n) for one_n, one_p
                 in zip(obo_n, obo_p)]
             eps = numpy.stack(eps)
             eps = numpy.swapaxes(eps, 0, 1)
             eps = eps.reshape((n,)+self.batch_shape+(self.p.shape[-1],))
-            eps = cuda.to_gpu(eps, cuda.get_device_from_array(self.p).id)
+            eps = cuda.to_gpu(eps, cuda.get_device_from_array(self.p.data).id)
         else:
+            obo_p = self.p.data.reshape(-1, self.p.shape[-1])
+            obo_n = numpy.broadcast_to(
+                self.n.data, self.batch_shape).reshape(-1)
             eps = [numpy.random.multinomial(
                 one_n, one_p, size=n) for one_n, one_p
                 in zip(obo_n, obo_p)]
