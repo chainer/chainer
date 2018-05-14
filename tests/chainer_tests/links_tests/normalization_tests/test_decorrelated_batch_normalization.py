53bc8d00778974ded2212374df2a51b591446d73
Tommi Kerola <tommi@preferred.jp>
2018-05-14 17:34:53 +0900

    Fix link tests

--- tests/chainer_tests/links_tests/normalization_tests/test_decorrelated_batch_normalization.py
+++ tests/chainer_tests/links_tests/normalization_tests/test_decorrelated_batch_normalization.py
@@ -1,7 +1,6 @@
 import unittest
 
 import numpy
-import six
 
 import chainer
 from chainer.backends import cuda
@@ -13,37 +12,49 @@ from chainer.testing import condition
 
 
 def _decorrelated_batch_normalization(args):
-    x, mean, projection = args
+    x, mean, projection, groups = args
     spatial_ndim = len(x.shape[2:])
     spatial_axis = tuple(range(2, 2 + spatial_ndim))
     b, c = x.shape[:2]
+    g = groups
+    C = c // g
 
-    x_hat = x.transpose((1, 0) + spatial_axis).reshape((c, -1))
+    x = x.reshape((b * g, C, ) + x.shape[2:])
+    x_hat = x.transpose((1, 0) + spatial_axis).reshape((C, -1))
 
     y_hat = projection.dot(x_hat - mean[:, None])
 
-    y = y_hat.reshape((c, b,) + x.shape[2:]).transpose((1, 0) + spatial_axis)
+    y = y_hat.reshape((C, b * g,) + x.shape[2:]).transpose(
+        (1, 0) + spatial_axis)
+    y = y.reshape((-1, c, ) + x.shape[2:])
     return y
 
 
-def _calc_projection(x, mean, eps):
+def _calc_projection(x, mean, eps, groups):
     spatial_ndim = len(x.shape[2:])
     spatial_axis = tuple(range(2, 2 + spatial_ndim))
     b, c = x.shape[:2]
-    m = b * numpy.prod(numpy.array([x.shape[i] for i in spatial_axis]))
+    g = groups
+    C = c // g
+    m = b * g
+    for i in spatial_axis:
+        m *= x.shape[i]
 
-    x_hat = x.transpose((1, 0) + spatial_axis).reshape((c, -1))
+    x = x.reshape((b * g, C, ) + x.shape[2:])
+    x_hat = x.transpose((1, 0) + spatial_axis).reshape((C, -1))
 
     mean = x_hat.mean(axis=1)
     x_hat = x_hat - mean[:, None]
 
-    cov = x_hat.dot(x_hat.T) / m + eps * numpy.eye(c, dtype=x.dtype)
+    cov = x_hat.dot(x_hat.T) / m + eps * numpy.eye(C, dtype=x.dtype)
     eigvals, eigvectors = numpy.linalg.eigh(cov)
     projection = eigvectors.dot(numpy.diag(eigvals ** -0.5)).dot(eigvectors.T)
     return projection
 
 
 @testing.parameterize(*(testing.product({
+    'n_channels': [8],
+    'groups': [1, 2],
     'test': [True, False],
     'ndim': [0, 2],
     # NOTE(tommi): xp.linalg.eigh does not support float16
@@ -52,30 +63,40 @@ def _calc_projection(x, mean, eps):
 class BatchNormalizationTest(unittest.TestCase):
 
     def setUp(self):
-        self.aggr_axes = (0,) + tuple(six.moves.range(2, self.ndim + 2))
+        C = self.n_channels // self.groups
+        head_ndim = 2
 
-        self.link = links.DecorrelatedBatchNormalization(3, dtype=self.dtype)
+        self.link = links.DecorrelatedBatchNormalization(self.n_channels,
+                                                         groups=self.groups,
+                                                         dtype=self.dtype)
         self.link.cleargrads()
 
-        shape = (5, 3) + (2,) * self.ndim
+        shape = (5, self.n_channels) + (2,) * self.ndim
         self.x = numpy.random.uniform(-1, 1, shape).astype(self.dtype)
         self.gy = numpy.random.uniform(-1, 1, shape).astype(self.dtype)
 
         if self.test:
-            self.mean = numpy.random.uniform(-1, 1, (3,)).astype(self.dtype)
-            self.projection = numpy.random.uniform(0.5, 1, (3, 3)).astype(
+            self.mean = numpy.random.uniform(-1, 1, (C,)).astype(self.dtype)
+            self.projection = numpy.random.uniform(0.5, 1, (C, C)).astype(
                 self.dtype)
             self.link.expected_mean[...] = self.mean
             self.link.expected_projection[...] = self.projection
         else:
-            self.mean = self.x.mean(axis=self.aggr_axes)
+            spatial_axis = tuple(range(head_ndim, self.x.ndim))
+            x_hat = self.x.reshape((5 * self.groups, C, ) + self.x.shape[2:])
+            x_hat = x_hat.transpose((1, 0) + spatial_axis).reshape((C, -1))
+            self.mean = x_hat.mean(axis=1)
             self.projection = _calc_projection(self.x, self.mean,
-                                               self.link.eps)
+                                               self.link.eps, self.groups)
         self.check_forward_optionss = {'atol': 1e-4, 'rtol': 1e-3}
-        self.check_backward_optionss = {'atol': 1e-4, 'rtol': 1e-3}
+        self.check_backward_optionss = {'atol': 1e-4, 'rtol': 1e-3, 'dtype':
+                                        numpy.float64}
         if self.dtype == numpy.float16:
             self.check_forward_optionss = {'atol': 1e-3, 'rtol': 1e-2}
             self.check_backward_optionss = {'atol': 5e-1, 'rtol': 1e-1}
+        elif self.dtype == numpy.float32:
+            self.check_backward_options = {
+                'dtype': numpy.float64, 'atol': 1e-2, 'rtol': 1e-2}
 
     def check_forward(self, x_data):
         with chainer.using_config('train', not self.test):
@@ -84,7 +105,7 @@ class BatchNormalizationTest(unittest.TestCase):
             self.assertEqual(y.data.dtype, self.dtype)
 
         y_expect = _decorrelated_batch_normalization((
-            self.x, self.mean, self.projection))
+            self.x, self.mean, self.projection, self.groups))
 
         testing.assert_allclose(
             y_expect, y.data, **self.check_forward_optionss)
