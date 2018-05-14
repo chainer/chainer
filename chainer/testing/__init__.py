f3ad86401a2f9c37b170a034bdf053e583420b7a
yoshikawa <yoshikawa@preferred.jp>
2018-05-15 07:53:05 +0900

    [add] test without scipy

--- chainer/testing/__init__.py
+++ chainer/testing/__init__.py
@@ -3,6 +3,7 @@ from chainer.testing.distribution_test import distribution_unittest  # NOQA
 from chainer.testing.helper import assert_warns  # NOQA
 from chainer.testing.helper import patch  # NOQA
 from chainer.testing.helper import with_requires  # NOQA
+from chainer.testing.helper import without_requires  # NOQA
 from chainer.testing.parameterized import parameterize  # NOQA
 from chainer.testing.parameterized import product  # NOQA
 from chainer.testing.parameterized import product_dict  # NOQA
