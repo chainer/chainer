39d3cf479cfb49b98f3c6509659f6f992f99103e
Seiya Tokui <beam.web@gmail.com>
2018-05-16 14:49:30 +0900

    Move sum tests to test_math.py

--- tests/python_tests/routines_tests/test_math.py
+++ tests/python_tests/routines_tests/test_math.py
@@ -150,3 +150,69 @@ def test_itruediv(xp, device, shape, numeric_dtype):
     else:
         lhs /= rhs
     return lhs
+
+
+# TODO(sonots): Fix type compatibility
+# TODO(niboshi): Remove strides_check=False
+@xchainer.testing.numpy_xchainer_array_equal(dtype_check=False, strides_check=False)
+@pytest.mark.parametrize('keepdims', [False, True])
+@pytest.mark.parametrize('large_dtype', ['int32', 'int64', 'float32', 'float64'])
+@pytest.mark.parametrize('shape,axis', [
+    ((), None),
+    ((), ()),
+    ((2,), None),
+    ((2,), ()),
+    ((2,), 0),
+    ((2,), (0,)),
+    ((2,), (-1,)),
+    ((2, 3), None),
+    ((2, 3), ()),
+    ((2, 3), 0),
+    ((2, 3), (0,)),
+    ((2, 3), (1,)),
+    ((2, 3), (-1,)),
+    ((2, 3), (-2,)),
+    ((2, 3), (0, 1)),
+    ((2, 3), (-2, -1)),
+    ((1, 3), None),  # sum over 1-dim axis
+    ((0, 3), None),  # sum over 0-dim axis
+    # Sum over axes that are in the middle or apart
+    ((2, 3, 4), (1,)),
+    ((2, 3, 4), (0, 2)),
+    # Sum over axes that are apart and/or unsorted
+    ((2, 3), (1, 0)),
+    ((2, 3, 4), (2, 0)),
+    ((2, 3, 4), (2, 0, 1)),
+    ((2, 3, 4), (-2, 2, 0)),
+])
+@pytest.mark.parametrize_device(['native:0', 'cuda:0'])
+def test_sum(is_module, xp, device, shape, axis, keepdims, large_dtype):
+    a = array_utils.create_dummy_ndarray(xp, shape, large_dtype)
+    if is_module:
+        return xp.sum(a, axis=axis, keepdims=keepdims)
+    else:
+        return a.sum(axis=axis, keepdims=keepdims)
+
+
+@xchainer.testing.numpy_xchainer_array_equal(accept_error=(xchainer.DimensionError, ValueError))
+@pytest.mark.parametrize('keepdims', [False, True])
+@pytest.mark.parametrize('shape,axis', [
+    # ((), 0), # TODO(sonots): Fix compatibility
+    ((), 1),
+    ((), (1,)),
+    ((2,), 2),
+    ((2,), (2,)),
+    ((2,), (-2,)),
+    ((2, 3,), (-3,)),
+    ((2, 3,), (-3, -4)),
+    ((2, 3,), (0, 0)),
+    ((2, 3,), (-1, -1)),
+    ((2, 3,), (0, 1, 1)),
+    ((2, 3,), (0, -2)),
+])
+def test_sum_invalid(is_module, xp, shape, axis, keepdims, dtype):
+    a = array_utils.create_dummy_ndarray(xp, shape, dtype)
+    if is_module:
+        xp.sum(a, axis=axis, keepdims=keepdims)
+    else:
+        a.sum(axis=axis, keepdims=keepdims)
