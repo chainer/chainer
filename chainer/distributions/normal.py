704fb3bbd338297b8942399af0d677e8349818b6
yoshikawa <yoshikawa@preferred.jp>
2018-05-17 08:22:00 +0900

    [fix] normal.py

--- chainer/distributions/normal.py
+++ chainer/distributions/normal.py
@@ -9,16 +9,28 @@ from chainer.functions.math import erf
 from chainer.functions.math import erfinv
 from chainer.functions.math import exponential
 import numpy
+import math
+
+
+ENTROPYC = 0.5 * math.log(2 * math.pi * math.e)
+LOGPROBC = - 0.5 * math.log(2 * math.pi)
+PROBC = 1. / (2 * math.pi) ** 0.5
 
 
 class Normal(distribution.Distribution):
 
     """Normal Distribution.
 
+    The probability density function of the distribution is expressed as
+
+    .. math::
+        p(x;\\mu,\\sigma) = \\frac{1}{\\sqrt{2\\pi\\sigma^2}}
+            \\exp\\left(-\\frac{(x-\\mu)^2}{2\\sigma^2}\\right)
+
     Args:
         loc(:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
         :class:`cupy.ndarray`): Parameter of distribution representing the \
-        location :math:`\\mu`.
+        location :math:`\\mu`. This is the mean parameter.
         scale(:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
         :class:`cupy.ndarray`): Parameter of distribution representing the \
         scale :math:`\\log(\\sigma^2)`.
@@ -36,58 +48,24 @@ class Normal(distribution.Distribution):
         else:
             self.scale = chainer.Variable(scale)
 
-    def __copy__(self):
-        return self._copy_to(Normal(self.loc, self.scale))
-
     @property
     def batch_shape(self):
         return self.loc.shape
 
     def cdf(self, x):
-        """Returns Cumulative Distribution Function for a input variable.
-
-        Args:
-            x(:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
-            :class:`cupy.ndarray`): Input variable representing a random
-            variable.
-
-        Returns:
-            ~chainer.Variable: Output variable representing Cumulative
-            Distribution Function.
-
-        """
         return 0.5 * (1. + erf.erf((
             x - broadcast.broadcast_to(self.loc, x.shape))
             / (2 ** 0.5 * broadcast.broadcast_to(self.scale, x.shape))))
 
     @property
     def entropy(self):
-        """Returns entropy.
-
-        Returns:
-            Output Variable representing entropy.
-
-        """
-        return exponential.log(self.scale) \
-            + 0.5 * numpy.log(2 * numpy.pi * numpy.e)
+        return exponential.log(self.scale) + ENTROPYC
 
     @property
     def event_shape(self):
         return ()
 
     def icdf(self, x):
-        """Returns Inverse Cumulative Distribution Function for a input Variable.
-
-        Args:
-            x(:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
-            :class:`cupy.ndarray`): Input variable representing a random
-            variable.
-
-        Returns:
-            ~chainer.Variable: Output variable representing Inverse Cumulative
-            Distribution Function.
-
-        """
         return erfinv.erfinv(2. * x - 1.) \
             * (2 ** 0.5) * broadcast.broadcast_to(self.scale, x.shape) \
             + broadcast.broadcast_to(self.loc, x.shape)
@@ -97,88 +75,27 @@ class Normal(distribution.Distribution):
         return isinstance(self.loc.data, cuda.ndarray)
 
     def log_cdf(self, x):
-        """Returns logarithm of Cumulative Distribution Function for a input Variable.
-
-        Args:
-            x(:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
-            :class:`cupy.ndarray`): Input variable representing a random
-            variable.
-
-        Returns:
-            ~chainer.Variable: Output variable representing logarithm of
-            Cumulative Distribution Function.
-
-        """
         return exponential.log(self.cdf(x))
 
     def log_prob(self, x):
-        """Returns logarithm logarithm of probability for a input variable.
-
-        Args:
-            x: Input variable representing a random variable.
-
-        Returns:
-            Output variable representing logarithm of probability.
-
-        """
-        return - 0.5 * numpy.log(2 * numpy.pi) \
-            - exponential.log(broadcast.broadcast_to(self.scale, x.shape)) \
+        return - exponential.log(broadcast.broadcast_to(self.scale, x.shape)) \
             - 0.5 * (x - broadcast.broadcast_to(self.loc, x.shape)) ** 2 \
-            / broadcast.broadcast_to(self.scale, x.shape) ** 2
+            / broadcast.broadcast_to(self.scale, x.shape) ** 2 + LOGPROBC
 
     def log_survival_function(self, x):
-        """Returns logarithm of survival function for a input Variable.
-
-        Args:
-            x(:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
-            :class:`cupy.ndarray`): Input variable representing a random
-            variable.
-
-        Returns:
-            ~chainer.Variable: Output variable representing logarithm of
-            survival function for a input variable.
-
-        """
         return exponential.log(self.survival_function(x))
 
     @property
     def mean(self):
-        """Returns mean value.
-
-        Returns:
-            ~chainer.Variable: Output variable representing mean value.
-
-        """
         return self.loc
 
     def prob(self, x):
-        """Returns probability for a input variable.
-
-        Args:
-            x(:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
-            :class:`cupy.ndarray`): Input variable representing a random
-            variable.
-
-        Returns:
-            ~chainer.Variable: Output variable representing probability.
-
-        """
-        return 1. / (2 * numpy.pi) ** 0.5 \
-            / broadcast.broadcast_to(self.scale, x.shape) * \
+        return PROBC / broadcast.broadcast_to(self.scale, x.shape) * \
             exponential.exp(
                 - 0.5 * (x - broadcast.broadcast_to(self.loc, x.shape)) ** 2
                 / broadcast.broadcast_to(self.scale, x.shape) ** 2)
 
     def _sample_n(self, n):
-        """Samples from this distribution.
-
-        Args:
-            n(`int`): Sampling size.
-
-        Returns:
-            ~chainer.Variable: Output variable representing sampled random
-            variable.
-        """
         if self._is_gpu:
             eps = cuda.cupy.random.standard_normal(
                 (n,)+self.loc.shape, dtype=self.loc.dtype)
@@ -194,49 +111,19 @@ class Normal(distribution.Distribution):
 
     @property
     def stddev(self):
-        """Returns standard deviation.
-
-        Returns:
-            ~chainer.Variable: Output variable representing standard deviation.
-
-        """
         return self.scale
 
     @property
     def support(self):
-        """Returns support.
-
-        Returns:
-            string: Output string that means support of this distribution.
-
-        """
         return 'real'
 
     def survival_function(self, x):
-        """Returns survival function for a input variable.
-
-        Args:
-            x(:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
-            :class:`cupy.ndarray`): Input variable representing a random
-            variable.
-
-        Returns:
-            ~chainer.Variable: Output variable representing survival function
-            for a input variable.
-
-        """
         return 0.5 * (1. - erf.erf(
             (x - broadcast.broadcast_to(self.loc, x.shape))
             / (2 ** 0.5 * broadcast.broadcast_to(self.scale, x.shape))))
 
     @property
     def variance(self):
-        """Returns variance.
-
-        Returns:
-            ~chainer.Variable: Output variable representing variance.
-
-        """
         return self.scale ** 2
 
 
