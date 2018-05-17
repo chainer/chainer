cfae38ef35d54dd4cffacffd308af0318c5182b0
yoshikawa <yoshikawa@preferred.jp>
2018-05-17 09:22:15 +0900

    [fix] fix not to numeric differential with large error.

--- tests/chainer_tests/functions_tests/math_tests/test_erfinv.py
+++ tests/chainer_tests/functions_tests/math_tests/test_erfinv.py
@@ -25,7 +25,7 @@ def _erfinv_expected(x, dtype):
 
 
 def make_data(shape, dtype):
-    x = numpy.random.uniform(-1, 1, shape).astype(dtype)
+    x = numpy.random.uniform(-0.9, 0.9, shape).astype(dtype)
     gy = numpy.random.uniform(-1, 1, shape).astype(dtype)
     ggx = numpy.random.uniform(-1, 1, shape).astype(dtype)
     return x, gy, ggx
@@ -36,8 +36,8 @@ def make_data(shape, dtype):
     func_expected=_erfinv_expected,
     make_data=make_data,
     forward_options={'atol': 1e-3, 'rtol': 1e-3},
-    backward_options={'eps': 1e-5},
-    double_backward_options={'eps': 1e-5}
+    backward_options={'eps': 1e-6},
+    double_backward_options={'eps': 1e-6}
 )
 @testing.with_requires('scipy')
 class TestErfinv(unittest.TestCase):
