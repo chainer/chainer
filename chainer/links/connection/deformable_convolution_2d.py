af0a3ca231867c6b95b5b2e53c9e4ff5dd3963fa
Yusuke Niitani <yuyuniitani@gmail.com>
2018-05-18 14:46:42 +0900

    fix parameters of link

--- chainer/links/connection/deformable_convolution_2d.py
+++ chainer/links/connection/deformable_convolution_2d.py
@@ -9,22 +9,65 @@ from chainer import variable
 class DeformableConvolution2D(link.Chain):
     """Deformable Convolution 2D.
 
+    This link wraps the
+    convolution layer for offset prediction and
+    the :func:`~chainer.functions.deformable_convolution_2d_sampler`
+    function.
+    This also holds the filter weights and bias vectors of two
+    convolution layers as parameters.
+
+    Args:
+        in_channels (int): Number of channels of input arrays. If ``None``,
+            parameter initialization will be deferred until the first forward
+            data pass at which time the size will be determined.
+        channel_multiplier (int): Channel multiplier number. Number of output
+            arrays equal ``in_channels * channel_multiplier``.
+        ksize (int or pair of ints): Size of filters (a.k.a. kernels).
+            ``ksize=k`` and ``ksize=(k, k)`` are equivalent.
+        stride (int or pair of ints): Stride of filter applications.
+            ``stride=s`` and ``stride=(s, s)`` are equivalent.
+        pad (int or pair of ints): Spatial padding width for input arrays.
+            ``pad=p`` and ``pad=(p, p)`` are equivalent.
+        offset_nobias (bool): If ``True``, then this link does not use the
+            bias term for the first convolution layer.
+        offset_initialW (:ref:`initializer <initializer>`): Initializer to
+            initialize the weight of the first convolution layer.
+            When it is :class:`numpy.ndarray`, its ``ndim`` should be 4.
+        offset_initial_bias (:ref:`initializer <initializer>`): Initializer to
+            initialize the bias of the first convolution layer.
+            If ``None``, the bias will be initialized to
+            zero. When it is :class:`numpy.ndarray`, its ``ndim`` should be 1.
+        deform_nobias (bool): If ``True``, then this link does not use the
+            bias term for the second convolution layer.
+        deform_initialW (:ref:`initializer <initializer>`): Initializer to
+            initialize the weight for the second convolution layer.
+            When it is :class:`numpy.ndarray`,
+            its ``ndim`` should be 4.
+        deform_initial_bias (:ref:`initializer <initializer>`): Initializer to
+            initialize the bias for the second convolution layer.
+            If ``None``, the bias will be initialized to
+            zero. When it is :class:`numpy.ndarray`, its ``ndim`` should be 1.
+
+    .. seealso::
+       See :func:`chainer.functions.deformable_convolution_2d_sampler`.
+
     """
 
     def __init__(self, in_channels, out_channels, ksize, stride=1, pad=0,
-                 nobias=False, initialW=None, initial_bias=None,
-                 offset_nobias=False,
-                 offset_initialW=None, offset_initial_bias=None):
+                 offset_nobias=False, offset_initialW=None,
+                 offset_initial_bias=None,
+                 deform_nobias=False,
+                 deform_initialW=None, deform_initial_bias=None):
         super(DeformableConvolution2D, self).__init__()
         kh, kw = _pair(ksize)
 
         with self.init_scope():
-            self.deform_conv = DeformableConvolution2DSampler(
-                in_channels, out_channels, ksize, stride, pad,
-                nobias, initialW, initial_bias)
             self.offset_conv = Convolution2D(
                 in_channels, 2 * kh * kw, ksize, stride, pad,
                 offset_nobias, offset_initialW, offset_initial_bias)
+            self.deform_conv = DeformableConvolution2DSampler(
+                in_channels, out_channels, ksize, stride, pad,
+                deform_nobias, deform_initialW, deform_initial_bias)
 
     def __call__(self, x):
         """Applies the deformable convolution.
@@ -41,7 +84,7 @@ class DeformableConvolution2D(link.Chain):
 
 
 class DeformableConvolution2DSampler(link.Link):
-    """Apply Two-dimensional deformable convolution layer using offset"""
+    """Apply a two-dimensional deformable convolution layer using offsets"""
 
     def __init__(self, in_channels, out_channels, ksize, stride=1, pad=0,
                  nobias=False, initialW=None, initial_bias=None):
