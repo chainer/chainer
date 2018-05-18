89ef2fbe90c764f48e40825048bcd6dad427b776
yoshikawa <yoshikawa@preferred.jp>
2018-05-18 09:33:32 +0900

    [fix] based on the comments

--- chainer/distributions/__init__.py
+++ chainer/distributions/__init__.py
@@ -1,6 +1,3 @@
 """Collection of distribution implementations."""
 
 from chainer.distributions.normal import Normal  # NOQA
-
-from chainer.distributions.kl_divergence import kl_divergence  # NOQA
-from chainer.distributions.kl_divergence import register_kl  # NOQA
