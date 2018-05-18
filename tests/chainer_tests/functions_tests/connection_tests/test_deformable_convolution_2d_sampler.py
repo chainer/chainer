1ad8094f9f242907adb92614edce9f2bc2128d9e
Yusuke Niitani <yuyuniitani@gmail.com>
2018-05-18 13:27:35 +0900

    pass tests

--- tests/chainer_tests/functions_tests/connection_tests/test_deformable_convolution_2d_sampler.py
+++ tests/chainer_tests/functions_tests/connection_tests/test_deformable_convolution_2d_sampler.py
@@ -21,7 +21,7 @@ from chainer.testing import attr
         (1, 2, 3, 4, 4, 5),
         (3, 3, 2, 2, 1, 1),
     ],
-    'use_cudnn': [True, False]
+    'use_cudnn': ['always', 'never']
 }))
 class TestDeformableConvolution2DSamplerFunctionZeroOffset(unittest.TestCase):
 
@@ -50,19 +50,19 @@ class TestDeformableConvolution2DSamplerFunctionZeroOffset(unittest.TestCase):
         self.offset = numpy.zeros(
             (batch_size, 2 * kh * kw, out_h, out_w), dtype=numpy.float32)
 
-    def check_forward(self, x, offset, W, b, stride, pad, use_cudnn=True):
-        x = chainer.Variable(x)
-        offset = chainer.Variable(offset)
-        out = deformable_convolution_2d_sampler(
-            x, offset, W, b, stride, pad, use_cudnn=use_cudnn).data
-        expeceted = convolution_2d(
-            x, W, b, stride, pad, use_cudnn=use_cudnn).data
+    def check_forward(self, x, offset, W, b, stride, pad):
+        with chainer.using_config('use_cudnn', self.use_cudnn):
+            x = chainer.Variable(x)
+            offset = chainer.Variable(offset)
+            out = deformable_convolution_2d_sampler(
+                x, offset, W, b, stride, pad).data
+            expeceted = convolution_2d(
+                x, W, b, stride, pad).data
         testing.assert_allclose(out, expeceted)
 
     def test_forward_cpu(self):
         self.check_forward(
-            self.x, self.offset, self.W, self.b, self.stride, self.pad,
-            use_cudnn=self.use_cudnn)
+            self.x, self.offset, self.W, self.b, self.stride, self.pad)
 
     @attr.gpu
     def test_forward_gpu(self):
@@ -71,7 +71,7 @@ class TestDeformableConvolution2DSamplerFunctionZeroOffset(unittest.TestCase):
             cuda.to_gpu(self.offset),
             cuda.to_gpu(self.W),
             cuda.to_gpu(self.b),
-            self.stride, self.pad, use_cudnn=self.use_cudnn)
+            self.stride, self.pad)
 
 
 @testing.parameterize(*testing.product({
@@ -83,7 +83,7 @@ class TestDeformableConvolution2DSamplerFunctionZeroOffset(unittest.TestCase):
         (1, 2, 3, 4, 4, 5),
         (3, 3, 2, 2, 1, 1),
     ],
-    'use_cudnn': [True, False]
+    'use_cudnn': ['always', 'never']
 }))
 class TestDeformableConvolution2DSamplerFunctionLeftBottomOffset(
         unittest.TestCase):
@@ -113,26 +113,26 @@ class TestDeformableConvolution2DSamplerFunctionLeftBottomOffset(
         self.offset = numpy.zeros(
             (batch_size, 2 * kh * kw, out_h, out_w), dtype=numpy.float32)
 
-    def check_forward(self, x, offset, W, b, stride, pad, use_cudnn=True):
-        _, _, h, w = x.shape
-        _, _, kh, kw = W.shape
-        offset[:, :kh * kw] = -1 * stride[1]
-        offset[:, kh * kw:] = 1 * stride[0]
+    def check_forward(self, x, offset, W, b, stride, pad):
+        with chainer.using_config('use_cudnn', self.use_cudnn):
+            _, _, h, w = x.shape
+            _, _, kh, kw = W.shape
+            offset[:, :kh * kw] = -1 * stride[1]
+            offset[:, kh * kw:] = 1 * stride[0]
 
-        x = chainer.Variable(x)
-        offset = chainer.Variable(offset)
-        out = deformable_convolution_2d_sampler(
-            x, offset, W, b, stride, pad, use_cudnn=use_cudnn).data
-        pad = (pad[0] + 1 * stride[0], pad[1] + 1 * stride[1])
-        expeceted = convolution_2d(
-            x, W, b, stride, pad, use_cudnn=use_cudnn).data
-        expeceted = expeceted[:, :, 2:, :-2]
+            x = chainer.Variable(x)
+            offset = chainer.Variable(offset)
+            out = deformable_convolution_2d_sampler(
+                x, offset, W, b, stride, pad).data
+            pad = (pad[0] + 1 * stride[0], pad[1] + 1 * stride[1])
+            expeceted = convolution_2d(
+                x, W, b, stride, pad).data
+            expeceted = expeceted[:, :, 2:, :-2]
         testing.assert_allclose(out, expeceted)
 
     def test_forward_cpu(self):
         self.check_forward(
-            self.x, self.offset, self.W, self.b, self.stride,
-            self.pad, use_cudnn=self.use_cudnn)
+            self.x, self.offset, self.W, self.b, self.stride, self.pad)
 
     @attr.gpu
     def test_forward_gpu(self):
@@ -141,7 +141,7 @@ class TestDeformableConvolution2DSamplerFunctionLeftBottomOffset(
             cuda.to_gpu(self.offset),
             cuda.to_gpu(self.W),
             cuda.to_gpu(self.b),
-            self.stride, self.pad, use_cudnn=self.use_cudnn)
+            self.stride, self.pad)
 
 
 testing.run_module(__name__, __file__)
