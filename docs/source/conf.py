3e06b93abf2f68416f56950a484d028549cf5920
keisuke-umezawa <keisuke.umezawa@gmail.com>
2018-06-01 15:53:21 +0900

    Add nbsphinx settings and render jupyter notebook

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -109,7 +109,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['**.ipynb_checkpoints']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
