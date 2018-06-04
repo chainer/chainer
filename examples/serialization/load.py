20ddcbbaf563c1fc8d5e7c0e7c22230c7224c63f
Shunta Saito <shunta.saito@gmail.com>
2018-06-04 12:07:27 +0900

    Fix flake8 errors

--- examples/serialization/load.py
+++ examples/serialization/load.py
@@ -8,6 +8,7 @@ def load_npz_file_to_model(npz_filename, model_object):
     chainer.serializers.load_npz(npz_filename, model_object)
     print('{} loaded!'.format(npz_filename))
 
+
 # Create model object first
 model1 = model.MLP()
 
@@ -18,6 +19,7 @@ load_npz_file_to_model('model.npz', model1)
 def load_hdf5_file_to_model(hdf5_filename, model_object):
     chainer.serializers.load_hdf5(hdf5_filename, model_object)
     print('{} loaded!'.format(hdf5_filename))
+
 
 # Create another model object first
 model2 = model.MLP()
@@ -31,4 +33,3 @@ for name, npz_param in model1.namedparams():
     h5_param = model2_params[name]
     np.testing.assert_array_equal(npz_param.array, h5_param.array)
     print(name, npz_param.shape)
-
