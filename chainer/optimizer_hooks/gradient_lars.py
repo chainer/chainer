91026869d5a3fbf4404991917ea488f9c65a9418
Kenta OONO <oono@preferred.jp>
2018-06-05 17:38:14 +0900

    Add call_for_each_param to optimizer hooks except GradientClipping

--- chainer/optimizer_hooks/gradient_lars.py
+++ chainer/optimizer_hooks/gradient_lars.py
@@ -60,6 +60,12 @@ class GradientLARS(object):
                          when this hook should be called by the
                          Optimizer/UpdateRule. Valid values are 'pre'
                          (before any updates) and 'post' (after any updates).
+        ~optimizer_hooks.GradientLARS.call_for_each_param (bool): Specifies
+                         if this hook is called for each parameter (`True`)
+                         or only once (`False`) by an optimizer to
+                         which this hook is registered. This function does
+                         not expect users to switch the value from default one,
+                         which is `True`.
 
     """
     name = 'GradientLARS'
