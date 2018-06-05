a86738dff041d6a5668d27697196ac75d91a5ace
Kenta OONO <oono@preferred.jp>
2018-06-05 16:08:53 +0900

    Fix update documents of extract method

--- chainer/links/model/vision/vgg.py
+++ chainer/links/model/vision/vgg.py
@@ -215,30 +215,34 @@ class VGG16Layers(link.Chain):
         it is also interpreted as a shortcut method that implicitly calls
         ``prepare`` and ``__call__`` functions.
 
+        Unlike ``predict`` method, this method does not override
+        ``chainer.config.train`` and ``chainer.config.enable_backprop``
+        configuration. If you want to extract features without updating
+        model parameters, you need to manually set configuration when
+        calling this method as follows:
+
+         .. code-block:: python
+
+             # model is an instance of VGG16Layers
+             with chainer.using_config('train', False):
+                 with chainer.using_config('enable_backprop', False):
+                     feature = model.extract([image])
+
         .. warning::
 
-           ``train`` and ``volatile`` arguments are not supported anymore since
-           v2.
-           Users should configure training and volatile mode
-           with thread-local variables ``chainer.config.train``
-           and ``chainer.config.enable_backprop``, respectively.
+           ``test`` and ``volatile`` arguments are not supported
+           anymore since v2. Instead, users should configure
+           training and volatile modes with ``train`` and
+           ``enable_backprop``, respectively.
 
-           Note that their default values are ``True``,
-           which behave differently if we used ``train`` and ``volatile`` in v1
-           options with default setting (``False`` and ``OFF``, respectively).
-           Therefore, users need to explicitly switch
-           ``chainer.config.train`` to ``False`` if they want to run the code
-           in test mode and ``chainer.config.enable_backprop``
-           to ``False`` if turn off construction of coputational graphs.
-           These variables can be configured with ``chainer.using_config``
-           as follows:
-
-           .. code-block:: python
-
-               # model is an instance of ResNetLayers (50 or 101 or 152 layers)
-               with chainer.using_config('train', False):
-                   with chainer.using_config('enable_backprop', False):
-                       feature = model.extract([image])
+           Note that default behavior of this method is different
+           between v1 and later versions. Specifically,
+           the default values of ``test`` in v1 were ``True`` (test mode).
+           But that of ``chainer.config.train`` is also ``True``
+           (train mode). Therefore, users need to explicitly switch
+           ``train`` to ``False`` to run the code in test mode and
+           ``enable_backprop`` to ``False`` to turn off
+           coputational graph construction.
 
            See the `upgrade guide <https://docs.chainer.org/en/stable\
            /upgrade_v2.html#training-mode-is-configured-by-a-thread-local-flag>`_.
