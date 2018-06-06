07cda7c06e983af579a129dbad1bbc157c8da2ef
sonots <sonots@gmail.com>
2018-06-06 12:10:28 +0900

    move Dot to dot.cu which intended to place all dot functions like vdot, tensordot

--- xchainer/cuda/cuda_device.h
+++ xchainer/cuda/cuda_device.h
@@ -89,6 +89,8 @@ public:
 
     void IfLessElseASSA(const Array& x1, Scalar x2, Scalar pos, const Array& neg, const Array& out) override;
 
+    // dot.cc
+
     void Dot(const Array& a, const Array& b, const Array& out) override;
 
     void Exp(const Array& x, const Array& out) override;
