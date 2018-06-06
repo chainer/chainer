4f6e1ad449db2c149a67f3d47ec5a7b64a5a3ca3
sonots <sonots@gmail.com>
2018-06-06 13:47:22 +0900

    Move comparison functions

--- xchainer/native/native_device.cc
+++ xchainer/native/native_device.cc
@@ -35,17 +35,6 @@
 namespace xchainer {
 namespace native {
 
-void NativeDevice::Equal(const Array& x1, const Array& x2, const Array& out) {
-    CheckDevicesCompatible(x1, x2, out);
-    VisitDtype(x1.dtype(), [&](auto pt) {
-        using T = typename decltype(pt)::type;
-        struct Impl {
-            void operator()(int64_t /*i*/, T x1, T x2, bool& out) { out = x1 == x2; }
-        };
-        Elementwise<const T, const T, bool>(Impl{}, x1, x2, out);
-    });
-}
-
 void NativeDevice::IfLessElseASSA(const Array& x1, Scalar x2, Scalar pos, const Array& neg, const Array& out) {
     CheckDevicesCompatible(x1, neg, out);
     VisitDtype(out.dtype(), [&](auto pt) {
