cdbe993fab63e15834b2c56c17b50fd5fe61d7c0
sonots <sonots@gmail.com>
2018-06-06 13:46:14 +0900

    move copy functions into copy.cc

--- xchainer/native/native_device.cc
+++ xchainer/native/native_device.cc
@@ -35,30 +35,6 @@
 namespace xchainer {
 namespace native {
 
-void NativeDevice::Copy(const Array& a, const Array& out) {
-    CheckDevicesCompatible(a, out);
-    VisitDtype(out.dtype(), [&](auto pt) {
-        using T = typename decltype(pt)::type;
-        struct Impl {
-            void operator()(int64_t /*i*/, T a, T& out) { out = a; }
-        };
-        Elementwise<const T, T>(Impl{}, a, out);
-    });
-}
-
-void NativeDevice::AsType(const Array& a, const Array& out) {
-    CheckDevicesCompatible(a, out);
-    auto do_astype = [&](auto in_pt, auto out_pt) {
-        using InT = typename decltype(in_pt)::type;
-        using OutT = typename decltype(out_pt)::type;
-        struct Impl {
-            void operator()(int64_t /*i*/, InT a, OutT& out) { out = static_cast<OutT>(a); }
-        };
-        Elementwise<const InT, OutT>(Impl{}, a, out);
-    };
-    VisitDtype(out.dtype(), [&](auto out_pt) { VisitDtype(a.dtype(), do_astype, out_pt); });
-}
-
 void NativeDevice::Equal(const Array& x1, const Array& x2, const Array& out) {
     CheckDevicesCompatible(x1, x2, out);
     VisitDtype(x1.dtype(), [&](auto pt) {
