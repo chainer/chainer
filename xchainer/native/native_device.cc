f62811184bac987aab5314309321d21b1504cb5a
sonots <sonots@gmail.com>
2018-06-06 14:00:48 +0900

    Move Exp and Log into exp_log.cc

--- xchainer/native/native_device.cc
+++ xchainer/native/native_device.cc
@@ -35,28 +35,6 @@
 namespace xchainer {
 namespace native {
 
-void NativeDevice::Exp(const Array& x, const Array& out) {
-    CheckDevicesCompatible(x, out);
-    VisitFloatingPointDtype(out.dtype(), [&](auto pt) {
-        using T = typename decltype(pt)::type;
-        struct Impl {
-            void operator()(int64_t /*i*/, T x, T& out) { out = std::exp(x); }
-        };
-        Elementwise<const T, T>(Impl{}, x, out);
-    });
-}
-
-void NativeDevice::Log(const Array& x, const Array& out) {
-    CheckDevicesCompatible(x, out);
-    VisitFloatingPointDtype(out.dtype(), [&](auto pt) {
-        using T = typename decltype(pt)::type;
-        struct Impl {
-            void operator()(int64_t /*i*/, T x, T& out) { out = std::log(x); }
-        };
-        Elementwise<const T, T>(Impl{}, x, out);
-    });
-}
-
 void NativeDevice::Take(const Array& a, const Array& indices, int8_t axis, const Array& out) {
     CheckDevicesCompatible(a, indices, out);
     VisitDtype(out.dtype(), [&](auto pt) {
