6fe91d63cf95395b868c436e55badbfa344eaa1c
sonots <sonots@gmail.com>
2018-06-06 14:21:15 +0900

    cleanup native_device.cc

--- xchainer/native/native_device.cc
+++ xchainer/native/native_device.cc
@@ -1,37 +1,5 @@
 #include "xchainer/native/native_device.h"
 
-#include <algorithm>
-#include <cassert>
-#include <cmath>
-#include <cstdint>
-#include <cstring>
-#include <limits>
-#include <numeric>
-#include <tuple>
-#include <type_traits>
-#include <vector>
-
-#include <gsl/gsl>
-
-#include "xchainer/array.h"
-#include "xchainer/array_index.h"
-#include "xchainer/axes.h"
-#include "xchainer/dtype.h"
-#include "xchainer/indexable_array.h"
-#include "xchainer/indexer.h"
-#include "xchainer/native/elementwise.h"
-#include "xchainer/native/reduce.h"
-#include "xchainer/numeric_limits.h"
-#include "xchainer/reduction_kernel_arg.h"
-#include "xchainer/routines/connection.h"
-#include "xchainer/routines/creation.h"
-#include "xchainer/routines/manipulation.h"
-#include "xchainer/routines/math.h"
-#include "xchainer/scalar.h"
-#include "xchainer/shape.h"
-#include "xchainer/slice.h"
-#include "xchainer/strides.h"
-
 namespace xchainer {
 namespace native {
 
