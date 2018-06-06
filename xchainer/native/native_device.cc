d61be5d0da41cf23417b5f231eb08bd82b3aed5d
sonots <sonots@gmail.com>
2018-06-06 13:52:53 +0900

    Move activation functions

--- xchainer/native/native_device.cc
+++ xchainer/native/native_device.cc
@@ -35,19 +35,6 @@
 namespace xchainer {
 namespace native {
 
-void NativeDevice::IfLessElseASSA(const Array& x1, Scalar x2, Scalar pos, const Array& neg, const Array& out) {
-    CheckDevicesCompatible(x1, neg, out);
-    VisitDtype(out.dtype(), [&](auto pt) {
-        using T = typename decltype(pt)::type;
-        struct Impl {
-            void operator()(int64_t /*i*/, T x1, T neg, T& out) { out = x1 < x2 ? pos : neg; }
-            T x2;
-            T pos;
-        };
-        Elementwise<const T, const T, T>(Impl{static_cast<T>(x2), static_cast<T>(pos)}, x1, neg, out);
-    });
-}
-
 void NativeDevice::Dot(const Array& a, const Array& b, const Array& out) {
     CheckDevicesCompatible(a, b, out);
     VisitDtype(out.dtype(), [&](auto pt) {
