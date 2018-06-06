8f103d25c433668f338142b7a3c08bf61be6acb7
sonots <sonots@gmail.com>
2018-06-06 13:32:29 +0900

    move memory related functions into memory.cc

--- xchainer/native/native_device.cc
+++ xchainer/native/native_device.cc
@@ -35,42 +35,10 @@
 namespace xchainer {
 namespace native {
 
-std::shared_ptr<void> NativeDevice::Allocate(size_t bytesize) { return std::make_unique<uint8_t[]>(bytesize); }
-
-void NativeDevice::MemoryCopyFrom(void* dst, const void* src, size_t bytesize, Device& src_device) {
-    (void)src_device;  // unused
-    assert(nullptr != dynamic_cast<NativeDevice*>(&src_device) && "Native device only supports copy between native devices");
-    std::memcpy(dst, src, bytesize);
-}
-
-void NativeDevice::MemoryCopyTo(void* dst, const void* src, size_t bytesize, Device& dst_device) {
-    (void)dst_device;  // unused
-    assert(nullptr != dynamic_cast<NativeDevice*>(&dst_device) && "Native device only supports copy between native devices");
-    std::memcpy(dst, src, bytesize);
-}
-
-std::shared_ptr<void> NativeDevice::TransferDataFrom(
-        Device& src_device, const std::shared_ptr<void>& src_ptr, size_t offset, size_t bytesize) {
-    std::shared_ptr<void> dst_ptr = Allocate(bytesize);
-    MemoryCopyFrom(dst_ptr.get(), &(static_cast<int8_t*>(src_ptr.get())[offset]), bytesize, src_device);
-    return dst_ptr;
-}
-
-std::shared_ptr<void> NativeDevice::TransferDataTo(
-        Device& dst_device, const std::shared_ptr<void>& src_ptr, size_t offset, size_t bytesize) {
-    return dst_device.TransferDataFrom(*this, src_ptr, offset, bytesize);
-}
-
-std::shared_ptr<void> NativeDevice::FromHostMemory(const std::shared_ptr<void>& src_ptr, size_t bytesize) {
-    (void)bytesize;  // unused
-    return src_ptr;
-}
-
 void NativeDevice::Fill(const Array& out, Scalar value) {
     VisitDtype(out.dtype(), [&](auto pt) {
         using T = typename decltype(pt)::type;
-        struct Impl {
-            void operator()(int64_t /*i*/, T& out) { out = value; }
+        struct Impl { void operator()(int64_t /*i*/, T& out) { out = value; }
             T value;
         };
         Elementwise<T>(Impl{static_cast<T>(value)}, out);
