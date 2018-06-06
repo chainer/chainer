684856d238f4e5d902571324e2c22dcc925ac0df
sonots <sonots@gmail.com>
2018-06-06 13:57:20 +0900

    move dot into dot.cc

--- xchainer/native/native_device.cc
+++ xchainer/native/native_device.cc
@@ -35,42 +35,6 @@
 namespace xchainer {
 namespace native {
 
-void NativeDevice::Dot(const Array& a, const Array& b, const Array& out) {
-    CheckDevicesCompatible(a, b, out);
-    VisitDtype(out.dtype(), [&](auto pt) {
-        using T = typename decltype(pt)::type;
-        IndexableArray<const T> a_iarray{a};
-        IndexableArray<const T> b_iarray{b};
-        IndexableArray<T> out_iarray{out};
-
-        // These asserts have to check iarray instead of the original array, otherwise clang-tidy fails bound-checking.
-        assert(a_iarray.ndim() == 2);
-        assert(b_iarray.ndim() == 2);
-        assert(out_iarray.ndim() == 2);
-
-        int64_t m = a.shape()[0];
-        int64_t k = a.shape()[1];
-        int64_t n = b.shape()[1];
-        assert(b.shape()[0] == k);
-        assert(out.shape()[0] == m);
-        assert(out.shape()[1] == n);
-
-        // TODO(beam2d): Use BLAS.
-        for (int64_t i = 0; i < m; ++i) {
-            for (int64_t j = 0; j < n; ++j) {
-                int64_t out_i[] = {i, j};
-                T& out_value = out_iarray[out_i];
-                out_value = 0;
-                for (int64_t l = 0; l < k; ++l) {
-                    int64_t a_i[] = {i, l};
-                    int64_t b_i[] = {l, j};
-                    out_value += a_iarray[a_i] * b_iarray[b_i];
-                }
-            }
-        }
-    });
-}
-
 void NativeDevice::Exp(const Array& x, const Array& out) {
     CheckDevicesCompatible(x, out);
     VisitFloatingPointDtype(out.dtype(), [&](auto pt) {
