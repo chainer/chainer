f776ac8d884215420991e5fe45afe57d38fdbe69
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-06-12 16:30:58 +0900

    Move declaration of ToAxes

--- xchainer/python/common.h
+++ xchainer/python/common.h
@@ -3,14 +3,9 @@
 
 #pragma once
 
-#include <vector>
-
-#include <nonstd/optional.hpp>
-
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-
-#include "xchainer/axes.h"
+#include <nonstd/optional.hpp>
 
 // Optional type caster
 // http://pybind11.readthedocs.io/en/stable/advanced/cast/stl.html
@@ -22,27 +17,3 @@ struct type_caster<nonstd::optional<T>> : optional_caster<nonstd::optional<T>> {
 
 }  // namespace detail
 }  // namespace pybind11
-
-namespace xchainer {
-namespace python {
-namespace internal {
-
-inline OptionalAxes ToAxes(const nonstd::optional<std::vector<int8_t>>& vec) {
-    if (vec.has_value()) {
-        return Axes{vec->begin(), vec->end()};
-    } else {
-        return nonstd::nullopt;
-    }
-}
-
-inline OptionalAxes ToAxes(const nonstd::optional<int8_t>& vec) {
-    if (vec.has_value()) {
-        return Axes{*vec};
-    } else {
-        return nonstd::nullopt;
-    }
-}
-
-}  // namespace internal
-}  // namespace python
-}  // namespace xchainer
