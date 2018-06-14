08bfcb63498753f82094121b3bcbffd99b451b3d
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-06-14 14:31:15 +0900

    flake8

--- tests/chainer_tests/functions_tests/noise_tests/test_dropout.py
+++ tests/chainer_tests/functions_tests/noise_tests/test_dropout.py
@@ -4,7 +4,6 @@ import numpy
 
 import chainer
 from chainer.backends import cuda
-from chainer.backends import intel64
 from chainer import functions
 from chainer import gradient_check
 from chainer import testing
