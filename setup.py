67364ee4fc568c19fc7d3b5f20ddf6a450f99320
niboshi <niboshi000@gmail.com>
2018-06-14 12:58:24 +0900

    Tentatively restrict pytest-timeout version to <1.3.0

--- setup.py
+++ setup.py
@@ -54,7 +54,9 @@ requirements = {
     'travis': [
         '-r stylecheck',
         '-r test',
-        'pytest-timeout',
+        # pytest-timeout>=1.3.0 requires pytest>=3.6.
+        # TODO(niboshi): Consider upgrading pytest to >=3.6
+        'pytest-timeout<1.3.0',
         'pytest-cov',
         'theano',
         'h5py',
@@ -62,7 +64,9 @@ requirements = {
     ],
     'appveyor': [
         '-r test',
-        'pytest-timeout',
+        # pytest-timeout>=1.3.0 requires pytest>=3.6.
+        # TODO(niboshi): Consider upgrading pytest to >=3.6
+        'pytest-timeout<1.3.0',
         'pytest-cov',
     ],
 }
