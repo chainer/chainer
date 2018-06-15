7276a9cfe9893b083ffd778aa4fa9fdd0c5da3a9
yoshikawa <yoshikawa@preferred.jp>
2018-06-15 15:53:17 +0900

    [add] Laplace Distribution

---
+++ tests/chainer_tests/distributions_tests/test_laplace.py
@@ -0,0 +1,39 @@
+from chainer import distributions
+from chainer import testing
+import numpy
+
+
+@testing.parameterize(*testing.product({
+    'shape': [(3, 2), (1,)],
+    'is_variable': [True, False],
+    'sample_shape': [(3, 2), ()],
+}))
+@testing.fix_random()
+@testing.with_requires('scipy')
+class TestLaplace(testing.distribution_unittest):
+
+    def setUp(self):
+        testing.distribution_unittest.setUp(self)
+        from scipy import stats
+        self.dist = distributions.Laplace
+        self.scipy_dist = stats.laplace
+        self.scipy_onebyone = True
+
+        self.test_targets = set([
+            "batch_shape", "cdf", "entropy", "event_shape", "log_prob",
+            "mean", "prob", "sample", "stddev", "support", "variance"])
+
+    def params_init(self):
+        loc = numpy.random.uniform(-1, 1, self.shape).astype(numpy.float32)
+        scale = numpy.exp(
+            numpy.random.uniform(-1, 1, self.shape)).astype(numpy.float32)
+        self.params = {"loc": loc, "scale": scale}
+        self.scipy_params = {"loc": loc, "scale": scale}
+
+    def sample_for_test(self):
+        smp = numpy.random.normal(
+            size=self.sample_shape + self.shape).astype(numpy.float32)
+        return smp
+
+
+testing.run_module(__name__, __file__)
