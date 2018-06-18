7466d490616800bc521b40b9e33283625a442b58
sonots <sonots@gmail.com>
2018-06-18 17:22:10 +0900

    Fix FixedBatchNorm

--- xchainer/device.cc
+++ xchainer/device.cc
@@ -73,13 +73,13 @@ Array Reciprocal(const Array& x) { return OnesLike(x, x.device()) / x; }
 
 struct ApplyBatchNormResult {
     Array out;
-    Array x_mean;
-    Array x_var;
-    Array x_inv_std;
+    Array mean;
+    Array var;
+    Array inv_std;
 };
 
-ApplyBatchNormResult ApplyBatchNorm(
-        const Array& x, const Array& gamma, const Array& beta, const Array& mean, const Array& var, Scalar eps, const Axes& axis) {
+ApplyBatchNormResult ApplyFixedBatchNorm(
+        const Array& x, const Array& gamma, const Array& beta, Array mean, Array var, Scalar eps, const Axes& axis) {
 #ifndef NDEBUG
     {
         Shape reduced_shape = xchainer::internal::ReduceShape(x.shape(), axis, true);
@@ -89,16 +89,25 @@ ApplyBatchNormResult ApplyBatchNorm(
         int64_t reduced_total_size = reduced_shape.GetTotalSize();
         assert(mean.GetTotalSize() == reduced_total_size);
         assert(var.GetTotalSize() == reduced_total_size);
+
+        assert(mean.IsConstant());
+        assert(var.IsConstant());
     }
 #endif  // NDEBUG
     Array x_const = x.AsConstant();
+    Array inv_std = Reciprocal(Sqrt(var + eps));
+
+    Array out = (x_const - mean) * inv_std * gamma.AsConstant() + beta.AsConstant();
+
+    return {std::move(out), std::move(mean), std::move(var), std::move(inv_std)};
+}
+
+ApplyBatchNormResult ApplyBatchNorm(const Array& x, const Array& gamma, const Array& beta, Scalar eps, const Axes& axis) {
+    Array x_const = x.AsConstant();
     Array x_mean = Mean(x_const, axis, true);
     Array x_var = Var(x_const, x_mean, axis, true);
-    Array x_inv_std = Reciprocal(Sqrt(x_var + eps));
 
-    Array out = (x_const - x_mean) * x_inv_std * gamma.AsConstant() + beta.AsConstant();
-
-    return {std::move(out), std::move(x_mean), std::move(x_var), std::move(x_inv_std)};
+    return ApplyFixedBatchNorm(x_const, gamma, beta, std::move(x_mean), std::move(x_var), eps, axis);
 }
 
 }  // namespace
@@ -112,11 +121,11 @@ Array GenericBatchNormForwardBackward::Forward(
         Scalar eps,
         Scalar decay,
         const Axes& axis) {
-    ApplyBatchNormResult result = ApplyBatchNorm(x, gamma, beta, running_mean, running_var, eps, axis);
+    ApplyBatchNormResult result = ApplyBatchNorm(x, gamma, beta, eps, axis);
     Array& out = result.out;
-    Array& x_mean = result.x_mean;
-    Array& x_var = result.x_var;
-    Array& x_inv_std = result.x_inv_std;
+    Array& x_mean = result.mean;
+    Array& x_var = result.var;
+    Array& x_inv_std = result.inv_std;
 
     Scalar inv_decay = Scalar{1.0 - static_cast<double>(decay)};
     int64_t n = x.GetTotalSize() / gamma.GetTotalSize();
@@ -188,7 +197,7 @@ std::array<Array, 3> GenericBatchNormForwardBackward::DoubleBackward(const Array
 
 Array Device::FixedBatchNorm(
         const Array& x, const Array& gamma, const Array& beta, const Array& mean, const Array& var, Scalar eps, const Axes& axis) {
-    ApplyBatchNormResult result = ApplyBatchNorm(x, gamma, beta, mean, var, eps, axis);
+    ApplyBatchNormResult result = ApplyFixedBatchNorm(x, gamma, beta, mean, var, eps, axis);
     return std::move(result.out);
 }
 
