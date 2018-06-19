223abe523f1d9ee9c614db3bfc9d7afa84ed44ed
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-06-19 16:08:46 +0900

    Fix old variable name

--- xchainer/cuda/cuda_device/pool.cc
+++ xchainer/cuda/cuda_device/pool.cc
@@ -107,7 +107,7 @@ Array CudaDevice::AveragePool(
         const StackVector<int64_t, kMaxNdim>& /*kernel_size*/,
         const StackVector<int64_t, kMaxNdim>& /*stride*/,
         const StackVector<int64_t, kMaxNdim>& /*pad*/,
-        AveragePoolPadMode /*count_include_pad*/) {
+        AveragePoolPadMode /*pad_mode*/) {
     // TODO(hvy): Implement me.
     throw NotImplementedError{};
 }
