fda005de83b48c6bca4d95d49e7bba9251a48ce9
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-06-20 23:18:26 +0900

    Check existience of MNIST dataset before downloading

--- examples/mnist/download.sh
+++ examples/mnist/download.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-mkdir -p mnist
-cd mnist
-wget http://yann.lecun.com/exdb/mnist/{train,t10k}-{images-idx3,labels-idx1}-ubyte.gz
+if [ ! -d mnist ]; then
+    mkdir -p mnist
+    cd mnist
+    wget http://yann.lecun.com/exdb/mnist/{train,t10k}-{images-idx3,labels-idx1}-ubyte.gz
+else
+    echo "Found already existing MNIST dataset directory. Skipping download."
+fi
