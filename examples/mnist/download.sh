3e200e22c1e33783132a907a54ea009ba7676c05
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-06-20 23:55:51 +0900

    Take dst dir as argument

--- examples/mnist/download.sh
+++ examples/mnist/download.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if [ ! -d mnist ]; then
-    mkdir -p mnist
-    cd mnist
-    wget http://yann.lecun.com/exdb/mnist/{train,t10k}-{images-idx3,labels-idx1}-ubyte.gz
-else
-    echo "Found already existing MNIST dataset directory. Skipping download."
+
+if [ $# != 1 ]; then
+    echo "Too many or too few arguments." >&2
+    exit 1
 fi
+
+mkdir -p $1
+cd $1
+wget http://yann.lecun.com/exdb/mnist/{train,t10k}-{images-idx3,labels-idx1}-ubyte.gz
