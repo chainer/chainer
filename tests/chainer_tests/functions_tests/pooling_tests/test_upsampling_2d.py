aeded94c8547b1c328618013a7056fb5af272b69
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-06-23 20:03:34 +0900

    Fix upsampling_2d test

--- tests/chainer_tests/functions_tests/pooling_tests/test_upsampling_2d.py
+++ tests/chainer_tests/functions_tests/pooling_tests/test_upsampling_2d.py
@@ -18,9 +18,12 @@ class TestUpsampling2D(unittest.TestCase):
 
     def setUp(self):
         self.x = numpy.random.uniform(-1, 1, self.in_shape).astype(self.dtype)
-        self.p = F.MaxPooling2D(2, 2)
+        self.ksize = 2
+        self.stride = 2
         with chainer.using_config('use_cudnn', 'never'):
-            self.pooled_y = self.p.apply((self.x,))[0]
+            self.pooled_y, self.indices = F.max_pooling_2d(
+                self.x, ksize=self.ksize, stride=self.stride,
+                return_indices=True)
         self.gy = numpy.random.uniform(
             -1, 1, self.in_shape).astype(self.dtype)
         self.ggx = numpy.random.uniform(
@@ -32,20 +35,21 @@ class TestUpsampling2D(unittest.TestCase):
 
     def check_forward(self, y):
         y = F.upsampling_2d(
-            self.pooled_y, self.p.indexes, ksize=(self.p.kh, self.p.kw),
-            stride=(self.p.sy, self.p.sx), pad=(self.p.ph, self.p.pw),
-            outsize=self.in_shape[2:], cover_all=self.p.cover_all)
-        if isinstance(y.data, numpy.ndarray):
-            y = conv.im2col_cpu(y.data, self.p.kh, self.p.kw,
-                                self.p.sy, self.p.sx, self.p.ph, self.p.pw)
+            self.pooled_y, self.indices, ksize=self.ksize,
+            stride=self.stride, outsize=self.in_shape[2:])
+        if isinstance(y.array, numpy.ndarray):
+            y = conv.im2col_cpu(
+                y.array, self.ksize, self.ksize, self.stride, self.stride,
+                0, 0)
         else:
-            y = conv.im2col_gpu(y.data, self.p.kh, self.p.kw,
-                                self.p.sy, self.p.sx, self.p.ph, self.p.pw)
+            y = conv.im2col_gpu(
+                y.array, self.ksize, self.ksize, self.stride, self.stride,
+                0, 0)
         for i in numpy.ndindex(y.shape):
             n, c, ky, kx, oy, ox = i
             up_y = y[n, c, ky, kx, oy, ox]
-            if ky * y.shape[3] + kx == self.p.indexes[n, c, oy, ox]:
-                in_y = self.pooled_y.data[n, c, oy, ox]
+            if ky * y.shape[3] + kx == self.indices[n, c, oy, ox]:
+                in_y = self.pooled_y.array[n, c, oy, ox]
                 testing.assert_allclose(in_y, up_y)
             else:
                 testing.assert_allclose(up_y, 0)
@@ -62,27 +66,25 @@ class TestUpsampling2D(unittest.TestCase):
     def check_backward(self, x_data, y_grad):
         def f(x):
             return F.upsampling_2d(
-                x, self.p.indexes, ksize=(self.p.kh, self.p.kw),
-                stride=(self.p.sy, self.p.sx), pad=(self.p.ph, self.p.pw),
-                outsize=self.in_shape[2:], cover_all=self.p.cover_all)
+                x, self.indices, ksize=self.ksize, stride=self.stride,
+                outsize=self.in_shape[2:])
         gradient_check.check_backward(
             f, x_data, y_grad, dtype='d', **self.check_backward_options)
 
     def test_backward_cpu(self):
-        self.check_backward(self.pooled_y.data, self.gy)
+        self.check_backward(self.pooled_y.array, self.gy)
 
     @attr.gpu
     def test_backward_gpu(self):
         self.check_backward(cuda.to_gpu(
-            self.pooled_y.data), cuda.to_gpu(self.gy))
+            self.pooled_y.array), cuda.to_gpu(self.gy))
 
     def check_double_backward(self, x_data, y_grad, x_grad_grad,
                               use_cudnn='always'):
         def f(x):
             y = F.upsampling_2d(
-                x, self.p.indexes, ksize=(self.p.kh, self.p.kw),
-                stride=(self.p.sy, self.p.sx), pad=(self.p.ph, self.p.pw),
-                outsize=self.in_shape[2:], cover_all=self.p.cover_all)
+                x, self.indices, ksize=self.ksize,
+                stride=self.stride, outsize=self.in_shape[2:])
             return y * y
         with chainer.using_config('use_cudnn', use_cudnn):
             gradient_check.check_double_backward(
@@ -91,25 +93,25 @@ class TestUpsampling2D(unittest.TestCase):
 
     def test_double_backward_cpu(self):
         self.check_double_backward(
-            self.pooled_y.data, self.gy, self.ggx, 'never')
+            self.pooled_y.array, self.gy, self.ggx, 'never')
 
     @attr.gpu
     def test_double_backward_gpu(self):
         self.check_double_backward(
-            cuda.to_gpu(self.pooled_y.data), cuda.to_gpu(self.gy),
+            cuda.to_gpu(self.pooled_y.array), cuda.to_gpu(self.gy),
             cuda.to_gpu(self.ggx))
 
     @attr.gpu
     def test_double_backward_gpu_non_contiguous(self):
         self.check_double_backward(
-            cuda.cupy.asfortranarray(cuda.to_gpu(self.pooled_y.data)),
+            cuda.cupy.asfortranarray(cuda.to_gpu(self.pooled_y.array)),
             cuda.cupy.asfortranarray(cuda.to_gpu(self.gy)),
             cuda.cupy.asfortranarray(cuda.to_gpu(self.ggx)))
 
     @attr.gpu
     def test_double_backward_gpu_no_cudnn(self):
         self.check_double_backward(
-            cuda.to_gpu(self.pooled_y.data), cuda.to_gpu(self.gy),
+            cuda.to_gpu(self.pooled_y.array), cuda.to_gpu(self.gy),
             cuda.to_gpu(self.ggx), 'never')
 
 
