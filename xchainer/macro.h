6c1401d932f9867a8270a04f8036abd569ba4e03
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-06-29 12:48:57 +0900

    Fix nolint and prefer do while(false) macro idiom

--- xchainer/macro.h
+++ xchainer/macro.h
@@ -14,10 +14,10 @@
 #define XCHAINER_NEVER_REACH() (std::abort())
 #else
 #include <cassert>
-#define XCHAINER_NEVER_REACH() \
-    ({                         \
-        assert(false);         \
-        std::abort();          \
-    })  // NOLINT: cert-dcl03-c
+#define XCHAINER_NEVER_REACH()                    \
+    do {                                          \
+        assert(false); /* NOLINT: cert-dcl03-c */ \
+        std::abort();                             \
+    } while (false)
 #endif  // NDEBUG
 #endif  // XCHAINER_NEVER_REACH
