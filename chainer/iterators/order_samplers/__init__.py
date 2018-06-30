5bf8ee74bf729f760dc61d4eb1cbbd5fe821b05c
corochann <acc1ssnn9terias@gmail.com>
2018-06-30 19:04:06 +0900

    add import

--- chainer/iterators/order_samplers/__init__.py
+++ chainer/iterators/order_samplers/__init__.py
@@ -1,2 +1,3 @@
+from chainer.iterators.order_samplers.balanced_order_sampler import BalancedOrderSampler  # NOQA
 from chainer.iterators.order_samplers.order_sampler import OrderSampler  # NOQA
 from chainer.iterators.order_samplers.shuffle_order_sampler import ShuffleOrderSampler  # NOQA
