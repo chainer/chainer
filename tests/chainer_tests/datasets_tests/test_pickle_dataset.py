6e3a2d1412e6ca546f160c231516c5e937cbb307
Yuya Unno <unnonouno@gmail.com>
2018-07-01 18:17:47 +0900

    Add test

--- tests/chainer_tests/datasets_tests/test_pickle_dataset.py
+++ tests/chainer_tests/datasets_tests/test_pickle_dataset.py
@@ -1,5 +1,9 @@
 import io
+import os
+import tempfile
 import unittest
+
+import mock
 
 from chainer import datasets
 
@@ -21,3 +25,37 @@ class TestPickleDataset(unittest.TestCase):
         assert dataset[0] == 1
         assert dataset[2] == 1.5
         assert dataset[1] == 'hello'
+
+    def test_reader_unseekable(self):
+        reader = mock.MagicMock()
+        reader.seekable = mock.MagicMock(return_value=False)
+        with self.assertRaises(ValueError):
+            datasets.PickleDataset(reader)
+
+
+class TestPickleDatasetHelper(unittest.TestCase):
+
+    def setUp(self):
+        _, self.path = tempfile.mkstemp()
+
+    def tearDown(self):
+        if os.path.exists(self.path):
+            os.remove(self.path)
+
+    def test_write_read(self):
+        with datasets.open_pickle_dataset_writer(self.path) as writer:
+            writer.write(1)
+
+        with datasets.open_pickle_dataset(self.path) as dataset:
+            assert dataset[0] == 1
+
+    def test_dataset_init_fail(self):
+        m = mock.mock_open(read_data=b'dummy')
+        with mock.patch('chainer.datasets.pickle_dataset.open', m):
+            try:
+                datasets.open_pickle_dataset(self.path)
+            except Exception:
+                # This dummy data causes an error in initializer
+                pass
+        # Even in this situation, the opened file needs to be closed
+        assert mock.call().close() in m.mock_calls
