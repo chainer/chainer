f0e0c680138e0d8ce10f80d34489a0511388a4c7
Toshiki Kataoka <kataoka@preferred.jp>
2018-07-02 09:11:31 +0900

    Hide running stats and warn on access

--- chainer/functions/normalization/batch_renormalization.py
+++ chainer/functions/normalization/batch_renormalization.py
@@ -28,8 +28,8 @@ class BatchRenormalizationFunction(function.Function):
 
     def __init__(self, eps=2e-5, mean=None, var=None, decay=0.9,
                  rmax=1, dmax=0, update_statistics=True):
-        self.running_mean = mean
-        self.running_var = var
+        self._running_mean = mean
+        self._running_var = var
         self.rmax = rmax
         self.dmax = dmax
         self.r = None
@@ -37,6 +37,25 @@ class BatchRenormalizationFunction(function.Function):
 
         self.eps = eps
         self.decay = decay
+
+    def _warn_accessing_property(self):
+        msg ='''\
+batch_renormalization function updates running statistics by default. The \
+properties of BatchRenormalizationFunction should not be accessed.
+        
+Set update_statistics=False to stop updating.'''
+        if self.update_statistics:
+            warnings.warn(msg, DeprecationWarning)
+
+    @property
+    def running_mean(self):
+        self._warn_accessing_property()
+        return self._running_mean
+
+    @property
+    def running_var(self):
+        self._warn_accessing_property()
+        return self._running_var
 
     def check_type_forward(self, in_types):
         type_check.expect(in_types.size() == 3)
@@ -60,8 +79,8 @@ class BatchRenormalizationFunction(function.Function):
         assert configuration.config.train
 
         if not self.update_statistics:
-            self.running_mean = xp.array(self.running_mean)
-            self.running_var = xp.array(self.running_var)
+            self._running_mean = xp.array(self._running_mean)
+            self._running_var = xp.array(self._running_var)
 
         head_ndim = gamma.ndim + 1
         expander = (None, Ellipsis) + (None,) * (x.ndim - head_ndim)
@@ -73,25 +92,25 @@ class BatchRenormalizationFunction(function.Function):
         var = x.var(axis=axis) + self.eps
         self.std = xp.sqrt(var, dtype=var.dtype)
 
-        running_sigma = xp.sqrt(self.running_var + self.eps,
-                                dtype=self.running_mean.dtype)
+        running_sigma = xp.sqrt(self._running_var + self.eps,
+                                dtype=self._running_mean.dtype)
         self.r = xp.clip(self.std / running_sigma,
                          1.0 / self.rmax, self.rmax)
-        d = xp.clip((mean - self.running_mean) / running_sigma,
+        d = xp.clip((mean - self._running_mean) / running_sigma,
                          -self.dmax, self.dmax)
 
         # Update running statistics:
         m = x.size // gamma[expander].size
-        self.running_mean *= self.decay
+        self._running_mean *= self.decay
         adjust = m / max(m - 1., 1.)  # unbiased estimation
         temp_ar = xp.array(mean)
         temp_ar *= (1 - self.decay)
-        self.running_mean += temp_ar
+        self._running_mean += temp_ar
         del temp_ar
-        self.running_var *= self.decay
+        self._running_var *= self.decay
         temp_ar = xp.array(var)
         temp_ar *= (1 - self.decay) * adjust
-        self.running_var += temp_ar
+        self._running_var += temp_ar
         del temp_ar
 
         gamma = gamma[expander]
@@ -152,7 +171,7 @@ class BatchRenormalizationFunction(function.Function):
 
 def batch_renormalization(x, gamma, beta, rmax, dmax, eps=2e-5,
                           running_mean=None, running_var=None, decay=0.9,
-                          update_statistics=None):
+                          update_statistics=True):
     """Batch renormalization function.
 
     This is an extension of batch normalization, which ensures that the
@@ -177,12 +196,6 @@ def batch_renormalization(x, gamma, beta, rmax, dmax, eps=2e-5,
     .. seealso:: :class:`functions.normalization.batch_normalization.BatchNormalization`  # NOQA
 
     """
-    if update_statistics is None:
-        warnings.warn(
-            'In future, batch_renormalization function will update statistics'
-            ' by default.',
-            FutureWarning)
-        update_statistics = False
     return BatchRenormalizationFunction(
         eps, running_mean, running_var, decay, rmax, dmax, update_statistics
     )(x, gamma, beta)
