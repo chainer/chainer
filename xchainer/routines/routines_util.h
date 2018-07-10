c5941aa9ea6604292667d7d6f57c3b247ef790d5
sonots <sonots@gmail.com>
2018-07-10 12:45:50 +0900

    routines_util.h: Use IsGradRequired instead of IsConstant

--- xchainer/routines/routines_util.h
+++ xchainer/routines/routines_util.h
@@ -10,19 +10,19 @@ namespace xchainer {
 namespace internal {
 
 void CheckNoInplaceWithRequiredGrad(const Array& out, std::initializer_list<std::reference_wrapper<const Array>> inputs) {
-    if (!out.IsConstant()) {
-        throw XchainerError{"In-place assignment to non-constant output array is not allowed."};
+    if (out.IsGradRequired(AnyGraph{})) {
+        throw XchainerError{"In-place assignment to output array requiring grad is not allowed."};
     }
 
-    bool any_non_const = false;
+    bool any_grad_required = false;
     bool any_inplace = false;
     for (const Array& input : inputs) {
-        any_non_const |= !input.IsConstant();
+        any_grad_required |= input.IsGradRequired(AnyGraph{});
         any_inplace |= (out.body() == input.body());
     }
 
-    if (any_non_const && any_inplace) {
-        throw XchainerError{"In-place assignment that involves non-constant input arrays is not allowed."};
+    if (any_grad_required && any_inplace) {
+        throw XchainerError{"In-place assignment that involves input arrays requiring grad is not allowed."};
     }
 }
 
