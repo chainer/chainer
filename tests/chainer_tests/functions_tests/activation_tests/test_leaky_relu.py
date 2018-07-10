aa6ed75120a8d0f48bd7e545099e0258ffb34e6f
DL Test <dl_test@intel.com>
2018-07-10 17:39:42 +0800

    add leaky_relu test case

--- tests/chainer_tests/functions_tests/activation_tests/test_leaky_relu.py
+++ tests/chainer_tests/functions_tests/activation_tests/test_leaky_relu.py
@@ -9,6 +9,7 @@ from chainer import functions
 from chainer import gradient_check
 from chainer import testing
 from chainer.testing import attr
+from chainer.testing import backend
 
 
 @testing.parameterize(*testing.product({
@@ -16,6 +17,12 @@ from chainer.testing import attr
     'dtype': [numpy.float16, numpy.float32, numpy.float64],
 }))
 @testing.fix_random()
+@backend.inject_backend_tests(
+    ['test_forward_cpu', 'test_backward_cpu', 'test_double_backward_cpu'],
+    # CPU tests
+    testing.product({
+        'use_ideep': ['never', 'always'],
+    }))
 class TestLeakyReLU(unittest.TestCase):
 
     def setUp(self):
@@ -33,9 +40,10 @@ class TestLeakyReLU(unittest.TestCase):
             self.check_backward_options = {'atol': 5e-4, 'rtol': 5e-3}
             self.check_double_backward_options = {'atol': 5e-3, 'rtol': 5e-2}
 
-    def check_forward(self, x_data):
+    def check_forward(self, x_data, backend_config):
         x = chainer.Variable(x_data)
-        y = functions.leaky_relu(x, slope=self.slope)
+        with backend_config:
+            y = functions.leaky_relu(x, slope=self.slope)
         self.assertEqual(y.data.dtype, self.dtype)
 
         expected = numpy.where(self.x >= 0, self.x, self.x * self.slope)
@@ -43,39 +51,41 @@ class TestLeakyReLU(unittest.TestCase):
         testing.assert_allclose(
             expected, y.data, **self.check_forward_options)
 
-    def test_forward_cpu(self):
-        self.check_forward(self.x)
+    def test_forward_cpu(self, backend_config):
+        self.check_forward(self.x, backend_config)
 
     @attr.gpu
     def test_forward_gpu(self):
         self.check_forward(cuda.to_gpu(self.x))
 
-    def check_backward(self, x_data, y_grad):
+    def check_backward(self, x_data, y_grad, backend_config):
         def f(x):
-            return functions.leaky_relu(x, self.slope)
+            with backend_config:
+                return functions.leaky_relu(x, self.slope)
 
         gradient_check.check_backward(
             f, x_data, y_grad, dtype=numpy.float64,
             **self.check_backward_options)
 
-    def test_backward_cpu(self):
-        self.check_backward(self.x, self.gy)
+    def test_backward_cpu(self, backend_config):
+        self.check_backward(self.x, self.gy, backend_config)
 
     @attr.gpu
     def test_backward_gpu(self):
         self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
 
-    def check_double_backward(self, x_data, y_grad, x_grad_grad):
+    def check_double_backward(self, x_data, y_grad,
+                              x_grad_grad, backend_config):
         def f(x):
-            y = functions.leaky_relu(x, self.slope)
-            return y * y
-
+            with backend_config:
+                y = functions.leaky_relu(x, self.slope)
+                return y * y
         gradient_check.check_double_backward(
             f, x_data, y_grad, x_grad_grad, dtype=numpy.float64,
             **self.check_double_backward_options)
 
-    def test_double_backward_cpu(self):
-        self.check_double_backward(self.x, self.gy, self.ggx)
+    def test_double_backward_cpu(self, backend_config):
+        self.check_double_backward(self.x, self.gy, self.ggx, backend_config)
 
     @attr.gpu
     def test_double_backward_gpu(self):
