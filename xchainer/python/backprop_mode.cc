dd34f56bc8d968af8e531792e82e8fd142338918
Seiya Tokui <beam.web@gmail.com>
2018-07-18 05:54:43 +0000

    Fix backprop mode and its tests for graph scope

--- xchainer/python/backprop_mode.cc
+++ xchainer/python/backprop_mode.cc
@@ -60,7 +60,7 @@ void InitXchainerBackpropMode(pybind11::module& m) {
 
     // TODO(sonots): Remove context because graph_id has context now.
     m.def("is_backprop_required",
-          [](const GraphId& graph_id, py::handle context) { return IsBackpropRequired(graph_id, GetContext(context)); },
+          [](const nonstd::optional<GraphId>& graph_id, py::handle context) { return IsBackpropRequired(graph_id, GetContext(context)); },
           py::arg("graph_id") = nullptr,
           py::arg("context") = py::none());
 }
