455d2d668eb524677167ef21d1663aa2f25e948b
Seiya Tokui <beam.web@gmail.com>
2018-07-18 04:45:19 +0000

    Stop using DEFAULT_GRAPH_ID

--- tests/python_tests/test_backward.py
+++ tests/python_tests/test_backward.py
@@ -12,7 +12,7 @@ def _assert_arrays_equal(array1, array2):
         assert array1._debug_flat_data == array2._debug_flat_data
 
 
-def _check_backprop(xs, expected_gxs, fprop, extra_xs, graph_id=xchainer.DEFAULT_GRAPH_ID):
+def _check_backprop(xs, expected_gxs, fprop, extra_xs, graph_id=None):
     # Checks for test validity
     assert isinstance(xs, tuple)
     assert isinstance(expected_gxs, tuple)
