25298496360b46655ed5b0bed6052d92af4db22c
sonots <sonots@gmail.com>
2018-07-18 14:05:52 +0900

    NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)

--- xchainer/python/slice.cc
+++ xchainer/python/slice.cc
@@ -9,7 +9,7 @@ namespace python_internal {
 namespace py = pybind11;
 
 Slice MakeSlice(const py::slice& slice) {
-    const auto* py_slice = reinterpret_cast<const PySliceObject*>(slice.ptr());  // NOLINT: reinterpret_cast
+    const auto* py_slice = reinterpret_cast<const PySliceObject*>(slice.ptr());  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
     auto to_optional = [](PyObject* var) -> nonstd::optional<int64_t> {
         if (var == Py_None) {
             return nonstd::nullopt;
