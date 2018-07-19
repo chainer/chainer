91fb9efefcd01b961a0ab2d8603ae895b6964c3c
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-07-19 11:00:37 +0900

    add cupy-cuda92 support

--- setup.py
+++ setup.py
@@ -111,6 +111,7 @@ def find_any_distribution(pkgs):
 # Currently cupy provides source package (cupy) and binary wheel packages
 # (cupy-cudaXX). Chainer can use any one of these packages.
 cupy_pkg = find_any_distribution([
+    'cupy-cuda92',
     'cupy-cuda91',
     'cupy-cuda90',
     'cupy-cuda80',
