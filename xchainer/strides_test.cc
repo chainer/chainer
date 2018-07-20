70b3b48723ab2539d9bde3f088c5a720d5fddef7
Akifumi Imanishi <imanishi@preferred.jp>
2018-07-20 12:25:53 +0900

    Define GetDataRange structure

--- xchainer/strides_test.cc
+++ xchainer/strides_test.cc
@@ -147,30 +147,32 @@ TEST(StridesTest, SpanFromStrides) {
     CheckSpanEqual({2, 3, 4}, gsl::make_span(strides));
 }
 
-using GetDataRangeTestType = std::tuple<Shape, Strides, size_t, int64_t, int64_t>;
-class GetDataRangeTest : public ::testing::TestWithParam<GetDataRangeTestType> {};
+struct GetDataRangeTestParams {
+    Shape shape;
+    Strides strides;
+    size_t itemsize;
+    int64_t first;
+    int64_t last;
+};
+
+class GetDataRangeTest : public ::testing::TestWithParam<GetDataRangeTestParams> {};
 INSTANTIATE_TEST_CASE_P(
         GetDataRangeTest,
         GetDataRangeTest,
         ::testing::Values(
-                GetDataRangeTestType{{2, 3, 4}, {96, 32, 8}, 8, 0, 192},
-                GetDataRangeTestType{{10, 12}, {160, 8}, 8, 0, 1536},
-                GetDataRangeTestType{{}, {}, 8, 0, 8},
-                GetDataRangeTestType{{3, 0, 3}, {24, 24, 8}, 8, 0, 0},
-                GetDataRangeTestType{{10, 3, 4}, {-96, 32, 8}, 8, -864, 96},
-                GetDataRangeTestType{{10, 3, 4}, {-96, -32, -8}, 8, -952, 8},
-                GetDataRangeTestType{{3, 4}, {8, 24}, 8, 0, 96},
-                GetDataRangeTestType{{100}, {24}, 8, 0, 2384}));
+                GetDataRangeTestParams{{2, 3, 4}, {96, 32, 8}, 8, 0, 192},
+                GetDataRangeTestParams{{10, 12}, {160, 8}, 8, 0, 1536},
+                GetDataRangeTestParams{{}, {}, 8, 0, 8},
+                GetDataRangeTestParams{{3, 0, 3}, {24, 24, 8}, 8, 0, 0},
+                GetDataRangeTestParams{{10, 3, 4}, {-96, 32, 8}, 8, -864, 96},
+                GetDataRangeTestParams{{10, 3, 4}, {-96, -32, -8}, 8, -952, 8},
+                GetDataRangeTestParams{{3, 4}, {8, 24}, 8, 0, 96},
+                GetDataRangeTestParams{{100}, {24}, 8, 0, 2384}));
 
 TEST_P(GetDataRangeTest, GetDataRange) {
-    Shape shape;
-    Strides strides;
-    size_t itemsize;
-    int64_t expected_first, expected_last, actual_first, actual_last;
-    std::tie(shape, strides, itemsize, expected_first, expected_last) = GetParam();
-    std::tie(actual_first, actual_last) = GetDataRange(shape, strides, itemsize);
-    EXPECT_EQ(actual_first, expected_first);
-    EXPECT_EQ(actual_last, expected_last);
+    GetDataRangeTestParams param = GetParam();
+    std::tuple<int64_t, int64_t> actual = GetDataRange(param.shape, param.strides, param.itemsize);
+    EXPECT_EQ(actual, std::make_tuple(param.first, param.last));
 }
 
 }  // namespace
