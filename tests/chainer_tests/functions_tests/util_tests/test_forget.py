ee02606ae9ea18a77568d5f0838f5a3444bb4546
Mitsuru Kusumoto <mkusumoto@preferred.jp>
2018-08-06 14:50:25 +0900

    Add tests for cases when output is tuple

--- tests/chainer_tests/functions_tests/util_tests/test_forget.py
+++ tests/chainer_tests/functions_tests/util_tests/test_forget.py
@@ -12,12 +12,16 @@ from chainer.testing import attr
 from chainer import variable
 
 
+@testing.parameterize(*testing.product({
+    'out_len': [1, 2],
+}))
 class TestForget(unittest.TestCase):
 
     def setUp(self):
         self.x = numpy.random.uniform(-1, 1, (3, 4)).astype(numpy.float32)
         self.y = numpy.random.uniform(-1, 1, (3, 4)).astype(numpy.float32)
-        self.gz = numpy.random.uniform(-1, 1, (3, 4)).astype(numpy.float32)
+        self.gz0 = numpy.random.uniform(-1, 1, (3, 4)).astype(numpy.float32)
+        self.gz1 = numpy.random.uniform(-1, 1, (3, 4)).astype(numpy.float32)
         self.ggx = numpy.random.uniform(-1, 1, (3, 4)).astype(numpy.float32)
         self.ggy = numpy.random.uniform(-1, 1, (3, 4)).astype(numpy.float32)
 
@@ -27,26 +31,41 @@ class TestForget(unittest.TestCase):
     def check_forward(self, x_data, y_data):
         x = chainer.Variable(x_data)
         y = chainer.Variable(y_data)
-        z = functions.forget(lambda x, y: (x + y + x,), x, y)
-        testing.assert_allclose(x_data + y_data + x_data, z.data)
+        if self.out_len == 1:
+            z = functions.forget(lambda x, y: (x + y + x,), x, y)
+            testing.assert_allclose(x_data + y_data + x_data, z.data)
+        elif self.out_len == 2:
+            z = functions.forget(lambda x, y: (x + y + x, x * y), x, y)
+            testing.assert_allclose(x_data + y_data + x_data, z[0].data)
+            testing.assert_allclose(x_data * y_data, z[1].data)
 
     def test_forward_cpu(self):
         self.check_forward(self.x, self.y)
 
-    def check_backward(self, x_data, y_data, gz_data):
+    def check_backward(self, x_data, y_data, *gz_data):
         def f(x, y):
-            return functions.forget(lambda x, y: (x + y + x), x, y)
+            if self.out_len == 1:
+                return functions.forget(lambda x, y: (x + y + x), x, y)
+            elif self.out_len == 2:
+                return functions.forget(lambda x, y: (x + y + x, x * y), x, y)
 
         gradient_check.check_backward(
             f, (x_data, y_data), gz_data, **self.check_backward_options)
 
     def test_backward_cpu(self):
-        self.check_backward(self.x, self.y, self.gz)
+        if self.out_len == 1:
+            self.check_backward(self.x, self.y, self.gz0)
+        elif self.out_len == 2:
+            self.check_backward(self.x, self.y, self.gz0, self.gz1)
 
     @attr.gpu
     def test_backward_gpu(self):
-        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.y),
-                            cuda.to_gpu(self.gz))
+        if self.out_len == 1:
+            self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.y),
+                                cuda.to_gpu(self.gz0))
+        elif self.out_len == 2:
+            self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.y),
+                                cuda.to_gpu(self.gz0), cuda.to_gpu(self.gz1))
 
 
 class TestForgetError(unittest.TestCase):
