cbe589084ba3ea8589776080c0010983a91b52eb
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-08-06 23:14:14 +0900

    Improve type check messages for some math functions

--- chainer/functions/math/einsum.py
+++ chainer/functions/math/einsum.py
@@ -72,7 +72,8 @@ class EinSum(function_node.FunctionNode):
         self.out_sub = out_sub
 
     def check_type_forward(self, in_types):
-        for in_type in in_types:
+        for i, in_type in enumerate(in_types):
+            type_check.argname((in_type,), ('x{}'.format(i),))
             type_check.expect(in_type.dtype.kind == 'f')
 
         in_subs = self.in_subs.split(',')
