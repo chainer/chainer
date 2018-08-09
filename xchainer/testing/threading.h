b42841b71491e1f3e3e4a3acaff8be3cc3428903
niboshi <niboshi000@gmail.com>
2018-08-09 18:14:00 +0900

    Use std::async

--- xchainer/testing/threading.h
+++ xchainer/testing/threading.h
@@ -2,7 +2,7 @@
 
 #include <condition_variable>
 #include <cstddef>
-#include <thread>
+#include <future>
 #include <vector>
 
 namespace xchainer {
@@ -15,12 +15,9 @@ auto RunThreads(size_t thread_count, const Func& func, Args&&... args) -> std::v
     std::mutex mutex;
     std::condition_variable cv_all_ready;
 
-    std::vector<ResultType> results;
-    results.resize(thread_count);
-
     size_t wait_count = thread_count;
 
-    auto thread_proc = [&mutex, &cv_all_ready, &wait_count, &func, &results, &args...](size_t thread_index) mutable {
+    auto thread_proc = [&mutex, &cv_all_ready, &wait_count, &func, &args...](size_t thread_index) mutable -> ResultType {
         {
             std::unique_lock<std::mutex> lock{mutex};
 
@@ -35,24 +32,22 @@ auto RunThreads(size_t thread_count, const Func& func, Args&&... args) -> std::v
             }
         }
 
-        ResultType result = func(thread_index, args...);
-
-        {
-            std::lock_guard<std::mutex> lock{mutex};
-            gsl::at(results, thread_index) = std::move(result);
-        }
+        return func(thread_index, args...);
     };
 
     // Launch threads
-    std::vector<std::thread> threads;
-    threads.reserve(thread_count);
+    std::vector<std::future<ResultType>> futures;
+    futures.reserve(thread_count);
+
     for (size_t i = 0; i < thread_count; ++i) {
-        threads.emplace_back(thread_proc, i);
+        futures.emplace_back(std::async(std::launch::async, thread_proc, i));
     }
 
-    // Join threads
-    for (std::thread& thread : threads) {
-        thread.join();
+    // Retrieve results
+    std::vector<ResultType> results;
+    results.reserve(thread_count);
+    for (size_t i = 0; i < thread_count; ++i) {
+        results.emplace_back(futures[i].get());
     }
 
     return results;
