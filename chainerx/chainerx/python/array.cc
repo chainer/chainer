1996f9223e50d60f098307aaee85b02267eeda94
Akifumi Imanishi <imanishi@preferred.jp>
2018-09-12 03:20:13 +0900

    Support zero-copy conversion to numpy ndarray

--- chainerx/chainerx/python/array.cc
+++ chainerx/chainerx/python/array.cc
@@ -89,12 +89,11 @@ ArrayBodyPtr MakeArray(const py::tuple& shape_tup, Dtype dtype, const py::list& 
 
 py::array MakeNumpyArrayFromArray(const ArrayBodyPtr& self) {
     Array array = Array{self}.ToNative();
-    return py::array{py::buffer_info{internal::GetRawOffsetData<void>(array),
-                                     array.item_size(),
-                                     std::string(1, GetCharCode(array.dtype())),
-                                     array.ndim(),
-                                     array.shape(),
-                                     array.strides()}};
+    return py::array{pybind11::dtype(std::string(1, GetCharCode(array.dtype()))),
+                     array.shape(),
+                     array.strides(),
+                     internal::GetRawOffsetData<void>(array),
+                     py::cast(internal::MoveArrayBody(std::move(array)))};
 }
 
 }  // namespace
