0b5a446b5bea56508f960fdf117c41688ea8fc3e
Masayuki Takagi <kamonama@gmail.com>
2018-09-13 15:31:44 +0900

    Add dtype axis to F.get_item parameterized test

--- tests/chainer_tests/functions_tests/array_tests/test_get_item.py
+++ tests/chainer_tests/functions_tests/array_tests/test_get_item.py
@@ -8,29 +8,34 @@ from chainer import functions
 from chainer import gradient_check
 from chainer import testing
 from chainer.testing import attr
-from chainer.testing import parameterize
 
 
-@parameterize(
-    {'axes': [1, 2], 'offsets': 0, 'sliced_shape': (4, 2, 1)},
-    {'axes': [1, 2], 'offsets': [0, 1, 1], 'sliced_shape': (4, 2, 1)},
-    {'axes': 1, 'offsets': 1, 'sliced_shape': (4, 2, 2)},
-    {'axes': 1, 'offsets': [0, 1, 1], 'sliced_shape': (4, 2, 2)},
-    {'axes': [], 'offsets': 0, 'new_axes': 0, 'sliced_shape': (1, 4, 3, 2)},
-    {'axes': [], 'offsets': 0, 'new_axes': 2, 'sliced_shape': (4, 3, 1, 2)},
-    {'axes': [], 'offsets': 0, 'new_axes': 3, 'sliced_shape': (4, 3, 2, 1)},
-    {'slices': (1, -1, 0), 'sliced_shape': ()},
-    {'slices': (1, -1), 'sliced_shape': (2,)},
-    {'slices': (1, Ellipsis, -1), 'sliced_shape': (3,)},
-    {'slices': (1, None, Ellipsis, None, -1), 'sliced_shape': (1, 3, 1)},
-)
+@testing.parameterize(*testing.product_dict(
+    [{'dtype': numpy.float16},
+     {'dtype': numpy.float32},
+     {'dtype': numpy.float64},
+     ],
+    [{'axes': [1, 2], 'offsets': 0, 'sliced_shape': (4, 2, 1)},
+     {'axes': [1, 2], 'offsets': [0, 1, 1], 'sliced_shape': (4, 2, 1)},
+     {'axes': 1, 'offsets': 1, 'sliced_shape': (4, 2, 2)},
+     {'axes': 1, 'offsets': [0, 1, 1], 'sliced_shape': (4, 2, 2)},
+     {'axes': [], 'offsets': 0, 'new_axes': 0, 'sliced_shape': (1, 4, 3, 2)},
+     {'axes': [], 'offsets': 0, 'new_axes': 2, 'sliced_shape': (4, 3, 1, 2)},
+     {'axes': [], 'offsets': 0, 'new_axes': 3, 'sliced_shape': (4, 3, 2, 1)},
+     {'slices': (1, -1, 0), 'sliced_shape': ()},
+     {'slices': (1, -1), 'sliced_shape': (2,)},
+     {'slices': (1, Ellipsis, -1), 'sliced_shape': (3,)},
+     {'slices': (1, None, Ellipsis, None, -1), 'sliced_shape': (1, 3, 1)},
+    ]
+))
 class TestGetItem(unittest.TestCase):
 
     def setUp(self):
-        self.x_data = numpy.random.uniform(-1, 1, (4, 3, 2))
+        self.x_data = numpy.random.uniform(-1, 1, (4, 3, 2)).astype(self.dtype)
         self.shape = (4, 2, 1)
-        self.gy_data = numpy.random.uniform(-1, 1, self.sliced_shape)
-        self.ggx_data = numpy.random.uniform(-1, 1, (4, 3, 2))
+        self.gy_data = numpy.random.uniform(
+            -1, 1, self.sliced_shape).astype(self.dtype)
+        self.ggx_data = numpy.random.uniform(-1, 1, (4, 3, 2)).astype(self.dtype)
 
         if not hasattr(self, 'slices'):
             # Convert axes, offsets and shape to slices
@@ -52,7 +57,7 @@ class TestGetItem(unittest.TestCase):
     def check_forward(self, x_data):
         x = chainer.Variable(x_data)
         y = functions.get_item(x, self.slices)
-        self.assertEqual(y.data.dtype, numpy.float)
+        self.assertEqual(y.data.dtype, self.dtype)
         numpy.testing.assert_equal(cuda.to_cpu(x_data)[self.slices],
                                    cuda.to_cpu(y.data))
 
@@ -95,53 +100,58 @@ class TestGetItem(unittest.TestCase):
                                    cuda.to_gpu(self.ggx_data))
 
 
-@testing.parameterize(
-    {'slices': [], 'sliced_shape': (0, 3, 2)},
-    {'slices': ([],), 'sliced_shape': (0, 3, 2)},
-    {'slices': ([[]],), 'sliced_shape': (1, 0, 3, 2)},
-    {'slices': numpy.array([], dtype=numpy.bool),
-        'sliced_shape': (0, 3, 2)},
-    {'slices': (1, [1]), 'sliced_shape': (1, 2)},
-    {'slices': ([1], slice(1, 2)), 'sliced_shape': (1, 1, 2)},
-    {'slices': [1, 0], 'sliced_shape': (2, 3, 2)},
-    {'slices': ([1, 0],), 'sliced_shape': (2, 3, 2)},
-    {'slices': numpy.array([[1, 0], [2, 3]]),
-        'sliced_shape': (2, 2, 3, 2)},
-    {'slices': ([1, 0], [1, 1]), 'sliced_shape': (2, 2)},
-    {'slices': ([1, 0], slice(None), [[1, 1], [1, 1]]),
-        'sliced_shape': (2, 2, 3)},
-    {'slices': ([1, 0], slice(1, 2), [0, 0]), 'sliced_shape': (2, 1)},
-    {'slices': ([[1, 1], [1, 0]], slice(1, 2), 1),
-        'sliced_shape': (2, 2, 1)},
-    {'slices': numpy.array([True] * 18 + [False] * 6).reshape(4, 3, 2),
-        'sliced_shape': (18,)},
-    {'slices': numpy.array([True, False, False, True]),
-        'sliced_shape': (2, 3, 2)},
-    {'slices': (slice(None), numpy.array([True, False, True])),
-        'sliced_shape': (4, 2, 2)},
-    {'slices': numpy.array([False, False, False, False]),
-        'sliced_shape': (0, 3, 2)},
-    {'slices': (3, 2, Ellipsis, 1),
-        'sliced_shape': ()},
-    {'slices': (numpy.array(False)),
-        'input_shape': (), 'sliced_shape': (0,)},
-    {'slices': (numpy.array(True)),
-        'input_shape': (), 'sliced_shape': (1,)},
-)
+@testing.parameterize(*testing.product_dict(
+    [{'dtype': numpy.float16},
+     {'dtype': numpy.float32},
+     {'dtype': numpy.float64},
+     ],
+    [{'slices': [], 'sliced_shape': (0, 3, 2)},
+     {'slices': ([],), 'sliced_shape': (0, 3, 2)},
+     {'slices': ([[]],), 'sliced_shape': (1, 0, 3, 2)},
+     {'slices': numpy.array([], dtype=numpy.bool),
+         'sliced_shape': (0, 3, 2)},
+     {'slices': (1, [1]), 'sliced_shape': (1, 2)},
+     {'slices': ([1], slice(1, 2)), 'sliced_shape': (1, 1, 2)},
+     {'slices': [1, 0], 'sliced_shape': (2, 3, 2)},
+     {'slices': ([1, 0],), 'sliced_shape': (2, 3, 2)},
+     {'slices': numpy.array([[1, 0], [2, 3]]),
+         'sliced_shape': (2, 2, 3, 2)},
+     {'slices': ([1, 0], [1, 1]), 'sliced_shape': (2, 2)},
+     {'slices': ([1, 0], slice(None), [[1, 1], [1, 1]]),
+         'sliced_shape': (2, 2, 3)},
+     {'slices': ([1, 0], slice(1, 2), [0, 0]), 'sliced_shape': (2, 1)},
+     {'slices': ([[1, 1], [1, 0]], slice(1, 2), 1),
+         'sliced_shape': (2, 2, 1)},
+     {'slices': numpy.array([True] * 18 + [False] * 6).reshape(4, 3, 2),
+         'sliced_shape': (18,)},
+     {'slices': numpy.array([True, False, False, True]),
+         'sliced_shape': (2, 3, 2)},
+     {'slices': (slice(None), numpy.array([True, False, True])),
+         'sliced_shape': (4, 2, 2)},
+     {'slices': numpy.array([False, False, False, False]),
+         'sliced_shape': (0, 3, 2)},
+     {'slices': (3, 2, Ellipsis, 1),
+         'sliced_shape': ()},
+     {'slices': (numpy.array(False)),
+         'input_shape': (), 'sliced_shape': (0,)},
+     {'slices': (numpy.array(True)),
+         'input_shape': (), 'sliced_shape': (1,)},
+     ]
+))
 class TestGetItemAdvanced(unittest.TestCase):
 
     input_shape = (4, 3, 2)
 
     def setUp(self):
         self.x_data = numpy.random.uniform(
-            -1, 1, self.input_shape).astype(numpy.float32)
+            -1, 1, self.input_shape).astype(self.dtype)
         self.gy_data = numpy.random.uniform(
-            -1, 1, self.sliced_shape).astype(numpy.float32)
+            -1, 1, self.sliced_shape).astype(self.dtype)
 
     def check_forward(self, x_data):
         x = chainer.Variable(x_data)
         y = functions.get_item(x, self.slices)
-        self.assertEqual(y.data.dtype, numpy.float32)
+        self.assertEqual(y.data.dtype, self.dtype)
         numpy.testing.assert_equal(cuda.to_cpu(x_data)[self.slices],
                                    cuda.to_cpu(y.data))
 
@@ -168,7 +178,7 @@ class TestGetItemAdvanced(unittest.TestCase):
                             cuda.to_gpu(self.gy_data))
 
 
-@parameterize(
+@testing.parameterize(
     {'slices': ([1, 0], [1, 1]), 'sliced_shape': (2, 2)},
     {'slices': ([1, 0], slice(None), [[1, 1], [1, 1]]),
      'sliced_shape': (2, 2, 3)},
