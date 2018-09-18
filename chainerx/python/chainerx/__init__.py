83061a983076c68f71447e9c95ab941ae59b09cc
niboshi <niboshi000@gmail.com>
2018-09-18 13:48:50 +0900

    Support for Python 2 in setup.py

--- chainerx/python/chainerx/__init__.py
+++ chainerx/python/chainerx/__init__.py
@@ -1,30 +1,23 @@
 # `testing` needs to be imported before `_core`, because importing `_core` would populate sys.modules['chainerx.testing'].
 from chainerx import testing  # NOQA
 
-try:
-    from chainerx._core import *  # NOQA
-    _available = True
-except ImportError:
-    _available = False
+from chainerx._core import *  # NOQA
 
-    del testing
+from builtins import bool, int, float  # NOQA
+
+from chainerx import _core
+
+from chainerx.creation.from_data import asanyarray  # NOQA
+from chainerx.creation.from_data import fromfile  # NOQA
+from chainerx.creation.from_data import fromfunction  # NOQA
+from chainerx.creation.from_data import fromiter  # NOQA
+from chainerx.creation.from_data import fromstring  # NOQA
+from chainerx.creation.from_data import loadtxt  # NOQA
 
 
-if _available:
-    from builtins import bool, int, float  # NOQA
-
-    from chainerx import _core
-
-    from chainerx.creation.from_data import asanyarray  # NOQA
-    from chainerx.creation.from_data import fromfile  # NOQA
-    from chainerx.creation.from_data import fromfunction  # NOQA
-    from chainerx.creation.from_data import fromiter  # NOQA
-    from chainerx.creation.from_data import fromstring  # NOQA
-    from chainerx.creation.from_data import loadtxt  # NOQA
-
-    _global_context = _core.Context()
-    _core.set_global_default_context(_global_context)
+_global_context = _core.Context()
+_core.set_global_default_context(_global_context)
 
 
 def is_available():
-    return _available
+    return True
