d0f3e997957203a8cfc80c9bf34d090e6e5dee07
Katsuhiko Nishimra <ktns.87@gmail.com>
2018-09-28 19:52:25 +0900

    Parametrize tests

--- tests/chainer_tests/training_tests/triggers_tests/test_minmax_trigger.py
+++ tests/chainer_tests/training_tests/triggers_tests/test_minmax_trigger.py
@@ -1,90 +1,172 @@
 import tempfile
 import unittest
 
-import numpy
-
 from chainer import serializers
 from chainer import testing
 from chainer.training import triggers
 
 
-def _test_trigger(self, trigger, key, accuracies, expected,
-                  resume=None, save=None):
-    trainer = testing.get_trainer_with_mock_updater(
-        stop_trigger=(len(accuracies), 'iteration'))
+class BestValueTriggerTester(object):
+    def _test_trigger(self, trigger, key, accuracies, expected,
+                      resume=None, save=None):
+        trainer = testing.get_trainer_with_mock_updater(
+            stop_trigger=(len(accuracies), 'iteration'),
+            iter_per_epoch=self.iter_per_epoch)
+        updater = trainer.updater
 
-    def set_observation(t):
-        t.observation = {key: accuracies[t.updater.iteration-1]}
-    trainer.extend(set_observation, name='set_observation',
-                   trigger=(1, 'iteration'), priority=2)
+        def _serialize_updater(serializer):
+            updater.iteration = serializer('iteration', updater.iteration)
+            updater.epoch = serializer('epoch', updater.epoch)
+            updater.is_new_epoch = serializer(
+                'is_new_epoch', updater.is_new_epoch)
+        trainer.updater.serialize = _serialize_updater
 
-    invoked_intervals = []
-    trainer.extend(lambda t: invoked_intervals.append(
-        t.updater.iteration), name='test', trigger=trigger, priority=1)
+        def set_observation(t):
+            t.observation = {key: accuracies[t.updater.iteration-1]}
+        trainer.extend(set_observation, name='set_observation',
+                       trigger=(1, 'iteration'), priority=2)
 
-    if resume is not None:
-        serializers.load_npz(resume, trainer)
+        invoked_iterations = []
 
-    trainer.run()
-    self.assertEqual(invoked_intervals, expected)
+        def record(t):
+            invoked_iterations.append(t.updater.iteration)
+        trainer.extend(record, name='record', trigger=trigger, priority=1)
 
-    if save is not None:
-        serializers.save_npz(save, trainer)
+        if resume is not None:
+            serializers.load_npz(resume, trainer)
 
+        trainer.run()
+        self.assertEqual(invoked_iterations, expected)
 
-class TestMaxValueTrigger(unittest.TestCase):
+        if save is not None:
+            serializers.save_npz(save, trainer)
 
-    def test_max_value_trigger(self):
-        key = 'main/accuracy'
-        trigger = triggers.MaxValueTrigger(key, trigger=(2, 'iteration'))
-        accuracies = numpy.asarray([0.5, 0.5, 0.5, 0.5, 0.4, 0.4, 0.6, 0.6],
-                                   dtype=numpy.float32)
-        expected = [2, 8]
-        _test_trigger(self, trigger, key, accuracies, expected)
-
-    def test_resumed_trigger(self):
-        key = 'main/accuracy'
-        trigger = triggers.MaxValueTrigger(key, trigger=(1, 'iteration'))
-        accuracies = numpy.asarray([0.5],
-                                   dtype=numpy.float32)
-        expected = [1]
-        with tempfile.NamedTemporaryFile() as npz:
-            _test_trigger(self, trigger, key, accuracies, expected, save=npz)
-            npz.flush()
-            trigger = triggers.MaxValueTrigger(key, trigger=(1, 'iteration'))
-            accuracies = numpy.asarray([None, 0.4, 0.5, 0.6],
-                                       dtype=numpy.float32)
-            expected = [4]
-            _test_trigger(self, trigger, key, accuracies,
-                          expected, resume=npz.name)
-
-
-class TestMinValueTrigger(unittest.TestCase):
-
-    def test_min_value_trigger(self):
-        key = 'main/accuracy'
-        trigger = triggers.MinValueTrigger(key, trigger=(2, 'iteration'))
-        accuracies = numpy.asarray([0.5, 0.5, 0.5, 0.5, 0.4, 0.4, 0.6, 0.6],
-                                   dtype=numpy.float32)
-        expected = [2, 6]
-        _test_trigger(self, trigger, key, accuracies, expected)
+    def test_trigger(self):
+        trigger = type(self).trigger_type(self.key, trigger=self.interval)
+        self._test_trigger(trigger, self.key, self.accuracies, self.expected)
 
     def test_resumed_trigger(self):
-        key = 'main/accuracy'
-        trigger = triggers.MinValueTrigger(key, trigger=(1, 'iteration'))
-        accuracies = numpy.asarray([0.5],
-                                   dtype=numpy.float32)
-        expected = [1]
-        _test_trigger(self, trigger, key, accuracies, expected)
+        trigger = type(self).trigger_type(self.key, trigger=self.interval)
         with tempfile.NamedTemporaryFile() as npz:
-            serializers.save_npz(npz, trigger)
+            self._test_trigger(
+                trigger, self.key, self.accuracies[:self.resume],
+                self.expected_before_resume, save=npz)
             npz.flush()
-            trigger = triggers.MinValueTrigger(key, trigger=(1, 'iteration'))
-            serializers.load_npz(npz.name, trigger)
-            accuracies = numpy.asarray([None, 0.6, 0.5, 0.4],
-                                       dtype=numpy.float32)
-            expected = [4]
-            _test_trigger(self, trigger, key, accuracies, expected)
+            trigger = type(self).trigger_type(self.key, trigger=self.interval)
+            self._test_trigger(trigger, self.key, self.accuracies,
+                               self.expected_after_resume, resume=npz.name)
+
+
+@testing.parameterize(
+    # interval = 1 iterations
+    {
+        'key': 'main/accuracy',
+        'iter_per_epoch': 1,
+        'interval': (1, 'iteration'),
+        'accuracies': [0.5, 0.5, 0.4, 0.6],
+        'expected': [1, 4],
+        'resume': 1,
+        'expected_before_resume': [1],
+        'expected_after_resume': [4]},
+    # interval = 2 iterations
+    {
+        'key': 'main/accuracy',
+        'iter_per_epoch': 1,
+        'interval': (2, 'iteration'),
+        'accuracies': [0.5, 0.5, 0.5, 0.5, 0.4, 0.4, 0.6, 0.6],
+        'expected': [2, 8],
+        'resume': 2,
+        'expected_before_resume': [2],
+        'expected_after_resume': [8]},
+    # interval = 2 iterations, unaligned resume
+    {
+        'key': 'main/accuracy',
+        'iter_per_epoch': 1,
+        'interval': (2, 'iteration'),
+        'accuracies': [0.5, 0.5, 0.5, 0.5, 0.4, 0.4, 0.6, 0.6],
+        'expected': [2, 8],
+        'resume': 3,
+        'expected_before_resume': [2],
+        'expected_after_resume': [8]},
+    # interval = 1 epoch, 1 epoch = 2 iterations
+    {
+        'key': 'main/accuracy',
+        'iter_per_epoch': 2,
+        'interval': (1, 'epoch'),
+        'accuracies': [0.5, 0.5, 0.5, 0.5, 0.4, 0.4, 0.6, 0.6],
+        'expected': [2, 8],
+        'resume': 2,
+        'expected_before_resume': [2],
+        'expected_after_resume': [8]},
+    # interval = 1 epoch, 1 epoch = 2 iterations, unaligned resume
+    {
+        'key': 'main/accuracy',
+        'iter_per_epoch': 2,
+        'interval': (1, 'epoch'),
+        'accuracies': [0.5, 0.5, 0.5, 0.5, 0.4, 0.4, 0.6, 0.6],
+        'expected': [2, 8],
+        'resume': 3,
+        'expected_before_resume': [2],
+        'expected_after_resume': [8]},
+)
+class TestMaxValueTrigger(unittest.TestCase, BestValueTriggerTester):
+    trigger_type = triggers.MaxValueTrigger
+
+
+@testing.parameterize(
+    # interval = 1 iterations
+    {
+        'key': 'main/accuracy',
+        'iter_per_epoch': 1,
+        'interval': (1, 'iteration'),
+        'accuracies': [0.5, 0.5, 0.4, 0.6],
+        'expected': [1, 3],
+        'resume': 1,
+        'expected_before_resume': [1],
+        'expected_after_resume': [3]},
+    # interval = 2 iterations
+    {
+        'key': 'main/accuracy',
+        'iter_per_epoch': 1,
+        'interval': (2, 'iteration'),
+        'accuracies': [0.5, 0.5, 0.5, 0.5, 0.4, 0.4, 0.6, 0.6],
+        'expected': [2, 6],
+        'resume': 2,
+        'expected_before_resume': [2],
+        'expected_after_resume': [6]},
+    # interval = 2 iterations, unaligned resume
+    {
+        'key': 'main/accuracy',
+        'iter_per_epoch': 1,
+        'interval': (2, 'iteration'),
+        'accuracies': [0.5, 0.5, 0.5, 0.5, 0.4, 0.4, 0.6, 0.6],
+        'expected': [2, 6],
+        'resume': 3,
+        'expected_before_resume': [2],
+        'expected_after_resume': [6]},
+    # interval = 1 epoch, 1 epoch = 2 iterations
+    {
+        'key': 'main/accuracy',
+        'iter_per_epoch': 2,
+        'interval': (1, 'epoch'),
+        'accuracies': [0.5, 0.5, 0.5, 0.5, 0.4, 0.4, 0.6, 0.6],
+        'expected': [2, 6],
+        'resume': 2,
+        'expected_before_resume': [2],
+        'expected_after_resume': [6]},
+    # interval = 1 epoch, 1 epoch = 2 iterations, unaligned resume
+    {
+        'key': 'main/accuracy',
+        'iter_per_epoch': 2,
+        'interval': (1, 'epoch'),
+        'accuracies': [0.5, 0.5, 0.5, 0.5, 0.4, 0.4, 0.6, 0.6],
+        'expected': [2, 6],
+        'resume': 3,
+        'expected_before_resume': [2],
+        'expected_after_resume': [6]},
+)
+class TestMinValueTrigger(unittest.TestCase, BestValueTriggerTester):
+    trigger_type = triggers.MinValueTrigger
 
 
 testing.run_module(__name__, __file__)
