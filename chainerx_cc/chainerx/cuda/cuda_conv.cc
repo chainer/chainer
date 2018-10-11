dfabb7b5265377e11e059213c252783ad3bb90f5
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-10-11 10:55:11 +0000

    Cosmetic

--- chainerx_cc/chainerx/cuda/cuda_conv.cc
+++ chainerx_cc/chainerx/cuda/cuda_conv.cc
@@ -345,7 +345,7 @@ Array CudaConv::ConvTranspose(
         const StackVector<int64_t, kMaxNdim>& out_size) {
     int8_t ndim = x.ndim() - 2;  // Number of spatial dimensions
 
-    // Checks if cover_all is false
+    // Check if cover_all is false
     for (int8_t i = 0; i < ndim; ++i) {
         if (x.shape()[i + 2] != internal::GetConvOutDim(out_size[i], w.shape()[i + 2], stride[i], pad[i], false)) {
             throw ChainerxError{"CUDA convolution transpose does not support cover all outsize"};
