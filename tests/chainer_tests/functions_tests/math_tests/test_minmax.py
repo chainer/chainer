a3a7b611d379bae2fcbb273a917ce72369530dd8
sonots <sonots@gmail.com>
2018-10-12 15:01:24 +0900

    Remove TODO(sonots): Fix argmin to get aborted since it has already been fixed

--- tests/chainer_tests/functions_tests/math_tests/test_minmax.py
+++ tests/chainer_tests/functions_tests/math_tests/test_minmax.py
@@ -310,9 +310,6 @@ class TestArgMinMax(unittest.TestCase):
         # TODO(sonots): Support float16
         if self.dtype == numpy.float16:
             raise unittest.SkipTest('ChainerX does not support float16')
-        # TODO(sonots): Fix argmin to get aborted
-        if self.function_name == 'argmin':
-            raise unittest.SkipTest('ChainerX does not work with argmin')
         self.check_backward(chainerx.array(self.x))
 
     def test_invalid_axis_type(self):
