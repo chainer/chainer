e0912d34fa36791665002d18ed8c7b8bbf372e42
Toshiki Kataoka <kataoka@preferred.jp>
2018-10-18 09:10:51 +0900

    Add stats tests of initializers.*Normal

--- tests/chainer_tests/initializer_tests/test_normal.py
+++ tests/chainer_tests/initializer_tests/test_normal.py
@@ -1,11 +1,25 @@
+import math
 import unittest
+
+import numpy
 
 from chainer import backend
 from chainer.backends import cuda
 from chainer import initializers
 from chainer import testing
 from chainer.testing import attr
-import numpy
+from chainer.testing import condition
+
+
+default_scale = {
+    initializers.Normal: 0.05,
+}
+
+default_fan = {
+    initializers.LeCunNormal: 'fan_in',
+    initializers.GlorotNormal: 'fan_avg',
+    initializers.HeNormal: 'fan_in',
+}
 
 
 @testing.parameterize(*(
@@ -17,23 +31,28 @@ import numpy
             {'target': initializers.HeNormal, 'fan_option': 'fan_in'},
             {'target': initializers.HeNormal, 'fan_option': 'fan_out'}
         ],
-        testing.product(
-            {'shape': [(2, 3), (2, 3, 4)],
-             'dtype': [numpy.float16, numpy.float32, numpy.float64]
-             }
-        )
+        [
+            {'shape': (2, 3), 'fans': (3, 2)},
+            {'shape': (2, 3, 4), 'fans': (12, 8)},
+        ],
+        testing.product({
+            'scale': [None, 7.3],
+            'dtype': [numpy.float16, numpy.float32, numpy.float64],
+        })
     )
 ))
 class NormalBase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        kwargs = {}
+        if self.scale is not None:
+            kwargs['scale'] = self.scale
+        if self.fan_option is not None:
+            kwargs['fan_option'] = self.fan_option
+        self.target_kwargs = kwargs
 
     def check_initializer(self, w):
-        if self.fan_option is None:
-            initializer = self.target(scale=0.1)
-        else:
-            initializer = self.target(scale=0.1, fan_option=self.fan_option)
+        initializer = self.target(**self.target_kwargs)
         initializer(w)
         self.assertTupleEqual(w.shape, self.shape)
         self.assertEqual(w.dtype, self.dtype)
@@ -48,7 +67,7 @@ class NormalBase(unittest.TestCase):
         self.check_initializer(w)
 
     def check_shaped_initializer(self, xp):
-        initializer = self.target(scale=0.1, dtype=self.dtype)
+        initializer = self.target(dtype=self.dtype, **self.target_kwargs)
         w = initializers.generate_array(initializer, self.shape, xp)
         self.assertIs(backend.get_array_module(w), xp)
         self.assertTupleEqual(w.shape, self.shape)
@@ -60,6 +79,48 @@ class NormalBase(unittest.TestCase):
     @attr.gpu
     def test_shaped_initializer_gpu(self):
         self.check_shaped_initializer(cuda.cupy)
+
+    def check_initializer_statistics(self, xp):
+        from scipy import stats
+
+        n = 100000
+        ws = xp.empty((n,) + self.shape, dtype=self.dtype)
+        for w in ws:
+            initializer = self.target(**self.target_kwargs)
+            initializer(w)
+
+        fan = self.fan_option or default_fan.get(self.target)
+        expected_std = self.scale or default_scale.get(self.target) or 1.
+        if self.target == initializers.HeNormal:
+            expected_std *= math.sqrt(2.)
+        if fan is not None:
+            if fan == 'fan_in':
+                expected_std *= math.sqrt(1. / self.fans[0])
+            elif fan == 'fan_out':
+                expected_std *= math.sqrt(1. / self.fans[1])
+            elif fan == 'fan_avg':
+                expected_std *= math.sqrt(2. / sum(self.fans))
+            else:
+                assert False
+
+        sampless = ws.reshape(n, -1).T
+        alpha = 0.05 / len(sampless)
+        for samples in sampless:
+            _, p = stats.kstest(samples, stats.norm(0, expected_std).cdf)
+            assert p >= alpha
+
+    @attr.slow
+    @testing.with_requires('scipy')
+    @condition.repeat_with_success_at_least(5, 3)
+    def test_initializer_statistics_cpu(self):
+        self.check_initializer_statistics(numpy)
+
+    @attr.slow
+    @attr.gpu
+    @testing.with_requires('scipy')
+    @condition.repeat_with_success_at_least(5, 3)
+    def test_initializer_statistics_gpu(self):
+        self.check_initializer_statistics(cuda.cupy)
 
 
 testing.run_module(__name__, __file__)
