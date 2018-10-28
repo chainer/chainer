4b19db4a8a4235202215d419de23ab3d87c75622
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-10-28 02:54:56 +0000

    Fix docs

--- chainerx_cc/chainerx/routines/manipulation.h
+++ chainerx_cc/chainerx/routines/manipulation.h
@@ -39,7 +39,7 @@ Array BroadcastTo(const Array& array, const Shape& shape);
 // If the dimension is not equally divisible, DimensionError is throws.
 std::vector<Array> Split(const Array& ary, int64_t sections, int8_t axis = 0);
 
-// Returns a set of arrays resulting from splitting the given array as at the specified indices along the specified axis.
+// Returns a set of arrays resulting from splitting the given array at the indices along the specified axis.
 std::vector<Array> Split(const Array& ary, std::vector<int64_t> indices, int8_t axis = 0);
 
 }  // namespace chainerx
