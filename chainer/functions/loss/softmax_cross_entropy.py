00b5b3f762489b8409215f61ab557bd887310771
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-10-29 02:14:33 +0000

    Optimize for non-ChainerX environments

--- chainer/functions/loss/softmax_cross_entropy.py
+++ chainer/functions/loss/softmax_cross_entropy.py
@@ -444,7 +444,8 @@ for row, column in enumerate(t)])
 
     """
 
-    is_chainerx = backend.get_array_module(x) is chainerx
+    is_chainerx = (chainerx.is_available()
+        and backend.get_array_module(x) is chainerx)
 
     if is_chainerx or not enable_double_backprop:
         # Optimized implementation.
