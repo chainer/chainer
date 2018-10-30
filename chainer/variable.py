7b3f18b6a5f22912e2ab2c7c85ea944e2122f269
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-10-30 10:12:40 +0000

    Grammatical change

--- chainer/variable.py
+++ chainer/variable.py
@@ -817,7 +817,7 @@ class Variable(object):
         if not self._requires_grad and g is not None:
             raise RuntimeError(
                 'Cannot set the gradient of a variable that is flagged to not '
-                'require any gradients.')
+                'require any gradient.')
         arr = self._data[0]
         if arr is None:
             if g is not None:
