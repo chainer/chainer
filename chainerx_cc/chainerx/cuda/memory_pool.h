481a225751a7fd28f7926a73831989409a00fa7c
Naotoshi Seo <sonots@gmail.com>
2018-11-05 12:09:48 +0900

    cpplint

--- chainerx_cc/chainerx/cuda/memory_pool.h
+++ chainerx_cc/chainerx/cuda/memory_pool.h
@@ -24,7 +24,7 @@ enum class AllocatorStatus { kSuccess = 0, kErrorMemoryAllocation };
 
 class OutOfMemoryError : public ChainerxError {
 public:
-    OutOfMemoryError(size_t bytesize) : ChainerxError{"Out of memory to allocate ", bytesize, " bytes."} {}
+    explicit OutOfMemoryError(size_t bytesize) : ChainerxError{"Out of memory to allocate ", bytesize, " bytes."} {}
 };
 
 // TODO(hvy): Add a member function to check for the last error, using e.g. cudaPeekAtLastError.
