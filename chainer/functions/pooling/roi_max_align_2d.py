8fd38a9815ca6ff3bfe8b600b60164232d383f46
Shingo Kitagawa <shingogo@hotmail.co.jp>
2018-11-07 16:53:19 +0900

    use numpy.issubdtype to support numpy.scalar in roi_max_align_2d.py

--- chainer/functions/pooling/roi_max_align_2d.py
+++ chainer/functions/pooling/roi_max_align_2d.py
@@ -42,23 +42,24 @@ class ROIMaxAlign2D(function.Function):
 
     def __init__(self, outsize, spatial_scale, sampling_ratio=None):
         outh, outw = _pair(outsize)
-        if not (isinstance(outh, int) and outh > 0):
+        if not (numpy.issubdtype(type(outh), numpy.integer) and outh > 0):
             raise TypeError(
                 'outsize[0] must be positive integer: {}, {}'
                 .format(type(outh), outh))
-        if not (isinstance(outw, int) and outw > 0):
+        if not (numpy.issubdtype(type(outw), numpy.integer) and outw > 0):
             raise TypeError(
                 'outsize[1] must be positive integer: {}, {}'
                 .format(type(outw), outw))
-        if isinstance(spatial_scale, int):
+        if numpy.issubdtype(type(spatial_scale), numpy.integer):
             spatial_scale = float(spatial_scale)
-        if not (isinstance(spatial_scale, float) and spatial_scale > 0):
+        if not (numpy.issubdtype(type(spatial_scale), numpy.floating) and
+                spatial_scale > 0):
             raise TypeError(
                 'spatial_scale must be a positive float number: {}, {}'
                 .format(type(spatial_scale), spatial_scale))
         sampling_ratio = _pair(sampling_ratio)
-        if not all((isinstance(s, int) and s >= 1) or s is None
-                   for s in sampling_ratio):
+        if not all((numpy.issubdtype(type(s), numpy.integer) and s >= 1) or
+                   s is None for s in sampling_ratio):
             raise TypeError(
                 'sampling_ratio must be integer >= 1 or a pair of it: {}'
                 .format(sampling_ratio))
