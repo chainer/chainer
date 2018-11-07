586cdc8979702d09bc35f0cf0c1d868d6fe5d3fc
Shingo Kitagawa <shingogo@hotmail.co.jp>
2018-11-07 19:02:53 +0900

    test spatial_scale >= 1.0 in test_roi_max_align_2d.py

--- tests/chainer_tests/functions_tests/pooling_tests/test_roi_max_align_2d.py
+++ tests/chainer_tests/functions_tests/pooling_tests/test_roi_max_align_2d.py
@@ -20,6 +20,7 @@ def _pair(x):
 @testing.parameterize(*testing.product({
     'sampling_ratio': [None, 1, 2, (None, 3), (1, 2)],
     'outsize': [5, 7, (5, 7)],
+    'spatial_scale': [0.6, 1.0, 2.0],
 }))
 class TestROIMaxAlign2D(unittest.TestCase):
 
@@ -41,7 +42,6 @@ class TestROIMaxAlign2D(unittest.TestCase):
         self.roi_indices = numpy.array([0, 2, 1, 0, 2], dtype=numpy.int32)
         n_rois = self.rois.shape[0]
         outsize = _pair(self.outsize)
-        self.spatial_scale = 0.6
         self.gy = numpy.random.uniform(
             -1, 1, (n_rois, n_channels,
                     outsize[0], outsize[1])).astype(numpy.float32)
