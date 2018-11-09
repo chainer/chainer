75abf2a9755fea293ee88f976f9d3493789d9e0a
Toshiki Kataoka <kataoka@preferred.jp>
2018-11-09 16:19:12 +0900

    Update style check tools to the versions compatible with pycodestyle 2.4

--- setup.py
+++ setup.py
@@ -28,10 +28,10 @@ requirements = {
         'six>=1.9.0',
     ],
     'stylecheck': [
-        'autopep8==1.3.5',
-        'flake8==3.5.0',
+        'autopep8>=1.4,<1.5',
+        'flake8>=3.6,<3.7',
         'pbr==4.0.4',
-        'pycodestyle==2.3.1',
+        'pycodestyle>=2.4,<2.5',
     ],
     'test': [
         'pytest',
