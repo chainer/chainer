fe808d82134ad8dfce504d06425474fa48b12a1f
Akifumi Imanishi <imanishi@preferred.jp>
2018-11-09 02:14:37 +0000

    Add more tests

--- chainerx_cc/chainerx/backward_builder_test.cc
+++ chainerx_cc/chainerx/backward_builder_test.cc
@@ -139,5 +139,83 @@ TEST(BackwardBuilderTest, FloatToInt_GetIntRetainOutputSecondParam) {
     EXPECT_ARRAY_EQ(e, *x.GetGrad());
 }
 
+TEST(BackwardBuilderTest, FloatToInt_SomeOfOutputArrayBodiesAreGone) {
+    testing::ContextSession context_session;
+    Shape shape{2, 3};
+
+    auto forward = [shape](const Array& x, Array& y1, Array& y2) {
+        y1 = testing::BuildArray(shape).WithData<float>({1, 2, 3, 4, 5, 6});
+        y2 = testing::BuildArray(shape).WithData<int32_t>({7, 8, 9, 10, 11, 12});
+
+        BackwardBuilder bb{"forward", x, {y1, y2}};
+        BackwardBuilder::Target bt = bb.CreateTarget();
+        EXPECT_TRUE(static_cast<bool>(bt));
+        bt.Define([out_tok = bb.RetainOutput(1)](BackwardContext& bctx) {
+            const nonstd::optional<Array>& y2 = bctx.GetRetainedOutput(out_tok);
+            ASSERT_TRUE(bctx.output_grad(0).has_value());
+            ASSERT_FALSE(bctx.output_grad(1).has_value());
+            EXPECT_TRUE(y2.has_value());
+            bctx.input_grad() = *bctx.output_grad(0) * y2->AsType(Dtype::kFloat32);
+        });
+        bb.Finalize();
+    };
+
+    Array x = Ones(shape, Dtype::kFloat32).RequireGrad();
+    Array e = testing::BuildArray(shape).WithData<float>({28, 40, 54, 70, 88, 108});
+    Array y1{};
+    Array z2{};
+    {
+        Array y2{};
+
+        forward(x, y1, y2);
+        EXPECT_TRUE(y1.IsBackpropRequired());
+        EXPECT_FALSE(y2.IsBackpropRequired());
+        z2 = y2.MakeView();
+    }
+
+    y1.SetGrad(testing::BuildArray(shape).WithData<float>({4, 5, 6, 7, 8, 9}));
+    Backward(y1);
+    EXPECT_TRUE(x.GetGrad().has_value());
+    EXPECT_ARRAY_EQ(e, *x.GetGrad());
+}
+
+TEST(BackwardBuilderTest, FloatToInt_SomeOfOutputArrayNodesAreGone) {
+    testing::ContextSession context_session;
+    Shape shape{2, 3};
+
+    auto forward = [shape](const Array& x, Array& y1, Array& y2) {
+        y1 = testing::BuildArray(shape).WithData<float>({1, 2, 3, 4, 5, 6});
+        y2 = testing::BuildArray(shape).WithData<int32_t>({7, 8, 9, 10, 11, 12});
+
+        BackwardBuilder bb{"forward", x, {y1, y2}};
+        BackwardBuilder::Target bt = bb.CreateTarget();
+        EXPECT_TRUE(static_cast<bool>(bt));
+        bt.Define([out_tok = bb.RetainOutput(1)](BackwardContext& bctx) {
+            const nonstd::optional<Array>& y2 = bctx.GetRetainedOutput(out_tok);
+            ASSERT_TRUE(bctx.output_grad(0).has_value());
+            ASSERT_FALSE(bctx.output_grad(1).has_value());
+            EXPECT_TRUE(y2.has_value());
+            bctx.input_grad() = *bctx.output_grad(0) * y2->AsType(Dtype::kFloat32);
+        });
+        bb.Finalize();
+    };
+
+    Array x = Ones(shape, Dtype::kFloat32).RequireGrad();
+    Array e = testing::BuildArray(shape).WithData<float>({28, 40, 54, 70, 88, 108});
+    Array y1{};
+    {
+        Array y2{};
+
+        forward(x, y1, y2);
+        EXPECT_TRUE(y1.IsBackpropRequired());
+        EXPECT_FALSE(y2.IsBackpropRequired());
+    }
+
+    y1.SetGrad(testing::BuildArray(shape).WithData<float>({4, 5, 6, 7, 8, 9}));
+    Backward(y1);
+    EXPECT_TRUE(x.GetGrad().has_value());
+    EXPECT_ARRAY_EQ(e, *x.GetGrad());
+}
+
 }  // namespace
 }  // namespace chainerx
