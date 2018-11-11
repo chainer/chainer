38f860f36355d414ea1d0fd6ce99a99298988d8a
Yuta Koreeda <koreyou@mac.com>
2018-11-11 20:27:38 +0900

    Fixed typo

--- examples/text_classification/text_datasets.py
+++ examples/text_classification/text_datasets.py
@@ -31,7 +31,7 @@ def read_dbpedia(tf, split, shrink=1, char_based=False):
     dataset = []
     f = tf.extractfile('dbpedia_csv/{}.csv'.format(split))
     if sys.version_info > (3, 0):
-         f = io.TextIOWrapper(f, encoding='utf-8')
+        f = io.TextIOWrapper(f, encoding='utf-8')
     for i, (label, title, text) in enumerate(csv.reader(f)):
         if i % shrink != 0:
             continue
