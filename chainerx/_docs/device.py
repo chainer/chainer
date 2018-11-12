8f22e0702ca06c1c38ec33f898554d8e63c651b1
niboshi <niboshi000@gmail.com>
2018-11-12 14:51:52 +0900

    Document backend and device

---
+++ chainerx/_docs/device.py
@@ -0,0 +1,118 @@
+import chainerx
+from chainerx import _docs
+
+
+def _set_docs_device():
+    Device = chainerx.Device
+
+    _docs.set_doc(
+        Device,
+        """Represents a physical computing unit.
+""")
+
+    _docs.set_doc(
+        Device.synchronize,
+        """Synchronizes the device.
+""")
+
+    _docs.set_doc(
+        Device.name,
+        """Device name.
+
+Returns:
+    str: Device name.
+""")
+
+    _docs.set_doc(
+        Device.backend,
+        """Backend to which this device belongs.
+
+Returns:
+    ~chainerx.Backend: Backend object.
+""")
+
+    _docs.set_doc(
+        Device.context,
+        """Context to which this device belongs.
+
+Returns:
+    ~chainerx.Context: Context object.
+""")
+
+    _docs.set_doc(
+        Device.index,
+        """Index of this device.
+
+Returns:
+    int: Index of this device.
+""")
+
+
+def set_docs():
+    _set_docs_device()
+
+    _docs.set_doc(
+        chainerx.get_device,
+        """get_device(*device)
+Returns a device specified by the argument.
+
+If the argument is a :class:`~chainerx.Device` instance, it's simply returned.
+
+Otherwise, there are three ways to specify a device:
+
+
+.. testcode::
+
+    # Specify backend name and device index separately.
+    chainerx.get_device('native', 0)
+
+    # Specify backend name and device index in a single string.
+    chainerx.get_device('native:0')
+
+    # Specify only backend name. In this case device index 0 is chosen.
+    chainerx.get_device('native')
+
+Returns:
+    ~chainerx.Device: Device object.
+""")
+
+    _docs.set_doc(
+        chainerx.get_default_device,
+        """get_default_device()
+Returns the default device associated with the current thread.
+
+Returns:
+    ~chainerx.Device: The default device.
+
+.. seealso::
+    * :func:`chainerx.set_default_device`
+    * :func:`chainerx.device_scope`
+""")
+
+    _docs.set_doc(
+        chainerx.set_default_device,
+        """set_default_device(device)
+Sets a given device as the default device of the current thread.
+
+Args:
+    device (~chainerx.Device or str): Device object or device name to set as
+        the default device.
+
+.. seealso::
+    * :func:`chainerx.get_default_device`
+    * :func:`chainerx.device_scope`
+""")
+
+    _docs.set_doc(
+        chainerx.device_scope,
+        """device_scope(device)
+Creates a context manager to temporarily set the default device.
+
+Args:
+    device (~chainerx.Device or str): Device object or device name to set as
+        the default device during the context.
+
+.. seealso::
+    * :func:`chainerx.get_default_device`
+    * :func:`chainerx.set_default_device`
+""")
--- docs/source/chainerx/reference/backend_and_device.rst
+++ docs/source/chainerx/reference/backend_and_device.rst
@@ -1,9 +1,29 @@
 Backend and Device
 ==================
 
+.. module:: chainerx
+
 Backend
 -------
+
+.. autosummary::
+   :toctree: generated/
+   :nosignatures:
+
+   chainerx.Backend
+   chainerx.get_backend
 
 Device
 ------
 .. _chainerx_device:
+
+.. autosummary::
+   :toctree: generated/
+   :nosignatures:
+
+   chainerx.Device
+   chainerx.get_device
+   chainerx.get_default_device
+   chainerx.set_default_device
+   chainerx.device_scope
+
