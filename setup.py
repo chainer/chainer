dc753c18fb663a71f164f20cea953f74df57af61
Toshiki Kataoka <kataoka@preferred.jp>
2018-11-13 19:47:17 +0900

    Use `autopep8 --exit-code`

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requirements = {
         'six>=1.9.0',
     ],
     'stylecheck': [
-        'autopep8>=1.4,<1.5',
+        'autopep8>=1.4.1,<1.5',
         'flake8>=3.6,<3.7',
         'pbr==4.0.4',
         'pycodestyle>=2.4,<2.5',
