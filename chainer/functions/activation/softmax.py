2060b74e1ed6d5f6c1fa2072900efb0a64a36cf6
Masayuki Takagi <kamonama@gmail.com>
2018-11-14 16:58:37 +0900

    Merge pull request #5566 from okuta/simplify-softmax
    Simplify softmax with cuDNN

--- chainer/functions/activation/softmax.py
+++ chainer/functions/activation/softmax.py
@@ -1,6 +1,3 @@
-import functools
-import operator
-
 import numpy
 
 import chainer
@@ -14,21 +11,6 @@ if cuda.cudnn_enabled:
     cudnn = cuda.cudnn
     libcudnn = cuda.cuda.cudnn
     _algorithm = libcudnn.CUDNN_SOFTMAX_ACCURATE
-    _mode_channel = libcudnn.CUDNN_SOFTMAX_MODE_CHANNEL
-    _mode_instance = libcudnn.CUDNN_SOFTMAX_MODE_INSTANCE
-
-
-def _get_tensor4d_shape(axis, shape):
-    left_shape = functools.reduce(operator.mul, shape[:axis], 1)
-    center_shape = shape[axis]
-    right_shape = functools.reduce(operator.mul, shape[axis:][1:], 1)
-    return left_shape, center_shape, right_shape, 1
-
-
-def _get_cudnn_mode(shape):
-    if shape[2] == 1 and shape[3] == 1:
-        return _mode_instance
-    return _mode_channel
 
 
 class Softmax(function_node.FunctionNode):
@@ -50,19 +32,7 @@ class Softmax(function_node.FunctionNode):
     def forward(self, x):
         xp = backend.get_array_module(*x)
         if xp is not numpy and chainer.should_use_cudnn('>=auto'):
-            oz_dtype = 'd' if x[0].dtype == 'd' else 'f'
-            one = numpy.array(1, dtype=oz_dtype).ctypes
-            zero = numpy.array(0, dtype=oz_dtype).ctypes
-            handle = cudnn.get_handle()
-            x_tensor4d = cuda.cupy.ascontiguousarray(
-                x[0].reshape(_get_tensor4d_shape(self.axis, x[0].shape)))
-            desc = cudnn.create_tensor_descriptor(x_tensor4d)
-            cudnn_mode = _get_cudnn_mode(x_tensor4d.shape)
-            y = xp.empty_like(x[0])
-            libcudnn.softmaxForward(
-                handle, _algorithm, cudnn_mode, one.data, desc.value,
-                x_tensor4d.data.ptr, zero.data, desc.value,
-                y.data.ptr)
+            y = cudnn.softmax_forward(x[0], self.axis, _algorithm)
         else:
             y = x[0] - x[0].max(axis=self.axis, keepdims=True)
             xp.exp(y, out=y)
@@ -87,20 +57,7 @@ class _SoftmaxGrad(function_node.FunctionNode):
         y, gy = inputs
         xp = backend.get_array_module(*y)
         if xp is not numpy and chainer.should_use_cudnn('>=auto'):
-            oz_dtype = 'd' if y[0].dtype == 'd' else 'f'
-            one = numpy.array(1, dtype=oz_dtype).ctypes
-            zero = numpy.array(0, dtype=oz_dtype).ctypes
-            handle = cudnn.get_handle()
-            gx = xp.empty_like(y)
-            gx_tensor4d = cuda.cupy.ascontiguousarray(
-                gx.reshape(_get_tensor4d_shape(self.axis, gx.shape)))
-            gy = cuda.cupy.ascontiguousarray(gy)
-            desc = cudnn.create_tensor_descriptor(gx_tensor4d)
-            cudnn_mode = _get_cudnn_mode(gx_tensor4d.shape)
-            libcudnn.softmaxBackward(
-                handle, _algorithm, cudnn_mode, one.data, desc.value,
-                y.data.ptr, desc.value, gy.data.ptr, zero.data,
-                desc.value, gx.data.ptr)
+            gx = cudnn.softmax_backward(y, gy, self.axis, _algorithm)
         else:
             gx = y * gy
             sumdx = gx.sum(axis=self.axis, keepdims=True)
