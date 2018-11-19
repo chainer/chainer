9b89b45bd4539470908bcc9b75d22e2bb32308f5
Akifumi Imanishi <imanishi@preferred.jp>
2018-11-19 05:36:35 +0000

    Rename parameters

--- chainerx/activation.py
+++ chainerx/activation.py
@@ -1,28 +1,28 @@
 import chainerx
 
 
-def relu(a):
+def relu(x):
     """Rectified Linear Unit function.
 
 Args:
-    a (~chainerx.ndarray): Input array.
+    x (~chainerx.ndarray): Input array.
 
 Returns:
     :class:`~chainerx.ndarray`: Returned array: :math:`y = \\max (0, x)`.
 
 Note:
     During backpropagation, this function propagates the gradient of the
-    output array to the input array ``a``.
+    output array to the input array ``x``.
 """
     # TODO(imanishi): The function should also be available to C++ users
-    return chainerx.maximum(0, a)
+    return chainerx.maximum(0, x)
 
 
-def sigmoid(a):
+def sigmoid(x):
     """Element-wise sigmoid logistic function.
 
 Args:
-    a (~chainerx.ndarray): Input array.
+    x (~chainerx.ndarray): Input array.
 
 Returns:
     :class:`~chainerx.ndarray`:
@@ -30,7 +30,7 @@ Returns:
 
 Note:
     During backpropagation, this function propagates the gradient of the
-    output array to the input array ``a``.
+    output array to the input array ``x``.
 """
     # TODO(imanishi): The function should also be available to C++ users
-    return (chainerx.tanh(a / 2.0) + 1.0) / 2.0
+    return (chainerx.tanh(x / 2.0) + 1.0) / 2.0
