618976c07023607dd0358bdafe9eceaf730702c8
Naotoshi Seo <sonots@gmail.com>
2018-11-26 18:10:49 +0900

    chunk class

--- chainerx_cc/chainerx/cuda/memory_pool.h
+++ chainerx_cc/chainerx/cuda/memory_pool.h
@@ -53,6 +53,49 @@ public:
     void Free(void* ptr) noexcept override { cudaFreeHost(ptr); }
 };
 
+// A chunk points to a device memory.
+//
+// A chunk might be a splitted memory block from a larger allocation.
+// The prev/next pointers contruct a doubly-linked list of memory addresses
+// sorted by base address that must be contiguous.
+class Chunk {
+public:
+    // TODO(sonots): Reconsider ptr type, and reinterpret_cast
+    Chunk(const std::shared_ptr<void>& mem, size_t offset, size_t size)
+        : mem_(mem), ptr_(reinterpret_cast<intptr_t>(mem.get()) + offset), offset_(offset), size_(size) {}
+    Chunk(const Chunk&) = default;
+    ~Chunk() {}
+
+    // Split contiguous block of a larger allocation
+    friend std::shared_ptr<Chunk> Split(std::shared_ptr<Chunk>& self, size_t size);
+
+    // Merge previously splitted block (chunk)
+    friend void Merge(std::shared_ptr<Chunk>& self, std::shared_ptr<Chunk> remaining);
+
+    void* ptr() const { return reinterpret_cast<void*>(ptr_); }
+    size_t offset() const { return offset_; }
+    size_t size() const { return size_; }
+    const std::shared_ptr<Chunk>& prev() const { return prev_; }
+    std::shared_ptr<Chunk>& prev() { return prev_; }
+    const std::shared_ptr<Chunk>& next() const { return next_; }
+    std::shared_ptr<Chunk>& next() { return next_; }
+    bool in_use() const { return in_use_; }
+
+    void SetPrev(const std::shared_ptr<Chunk>& prev) { prev_ = prev; }
+    void SetNext(const std::shared_ptr<Chunk>& next) { next_ = next; }
+    void SetInUse(bool in_use) { in_use_ = in_use; }
+
+private:
+    std::shared_ptr<void> mem_;  // The memory buffer.
+    intptr_t ptr_ = 0;  // Memory address.
+    size_t offset_ = 0;  // An offset bytes from the head of the buffer.
+    size_t size_ = 0;  // Chunk size in bytes.
+    int device_index_;  // GPU device id whose memory the pointer refers to.
+    std::shared_ptr<Chunk> prev_;  // prev memory pointer if split from a larger allocation
+    std::shared_ptr<Chunk> next_;  // next memory pointer if split from a larger allocation
+    bool in_use_ = false;  // chunk is in use
+};
+
 // Memory pool.
 // This class is thread safe.
 class MemoryPool {
