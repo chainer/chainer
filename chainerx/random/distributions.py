3a0b19ed032768e99eea425a447ea4167b307a65
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-11-28 07:06:30 +0000

    Pop instead to reduce changes

--- chainerx/random/distributions.py
+++ chainerx/random/distributions.py
@@ -4,26 +4,34 @@ import chainerx
 
 
 # TODO(sonots): Implement in C++, especially in CUDA
-def normal(loc=0.0, scale=1.0, size=None, device=None):
-    """Draws random samples from a normal (Gaussian) distribution.
+def normal(*args, **kwargs):
+    """normal(*args, **kwargs, device=None)
+
+    Draws random samples from a normal (Gaussian) distribution.
 
     This is currently equivalent to :func:`numpy.random.normal`
     wrapped by :func:`chainerx.array`, given the device argument.
 
     .. seealso:: :func:`numpy.random.normal`
     """
-    a = numpy.random.normal(loc, scale, size)
+    device = kwargs.pop('device', None)
+
+    a = numpy.random.normal(*args, **kwargs)
     return chainerx.array(a, device=device, copy=False)
 
 
 # TODO(sonots): Implement in C++, especially in CUDA
-def uniform(low=0.0, high=1.0, size=None, device=None):
-    """Draws samples from a uniform distribution.
+def uniform(*args, **kwargs):
+    """uniform(*args, **kwargs, device=None)
+
+    Draws samples from a uniform distribution.
 
     This is currently equivalent to :func:`numpy.random.normal`
     wrapped by :func:`chainerx.array`, given the device argument.
 
     .. seealso:: :func:`numpy.random.uniform`
     """
-    a = numpy.random.uniform(low, high, size)
+    device = kwargs.pop('device', None)
+
+    a = numpy.random.uniform(*args, **kwargs)
     return chainerx.array(a, device=device, copy=False)
