0dff01cf151df146016700ef83c2cc2c38b43fc5
Toshiki Kataoka <kataoka@preferred.jp>
2018-12-03 20:45:14 +0900

    Revert "use sticky mode in StandardUpdater"
    This reverts commit cd66f0f291a468b61eb6491523c685de0cf846c0.

--- chainer/training/updaters/standard_updater.py
+++ chainer/training/updaters/standard_updater.py
@@ -75,10 +75,7 @@ class StandardUpdater(_updater.Updater):
 
         if device is not None and device >= 0:
             for optimizer in six.itervalues(self._optimizers):
-                # Use sticky mode; if the target is manually transferred to
-                # the GPU by user (e.g., model-parallel use-case), we should
-                # not reloacte it.
-                optimizer.target.to_gpu(device, sticky=True)
+                optimizer.target.to_gpu(device)
 
         self.converter = converter
         self.loss_func = loss_func
