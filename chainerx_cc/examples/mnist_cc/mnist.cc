e623ec99a37105489d02061ac5653c8cd797f375
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2018-12-05 06:39:04 +0000

    Clean up MNIST data loader

--- chainerx_cc/examples/mnist_cc/mnist.cc
+++ chainerx_cc/examples/mnist_cc/mnist.cc
@@ -15,25 +15,25 @@
 
 namespace {
 
-std::unique_ptr<char[]> ReadFile(const std::string& filename) {
-    std::ifstream ifs(filename.c_str(), std::ios::in | std::ios::binary | std::ios::ate);
-    if (!ifs.is_open()) {
-        throw std::runtime_error("Could not open " + filename);
-    }
-
-    size_t size = ifs.tellg();
-
-    std::unique_ptr<char[]> buffer{new char[size]};
-
-    ifs.seekg(0, std::ios::beg);
-    ifs.read(buffer.get(), size);
-    ifs.close();
-
-    return buffer;
+template <typename T>
+T ReadValue(std::ifstream& ifs) {
+    T val{0};
+    ifs.read(reinterpret_cast<char*>(&val), sizeof(val));
+    return val;
 }
 
-int32_t ReadHeader(const std::unique_ptr<char[]>& buffer, size_t offset) {
-    uint32_t header = *reinterpret_cast<uint32_t*>(buffer.get() + offset);
+chainerx::Array ReadArray(std::ifstream& ifs, const chainerx::Shape& shape) {
+    int64_t n = shape.GetTotalSize();
+
+    std::shared_ptr<uint8_t> data{new uint8_t[n], std::default_delete<uint8_t[]>{}};
+    std::generate_n(data.get(), n, [&ifs]() { return ReadValue<uint8_t>(ifs); });
+
+    return chainerx::FromContiguousHostData(
+            shape, chainerx::Dtype::kUInt8, static_cast<std::shared_ptr<void>>(data), chainerx::GetDefaultDevice());
+}
+
+int32_t ReadHeader(std::ifstream& ifs) {
+    int32_t header = ReadValue<int32_t>(ifs);
 
     // Swap byte order.
     uint32_t b1 = header & 255;
@@ -44,38 +44,42 @@ int32_t ReadHeader(const std::unique_ptr<char[]>& buffer, size_t offset) {
     return (b1 << 24) | (b2 << 16) | (b3 << 8) | b4;
 }
 
-chainerx::Array ReadArray(const std::unique_ptr<char[]>& buffer, const chainerx::Shape& shape, size_t offset) {
-    int64_t n = shape.GetTotalSize();
-
-    std::shared_ptr<uint8_t> data{new uint8_t[n], std::default_delete<uint8_t[]>{}};
-    std::copy_n(reinterpret_cast<uint8_t*>(buffer.get() + offset), n, data.get());
-
-    return chainerx::FromContiguousHostData(
-            shape, chainerx::Dtype::kUInt8, static_cast<std::shared_ptr<void>>(data), chainerx::GetDefaultDevice());
-}
-
 }  // namespace
 
 chainerx::Array ReadMnistImages(const std::string& filename) {
-    std::unique_ptr<char[]> buffer = ReadFile(filename);
+    std::ifstream ifs{filename.c_str(), std::ios::in | std::ios::binary};
+    if (!ifs.is_open()) {
+        throw std::runtime_error("Could not open MNIST images: " + filename);
+    }
 
-    int32_t n = ReadHeader(buffer, 4);
-    int32_t height = ReadHeader(buffer, 8);
-    int32_t width = ReadHeader(buffer, 12);
+    if (ReadHeader(ifs) != 0x803) {
+        throw std::runtime_error("Bad MNIST images file: " + filename);
+    }
+
+    int32_t n = ReadHeader(ifs);
+    int32_t height = ReadHeader(ifs);
+    int32_t width = ReadHeader(ifs);
 
     assert(n == 60000 || n == 10000);
     assert(height == 28);
     assert(width == 28);
 
-    return ReadArray(buffer, {n, height * width}, 16);
+    return ReadArray(ifs, {n, height * width});
 }
 
 chainerx::Array ReadMnistLabels(const std::string& filename) {
-    std::unique_ptr<char[]> buffer = ReadFile(filename);
+    std::ifstream ifs{filename.c_str(), std::ios::in | std::ios::binary};
+    if (!ifs.is_open()) {
+        throw std::runtime_error("Could not open MNIST labels: " + filename);
+    }
 
-    int32_t n = ReadHeader(buffer, 4);
+    if (ReadHeader(ifs) != 0x801) {
+        throw std::runtime_error("Bad MNIST labels file: " + filename);
+    }
+
+    int32_t n = ReadHeader(ifs);
 
     assert(n == 60000 || n == 10000);
 
-    return ReadArray(buffer, {n}, 8);
+    return ReadArray(ifs, {n});
 }
