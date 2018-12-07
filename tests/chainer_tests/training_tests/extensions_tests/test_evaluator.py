54adc5ffb03d4666e9ab6c3acc339c600b5ee7f1
niboshi <niboshi000@gmail.com>
2018-12-07 15:34:54 +0900

    Add test for conveter device argument in Evaluator

--- tests/chainer_tests/training_tests/extensions_tests/test_evaluator.py
+++ tests/chainer_tests/training_tests/extensions_tests/test_evaluator.py
@@ -3,6 +3,7 @@ import unittest
 import numpy
 
 import chainer
+from chainer.backends import _cpu
 from chainer import dataset
 from chainer import iterators
 from chainer import testing
@@ -121,6 +122,17 @@ class TestEvaluator(unittest.TestCase):
         self.assertEqual(reporter.observation, mean)
 
 
+@chainer.testing.backend.inject_backend_tests(
+    None,
+    [
+        # NumPy
+        {},
+        # CuPy
+        {'use_cuda': True, 'cuda_device': 0},
+        {'use_cuda': True, 'cuda_device': 1},
+
+        # Custom converter is not supported for ChainerX.
+    ])
 class TestEvaluatorTupleData(unittest.TestCase):
 
     def setUp(self):
@@ -131,31 +143,44 @@ class TestEvaluatorTupleData(unittest.TestCase):
              numpy.random.uniform(-1, 1, (2, 3, 4)).astype('f'))
             for _ in range(2)]
 
-        self.iterator = DummyIterator(self.data)
-        self.converter = DummyConverter(self.batches)
-        self.target = DummyModelTwoArgs(self)
-        self.evaluator = extensions.Evaluator(
-            self.iterator, self.target, converter=self.converter, device=-1)
+    def prepare(self, data, batches, device):
+        iterator = DummyIterator(data)
+        converter = DummyConverter(batches)
+        target = DummyModelTwoArgs(self)
+        evaluator = extensions.Evaluator(
+            iterator, target, converter=converter, device=device)
+        return iterator, converter, target, evaluator
 
-    def test_evaluate(self):
+    def test_evaluate(self, backend_config):
+        data = backend_config.get_array(self.data)
+        batches = [backend_config.get_array(b) for b in self.batches]
+        device = backend_config.device
+
+        iterator, converter, target, evaluator = (
+            self.prepare(data, batches, device))
+
         reporter = chainer.Reporter()
-        reporter.add_observer('target', self.target)
+        reporter.add_observer('target', target)
         with reporter:
-            mean = self.evaluator.evaluate()
+            mean = evaluator.evaluate()
 
         # The converter gets results of the iterator and the device number.
-        self.assertEqual(len(self.converter.args), len(self.data))
-        for i in range(len(self.data)):
+        self.assertEqual(len(converter.args), len(data))
+        if backend_config.use_cuda:
+            expected_device_arg = backend_config.cuda_device
+        else:
+            expected_device_arg = -1
+
+        for i in range(len(data)):
             numpy.testing.assert_array_equal(
-                self.converter.args[i]['batch'], self.data[i])
-            self.assertEqual(
-                self.converter.args[i]['device'], -1)
+                _cpu._to_cpu(converter.args[i]['batch']), self.data[i])
+            self.assertEqual(converter.args[i]['device'], expected_device_arg)
 
         # The model gets results of converter.
-        self.assertEqual(len(self.target.args), len(self.batches))
-        for i in range(len(self.batches)):
+        self.assertEqual(len(target.args), len(batches))
+        for i in range(len(batches)):
             numpy.testing.assert_array_equal(
-                self.target.args[i], self.batches[i])
+                _cpu._to_cpu(target.args[i]), self.batches[i])
 
         expect_mean = numpy.mean([numpy.sum(x) for x in self.batches])
         self.assertAlmostEqual(mean['target/loss'], expect_mean, places=4)
