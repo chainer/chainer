db1ab78140427d1fec0914c80b57dd8cb0623ef9
himkt <himkt@klis.tsukuba.ac.jp>
2018-12-08 18:09:39 +0900

    Fix conflict

--- chainer/links/loss/crf1d.py
+++ chainer/links/loss/crf1d.py
@@ -1,4 +1,7 @@
 from chainer.functions.loss import crf1d
+from chainer.functions.array import transpose_sequence
+from chainer.links.connection.n_step_rnn import argsort_list_descent
+from chainer.links.connection.n_step_rnn import permutate_list
 from chainer import initializers
 from chainer import link
 from chainer import variable
@@ -24,17 +27,29 @@ class CRF1d(link.Link):
         cost (~chainer.Variable): Transition cost parameter.
     """
 
-    def __init__(self, n_label, initial_cost=None):
+    def __init__(self, n_label, initial_cost=None, transpose=False):
         super(CRF1d, self).__init__()
         if initial_cost is None:
             initial_cost = initializers.constant.Zero()
 
+        self.transpose = transpose
         with self.init_scope():
             self.cost = variable.Parameter(initializer=initial_cost,
                                            shape=(n_label, n_label))
 
     def forward(self, xs, ys, reduce='mean'):
-        return crf1d.crf1d(self.cost, xs, ys, reduce)
+        if self.transpose:
+            indices = argsort_list_descent(xs)
+            xs = permutate_list(xs, indices, inv=False)
+            ys = permutate_list(ys, indices, inv=False)
+            trans_x = transpose_sequence.transpose_sequence(xs)
+            trans_y = transpose_sequence.transpose_sequence(ys)
+            loss = crf1d.crf1d(self.cost, trans_x, trans_y, reduce)
+
+        else:
+            loss = crf1d.crf1d(self.cost, xs, ys, reduce)
+
+        return loss
 
     def argmax(self, xs):
         """Computes a state that maximizes a joint probability.
@@ -50,4 +65,18 @@ class CRF1d(link.Link):
            detail.
 
         """
-        return crf1d.argmax_crf1d(self.cost, xs)
+
+        if self.transpose:
+            indices = argsort_list_descent(xs)
+            xs = permutate_list(xs, indices, inv=False)
+            trans_x = transpose_sequence.transpose_sequence(xs)
+            score, path = crf1d.argmax_crf1d(self.cost, trans_x)
+
+            path = transpose_sequence.transpose_sequence(path)
+            path = [p.array for p in path]
+            path = permutate_list(path, indices, inv=True)
+
+        else:
+            score, path = crf1d.argmax_crf1d(self.cost, xs)
+
+        return score, path
