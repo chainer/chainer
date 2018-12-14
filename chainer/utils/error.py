eec78569a5e7264607cb1ec8ce9b99e40a7008d7
niboshi <niboshi000@gmail.com>
2018-12-14 16:23:56 +0900

    Remove unused local function

--- chainer/utils/error.py
+++ chainer/utils/error.py
@@ -1,8 +1,5 @@
 def _format_array_props(arrays):
     # Formats array shapes and dtypes for error messages.
-    def format_props(arr):
-        return '{}:{}'.format(arr.shape, arr.dtype.name)
-
     return ', '.join([
         None if arr is None
         else '{}:{}'.format(arr.shape, arr.dtype.name)
