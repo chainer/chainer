50003e82aa0a92a0d272125e9594fd1530d74676
hitsgub <hitstales@gmail.com>
2018-12-20 21:38:35 +0900

    fix test

--- tests/chainer_tests/training_tests/triggers_tests/test_once_trigger.py
+++ tests/chainer_tests/training_tests/triggers_tests/test_once_trigger.py
@@ -47,7 +47,7 @@ class TestOnceTrigger(unittest.TestCase):
     def test_trigger(self):
         trainer = testing.get_trainer_with_mock_updater(
             stop_trigger=None, iter_per_epoch=self.iter_per_epoch)
-        trigger = training.triggers.OnceTrigger(*self.interval)
+        trigger = training.triggers.OnceTrigger(self.call_on_resume)
         # before the first iteration, trigger should be False
         for expected in [False] + self.expected:
             self.assertEqual(trigger(trainer), expected)
@@ -57,13 +57,13 @@ class TestOnceTrigger(unittest.TestCase):
         trainer = testing.get_trainer_with_mock_updater(
             stop_trigger=None, iter_per_epoch=self.iter_per_epoch)
         with tempfile.NamedTemporaryFile(delete=False) as f:
-            trigger = training.triggers.OnceTrigger(*self.interval)
+            trigger = training.triggers.OnceTrigger(self.call_on_resume)
             for expected in self.expected[:self.resume]:
                 trainer.updater.update()
                 self.assertEqual(trigger(trainer), expected)
             serializers.save_npz(f.name, trigger)
 
-            trigger = training.triggers.OnceTrigger(*self.interval)
+            trigger = training.triggers.OnceTrigger(self.call_on_resume)
             serializers.load_npz(f.name, trigger)
             for expected in self.expected[self.resume:]:
                 trainer.updater.update()
@@ -73,7 +73,7 @@ class TestOnceTrigger(unittest.TestCase):
     def test_trigger_sparse_call(self):
         trainer = testing.get_trainer_with_mock_updater(
             stop_trigger=None, iter_per_epoch=self.iter_per_epoch)
-        trigger = training.triggers.OnceTrigger(*self.interval)
+        trigger = training.triggers.OnceTrigger(self.call_on_resume)
         accumulated = False
         # before the first iteration, trigger should be False
         for expected in [False] + self.expected:
@@ -89,7 +89,7 @@ class TestOnceTrigger(unittest.TestCase):
             stop_trigger=None, iter_per_epoch=self.iter_per_epoch)
         accumulated = False
         with tempfile.NamedTemporaryFile(delete=False) as f:
-            trigger = training.triggers.OnceTrigger(*self.interval)
+            trigger = training.triggers.OnceTrigger(self.call_on_resume)
             for expected in self.expected[:self.resume]:
                 trainer.updater.update()
                 accumulated = accumulated or expected
@@ -98,7 +98,7 @@ class TestOnceTrigger(unittest.TestCase):
                     accumulated = False
             serializers.save_npz(f.name, trigger)
 
-            trigger = training.triggers.OnceTrigger(*self.interval)
+            trigger = training.triggers.OnceTrigger(self.call_on_resume)
             serializers.load_npz(f.name, trigger)
             for expected in self.expected[self.resume:]:
                 trainer.updater.update()
@@ -111,14 +111,14 @@ class TestOnceTrigger(unittest.TestCase):
         trainer = testing.get_trainer_with_mock_updater(
             stop_trigger=None, iter_per_epoch=self.iter_per_epoch)
         with tempfile.NamedTemporaryFile(delete=False) as f:
-            trigger = training.triggers.OnceTrigger(*self.interval)
+            trigger = training.triggers.OnceTrigger(self.call_on_resume)
             for expected in self.expected[:self.resume]:
                 trainer.updater.update()
                 self.assertEqual(trigger(trainer), expected)
             # old version does not save anything
             np.savez(f, dummy=0)
 
-            trigger = training.triggers.OnceTrigger(*self.interval)
+            trigger = training.triggers.OnceTrigger(self.call_on_resume)
             with testing.assert_warns(UserWarning):
                 serializers.load_npz(f.name, trigger)
             for expected in self.expected[self.resume:]:
