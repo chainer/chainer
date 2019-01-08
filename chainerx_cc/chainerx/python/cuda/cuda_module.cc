3839e270cf9a292d6a56c9c686e34b13f951e1e7
Akifumi Imanishi <imanishi@preferred.jp>
2019-01-08 04:30:51 +0000

    Fix for cpplint

--- chainerx_cc/chainerx/python/cuda/cuda_module.cc
+++ chainerx_cc/chainerx/python/cuda/cuda_module.cc
@@ -24,13 +24,13 @@ using CudaDevice = chainerx::cuda::CudaDevice;
 
 void* Malloc(void* backend, size_t bytesize, int device_id) {
     CudaDevice* device = dynamic_cast<CudaDevice*>(
-            &reinterpret_cast<Backend*>(backend)->GetDevice(device_id));  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
+            &(reinterpret_cast<Backend*>(backend)->GetDevice(device_id)));  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
     return device->device_memory_pool()->Malloc(bytesize);
 }
 
 void Free(void* backend, void* ptr, int device_id) {
     CudaDevice* device = dynamic_cast<CudaDevice*>(
-            &reinterpret_cast<Backend*>(backend)->GetDevice(device_id));  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
+            &(reinterpret_cast<Backend*>(backend)->GetDevice(device_id)));  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
     return device->device_memory_pool()->Free(ptr);
 }
 
