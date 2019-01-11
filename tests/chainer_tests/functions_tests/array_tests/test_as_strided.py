bc710a297a7fa03de87925de2c43c36aeca3c649
Yuki Hashimoto <hashimoto@hal.t.u-tokyo.ac.jp>
2019-01-11 19:18:06 +0900

    avoid duplicated definition for cpu and gpu tests. + use testing.assert_allclose.

--- tests/chainer_tests/functions_tests/array_tests/test_as_strided.py
+++ tests/chainer_tests/functions_tests/array_tests/test_as_strided.py
@@ -17,84 +17,80 @@ from chainer.functions.array.as_strided import _stride_array
     {'dtype': np.int64}
 )
 class TestStrideArray(unittest.TestCase):
-    def test_flip_cpu(self):
-        x = np.arange(4, dtype=self.dtype)
+    def check_flip(self, xp):
+        x = xp.arange(4, dtype=self.dtype)
         y = _stride_array(x, (4,), (-1,), 3)  # [3, 2, 1, 0]
-        y_expected = np.flip(x, axis=0)
-        assert (y == y_expected).all()
+        y_expected = xp.flip(x, axis=0)
+        testing.assert_allclose(y, y_expected)
+
+    def test_flip_cpu(self):
+        self.check_flip(np)
 
     @testing.attr.gpu
     def test_flip_gpu(self):
         import cupy as cp
-        x = cp.arange(4, dtype=self.dtype)
-        y = _stride_array(x, (4,), (-1,), 3)
-        y_expected = cp.flip(x, axis=0)
-        assert (y == y_expected).all()
+        self.check_flip(cp)
 
-    def test_broadcast_cpu(self):
-        x = np.arange(12, dtype=self.dtype).reshape((3, 4)).copy()
+    def check_broadcast(self, xp):
+        x = xp.arange(12, dtype=self.dtype).reshape((3, 4)).copy()
         # [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11]]
         y = _stride_array(x, (2, 3, 4), (0, 4, 1), 0)
-        y_expected = np.broadcast_to(x, (2, 3, 4))
-        assert (y == y_expected).all()
+        y_expected = xp.broadcast_to(x, (2, 3, 4))
+        testing.assert_allclose(y, y_expected)
+
+    def test_broadcast_cpu(self):
+        self.check_broadcast(np)
 
     @testing.attr.gpu
     def test_broadcast_gpu(self):
         import cupy as cp
-        x = cp.arange(12, dtype=self.dtype).reshape((3, 4)).copy()
-        y = _stride_array(x, (2, 3, 4), (0, 4, 1), 0)
-        y_expected = cp.broadcast_to(x, (2, 3, 4))
-        assert (y == y_expected).all()
+        self.check_broadcast(cp)
+
+    def check_unstride(self, xp):
+        x = xp.flip(xp.arange(12, dtype=self.dtype).reshape((3, 4)), 0)
+        y = _stride_array(x, (12,), (1,), 0)
+        y_expected = xp.arange(12, dtype=self.dtype)
+        testing.assert_allclose(y, y_expected)
 
     def test_unstride_cpu(self):
-        x = np.flip(np.arange(12, dtype=self.dtype).reshape((3, 4)), 0)
-        y = _stride_array(x, (12,), (1,), 0)
-        y_expected = np.arange(12, dtype=self.dtype)
-        assert (y == y_expected).all()
+        self.check_unstride(np)
 
     @testing.attr.gpu
     def test_unstride_gpu(self):
         import cupy as cp
-        x = cp.flip(cp.arange(12, dtype=self.dtype).reshape((3, 4)), 0)
-        y = _stride_array(x, (12,), (1,), 0)
-        y_expected = cp.arange(12, dtype=self.dtype)
-        assert (y == y_expected).all()
+        self.check_unstride(cp)
 
-    def test_general_stride_cpu(self):
-        x = np.arange(8, dtype=self.dtype)
+    def check_general_stride(self, xp):
+        x = xp.arange(8, dtype=self.dtype)
         y = _stride_array(x, (3, 3), (-1, 2), 3)
-        y_expected = np.array(
+        y_expected = xp.array(
             [[3, 5, 7],
              [2, 4, 6],
              [1, 3, 5]],
             dtype=self.dtype
         )
-        assert (y == y_expected).all()
+        testing.assert_allclose(y, y_expected)
+
+    def test_general_stride_cpu(self):
+        self.check_general_stride(np)
 
     @testing.attr.gpu
     def test_general_stride_gpu(self):
         import cupy as cp
-        x = cp.arange(8, dtype=self.dtype)
-        y = _stride_array(x, (3, 3), (-1, 2), 3)
-        y_expected = cp.array(
-            [[3, 5, 7],
-             [2, 4, 6],
-             [1, 3, 5]],
-            dtype=self.dtype
-        )
-        assert (y == y_expected).all()
+        self.check_general_stride(cp)
 
-    def test_invalid_negative_index_cpu(self):
-        x = np.arange(8, dtype=self.dtype)
+    def check_invalid_negative_index(self, xp):
+        x = xp.arange(8, dtype=self.dtype)
         with self.assertRaises(ValueError):
             _stride_array(x, (3, 3), (-1, 2), 1)
+
+    def test_invalid_negative_index_cpu(self):
+        self.check_invalid_negative_index(np)
 
     @testing.attr.gpu
     def test_invalid_negative_index_gpu(self):
         import cupy as cp
-        x = cp.arange(8, dtype=self.dtype)
-        with self.assertRaises(ValueError):
-            _stride_array(x, (3, 3), (-1, 2), 1)
+        self.check_invalid_negative_index(cp)
 
 
 @testing.parameterize(
@@ -106,75 +102,68 @@ class TestStrideArray(unittest.TestCase):
     {'dtype': np.int64}
 )
 class TestAsStridedForward(unittest.TestCase):
-    def test_flip_forward_cpu(self):
-        x = np.arange(4, dtype=self.dtype)
+    def check_flip_forward(self, xp):
+        x = xp.arange(4, dtype=self.dtype)
         v = Variable(x)
         y = as_strided(v, (4,), (-1,), 3)
-        y_expected = np.flip(x, axis=0)
-        assert (y.array == y_expected).all()
+        y_expected = xp.flip(x, axis=0)
+        testing.assert_allclose(y.array, y_expected)
+
+    def test_flip_forward_cpu(self):
+        self.check_flip_forward(np)
 
     @testing.attr.gpu
     def test_flip_forward_gpu(self):
         import cupy as cp
-        x = cp.arange(4, dtype=self.dtype)
-        v = Variable(x)
-        y = as_strided(v, (4,), (-1,), 3)
-        y_expected = cp.flip(x, axis=0)
-        assert (y.array == y_expected).all()
+        self.check_flip_forward(cp)
 
-    def test_broadcast_forward_cpu(self):
-        x = np.arange(12, dtype=self.dtype).reshape((3, 4)).copy()
+    def check_broadcast_forward(self, xp):
+        x = xp.arange(12, dtype=self.dtype).reshape((3, 4)).copy()
         v = Variable(x)
         y = as_strided(v, (2, 3, 4), (0, 4, 1), 0)
-        y_expected = np.broadcast_to(x, (2, 3, 4))
-        assert (y.array == y_expected).all()
+        y_expected = xp.broadcast_to(x, (2, 3, 4))
+        testing.assert_allclose(y.array, y_expected)
+
+    def test_broadcast_forward_cpu(self):
+        self.check_broadcast_forward(np)
 
     @testing.attr.gpu
     def test_broadcast_forward_gpu(self):
         import cupy as cp
-        x = cp.arange(12, dtype=self.dtype).reshape((3, 4)).copy()
-        v = Variable(x)
-        y = as_strided(v, (2, 3, 4), (0, 4, 1), 0)
-        y_expected = cp.broadcast_to(x, (2, 3, 4))
-        assert (y.array == y_expected).all()
+        self.check_broadcast_forward(cp)
 
-    def test_unstride_forward_cpu(self):
-        x = np.flip(np.arange(12, dtype=self.dtype).reshape((3, 4)), 0)
+    def check_unstride_forward(self, xp):
+        x = xp.flip(xp.arange(12, dtype=self.dtype).reshape((3, 4)), 0)
         v = Variable(x)
         y = as_strided(v, (12,), (1,), 0)
-        y_expected = np.arange(12, dtype=self.dtype)
-        assert (y.array == y_expected).all()
+        y_expected = xp.arange(12, dtype=self.dtype)
+        testing.assert_allclose(y.array, y_expected)
+
+    def test_unstride_forward_cpu(self):
+        self.check_unstride_forward(np)
 
     @testing.attr.gpu
     def test_unstride_forward_gpu(self):
         import cupy as cp
-        x = cp.flip(cp.arange(12, dtype=self.dtype).reshape((3, 4)), 0)
-        v = Variable(x)
-        y = as_strided(v, (12,), (1,), 0)
-        y_expected = cp.arange(12, dtype=self.dtype)
-        assert (y.array == y_expected).all()
+        self.check_unstride_forward(cp)
 
-    def test_general_stride_forward_cpu(self):
-        x = _stride_array(np.arange(8, dtype=self.dtype), (3, 3), (-1, 2), 3)
+    def check_general_stride(self, xp):
+        x = _stride_array(xp.arange(8, dtype=self.dtype), (3, 3), (-1, 2), 3)
         # [[3., 5., 7.], [2., 4., 6.], [1., 3., 5.]]
         v = Variable(x)
         y = as_strided(v, (3, 3), (1, 2), 0)
         # [[0., 2., 4.], [1., 3., 5.,], [2., 4., 6.]]
-        y_expected = _stride_array(np.arange(8, dtype=self.dtype),
+        y_expected = _stride_array(xp.arange(8, dtype=self.dtype),
                                    (3, 3), (1, 2), 0)
         assert (y.array == y_expected).all()
+
+    def test_general_stride_forward_cpu(self):
+        self.check_general_stride(np)
 
     @testing.attr.gpu
     def test_general_stride_forward_gpu(self):
         import cupy as cp
-        x = _stride_array(cp.arange(8, dtype=self.dtype), (3, 3), (-1, 2), 3)
-        # [[3., 5., 7.], [2., 4., 6.], [1., 3., 5.]]
-        v = Variable(x)
-        y = as_strided(v, (3, 3), (1, 2), 0)
-        # [[0., 2., 4.], [1., 3., 5.,], [2., 4., 6.]]
-        y_expected = _stride_array(cp.arange(8, dtype=self.dtype),
-                                   (3, 3), (1, 2), 0)
-        assert (y.array == y_expected).all()
+        self.check_general_stride(cp)
 
 
 @testing.parameterize(
@@ -183,89 +172,78 @@ class TestAsStridedForward(unittest.TestCase):
     {'dtype': np.float64}
 )
 class TestAsStridedBackward(unittest.TestCase):
-    def test_flip_backward_cpu(self):
-        x = np.arange(4, dtype=self.dtype)
+    def check_flip_backward(self, xp):
+        x = xp.arange(4, dtype=self.dtype)
         v = Variable(x)
         y = as_strided(v, (4,), (-1,), 3)
-        y.grad = np.ones((4,), dtype=self.dtype)
+        y.grad = xp.ones((4,), dtype=self.dtype)
         gx, = grad((y,), (v,))
-        assert (gx.array == np.ones((4,), dtype=self.dtype)).all()
+        testing.assert_allclose(gx.array, xp.ones((4,), dtype=self.dtype))
+
+    def test_flip_backward_cpu(self):
+        self.check_flip_backward(np)
 
     @testing.attr.gpu
     def test_flip_backward_gpu(self):
         import cupy as cp
-        x = cp.arange(4, dtype=self.dtype)
-        v = Variable(x)
-        y = as_strided(v, (4,), (-1,), 3)
-        y.grad = cp.ones((4,), dtype=self.dtype)
-        gx, = grad((y,), (v,))
-        assert (gx.array == cp.ones((4,), dtype=self.dtype)).all()
+        self.check_flip_backward(cp)
 
-    def test_broadcast_backward_cpu(self):
-        x = np.arange(12, dtype=self.dtype).reshape((3, 4)).copy()
+    def check_broadcast_backward(self, xp):
+        x = xp.arange(12, dtype=self.dtype).reshape((3, 4)).copy()
         v = Variable(x)
         y = as_strided(v, (2, 3, 4), (0, 4, 1), 0)
-        y.grad = np.ones((2, 3, 4), dtype=self.dtype)
+        y.grad = xp.ones((2, 3, 4), dtype=self.dtype)
         gx, = grad((y,), (v,))
-        assert (gx.array == np.ones(x.shape, dtype=self.dtype) * 2).all()
+        testing.assert_allclose(gx.array,
+                                xp.ones(x.shape, dtype=self.dtype) * 2)
+
+    def test_broadcast_backward_cpu(self):
+        self.check_broadcast_backward(np)
 
     @testing.attr.gpu
     def test_broadcast_backward_gpu(self):
         import cupy as cp
-        x = cp.arange(12, dtype=self.dtype).reshape((3, 4)).copy()
-        v = Variable(x)
-        y = as_strided(v, (2, 3, 4), (0, 4, 1), 0)
-        y.grad = cp.ones((2, 3, 4), dtype=self.dtype)
-        gx, = grad((y,), (v,))
-        assert (gx.array == cp.ones(x.shape, dtype=self.dtype) * 2).all()
+        self.check_broadcast_backward(cp)
 
-    def test_unstride_backward_cpu(self):
-        x = np.flip(np.arange(12, dtype=self.dtype).reshape((3, 4)), 0)
+    def check_unstride_backward(self, xp):
+        x = xp.flip(xp.arange(12, dtype=self.dtype).reshape((3, 4)), 0)
         v = Variable(x)
         y = as_strided(v, (12,), (1,), 0)
-        y.grad = np.ones((12,), dtype=self.dtype)
+        y.grad = xp.ones((12,), dtype=self.dtype)
         gx, = grad((y,), (v,))
-        assert (gx.array == np.ones(x.shape, dtype=self.dtype)).all()
+        testing.assert_allclose(gx.array, xp.ones(x.shape, dtype=self.dtype))
+
+    def test_unstride_backward_cpu(self):
+        self.check_unstride_backward(np)
 
     @testing.attr.gpu
     def test_unstride_backward_gpu(self):
         import cupy as cp
-        x = cp.flip(cp.arange(12, dtype=self.dtype).reshape((3, 4)), 0)
-        v = Variable(x)
-        y = as_strided(v, (12,), (1,), 0)
-        y.grad = cp.ones((12,), dtype=self.dtype)
-        gx, = grad((y,), (v,))
-        assert (gx.array == cp.ones(x.shape, dtype=self.dtype)).all()
+        self.check_unstride_backward(cp)
 
-    def test_general_stride_backward_cpu(self):
-        x = _stride_array(np.arange(8, dtype=self.dtype), (3, 3), (-1, 2), 3)
+    def check_general_stride_backward(self, xp):
+        x = _stride_array(xp.arange(8, dtype=self.dtype), (3, 3), (-1, 2), 3)
         # [[3., 5., 7.], [2., 4., 6.], [1., 3., 5.]]
         v = Variable(x)
         y = as_strided(v, (3, 3), (1, 2), 0)
         # [[0., 2., 4.], [1., 3., 5.,], [2., 4., 6.]]
-        y.grad = np.ones(y.shape, dtype=self.dtype)
+        y.grad = xp.ones(y.shape, dtype=self.dtype)
         gx, = grad((y,), (v,))
-        assert (gx.array == np.array([
-            [0.5, 0.5, 0.],
-            [2., 2., 1.],
-            [1., 0.5, 0.5]
-        ])).all()
+        testing.assert_allclose(gx.array,
+                                xp.array([
+                                    [0.5, 0.5, 0.],
+                                    [2., 2., 1.],
+                                    [1., 0.5, 0.5]
+                                ], dtype=self.dtype)
+                                )
+
+    def test_general_stride_backward_cpu(self):
+        self.check_general_stride_backward(np)
 
     @testing.attr.gpu
     def test_general_stride_backward_gpu(self):
         import cupy as cp
-        x = _stride_array(cp.arange(8, dtype=self.dtype), (3, 3), (-1, 2), 3)
-        # [[3., 5., 7.], [2., 4., 6.], [1., 3., 5.]]
-        v = Variable(x)
-        y = as_strided(v, (3, 3), (1, 2), 0)
-        # [[0., 2., 4.], [1., 3., 5.,], [2., 4., 6.]]
-        y.grad = cp.ones(y.shape, dtype=self.dtype)
-        gx, = grad((y,), (v,))
-        assert (gx.array == cp.array([
-            [0.5, 0.5, 0.],
-            [2., 2., 1.],
-            [1., 0.5, 0.5]
-        ])).all()
+        self.check_general_stride_backward(cp)
 
 
 @testing.parameterize(
@@ -274,81 +252,71 @@ class TestAsStridedBackward(unittest.TestCase):
     {'dtype': np.int64}
 )
 class TestAsStridedBackwardInvalidType(unittest.TestCase):
-    def test_flip_backward_cpu(self):
-        x = np.arange(4, dtype=self.dtype)
+    def check_flip_backward(self, xp):
+        x = xp.arange(4, dtype=self.dtype)
         v = Variable(x)
         y = as_strided(v, (4,), (-1,), 3)
-        y.grad = np.ones((4,), dtype=self.dtype)
+        y.grad = xp.ones((4,), dtype=self.dtype)
         with self.assertRaises(TypeError):
             gx, = grad((y,), (v,))
+
+    def test_flip_backward_cpu(self):
+        self.check_flip_backward(np)
 
     @testing.attr.gpu
     def test_flip_backward_gpu(self):
         import cupy as cp
-        x = cp.arange(4, dtype=self.dtype)
+        self.check_flip_backward(cp)
+
+    def check_broadcast_backward(self, xp):
+        x = xp.arange(12, dtype=self.dtype).reshape((3, 4)).copy()
         v = Variable(x)
-        y = as_strided(v, (4,), (-1,), 3)
-        y.grad = cp.ones((4,), dtype=self.dtype)
+        y = as_strided(v, (2, 3, 4), (0, 4, 1), 0)
+        y.grad = xp.ones((2, 3, 4), dtype=self.dtype)
         with self.assertRaises(TypeError):
             gx, = grad((y,), (v,))
 
     def test_broadcast_backward_cpu(self):
-        x = np.arange(12, dtype=self.dtype).reshape((3, 4)).copy()
-        v = Variable(x)
-        y = as_strided(v, (2, 3, 4), (0, 4, 1), 0)
-        y.grad = np.ones((2, 3, 4), dtype=self.dtype)
-        with self.assertRaises(TypeError):
-            gx, = grad((y,), (v,))
+        self.check_broadcast_backward(np)
 
     @testing.attr.gpu
     def test_broadcast_backward_gpu(self):
         import cupy as cp
-        x = cp.arange(12, dtype=self.dtype).reshape((3, 4)).copy()
+        self.check_broadcast_backward(cp)
+
+    def check_unstride_backward(self, xp):
+        x = xp.flip(xp.arange(12, dtype=self.dtype).reshape((3, 4)), 0)
         v = Variable(x)
-        y = as_strided(v, (2, 3, 4), (0, 4, 1), 0)
-        y.grad = cp.ones((2, 3, 4), dtype=self.dtype)
+        y = as_strided(v, (12,), (1,), 0)
+        y.grad = xp.ones((12,), dtype=self.dtype)
         with self.assertRaises(TypeError):
             gx, = grad((y,), (v,))
 
     def test_unstride_backward_cpu(self):
-        x = np.flip(np.arange(12, dtype=self.dtype).reshape((3, 4)), 0)
-        v = Variable(x)
-        y = as_strided(v, (12,), (1,), 0)
-        y.grad = np.ones((12,), dtype=self.dtype)
-        with self.assertRaises(TypeError):
-            gx, = grad((y,), (v,))
+        self.check_unstride_backward(np)
 
     @testing.attr.gpu
     def test_unstride_backward_gpu(self):
         import cupy as cp
-        x = cp.flip(cp.arange(12, dtype=self.dtype).reshape((3, 4)), 0)
+        self.check_unstride_backward(cp)
+
+    def check_general_stride_backward(self, xp):
+        x = _stride_array(xp.arange(8, dtype=self.dtype), (3, 3), (-1, 2), 3)
+        # [[3., 5., 7.], [2., 4., 6.], [1., 3., 5.]]
         v = Variable(x)
-        y = as_strided(v, (12,), (1,), 0)
-        y.grad = cp.ones((12,), dtype=self.dtype)
+        y = as_strided(v, (3, 3), (1, 2), 0)
+        # [[0., 2., 4.], [1., 3., 5.,], [2., 4., 6.]]
+        y.grad = xp.ones(y.shape, dtype=self.dtype)
         with self.assertRaises(TypeError):
             gx, = grad((y,), (v,))
 
     def test_general_stride_backward_cpu(self):
-        x = _stride_array(np.arange(8, dtype=self.dtype), (3, 3), (-1, 2), 3)
-        # [[3., 5., 7.], [2., 4., 6.], [1., 3., 5.]]
-        v = Variable(x)
-        y = as_strided(v, (3, 3), (1, 2), 0)
-        # [[0., 2., 4.], [1., 3., 5.,], [2., 4., 6.]]
-        y.grad = np.ones(y.shape, dtype=self.dtype)
-        with self.assertRaises(TypeError):
-            gx, = grad((y,), (v,))
+        self.check_general_stride_backward(np)
 
     @testing.attr.gpu
     def test_general_stride_backward_gpu(self):
         import cupy as cp
-        x = _stride_array(cp.arange(8, dtype=self.dtype), (3, 3), (-1, 2), 3)
-        # [[3., 5., 7.], [2., 4., 6.], [1., 3., 5.]]
-        v = Variable(x)
-        y = as_strided(v, (3, 3), (1, 2), 0)
-        # [[0., 2., 4.], [1., 3., 5.,], [2., 4., 6.]]
-        y.grad = cp.ones(y.shape, dtype=self.dtype)
-        with self.assertRaises(TypeError):
-            gx, = grad((y,), (v,))
+        self.check_general_stride_backward(cp)
 
 
 testing.run_module(__name__, __file__)
