572e0d234096bdeb8373272ee40ad99aa56c608c
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2019-01-17 04:03:32 +0000

    Fix import error

--- chainerx/_cuda.py
+++ chainerx/_cuda.py
@@ -1,11 +1,17 @@
 import chainerx
 
 try:
+    # _pybind_cuda is unavailable if ChainerX is built without CUDA.
+    from chainerx import _pybind_cuda
+    _available = True
+except Exception:
+    _available = False
+
+try:
     import cupy
     _cupy_available = True
 except Exception:
     _cupy_available = False
-
 
 _chainerx_allocator = None
 
@@ -15,22 +21,15 @@ def cupy_share_allocator(owner=chainerx._global_context):
     # the CUDA backend. This is needed in order to share the GPU memory
     # without having both modules using separate memory pools.
 
-    # TODO(imanishi): Make sure this allocator works when the global
-    # default context is changed by the user. It currently will not
-    # since the allocator is only configured here once.
-    try:
-        owner.get_backend('cuda')
-    except chainerx.BackendError:
+    if not _available:
         raise RuntimeError(
             'Cannot share allocator with CuPy without the CUDA backend.')
-
     if not _cupy_available:
         raise RuntimeError(
             'Cannot share allocator with CuPy since CuPy is not available.')
 
-    param = chainerx._pybind_cuda.get_backend_ptr()
-    malloc_func, free_func = (
-        chainerx._pybind_cuda.get_backend_malloc_free_ptrs())
+    param = _pybind_cuda.get_backend_ptr()
+    malloc_func, free_func = _pybind_cuda.get_backend_malloc_free_ptrs()
 
     global _chainerx_allocator
     _chainerx_allocator = cupy.cuda.memory.CFunctionAllocator(
