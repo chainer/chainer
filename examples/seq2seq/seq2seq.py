89cca329595f9cbdd3c5eafc6300b37ae5718c47
Shunta Saito <shunta.saito@gmail.com>
2019-01-25 22:20:39 +0900

    Fix seq2seq example

--- examples/seq2seq/seq2seq.py
+++ examples/seq2seq/seq2seq.py
@@ -136,7 +136,7 @@ class CalculateBleu(chainer.training.Extension):
         self.device = device
         self.max_length = max_length
 
-    def forward(self, trainer):
+    def __init__(self, trainer):
         with chainer.no_backprop_mode():
             references = []
             hypotheses = []
