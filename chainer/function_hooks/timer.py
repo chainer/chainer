6f94b7d0efb5cfc2390616779f3eaf620ecc1714
hitsgub <hitstales@gmail.com>
2019-02-16 12:24:08 +0900

    add option `align` to `chainer.TimerHook.print_report()`

--- chainer/function_hooks/timer.py
+++ chainer/function_hooks/timer.py
@@ -132,11 +132,31 @@ class TimerHook(function_hook.FunctionHook):
             second *= 1000.0
         return '%.2f%s' % (second, 'ns')
 
-    def print_report(self, file=sys.stdout):
-        """Prints a summary report of time profiling in functions."""
+    def _align(self, second):
+        """Returns align info: (factor, unit)."""
+        factor = 1
+        for unit in ['sec', 'ms', 'us']:
+            if second * factor >= 1:
+                return factor, unit
+            factor *= 1000.0
+        return factor, 'ns'
+
+    def print_report(self, align=False, file=sys.stdout):
+        """Prints a summary report of time profiling in functions.
+
+        Args:
+            align (bool): If `True`, units of times are aligned to the largest.
+        """
         entries = [['FunctionName', 'ElapsedTime', 'Occurrence']]
+        if align:
+            max_time = max(record['elapsed_time'] for record in self.summary().values())
+            factor, unit = self._align(max_time)
         for function_name, record in self.summary().items():
-            elapsed_time = self._humanized_time(record['elapsed_time'])
+            second = record['elapsed_time']
+            if align:
+                elapsed_time = '%3.2f%s' % (second * factor, unit)
+            else:
+                elapsed_time = self._humanized_time(second)
             occurrence = str(record['occurrence'])
             entries.append([function_name, elapsed_time, occurrence])
         entry_widths = []
