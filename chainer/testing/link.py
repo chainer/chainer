51576bb672124bdff9a37cbc6586951ec9b5b3ac
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2019-02-18 11:40:59 +0000

    Initial work on Link test base class

--- chainer/testing/link.py
+++ chainer/testing/link.py
@@ -1,6 +1,7 @@
 import copy
 import unittest
 
+import numpy
 
 import chainer
 from chainer.testing import array as array_module
@@ -9,18 +10,32 @@ from chainer.testing import function as function_module
 
 class LinkTestCase(unittest.TestCase):
 
-    def create_link(self, named_init_params):
-        # Returns a link object to be tested.
-        # assert all(
-        #     isinstance(name, str) and isinstance(p, chainer.Parameter)
-        #     for name, p in named_init_params.items()))
-        raise NotImplementedError('create_link() is not implemented.')
+    def generate_forward_backward_initializers(self):
+        # Returns a tuple of initializers for forward and backward tests.
+        #
+        # The lengh of the tuple should be the same as the number of
+        # parameters.
+        raise NotImplementedError
+
+    def generate_initializers(self):
+        # Returns a tuple of lists, each list containing all initializers to be
+        # tested for a particular parameter.
+        #
+        # The lengh of the tuple should be the same as the number of
+        # parameters.
+        raise NotImplementedError
+
+    def create_link(self, initializers):
+        # Returns a link object that is to be tested.
+        raise NotImplementedError
 
     def generate_inputs(self):
         # Returns a tuple of input numpy.ndarrays to the ``__call__`` method.
-        raise NotImplementedError('generate_inputs() is not implemented.')
+        raise NotImplementedError
 
     def forward(self, link, inputs):
+        # Default implementation of forward. This method may be overridden.
+        #
         # assert all(isinstance(link, chainer.Link))
         # assert all(isinstance(a, chainer.Variable) for a in inputs)
         return link(*inputs)
@@ -30,11 +45,6 @@ class LinkTestCase(unittest.TestCase):
         # assert all(isinstance(p, numpy.ndarray) for p in params)
         raise NotImplementedError('forward_expected() is not implemented.')
 
-    def generate_init_params(self):
-        # Returns a dict of named params to the ``__init__`` method of the
-        # link., i.e. {str: chainer.Parameter}.
-        {}
-
     def on_sample(self):
         # Sets attributes of self that are to be used in the forward
         # definitions.
@@ -43,6 +53,26 @@ class LinkTestCase(unittest.TestCase):
         # e.g. if the attribute is an array representing a nondifferentiable
         # point.
         pass
+
+    def _generate_forward_backward_initializers(self, check=):
+        initializers = self.generate_forward_backward_initializers()
+        for i in initializers:
+            assert (
+                i is None
+                or numpy.isscalar(i)
+                or isinstance(i, chainer.Initializer)
+                or isinstance(i, chainer.get_array_types()))
+        return initializers
+
+    def _generate_initializers(self):
+        # TODO(hvy): Implement me.
+        pass
+
+    def _create_link(self, initializers):
+        link = self.create_link(initializers)
+        if not isinstance(link, chainer.Link):
+            raise RuntimeError
+        return link
 
     def _forward(self, inputs, backend_config):
         assert all(isinstance(a, chainer.Variable) for a in inputs)
