de38540c6fbe667272b4d36affa3d9903385ab1b
Toshiki Kataoka <kataoka@preferred.jp>
2019-02-20 16:43:19 +0900

    Fix tolerance of rrelu test

--- tests/chainer_tests/functions_tests/activation_tests/test_rrelu.py
+++ tests/chainer_tests/functions_tests/activation_tests/test_rrelu.py
@@ -38,7 +38,7 @@ class TestRReLU(testing.FunctionTestCase):
             self.l, self.u = self.u, self.l
 
         if self.dtype == numpy.float16:
-            self.check_forward_options = {'atol': 1e-4, 'rtol': 1e-3}
+            self.check_forward_options = {'atol': 2e-4, 'rtol': 2e-3}
             self.check_backward_options = {'atol': 5e-4, 'rtol': 5e-3}
             self.check_double_backward_options = {'atol': 5e-4, 'rtol': 5e-3}
 
