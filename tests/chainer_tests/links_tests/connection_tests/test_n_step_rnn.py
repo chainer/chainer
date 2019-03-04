d91f77b1e645c14f0d1aab32ad9dcd7367c804d0
crcrpar <masaki.kozuki.2014@gmail.com>
2019-03-04 16:40:15 +0900

    add tests

--- tests/chainer_tests/links_tests/connection_tests/test_n_step_rnn.py
+++ tests/chainer_tests/links_tests/connection_tests/test_n_step_rnn.py
@@ -118,6 +118,33 @@ class TestNStepRNN(unittest.TestCase):
                 cuda.to_gpu(self.h),
                 [cuda.to_gpu(x) for x in self.xs])
 
+    def check_multi_gpu_forward(self, train=True):
+        msg = None
+        rnn = self.rnn.copy('copy')
+        rnn.dropout = .5
+        with cuda.get_device_from_id(1):
+            if self.hidden_none:
+                h = None
+            else:
+                h = cuda.to_gpu(self.h)
+            xs = [cuda.to_gpu(x) for x in self.xs]
+            rnn = rnn.to_gpu()
+        with cuda.get_device_from_id(0),\
+                chainer.using_config('train', train):
+            try:
+                rnn(h, xs)
+            except Exception as e:
+                msg = e
+        assert msg is None
+
+    @attr.multi_gpu(2)
+    def test_multi_gpu_forward_training(self):
+        self.check_multi_gpu_forward(True)
+
+    @attr.multi_gpu(2)
+    def test_multi_gpu_forward_test(self):
+        self.check_multi_gpu_forward(False)
+
     def check_backward(
             self, h_data, xs_data, gh_data, gys_data):
 
@@ -289,6 +316,33 @@ class TestNStepBiRNN(unittest.TestCase):
             self.check_forward(
                 cuda.to_gpu(self.h),
                 [cuda.to_gpu(x) for x in self.xs])
+
+    def check_multi_gpu_forward(self, train=True):
+        rnn = self.rnn.copy('copy')
+        rnn.dropout = .5
+        msg = None
+        with cuda.get_device_from_id(1):
+            if self.hidden_none:
+                h = None
+            else:
+                h = cuda.to_gpu(self.h)
+            xs = [cuda.to_gpu(x) for x in self.xs]
+            rnn = rnn.to_gpu()
+        with cuda.get_device_from_id(0),\
+                chainer.using_config('train', True):
+            try:
+                rnn(h, xs)
+            except Exception as e:
+                msg = e
+        assert msg is None, msg
+
+    @attr.multi_gpu(2)
+    def test_multi_gpu_forward_training(self):
+        self.check_multi_gpu_forward(True)
+
+    @attr.multi_gpu(2)
+    def test_multi_gpu_forward_test(self):
+        self.check_multi_gpu_forward(False)
 
     def check_backward(
             self, h_data, xs_data, gh_data, gys_data):
