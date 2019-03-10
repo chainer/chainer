a96a8fcbdda119cfaaa48c55de9c09d6501a3bd2
aniket <adidolkar123@gmail.com>
2019-03-10 18:19:25 +0530

    fix formatting issues

--- chainerx_cc/chainerx/routines/linalg_test.cc
+++ chainerx_cc/chainerx/routines/linalg_test.cc
@@ -81,12 +81,12 @@ TEST_P(LinalgTest, DotHighDim) {
     Array a = testing::BuildArray({2, 1, 3, 2}).WithLinearData(0.f);
     Array b = testing::BuildArray({1, 4, 2, 2}).WithLinearData(0.f);
     Array c = Dot(a, b);
-    Array e = testing::BuildArray({2, 1, 3, 1, 4, 2}).WithData<float>({2.f, 3.f, 6.f, 7.f, 10.f, 11.f, 14.f, 15.f,
-                                                                    6.f, 11.f, 26.f, 31.f, 46.f, 51.f, 66.f, 71.f,
-                                                                    10.f, 19.f, 46.f, 55.f, 82.f, 91.f, 118.f, 127.f,
-                                                                    14.f, 27.f, 66.f, 79.f, 118.f, 131.f, 170.f, 183.f,
-                                                                    18.f, 35.f, 86.f, 103.f, 154.f, 171.f, 222.f, 239.f, 22.f,
-                                                                    43.f, 106.f, 127.f, 190.f, 211.f, 274.f, 295.f});
+    Array e = testing::BuildArray({2, 1, 3, 1, 4, 2}).WithData<float>({2.f,  3.f,  6.f,  7.f,  10.f,  11.f,  14.f,  15.f, 6.f,  11.f,
+                                                                    26.f,  31.f,  46.f,  51.f,  66.f,  71.f,  10.f,  19.f,  46.f,  55.f,
+                                                                    82.f,  91.f,  118.f, 127.f, 14.f,  27.f,  66.f,  79.f,  118.f, 131.f,
+                                                                    170.f, 183.f, 18.f,  35.f,  86.f,  103.f, 154.f, 171.f, 222.f, 239.f,
+                                                                    22.f,  43.f,  106.f, 127.f, 190.f, 211.f, 274.f, 295.f});
+
     EXPECT_ARRAY_EQ(e, c);
 }
 
@@ -117,7 +117,6 @@ TEST_P(LinalgTest, DotMatVecBackward) {
 
     CheckBackward([](const std::vector<Array>& xs) -> std::vector<Array> { return {Dot(xs[0], xs[1])}; }, {a, b}, {go}, {a_eps, b_eps});
 }
-
 
 TEST_P(LinalgTest, DotHighDimBackward) {
     Array a = (*testing::BuildArray({2, 1, 3, 2}).WithLinearData(1.f)).RequireGrad();
