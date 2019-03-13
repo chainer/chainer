421d533eb2f48d49b331ca0d3e2ebe5f2ef7b4a6
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2019-03-13 07:03:10 +0000

    Callables are initializer-like

--- chainer/initializers/__init__.py
+++ chainer/initializers/__init__.py
@@ -114,6 +114,7 @@ def _get_initializer(initializer):
 
 def _check_is_initializer_like(initializer):
     if not (isinstance(initializer, chainer.Initializer)
+            or callable(initializer)
             or isinstance(initializer, chainer.get_array_types())
             or numpy.isscalar(initializer)):
         raise TypeError(
