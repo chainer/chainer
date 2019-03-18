324e45ea9e89fa366d0022916b12573c326e4675
crcrpar <masaki.kozuki.2014@gmail.com>
2019-03-18 16:00:39 +0900

    update instance_normalization function tests

--- tests/chainer_tests/functions_tests/normalization_tests/test_instance_normalization.py
+++ tests/chainer_tests/functions_tests/normalization_tests/test_instance_normalization.py
@@ -109,4 +109,64 @@ class TestInstanceNormalization(testing.FunctionTestCase):
         return y,
 
 
+@testing.parameterize(*(testing.product({
+    'shape': [(1, 4, 5, 5), (5, 4, 15)],
+    'dtype': [numpy.float16, numpy.float32, numpy.float64],
+    'eps': [2e-5],
+    'decay': [0.9],
+})))
+@backend.inject_backend_tests(
+    None,
+    # # CPU tests
+    testing.product({
+        'use_cuda': [False],
+        'use_ideep': ['never', 'always'],
+    })
+    # GPU tests
+    + testing.product({
+        'use_cuda': [True],
+        'use_cudnn': ['never', 'always'],
+        'cudnn_fast_batch_normalization': [True, False],
+    })
+    # ChainerX tests
+    + testing.product({
+        'use_chainerx': [True],
+        'chainerx_device': ['native:0', 'cuda:0'],
+    })
+)
+class TestFixedInstanceNormalization(testing.FunctionTestCase):
+
+    def setUp(self):
+        self.check_forward_options.update({'atol': 1e-4, 'rtol': 1e-3})
+        self.check_backward_options.update({'atol': 1e-5, 'rtol': 1e-4})
+        self.check_double_backward_options.update({'atol': 1e-3, 'rtol': 1e-2})
+        if self.dtype == numpy.float16:
+            self.check_forward_options.update({'atol': 1e-2, 'rtol': 1e-2})
+            self.check_backward_options.update({'atol': 1e-2, 'rtol': 1e-2})
+            self.check_double_backward_options.update(
+                {'atol': 1e-2, 'rtol': 1e-2})
+        if self.running_statistics:
+            c = self.shape[1]
+
+    def generate_inputs(self):
+        shape, dtype = self.shape, self.dtype
+        x = numpy.random.uniform(-1, 1, shape).astype(dtype)
+        gamma = numpy.random.uniform(0.5, 1, shape[1]).astype(dtype)
+        beta = numpy.random.uniform(-1, 1, shape[1]).astype(dtype)
+        mean = numpy.random.uniform(-1, 1, shape[1]).astype(self.dtype)
+        var = numpy.random.uniform(-1, 1, shape[1]).astype(self.dtype)
+        return x, gamma, beta, mean, var
+
+    def forward(self, inputs, device):
+        x, gamma, beta, mean, var = inputs
+        return functions.fixed_instance_normalization(
+            x, gamma, beta, mean, var, eps=self.eps, decay=self.decay),
+
+    def forward_expected(self, inputs):
+        x, gamma, beta, mean, var = inputs
+        y = _fixed_instance_normalization(
+            x, gamma, beta, mean, var, self.eps, self.decay)
+        return y,
+
+
 testing.run_module(__name__, __file__)
