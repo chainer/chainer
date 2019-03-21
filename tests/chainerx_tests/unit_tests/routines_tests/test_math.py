7a8bdfd5a51f24a32253a6baa168e7ce7ff29731
aksub99 <akshaysubramanian99@gmail.com>
2019-03-21 18:53:33 +0530

    skip inf

--- tests/chainerx_tests/unit_tests/routines_tests/test_math.py
+++ tests/chainerx_tests/unit_tests/routines_tests/test_math.py
@@ -1062,6 +1062,11 @@ class TestSquare(op_utils.NumpyOpTest):
         self.chx_dtype = out_dtype
         self.contiguous = contiguous
 
+        # Backward and double backward tests skipped to avoid
+        # numerical gradient calculation problem
+        if numpy.isinf(input).any():
+            op_utils.NumpyOpTest.skip_backward_test = True
+            op_utils.NumpyOpTest.skip_double_backward_test = True
         if in_dtype == 'float16':
             self.check_backward_options = {'atol': 5e-4, 'rtol': 5e-3}
             self.check_double_backward_options = {'atol': 5e-3, 'rtol': 5e-2}
