59f431d8dc02104f6d1e806b350b88c2a872b502
aksub99 <akshaysubramanian99@gmail.com>
2019-03-22 09:21:07 +0530

    Make changes

--- tests/chainerx_tests/unit_tests/routines_tests/test_math.py
+++ tests/chainerx_tests/unit_tests/routines_tests/test_math.py
@@ -1065,8 +1065,8 @@ class TestSquare(op_utils.NumpyOpTest):
         # Backward and double backward tests skipped to avoid
         # numerical gradient calculation problem
         if numpy.isinf(input).any():
-            op_utils.NumpyOpTest.skip_backward_test = True
-            op_utils.NumpyOpTest.skip_double_backward_test = True
+            self.skip_backward_test = True
+            self.skip_double_backward_test = True
         if in_dtype == 'float16':
             self.check_backward_options = {'atol': 5e-4, 'rtol': 5e-3}
             self.check_double_backward_options = {'atol': 5e-3, 'rtol': 5e-2}
