9534550179630ddc827996560cc56f0384e532b1
crcrpar <masaki.kozuki.2014@gmail.com>
2019-03-27 10:50:49 +0900

    change depthwise conv test to use FunctionTestCase

--- tests/chainer_tests/functions_tests/connection_tests/test_depthwise_convolution_2d.py
+++ tests/chainer_tests/functions_tests/connection_tests/test_depthwise_convolution_2d.py
@@ -1,105 +1,79 @@
-import unittest
-
 import numpy
 
-from chainer.backends import cuda
 from chainer import functions
-from chainer import gradient_check
 from chainer import testing
-from chainer.testing import attr
-from chainer.testing import condition
 
 
 @testing.parameterize(*(testing.product({
     'x_dtype': [numpy.float16, numpy.float32, numpy.float64],
     'W_dtype': [numpy.float16, numpy.float32, numpy.float64],
+    'nobias': [True, False],
 })))
-class TestDepthwiseConvolution2DFunction(unittest.TestCase):
+@testing.inject_backend_tests(
+    None,
+    [{}]
+    + testing.product({
+        'use_cuda': [True],
+        'use_cudnn': ['never', 'always'],
+    })
+    + testing.product({
+        'use_chainerx': [True],
+        'chainerx_device': ['native:0', 'cuda:0'],
+    })
+)
+class TestDepthwiseConvolution2DFunction(testing.FunctionTestCase):
 
     def setUp(self):
-        in_channels = 3
-        channel_multiplier = 2
-        kh, kw = (3, 3)
+        self.in_channels = 3
+        self.channel_multiplier = 2
+        self.kh, self.kw = (3, 3)
         self.stride = 2
         self.pad = 1
-        self.W = numpy.random.normal(
-            0, numpy.sqrt(1. / (kh * kw * in_channels)),
-            (channel_multiplier, in_channels, kh, kw)).astype(self.W_dtype)
-        self.b = numpy.random.uniform(
-            -1, 1, in_channels * channel_multiplier).astype(self.x_dtype)
+        self.check_forward_options.update({'atol': 5e-4, 'rtol': 5e-3})
+        self.check_backward_options.update({'atol': 5e-4, 'rtol': 5e-3})
+        self.check_double_backward_options.update({'atol': 5e-4, 'rtol': 5e-3})
+        if numpy.float16 in (self.x_dtype, self.W_dtype):
+            self.check_forward_options.update({'atol': 1e-3, 'rtol': 1e-2})
+            self.check_backward_options.update({'atol': 1e-3, 'rtol': 1e-3})
+            self.check_double_backward_options.update({
+                'atol': 1e-3, 'rtol': 1e-3})
 
-        self.x = numpy.random.uniform(
+    def generate_inputs(self):
+        x = numpy.random.uniform(
             -1, 1, (2, 3, 4, 3)).astype(self.x_dtype)
-        self.gy = numpy.random.uniform(
-            -1, 1, (2, 6, 2, 2)).astype(self.x_dtype)
-        self.check_forward_options = {}
-        self.check_backward_options = {'dtype': numpy.float64}
-        if self.x_dtype == numpy.float16 or self.W_dtype == numpy.float16:
-            self.check_forward_options = {'atol': 5e-3, 'rtol': 5e-3}
-            self.check_backward_options = {
-                'dtype': numpy.float64, 'atol': 5e-3, 'rtol': 5e-3}
+        W = numpy.random.normal(
+            0, numpy.sqrt(1. / (self.kh * self.kw * self.in_channels)),
+            (self.channel_multiplier, self.in_channels, self.kh, self.kw)
+        ).astype(self.W_dtype)
+        if self.nobias:
+            return x, W
+        b = numpy.random.uniform(
+            -1, 1, self.in_channels * self.channel_multiplier
+        ).astype(self.x_dtype)
+        return x, W, b
 
-    def check_forward(self, x_data, W_data, b_data):
-        args1 = (x_data, W_data)
-        args2 = (x_data, W_data)
-        if b_data is not None:
-            args1 = args1 + (b_data,)
-            b_data = sum(numpy.split(b_data, W_data.shape[1]))
-            args2 = args2 + (b_data,)
+    def forward(self, inputs, device):
+        if self.nobias:
+            x, W = inputs
+            b = None
+        else:
+            x, W, b = inputs
 
-        y1 = functions.depthwise_convolution_2d(
-            *args1, stride=self.stride, pad=self.pad)
-        arys = numpy.split(y1.array, self.W.shape[1], axis=1)
-        y1 = sum(arys)
+        y = functions.depthwise_convolution_2d(
+            x, W, b, stride=self.stride, pad=self.pad)
+        y = sum(functions.split_axis(y, W.shape[1], axis=1))
+        return y,
 
-        y2 = functions.convolution_2d(
-            *args2, stride=self.stride, pad=self.pad).array
-        testing.assert_allclose(y1, y2, **self.check_forward_options)
-
-    def test_forward_cpu(self):
-        self.check_forward(self.x, self.W, self.b)
-
-    def test_forward_cpu_nobias(self):
-        self.check_forward(self.x, self.W, None)
-
-    @attr.gpu
-    def test_forward_gpu(self):
-        self.check_forward(cuda.to_gpu(self.x), cuda.to_gpu(self.W),
-                           cuda.to_gpu(self.b))
-
-    @attr.gpu
-    def test_forward_gpu_nobias(self):
-        self.check_forward(cuda.to_gpu(self.x), cuda.to_gpu(self.W), None)
-
-    def check_backward(self, x_data, W_data, b_data, y_grad):
-        args = (x_data, W_data)
-        if b_data is not None:
-            args = args + (b_data,)
-
-        gradient_check.check_backward(
-            lambda *inputs: functions.depthwise_convolution_2d(
-                *inputs, stride=self.stride, pad=self.pad),
-            args, y_grad, **self.check_backward_options)
-
-    @condition.retry(3)
-    def test_backward_cpu(self):
-        self.check_backward(self.x, self.W, self.b, self.gy)
-
-    @condition.retry(3)
-    def test_backward_cpu_nobias(self):
-        self.check_backward(self.x, self.W, None, self.gy)
-
-    @attr.gpu
-    @condition.retry(3)
-    def test_backward_gpu(self):
-        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.W),
-                            cuda.to_gpu(self.b), cuda.to_gpu(self.gy))
-
-    @attr.gpu
-    @condition.retry(3)
-    def test_backward_gpu_nobias(self):
-        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.W),
-                            None, cuda.to_gpu(self.gy))
+    def forward_expected(self, inputs):
+        if self.nobias:
+            x, W = inputs
+            b = None
+        else:
+            x, W, b = inputs
+            b = sum(numpy.split(b, W.shape[1]))
+        y = functions.convolution_2d(
+            x, W, b, stride=self.stride, pad=self.pad).array
+        return y,
 
 
 testing.run_module(__name__, __file__)
