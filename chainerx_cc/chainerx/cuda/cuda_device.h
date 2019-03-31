1ace8fbb6a88f34ded638ba7a8487124746e68e9
kshitij12345 <kshitijkalambarkar@gmail.com>
2019-03-31 22:34:05 +0530

    add cuda implementation

--- chainerx_cc/chainerx/cuda/cuda_device.h
+++ chainerx_cc/chainerx/cuda/cuda_device.h
@@ -145,6 +145,8 @@ public:
     void IsNan(const Array& x, const Array& out) override;
     void IsInf(const Array& x, const Array& out) override;
 
+    void Floor(const Array& x, const Array& out) override;
+
     // conv.cc
 
     Array Conv(
