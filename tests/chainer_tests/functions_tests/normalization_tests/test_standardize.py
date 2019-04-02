5d8b64ea2d6a73ea49049e02edda069b9de2ac78
hitsgub <hitstales@gmail.com>
2019-04-03 07:50:03 +0900

    fix bug

--- tests/chainer_tests/functions_tests/normalization_tests/test_standardize.py
+++ tests/chainer_tests/functions_tests/normalization_tests/test_standardize.py
@@ -35,14 +35,16 @@ def _is_good_param(param):
         'dtype': [numpy.float32, numpy.float16],
     })
     + testing.product([
-        # same (str): flag whether input elems are same values.
-        #   'no'   : all elems are randamly-chosen,
-        #   'equal': all elems are equal,
-        #   'near' : all elems are (randomly-chosen small values + same value).
-        {'eps': 1e-5, 'same': 'no'},
-        {'eps': 1e-1, 'same': 'no'},
-        {'eps': 1e-1, 'same': 'equal'},
-        {'eps': 1e-1, 'same': 'near'},
+        [
+            # same (str): flag whether input elems are same values.
+            #   'no'   : all elems are randamly-chosen,
+            #   'equal': all elems are equal,
+            #   'near' : all elems are (randomly-chosen small values + same value).
+            {'eps': 1e-5, 'same': 'no'},
+            {'eps': 1e-1, 'same': 'no'},
+            {'eps': 1e-1, 'same': 'equal'},
+            {'eps': 1e-1, 'same': 'near'},
+        ],
     ])
 ))
 @testing.backend.inject_backend_tests(
