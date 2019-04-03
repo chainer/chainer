54c371ee6979c9dd0128e8bd44d7239c66a83ae0
Hari <hsezhiyan@ucdavis.edu>
2019-04-03 01:07:04 -0700

    Converting tabs to spaces
    Converting tabs to spaces to pass flake8 tests.

--- chainer/functions/loss/mean_absolute_error.py
+++ chainer/functions/loss/mean_absolute_error.py
@@ -8,84 +8,84 @@ from chainer.utils import type_check
 
 class MeanAbsoluteError(function_node.FunctionNode):
 
-	"""Mean absolute error function."""
+    """Mean absolute error function."""
 
-	def check_type_forward(self, in_types):
-		type_check._argname(in_types, ('x0', 'x1'))
-		type_check.expect(
-			in_types[0].dtype.kind == 'f',
-			in_types[0].dtype == in_types[1].dtype,
-			in_types[0].shape == in_types[1].shape
-		)
+    def check_type_forward(self, in_types):
+        type_check._argname(in_types, ('x0', 'x1'))
+        type_check.expect(
+            in_types[0].dtype.kind == 'f',
+            in_types[0].dtype == in_types[1].dtype,
+            in_types[0].shape == in_types[1].shape
+        )
 
-	def forward_cpu(self, inputs):
-		x0, x1 = inputs
-		self.diff = x0 - x1
-		diff = self.diff.ravel()
-		return numpy.array(abs(diff).sum() / diff.size, dtype=diff.dtype),
+    def forward_cpu(self, inputs):
+        x0, x1 = inputs
+        self.diff = x0 - x1
+        diff = self.diff.ravel()
+        return numpy.array(abs(diff).sum() / diff.size, dtype=diff.dtype),
 
-	def forward_gpu(self, inputs):
-		x0, x1 = inputs
-		self.diff = x0 - x1
-		diff = self.diff.ravel()
-		return abs(diff).sum() / diff.dtype.type(diff.size),
+    def forward_gpu(self, inputs):
+        x0, x1 = inputs
+        self.diff = x0 - x1
+        diff = self.diff.ravel()
+        return abs(diff).sum() / diff.dtype.type(diff.size),
 
-	def backward(self, indexes, grad_outputs):
-		gy, = grad_outputs
-		coeff = gy * gy.data.dtype.type(1. / self.diff.size)
-		coeff = chainer.functions.broadcast_to(coeff, self.diff.shape)
-		gx0 = coeff * backend.get_array_module(gy.data).sign(self.diff)
-		return gx0, -gx0
+    def backward(self, indexes, grad_outputs):
+        gy, = grad_outputs
+        coeff = gy * gy.data.dtype.type(1. / self.diff.size)
+        coeff = chainer.functions.broadcast_to(coeff, self.diff.shape)
+        gx0 = coeff * backend.get_array_module(gy.data).sign(self.diff)
+        return gx0, -gx0
 
 
 def mean_absolute_error(x0, x1):
-	"""Mean absolute error function.
+    """Mean absolute error function.
 
-	The function computes the mean absolute error between two variables. The
-	mean is taken over the minibatch. Args ``x0`` and ``x1`` must have the
-	same dimensions. This function first calculates the absolute value
-	differences between the corresponding elements in x0 and x1, and then
-	returns the mean of those differences.
+    The function computes the mean absolute error between two variables. The
+    mean is taken over the minibatch. Args ``x0`` and ``x1`` must have the
+    same dimensions. This function first calculates the absolute value
+    differences between the corresponding elements in x0 and x1, and then
+    returns the mean of those differences.
 
-	Args:
-		x0 (:class:`~chainer.Variable` or :ref:`ndarray`): Input variable.
-		x1 (:class:`~chainer.Variable` or :ref:`ndarray`): Input variable.
+    Args:
+        x0 (:class:`~chainer.Variable` or :ref:`ndarray`): Input variable.
+        x1 (:class:`~chainer.Variable` or :ref:`ndarray`): Input variable.
 
-	Returns:
-		~chainer.Variable:
-			A variable holding an array representing the mean absolute
-			error of two inputs.
+    Returns:
+        ~chainer.Variable:
+            A variable holding an array representing the mean absolute
+            error of two inputs.
 
-	.. admonition:: Example
+    .. admonition:: Example
 
-		1D array examples:
+        1D array examples:
 
-		>>> x = np.array([1, 2, 3]).astype(np.float32)
-		>>> y = np.array([0, 0, 0]).astype(np.float32)
-		>>> F.mean_absolute_error(x, y)
-		variable(2.)
-		>>> x = np.array([1, 2, 3, 4, 5, 6]).astype(np.float32)
-		>>> y = np.array([7, 8, 9, 10, 11, 12]).astype(np.float32)
-		>>> F.mean_absolute_error(x, y)
-		variable(6.)
+        >>> x = np.array([1, 2, 3]).astype(np.float32)
+        >>> y = np.array([0, 0, 0]).astype(np.float32)
+        >>> F.mean_absolute_error(x, y)
+        variable(2.)
+        >>> x = np.array([1, 2, 3, 4, 5, 6]).astype(np.float32)
+        >>> y = np.array([7, 8, 9, 10, 11, 12]).astype(np.float32)
+        >>> F.mean_absolute_error(x, y)
+        variable(6.)
 
-		2D array example:
+        2D array example:
 
-		- In this example, there are 4 elements, and thus 4 errors
-		>>> x = np.array([[1, 2], [3, 4]]).astype(np.float32)
-		>>> y = np.array([[8, 8], [8, 8]]).astype(np.float32)
-		>>> F.mean_absolute_error(x, y)
-		variable(5.5)
+        - In this example, there are 4 elements, and thus 4 errors
+        >>> x = np.array([[1, 2], [3, 4]]).astype(np.float32)
+        >>> y = np.array([[8, 8], [8, 8]]).astype(np.float32)
+        >>> F.mean_absolute_error(x, y)
+        variable(5.5)
 
-		3D array example:
+        3D array example:
 
-		- In this example, there are 8 elements, and thus 8 errors
-		>>> x = np.reshape(np.array([1, 2, 3, 4, 5, 6, 7, 8]), (2, 2, 2))
-		>>> y = np.reshape(np.array([8, 8, 8, 8, 8, 8, 8, 8]), (2, 2, 2))
-		>>> x = x.astype(np.float32)
-		>>> y = y.astype(np.float32)
-		>>> F.mean_absolute_error(x, y)
-		variable(3.5)
+        - In this example, there are 8 elements, and thus 8 errors
+        >>> x = np.reshape(np.array([1, 2, 3, 4, 5, 6, 7, 8]), (2, 2, 2))
+        >>> y = np.reshape(np.array([8, 8, 8, 8, 8, 8, 8, 8]), (2, 2, 2))
+        >>> x = x.astype(np.float32)
+        >>> y = y.astype(np.float32)
+        >>> F.mean_absolute_error(x, y)
+        variable(3.5)
 
-	"""
-	return MeanAbsoluteError().apply((x0, x1))[0]
+    """
+    return MeanAbsoluteError().apply((x0, x1))[0]
--- chainer/functions/loss/mean_squared_error.py
+++ chainer/functions/loss/mean_squared_error.py
@@ -7,85 +7,85 @@ from chainer.utils import type_check
 
 class MeanSquaredError(function_node.FunctionNode):
 
-	"""Mean squared error (a.k.a. Euclidean loss) function."""
+    """Mean squared error (a.k.a. Euclidean loss) function."""
 
-	def check_type_forward(self, in_types):
-		type_check._argname(in_types, ('x0', 'x1'))
-		type_check.expect(
-			in_types[0].dtype.kind == 'f',
-			in_types[0].dtype == in_types[1].dtype,
-			in_types[0].shape == in_types[1].shape
-		)
+    def check_type_forward(self, in_types):
+        type_check._argname(in_types, ('x0', 'x1'))
+        type_check.expect(
+            in_types[0].dtype.kind == 'f',
+            in_types[0].dtype == in_types[1].dtype,
+            in_types[0].shape == in_types[1].shape
+        )
 
-	def forward_cpu(self, inputs):
-		self.retain_inputs((0, 1))
-		diff = (inputs[0] - inputs[1]).ravel()
-		return numpy.array(diff.dot(diff) / diff.size, dtype=diff.dtype),
+    def forward_cpu(self, inputs):
+        self.retain_inputs((0, 1))
+        diff = (inputs[0] - inputs[1]).ravel()
+        return numpy.array(diff.dot(diff) / diff.size, dtype=diff.dtype),
 
-	def forward_gpu(self, inputs):
-		self.retain_inputs((0, 1))
-		diff = (inputs[0] - inputs[1]).ravel()
-		return diff.dot(diff) / diff.dtype.type(diff.size),
+    def forward_gpu(self, inputs):
+        self.retain_inputs((0, 1))
+        diff = (inputs[0] - inputs[1]).ravel()
+        return diff.dot(diff) / diff.dtype.type(diff.size),
 
-	def backward(self, indexes, gy):
-		x0, x1 = self.get_retained_inputs()
-		ret = []
-		diff = x0 - x1
-		gy0 = chainer.functions.broadcast_to(gy[0], diff.shape)
-		gx0 = gy0 * diff * (2. / diff.size)
-		if 0 in indexes:
-			ret.append(gx0)
-		if 1 in indexes:
-			ret.append(-gx0)
-		return ret
+    def backward(self, indexes, gy):
+        x0, x1 = self.get_retained_inputs()
+        ret = []
+        diff = x0 - x1
+        gy0 = chainer.functions.broadcast_to(gy[0], diff.shape)
+        gx0 = gy0 * diff * (2. / diff.size)
+        if 0 in indexes:
+            ret.append(gx0)
+        if 1 in indexes:
+            ret.append(-gx0)
+        return ret
 
 
 def mean_squared_error(x0, x1):
-	"""Mean squared error function.
+    """Mean squared error function.
 
-	The function computes the mean squared error between two variables. The
-	mean is taken over the minibatch. Args ``x0`` and ``x1`` must have the
-	same dimensions. Note that the error is not scaled by 1/2.
+    The function computes the mean squared error between two variables. The
+    mean is taken over the minibatch. Args ``x0`` and ``x1`` must have the
+    same dimensions. Note that the error is not scaled by 1/2.
 
-	Args:
-		x0 (:class:`~chainer.Variable` or :ref:`ndarray`): Input variable.
-		x1 (:class:`~chainer.Variable` or :ref:`ndarray`): Input variable.
+    Args:
+        x0 (:class:`~chainer.Variable` or :ref:`ndarray`): Input variable.
+        x1 (:class:`~chainer.Variable` or :ref:`ndarray`): Input variable.
 
-	Returns:
-		~chainer.Variable:
-			A variable holding an array representing the mean squared
-			error of two inputs.
+    Returns:
+        ~chainer.Variable:
+            A variable holding an array representing the mean squared
+            error of two inputs.
 
-	 .. admonition:: Example
+     .. admonition:: Example
 
-		1D array examples:
+        1D array examples:
 
-		>>> x = np.array([1, 2, 3, 4]).astype(np.float32)
-		>>> y = np.array([0, 0, 0, 0]).astype(np.float32)
-		>>> F.mean_squared_error(x, y)
-		variable(7.5)
-		>>> x = np.array([1, 2, 3, 4, 5, 6]).astype(np.float32)
-		>>> y = np.array([7, 8, 9, 10, 11, 12]).astype(np.float32)
-		>>> F.mean_squared_error(x, y)
-		variable(36.)
+        >>> x = np.array([1, 2, 3, 4]).astype(np.float32)
+        >>> y = np.array([0, 0, 0, 0]).astype(np.float32)
+        >>> F.mean_squared_error(x, y)
+        variable(7.5)
+        >>> x = np.array([1, 2, 3, 4, 5, 6]).astype(np.float32)
+        >>> y = np.array([7, 8, 9, 10, 11, 12]).astype(np.float32)
+        >>> F.mean_squared_error(x, y)
+        variable(36.)
 
-		2D array example:
+        2D array example:
 
-		- In this example, there are 4 elements, and thus 4 errors
-		>>> x = np.array([[1, 2], [3, 4]]).astype(np.float32)
-		>>> y = np.array([[8, 8], [8, 8]]).astype(np.float32)
-		>>> F.mean_squared_error(x, y)
-		variable(31.5)
+        - In this example, there are 4 elements, and thus 4 errors
+        >>> x = np.array([[1, 2], [3, 4]]).astype(np.float32)
+        >>> y = np.array([[8, 8], [8, 8]]).astype(np.float32)
+        >>> F.mean_squared_error(x, y)
+        variable(31.5)
 
-		3D array example:
+        3D array example:
 
-		- In this example, there are 8 elements, and thus 8 errors
-		>>> x = np.reshape(np.array([1, 2, 3, 4, 5, 6, 7, 8]), (2, 2, 2))
-		>>> y = np.reshape(np.array([8, 8, 8, 8, 8, 8, 8, 8]), (2, 2, 2))
-		>>> x = x.astype(np.float32)
-		>>> y = y.astype(np.float32)
-		>>> F.mean_squared_error(x, y)
-		variable(17.5)
+        - In this example, there are 8 elements, and thus 8 errors
+        >>> x = np.reshape(np.array([1, 2, 3, 4, 5, 6, 7, 8]), (2, 2, 2))
+        >>> y = np.reshape(np.array([8, 8, 8, 8, 8, 8, 8, 8]), (2, 2, 2))
+        >>> x = x.astype(np.float32)
+        >>> y = y.astype(np.float32)
+        >>> F.mean_squared_error(x, y)
+        variable(17.5)
 
-	"""
-	return MeanSquaredError().apply((x0, x1))[0]
+    """
+    return MeanSquaredError().apply((x0, x1))[0]
