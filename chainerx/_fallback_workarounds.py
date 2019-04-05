2949b1f81ec381d56079e842cbe6f37b2a535ec6
niboshi <niboshi000@gmail.com>
2019-04-05 15:03:36 +0000

    Support ChainerX in text classification example

--- chainerx/_fallback_workarounds.py
+++ chainerx/_fallback_workarounds.py
@@ -152,7 +152,10 @@ def _populate_ndarray():
         is_backprop_required = arr.is_backprop_required()
 
         xp, dev, arr = _from_chx(arr, check_backprop=False)
-        _, _, key = _from_chx(key, check_backprop=False)
+        if isinstance(key, tuple):
+            key = tuple([_from_chx(k, check_backprop=False)[2] for k in key])
+        else:
+            _, _, key = _from_chx(key, check_backprop=False)
 
         with dev:
             ret = arr[key]
@@ -208,6 +211,14 @@ def _populate_ndarray():
         return _to_chx(ret)
 
     ndarray.any = _any
+
+    def tolist(arr):
+        _, dev, arr = _from_chx(arr)
+        with dev:
+            ret = arr.tolist()
+        return ret
+
+    ndarray.tolist = tolist
 
 
 def populate():
--- examples/text_classification/nlp_utils.py
+++ examples/text_classification/nlp_utils.py
@@ -4,7 +4,6 @@ import io
 import numpy
 
 import chainer
-from chainer.backends import cuda
 
 
 def split_text(text, char_based=False):
@@ -62,20 +61,19 @@ def transform_to_array(dataset, vocab, with_label=True):
                 for tokens in dataset]
 
 
+@chainer.dataset.converter()
 def convert_seq(batch, device=None, with_label=True):
     def to_device_batch(batch):
         if device is None:
             return batch
-        elif device < 0:
-            return [chainer.dataset.to_device(device, x) for x in batch]
-        else:
-            xp = cuda.cupy.get_array_module(*batch)
-            concat = xp.concatenate(batch, axis=0)
-            sections = numpy.cumsum([len(x)
-                                     for x in batch[:-1]], dtype=numpy.int32)
-            concat_dev = chainer.dataset.to_device(device, concat)
-            batch_dev = cuda.cupy.split(concat_dev, sections)
-            return batch_dev
+        src_xp = chainer.backend.get_array_module(*batch)
+        xp = device.xp
+        concat = src_xp.concatenate(batch, axis=0)
+        sections = numpy.cumsum([len(x)
+                                 for x in batch[:-1]], dtype=numpy.int32)
+        concat_dev = chainer.dataset.to_device(device, concat)
+        batch_dev = xp.split(concat_dev, sections)
+        return batch_dev
 
     if with_label:
         return {'xs': to_device_batch([x for x, _ in batch]),
