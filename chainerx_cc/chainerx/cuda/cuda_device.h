455c71f15091dd69c11db6b69bdee2a2b738a720
Hiroyuki Vincent Yamazaki <hiroyuki.vincent.yamazaki@gmail.com>
2019-04-05 19:09:00 +0530

    revert comment change
    Co-Authored-By: dido1998 <adidolkar123@gmail.com>

--- chainerx_cc/chainerx/cuda/cuda_device.h
+++ chainerx_cc/chainerx/cuda/cuda_device.h
@@ -231,7 +231,7 @@ private:
     std::shared_ptr<void> AllocatePinnedMemory(size_t bytesize);
 
     // Asynchronous transfer from host to this device, w.r.t. host, using temporary pinned memory.
-    // The current device must be set to this device, prior to calling this  function.
+    // The current device must be set to this device, prior to calling this function.
     void MemoryCopyFromHostAsync(void* dst, const void* src, size_t bytesize);
 
     std::shared_ptr<MemoryPool> device_memory_pool_;
