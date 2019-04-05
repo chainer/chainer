8352fbe4212f527a2383af1464cd160db9d9eebe
kshitij12345 <kshitijkalambarkar@gmail.com>
2019-04-06 03:31:35 +0530

    cuda impl

--- chainerx_cc/chainerx/cuda/numeric.cuh
+++ chainerx_cc/chainerx/cuda/numeric.cuh
@@ -23,6 +23,46 @@ __device__ inline bool IsInf(double value) { return isinf(value); }
 __device__ inline bool IsInf(float value) { return isinf(value); }
 
 template <typename T>
+struct Sinh {
+    __device__ inline T operator()(T x) { return std::sinh(x); }
+};
+
+template <>
+struct Sinh<cuda::Float16> {
+    __device__ inline cuda::Float16 operator()(cuda::Float16 x) { return cuda::Float16{std::sinh(static_cast<float>(x))}; }
+};
+
+template <typename T>
+struct Cosh {
+    __device__ inline T operator()(T x) { return std::cosh(x); }
+};
+
+template <>
+struct Cosh<cuda::Float16> {
+    __device__ inline cuda::Float16 operator()(cuda::Float16 x) { return cuda::Float16{std::cosh(static_cast<float>(x))}; }
+};
+
+template <typename T>
+struct Arcsinh {
+    __device__ inline T operator()(T x) { return std::asinh(x); }
+};
+
+template <>
+struct Arcsinh<cuda::Float16> {
+    __device__ inline cuda::Float16 operator()(cuda::Float16 x) { return cuda::Float16{std::asinh(static_cast<float>(x))}; }
+};
+
+template <typename T>
+struct Arccosh {
+    __device__ inline T operator()(T x) { return std::acosh(x); }
+};
+
+template <>
+struct Arccosh<cuda::Float16> {
+    __device__ inline cuda::Float16 operator()(cuda::Float16 x) { return cuda::Float16{std::acosh(static_cast<float>(x))}; }
+};
+
+template <typename T>
 __device__ inline T Tanh(T x) {
     return std::tanh(x);
 }
