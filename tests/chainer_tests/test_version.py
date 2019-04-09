46c8ce8f1288fa12dcc5a6490670df2641fce990
Toshiki Kataoka <tos.lunar@gmail.com>
2019-04-09 18:29:38 +0900

    Update tests/chainer_tests/test_version.py
    Append testing.run_module
    
    Co-Authored-By: ktns <ktns.87@gmail.com>

--- tests/chainer_tests/test_version.py
+++ tests/chainer_tests/test_version.py
@@ -8,3 +8,6 @@ class TestVersion(unittest.TestCase):
     def test_pep440(self):
         version = packaging.version.parse(chainer.__version__)
         self.assertIsInstance(version, packaging.version.Version)
+
+
+testing.run_module(__name__, __file__)
