62ad63d8389b86e9dbcdbd6a6f2fb0a2dd6d3e24
niboshi <niboshi000@gmail.com>
2019-04-10 23:06:05 +0900

    Op registration: conv

--- chainerx_cc/chainerx/device.h
+++ chainerx_cc/chainerx/device.h
@@ -183,47 +183,6 @@ public:
     virtual void IsNan(const Array& x, const Array& out) = 0;
     virtual void IsInf(const Array& x, const Array& out) = 0;
 
-    // Computes the n-dimensional convolution.
-    //
-    // x: (batch_size, in_channels, in_1, in_2, ..., in_n)
-    // w: (out_channels, in_channels, k_1, k_2, ..., k_n)
-    // b: (out_channels)
-    //
-    // Returns an array of shape (batch_size, out_channels, out_1, out_2, ..., out_n).
-    virtual Array Conv(
-            const Array& x,
-            const Array& w,
-            const nonstd::optional<Array>& b,
-            const StackVector<int64_t, kMaxNdim>& stride,
-            const StackVector<int64_t, kMaxNdim>& pad,
-            bool cover_all,
-            Dtype out_dtype) = 0;
-
-    virtual Array ConvGradWeight(
-            Dtype w_dtype,
-            const Shape& w_shape,
-            const Array& x,
-            const Array& gy,
-            const StackVector<int64_t, kMaxNdim>& stride,
-            const StackVector<int64_t, kMaxNdim>& pad,
-            bool cover_all) = 0;
-
-    // Computes the n-dimensional transposed convolution.
-    //
-    // x: (batch_size, in_channels, in_1, in_2, ..., in_n)
-    // w: (in_channels, out_channels, k_1, k_2, ..., k_n)
-    // b: (out_channels)
-    //
-    // Returns an array of shape (batch_size, out_channels, out_1, out_2, ..., out_n).
-    virtual Array ConvTranspose(
-            const Array& x,
-            const Array& w,
-            const nonstd::optional<Array>& b,
-            const StackVector<int64_t, kMaxNdim>& stride,
-            const StackVector<int64_t, kMaxNdim>& pad,
-            const StackVector<int64_t, kMaxNdim>& out_size,
-            Dtype out_dtype) = 0;
-
     virtual std::unique_ptr<MaxPoolForwardBackward> GetMaxPoolForwardBackward(
             const StackVector<int64_t, kMaxNdim>& kernel_size,
             const StackVector<int64_t, kMaxNdim>& stride,
