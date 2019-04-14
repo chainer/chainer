072eedaa737cbc763faf6f498faa76d80383e17c
ishanrai05 <ishanrai05@gmail.com>
2019-04-14 08:09:12 +0530

    Simplify `F.spatial_transformer_grid` test
    Simplifies `test_spatial_transformer_grid` using
    `testing.FunctionTestCase`

--- tests/chainer_tests/functions_tests/array_tests/test_spatial_transformer_grid.py
+++ tests/chainer_tests/functions_tests/array_tests/test_spatial_transformer_grid.py
@@ -1,27 +1,36 @@
-import unittest
-
 import numpy
 
-import chainer
-from chainer.backends import cuda
 from chainer import functions
-from chainer import gradient_check
 from chainer import testing
-from chainer.testing import attr
 
 
 @testing.parameterize(*testing.product({
     'dtype': [numpy.float16, numpy.float32, numpy.float64],
-    'use_cudnn': ['always', 'never'],
+    'use_cudnn': ['never', 'always'],
 }))
-class TestSpatialTransformerGrid(unittest.TestCase):
+@testing.inject_backend_tests(
+    None,
+    # CPU tests
+    [
+        {},
+    ]
+    # GPU tests
+    + testing.product({
+        'use_cuda': [True],
+        'cuda_device': [0, 1],
+        'use_cudnn': ['always'],
+    })
+    # ChainerX tests
+    + [
+        {'use_chainerx': True, 'chainerx_device': 'native:0'},
+        {'use_chainerx': True, 'chainerx_device': 'cuda:0'},
+        {'use_chainerx': True, 'chainerx_device': 'cuda:1'},
+    ])
+class TestSpatialTransformerGrid(testing.FunctionTestCase):
 
     def setUp(self):
-        B = 3
-        self.theta = numpy.random.uniform(size=(B, 2, 3)).astype(self.dtype)
+        self.skip_double_backward_test = True
         self.output_shape = (5, 6)
-        self.grads = numpy.random.uniform(
-            size=(B, 2) + self.output_shape).astype(self.dtype)
 
         if self.dtype == numpy.float16:
             self.check_forward_options = {'atol': 1e-3, 'rtol': 1e-3}
@@ -30,48 +39,29 @@ class TestSpatialTransformerGrid(unittest.TestCase):
             self.check_forward_options = {}
             self.check_backward_options = {}
 
-    def check_forward(self, theta, output_shape):
-        grid = functions.spatial_transformer_grid(theta, output_shape).data
+    def generate_inputs(self):
+        theta = numpy.random.uniform(size=(3, 2, 3)).astype(self.dtype)
+        return theta,
 
-        theta = cuda.to_cpu(theta)
+    def forward_expected(self, inputs):
+        theta, = inputs
         B = theta.shape[0]
-        H, W = output_shape
+        H, W = self.output_shape
 
         expected = []
         for b in range(B):
             for i in numpy.linspace(-1., 1., H):
                 for j in numpy.linspace(-1., 1., W):
                     coord = numpy.array([j, i, 1])
-                    expected.append(self.theta[b].dot(coord))
+                    expected.append(theta[b].dot(coord))
         expected = numpy.array(
             expected).reshape(B, H, W, 2).transpose(0, 3, 1, 2)
-        testing.assert_allclose(grid, expected, **self.check_forward_options)
-        self.assertEqual(grid.dtype, self.dtype)
+        return expected.astype(self.dtype),
 
-    def test_forward_cpu(self):
-        self.check_forward(self.theta, self.output_shape)
-
-    @attr.gpu
-    def test_forward_gpu(self):
-        self.check_forward(cuda.to_gpu(self.theta), self.output_shape)
-
-    def check_backward(self, theta, output_shape, grads):
-        def f(theta):
-            return functions.spatial_transformer_grid(theta, output_shape)
-
-        with chainer.using_config('use_cudnn', self.use_cudnn):
-            gradient_check.check_backward(
-                f, (theta,), (grads,), dtype=numpy.float64,
-                **self.check_backward_options)
-
-    def test_backward_cpu(self):
-        self.check_backward(self.theta, self.output_shape, self.grads)
-
-    @attr.gpu
-    def test_backward_gpu(self):
-        with chainer.using_config('use_cudnn', self.use_cudnn):
-            self.check_backward(cuda.to_gpu(self.theta), self.output_shape,
-                                cuda.to_gpu(self.grads))
+    def forward(self, inputs, device):
+        theta, = inputs
+        grid = functions.spatial_transformer_grid(theta, self.output_shape)
+        return grid,
 
 
 testing.run_module(__name__, __file__)
