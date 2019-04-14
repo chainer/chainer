da46bc0e2f1c911a9cdd7aa2aa81b56597ed7bca
ishanrai05 <ishanrai05@gmail.com>
2019-04-14 05:30:02 +0530

    Simplify `F.prelu` test
    Simplifies `test_prelu` using testing.FunctionTestCase

--- tests/chainer_tests/functions_tests/activation_tests/test_prelu.py
+++ tests/chainer_tests/functions_tests/activation_tests/test_prelu.py
@@ -1,10 +1,7 @@
-import unittest
-
 import numpy
 
 import chainer
 from chainer import functions
-from chainer import gradient_check
 from chainer import testing
 
 
@@ -27,62 +24,37 @@ from chainer import testing
         {'use_chainerx': True, 'chainerx_device': 'cuda:0'},
         {'use_chainerx': True, 'chainerx_device': 'cuda:1'},
     ])
-class TestPReLU(unittest.TestCase):
+class TestPReLU(testing.FunctionTestCase):
 
     def setUp(self):
-        # Avoid unstability of numerical grad
-        self.x = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
-        self.x[(-0.05 < self.x) & (self.x < 0.05)] = 0.5
-        self.W = numpy.random.uniform(
-            -1, 1, self.shape[1:1 + self.Wdim]).astype(self.dtype)
-        self.gy = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
-        self.ggx = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
-        self.ggW = numpy.random.uniform(
-            -1, 1, self.W.shape).astype(self.dtype)
-
-        self.check_backward_options = {'dtype': numpy.float64}
+        self.check_backward_options = {}
         if self.dtype == numpy.float16:
             self.check_backward_options.update({'atol': 5e-4, 'rtol': 5e-3})
 
-        self.check_double_backward_options = {
-            'dtype': numpy.float64, 'atol': 5e-4, 'rtol': 5e-3}
+        self.check_double_backward_options = {'atol': 5e-4, 'rtol': 5e-3}
         if self.dtype == numpy.float16:
             self.check_double_backward_options.update(
                 {'atol': 5e-3, 'rtol': 5e-2})
 
-    def test_forward(self, backend_config):
-        x_data, W_data = backend_config.get_array((self.x, self.W))
-        x = chainer.Variable(x_data)
-        W = chainer.Variable(W_data)
-        y = functions.prelu(x, W)
-        self.assertEqual(y.data.dtype, self.dtype)
+    def generate_inputs(self):
+        x = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        x[(-0.05 < x) & (x < 0.05)] = 0.5
+        W = numpy.random.uniform(
+            -1, 1, self.shape[1:1 + self.Wdim]).astype(self.dtype)
+        return x, W
 
-        y_expect = self.x.copy()
+    def forward_expected(self, inputs):
+        x, W = inputs
+        y_expect = x.copy()
         masked = numpy.ma.masked_greater_equal(y_expect, 0, copy=False)
         shape = (1,) + W.shape + (1,) * (x.ndim - W.ndim - 1)
-        masked *= self.W.reshape(shape)
-        testing.assert_allclose(y_expect, y.data)
+        masked *= W.reshape(shape)
+        return y_expect,
 
-    def test_backward(self, backend_config):
-        x_data, W_data, y_grad = (
-            backend_config.get_array((self.x, self.W, self.gy)))
-
-        with backend_config:
-            gradient_check.check_backward(
-                functions.prelu, (x_data, W_data), y_grad,
-                **self.check_backward_options)
-
-    def test_double_backward(self, backend_config):
-        x_data, W_data = backend_config.get_array((self.x, self.W))
-        y_grad = backend_config.get_array(self.gy)
-        x_grad_grad, W_grad_grad = (
-            backend_config.get_array((self.ggx, self.ggW)))
-
-        with backend_config:
-            gradient_check.check_double_backward(
-                functions.prelu, (x_data, W_data), y_grad,
-                (x_grad_grad, W_grad_grad),
-                **self.check_double_backward_options)
+    def forward(self, inputs, device):
+        x, W = inputs
+        y = functions.prelu(x, W)
+        return y,
 
 
 testing.run_module(__name__, __file__)
