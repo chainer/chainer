93422e8842911140af536191a70048643536bdb8
niboshi <niboshi000@gmail.com>
2019-04-17 07:21:39 +0000

    Fix VGG download message

--- chainer/links/model/vision/vgg.py
+++ chainer/links/model/vision/vgg.py
@@ -486,19 +486,19 @@ def _max_pooling_2d(x):
     return max_pooling_2d(x, ksize=2)
 
 
-def _make_npz(path_npz, url, model):
-    path_caffemodel = download.cached_download(url)
-    sys.stderr.write(
-        'Now loading caffemodel (usually it may take few minutes)\n')
-    sys.stderr.flush()
-    VGGLayers.convert_caffemodel_to_npz(path_caffemodel, path_npz)
-    npz.load_npz(path_npz, model)
-    return model
-
-
 def _retrieve(name, url, model):
     root = download.get_dataset_directory('pfnet/chainer/models/')
     path = os.path.join(root, name)
-    return download.cache_or_load_file(
-        path, lambda path: _make_npz(path, url, model),
-        lambda path: npz.load_npz(path, model))
+
+    def create_cache(path):
+        sys.stderr.write(
+            'Loading caffemodel (it may take few minutes)\n')
+        sys.stderr.flush()
+        path_caffemodel = download.cached_download(url)
+        VGGLayers.convert_caffemodel_to_npz(path_caffemodel, path)
+        return npz.load_npz(path, model)
+
+    def load_cache(path):
+        return npz.load_npz(path, model)
+
+    return download.cache_or_load_file(path, create_cache, load_cache)
