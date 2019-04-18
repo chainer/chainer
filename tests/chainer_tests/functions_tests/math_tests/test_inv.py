ee2c1784d18fe6a55a877f5fc2119b97721a1c6a
Alexandr Morev <binom7151@mail.ru>
2019-04-19 01:24:17 +0300

    Simplify F.inv, F.batch_inv test

--- tests/chainer_tests/functions_tests/math_tests/test_inv.py
+++ tests/chainer_tests/functions_tests/math_tests/test_inv.py
@@ -5,7 +5,6 @@ import numpy
 import chainer
 from chainer.backends import cuda
 from chainer import functions
-from chainer import gradient_check
 from chainer import testing
 from chainer.testing import attr
 from chainer.utils import type_check
@@ -30,167 +29,120 @@ def _make_eye(shape):
     'dtype': [numpy.float16, numpy.float32, numpy.float64],
     'shape': [(1, 1), (5, 5)],
 }))
-class InvFunctionTest(unittest.TestCase):
+@testing.fix_random()
+@testing.inject_backend_tests(
+    None,
+    # CPU tests
+    [
+        {},
+    ]
+    # GPU tests
+    + testing.product({
+        'use_cuda': [True],
+        'cuda_device': [0, 1],
+    })
+    # ChainerX tests
+    + testing.product({
+        'use_chainerx': [True],
+        'chainerx_device': ['native:0', 'cuda:0', 'cuda:1'],
+    })
+)
+class InvFunctionTest(testing.FunctionTestCase):
 
     def setUp(self):
-        self.x = (numpy.eye(self.shape[-1]) +
-                  numpy.random.uniform(-0.01, 0.01, self.shape)).astype(
-            self.dtype)
-        self.gy = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
-        self.ggx = (numpy.eye(self.shape[-1]) +
-                    numpy.random.uniform(-0.01, 0.01, self.shape)).astype(
-            self.dtype)
-
         if self.dtype == numpy.float16:
             self.check_forward_dtype = numpy.float32
-            self.check_forward_options = {'atol': 1e-3, 'rtol': 1e-3}
-            self.check_backward_options = {
-                'dtype': numpy.float64, 'atol': 1e-3, 'rtol': 1e-3}
-            self.check_double_backward_options = {
-                'dtype': numpy.float64, 'atol': 5e-3, 'rtol': 5e-3}
+            self.check_forward_options.update({'atol': 1e-3, 'rtol': 1e-3})
+            self.check_backward_options.update({'atol': 1e-3, 'rtol': 1e-3})
+            self.check_double_backward_options.update({
+                'atol': 5e-3, 'rtol': 5e-3})
         else:
             self.check_forward_dtype = self.dtype
-            self.check_forward_options = {'atol': 1e-4, 'rtol': 1e-4}
-            self.check_backward_options = {'atol': 5e-4, 'rtol': 5e-4}
-            self.check_double_backward_options = {'atol': 5e-4, 'rtol': 5e-4}
+            self.check_forward_options.update({'atol': 1e-4, 'rtol': 1e-4})
+            self.check_backward_options.update({'atol': 5e-4, 'rtol': 5e-4})
+            self.check_double_backward_options.update({
+                'atol': 5e-4, 'rtol': 5e-4})
 
-    def check_forward(self, x_data):
-        x = chainer.Variable(x_data)
-        y = functions.inv(x)
-        x1 = self.x.astype(self.check_forward_dtype, copy=False)
-        testing.assert_allclose(_inv(x1), y.data, **self.check_forward_options)
+    def generate_inputs(self):
+        x = (numpy.eye(self.shape[-1]) +
+             numpy.random.uniform(-0.01, 0.01, self.shape)).astype(self.dtype)
+        return x,
 
-    def check_backward(self, x_data, y_grad):
-        gradient_check.check_backward(
-            functions.inv, x_data, y_grad, **self.check_backward_options)
+    def forward(self, inputs, device):
+        x, = inputs
+        return functions.inv(x),
 
-    def check_double_backward(self, x_data, y_grad, x_grad_grad):
-        gradient_check.check_double_backward(
-            functions.inv, x_data, y_grad, x_grad_grad,
-            **self.check_double_backward_options)
+    def forward_expected(self, inputs):
+        x, = inputs
+        x1 = x.astype(self.check_forward_dtype, copy=False)
+        return _inv(x1).astype(self.dtype),
 
-    def test_identity_cpu(self):
-        eye = _make_eye(self.x.shape)
-        x = chainer.Variable(self.x)
+    def test_identity(self, backend_config):
+        x, = self.generate_inputs()
+        x = chainer.Variable(backend_config.get_array(x))
         y = functions.matmul(x, functions.inv(x))
         testing.assert_allclose(
-            y.data, eye, **self.check_forward_options)
-
-    @attr.gpu
-    def test_identity_gpu(self):
-        eye = cuda.to_gpu(_make_eye(self.x.shape))
-        x = chainer.Variable(cuda.to_gpu(self.x))
-        y = functions.matmul(x, functions.inv(x))
-        testing.assert_allclose(
-            y.data, eye, **self.check_forward_options)
-
-    def test_forward_cpu(self):
-        self.check_forward(self.x)
-
-    @attr.gpu
-    def test_forward_gpu(self):
-        self.check_forward(cuda.to_gpu(self.x))
-
-    def test_backward_cpu(self):
-        self.check_backward(self.x, self.gy)
-
-    @attr.gpu
-    def test_backward_gpu(self):
-        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
-
-    def test_double_backward_cpu(self):
-        self.check_double_backward(self.x, self.gy, self.ggx)
-
-    @attr.gpu
-    def test_double_backward_gpu(self):
-        self.check_double_backward(
-            cuda.to_gpu(self.x),
-            cuda.to_gpu(self.gy),
-            cuda.to_gpu(self.ggx))
+            y.data, _make_eye(x.shape), **self.check_forward_options)
 
 
 @testing.parameterize(*testing.product({
     'dtype': [numpy.float16, numpy.float32, numpy.float64],
     'shape': [(5, 1, 1), (3, 5, 5)],
 }))
-class BatchInvFunctionTest(unittest.TestCase):
+@testing.fix_random()
+@testing.inject_backend_tests(
+    None,
+    # CPU tests
+    [
+        {},
+    ]
+    # GPU tests
+    + testing.product({
+        'use_cuda': [True],
+        'cuda_device': [0, 1],
+    })
+    # ChainerX tests
+    + testing.product({
+        'use_chainerx': [True],
+        'chainerx_device': ['native:0', 'cuda:0', 'cuda:1'],
+    })
+)
+class BatchInvFunctionTest(testing.FunctionTestCase):
 
     def setUp(self):
-        self.x = (numpy.eye(self.shape[-1]) +
-                  numpy.random.uniform(-0.01, 0.01, self.shape)).astype(
-            self.dtype)
-        self.gy = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
-        self.ggx = (numpy.eye(self.shape[-1]) +
-                    numpy.random.uniform(-0.01, 0.01, self.shape)).astype(
-            self.dtype)
-
         if self.dtype == numpy.float16:
             self.check_forward_dtype = numpy.float32
-            self.check_forward_options = {'atol': 1e-3, 'rtol': 1e-3}
-            self.check_backward_options = {
-                'dtype': numpy.float64, 'atol': 1e-3, 'rtol': 1e-3}
-            self.check_double_backward_options = {
-                'dtype': numpy.float64, 'atol': 5e-3, 'rtol': 5e-3}
+            self.check_forward_options.update({'atol': 1e-3, 'rtol': 1e-3})
+            self.check_backward_options.update({'atol': 1e-3, 'rtol': 1e-3})
+            self.check_double_backward_options.update({
+                'atol': 5e-3, 'rtol': 5e-3})
         else:
             self.check_forward_dtype = self.dtype
-            self.check_forward_options = {'atol': 1e-4, 'rtol': 1e-4}
-            self.check_backward_options = {'atol': 5e-4, 'rtol': 5e-4}
-            self.check_double_backward_options = {'atol': 1e-3, 'rtol': 1e-3}
+            self.check_forward_options.update({'atol': 1e-4, 'rtol': 1e-4})
+            self.check_backward_options.update({'atol': 5e-4, 'rtol': 5e-4})
+            self.check_double_backward_options.update({
+                'atol': 1e-3, 'rtol': 1e-3})
 
-    def check_forward(self, x_data):
-        x = chainer.Variable(x_data)
-        y = functions.batch_inv(x)
-        x1 = self.x.astype(self.check_forward_dtype, copy=False)
-        testing.assert_allclose(_inv(x1), y.data, **self.check_forward_options)
+    def generate_inputs(self):
+        x = (numpy.eye(self.shape[-1]) +
+             numpy.random.uniform(-0.01, 0.01, self.shape)).astype(self.dtype)
+        return x,
 
-    def check_backward(self, x_data, y_grad):
-        gradient_check.check_backward(
-            functions.batch_inv, x_data, y_grad,
-            **self.check_backward_options)
+    def forward(self, inputs, device):
+        x, = inputs
+        return functions.batch_inv(x),
 
-    def check_double_backward(self, x_data, y_grad, x_grad_grad):
-        gradient_check.check_double_backward(
-            functions.batch_inv, x_data, y_grad, x_grad_grad,
-            **self.check_double_backward_options)
+    def forward_expected(self, inputs):
+        x, = inputs
+        x1 = x.astype(self.check_forward_dtype, copy=False)
+        return _inv(x1).astype(self.dtype),
 
-    def test_identity_cpu(self):
-        eye = _make_eye(self.x.shape)
-        x = chainer.Variable(self.x)
+    def test_identity(self, backend_config):
+        x, = self.generate_inputs()
+        x = chainer.Variable(backend_config.get_array(x))
         y = functions.matmul(x, functions.batch_inv(x))
         testing.assert_allclose(
-            y.data, eye, **self.check_forward_options)
-
-    @attr.gpu
-    def test_identity_gpu(self):
-        eye = cuda.to_gpu(_make_eye(self.x.shape))
-        x = chainer.Variable(cuda.to_gpu(self.x))
-        y = functions.matmul(x, functions.batch_inv(x))
-        testing.assert_allclose(
-            y.data, eye, **self.check_forward_options)
-
-    def test_forward_cpu(self):
-        self.check_forward(self.x)
-
-    @attr.gpu
-    def test_forward_gpu(self):
-        self.check_forward(cuda.to_gpu(self.x))
-
-    def test_backward_cpu(self):
-        self.check_backward(self.x, self.gy)
-
-    @attr.gpu
-    def test_backward_gpu(self):
-        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
-
-    def test_double_backward_cpu(self):
-        self.check_double_backward(self.x, self.gy, self.ggx)
-
-    @attr.gpu
-    def test_double_backward_gpu(self):
-        self.check_double_backward(
-            cuda.to_gpu(self.x),
-            cuda.to_gpu(self.gy),
-            cuda.to_gpu(self.ggx))
+            y.data, _make_eye(x.shape), **self.check_forward_options)
 
 
 class InvFunctionRaiseTest(unittest.TestCase):
