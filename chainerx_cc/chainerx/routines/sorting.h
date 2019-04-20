969f201fe173205dd13d7a9b2660df06f73cec33
Harshan Baskar <Harshan01@gmail.com>
2019-04-20 18:21:18 +0530

    Update sorting.h

--- chainerx_cc/chainerx/routines/sorting.h
+++ chainerx_cc/chainerx/routines/sorting.h
@@ -9,16 +9,6 @@ namespace chainerx {
 
 Array ArgMax(const Array& a, const OptionalAxes& axis = nonstd::nullopt);
 
-class ArgMinOp : public Op {
-public:
-    static const char* name() { return "ArgMin"; }
-
-    virtual Array Call(const Array& a, const OptionalAxes& axis);
-
-protected:
-    virtual void Impl(const Array& a, const Axes& axis, const Array& out) = 0;
-};
-
-inline Array ArgMin(const Array& a, const OptionalAxes& axis = nonstd::nullopt) { return a.device().backend().CallOp<ArgMinOp>(a, axis); }
+Array ArgMin(const Array& a, const OptionalAxes& axis = nonstd::nullopt);
 
 }  // namespace chainerx
