d4f373fe7ec2f7534a34e0ace9ad4ec6d94658e8
ishanrai05 <ishanrai05@gmail.com>
2019-04-21 21:41:01 +0530

    [chainerx::log10] Adds native implementation

--- chainerx_cc/chainerx/native/native_device/exp_log.cc
+++ chainerx_cc/chainerx/native/native_device/exp_log.cc
@@ -48,6 +48,23 @@ public:
 
 CHAINERX_NATIVE_REGISTER_KERNEL(LogKernel, NativeLogKernel);
 
+class NativeLog10Kernel : public Log10Kernel {
+public:
+    void Call(const Array& x, const Array& out) override {
+        x.device().CheckDevicesCompatible(x, out);
+        const Array& x_cast = x.dtype() == out.dtype() ? x : x.AsType(out.dtype());
+        VisitFloatingPointDtype(out.dtype(), [&x_cast, &out](auto pt) {
+            using T = typename decltype(pt)::type;
+            struct Impl {
+                void operator()(int64_t /*i*/, T x, T& out) { out = chainerx::Log10(x); }
+            };
+            Elementwise<const T, T>(Impl{}, x_cast, out);
+        });
+    }
+};
+
+CHAINERX_NATIVE_REGISTER_KERNEL(Log10Kernel, NativeLog10Kernel);
+
 }  // namespace
 }  // namespace native
 }  // namespace chainerx
