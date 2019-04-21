f4474b789fc3f06bee4450fc5911bdcb7772db5b
ishanrai05 <ishanrai05@gmail.com>
2019-04-21 18:44:47 +0530

    [chainerx::logical_xor] Adds native implementation

--- chainerx_cc/chainerx/native/native_device/comparison.cc
+++ chainerx_cc/chainerx/native/native_device/comparison.cc
@@ -152,6 +152,26 @@ public:
 
 CHAINERX_NATIVE_REGISTER_KERNEL(LogicalOrKernel, NativeLogicalOrKernel);
 
+class NativeLogicalXorKernel : public LogicalXorKernel {
+public:
+    void Call(const Array& x1, const Array& x2, const Array& out) override {
+        Device& device = x1.device();
+        device.CheckDevicesCompatible(x1, x2, out);
+        Dtype dtype = PromoteTypes(x1.dtype(), x2.dtype());
+        const Array& x1_cast = x1.dtype() == dtype ? x1 : x1.AsType(dtype);
+        const Array& x2_cast = x2.dtype() == dtype ? x2 : x2.AsType(dtype);
+        VisitDtype(dtype, [&](auto pt) {
+            using T = typename decltype(pt)::type;
+            struct Impl {
+                void operator()(int64_t /*i*/, T x1, T x2, bool& out) { out = !x1 != !x2; }
+            };
+            Elementwise<const T, const T, bool>(Impl{}, x1_cast, x2_cast, out);
+        });
+    }
+};
+
+CHAINERX_NATIVE_REGISTER_KERNEL(LogicalXorKernel, NativeLogicalXorKernel);
+
 class NativeAllKernel : public AllKernel {
 public:
     void Call(const Array& a, const Axes& axis, const Array& out) override {
