bd0771f4aad10938b91a9ca363a79bc16d118260
niboshi <niboshi000@gmail.com>
2019-05-08 06:56:35 +0000

    Revert "Merge pull request #7043 from grafi-tt/feature/chainerx-native-pairwise-reduction"
    This reverts commit ea6869dec89ae028fc6eb6121ca13948266c5dc1, reversing
    changes made to 71be68c97c13980e34eabc80d5b9d687eccf16dc.

--- chainerx_cc/chainerx/native/reduce.h
+++ chainerx_cc/chainerx/native/reduce.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <array>
 #include <cstdint>
 
 #include "chainerx/array.h"
@@ -12,85 +11,19 @@ namespace chainerx {
 namespace native {
 namespace reduce_detail {
 
-// The number of items those are processed by the statically expanded pairwise reduction routine.
-// Reduction performance is sensitive to this parameter. Increasing the parameter may improve performance,
-// but it can also severely degrade the performance due to register pressure.
-// Must be a power of 2.
-constexpr int64_t ExpandLen = 4;
-// The number of items those are reduced by the serial loop. The loop runs on the outside of the statically expanded
-// reduction. A large value makes overhead for pairwise reduction small. So increasing this parameter will improve
-// performance a little, in exchange for numerical precision.
-// Must be a power of 2.
-constexpr int64_t SerialLen = 8;
-
-template <typename In, typename ReductionImpl, int8_t InNdim, typename T, int64_t n>
-struct ExpandedPairwiseReduction {
-    static T run(const IndexableArray<const In, InNdim>& in, IndexIterator<InNdim>& it_in, ReductionImpl&& impl, int64_t& i_reduce) {
-        T accum = ExpandedPairwiseReduction<In, ReductionImpl, InNdim, T, n / 2>::run(in, it_in, impl, i_reduce);
-        impl.Reduce(ExpandedPairwiseReduction<In, ReductionImpl, InNdim, T, n / 2>::run(in, it_in, impl, i_reduce), accum);
-        return accum;
-    }
-};
-
-template <typename In, typename ReductionImpl, int8_t InNdim, typename T>
-struct ExpandedPairwiseReduction<In, ReductionImpl, InNdim, T, 1> {
-    static T run(const IndexableArray<const In, InNdim>& in, IndexIterator<InNdim>& it_in, ReductionImpl&& impl, int64_t& i_reduce) {
-        T accum = impl.MapIn(native_internal::StorageToDataType<const In>(in[it_in]), i_reduce);
-        ++it_in, ++i_reduce;
-        return accum;
-    }
-};
-
-constexpr int log2(int64_t v) { return v == 1 ? 0 : log2(v >> 1) + 1; }
-
-template <typename In, typename ReductionImpl, int8_t InNdim, typename T>
-T PairwiseReduction(const IndexableArray<const In, InNdim>& in, IndexIterator<InNdim>& it_in, ReductionImpl&& impl, int64_t reduce_len) {
-    int64_t i_reduce = 0;
-    T accum = impl.Identity();
-
-    constexpr int TreeDepth = 63 - log2(ExpandLen * SerialLen);
-    std::array<T, TreeDepth> tree_accum;  // NOLINT(cppcoreguidelines-pro-type-member-init)
-
-    while (i_reduce < reduce_len - (reduce_len % ExpandLen)) {
-        // Invoke dynamic pairwise reduction if `i_reduce` is multiple of `SerialLen * ExpandLen`.
-        if (i_reduce != 0 && i_reduce % (SerialLen * ExpandLen) == 0) {
-            int i = 0;
-            for (int64_t k = i_reduce >> 1; k % (SerialLen * ExpandLen) == 0; k >>= 1, ++i) {
-                impl.Reduce(tree_accum[i], accum);  // NOLINT(cppcoreguidelines-pro-bounds-constant-array-index)
-            }
-            tree_accum[i] = accum;  // NOLINT(cppcoreguidelines-pro-bounds-constant-array-index)
-            accum = impl.Identity();
-        }
-        // This increments `i_reduce` by `ExpandLen`.
-        impl.Reduce(ExpandedPairwiseReduction<In, ReductionImpl, InNdim, T, ExpandLen>::run(in, it_in, impl, i_reduce), accum);
-    }
-
-    // Accumulate residuals.
-    while (i_reduce < reduce_len) {
-        impl.Reduce(impl.MapIn(native_internal::StorageToDataType<const In>(in[it_in]), i_reduce), accum);
-        ++it_in, ++i_reduce;
-    }
-
-    // Accumulate tree nodes.
-    int i = 0;
-    for (int64_t k = (reduce_len / ExpandLen - 1) / SerialLen; k > 0; k >>= 1, ++i) {
-        if (k & 1) {
-            impl.Reduce(tree_accum[i], accum);  // NOLINT(cppcoreguidelines-pro-bounds-constant-array-index)
-        }
-    }
-
-    return accum;
-}
-
 template <typename In, typename Out, typename ReductionImpl, int8_t InNdim = kDynamicNdim, int8_t OutNdim = kDynamicNdim>
 void ReductionKernel(ReductionKernelArg<In, Out, InNdim, OutNdim> arg, ReductionImpl&& impl) {
     auto it_in = arg.in_indexer.It(0, arg.out_indexer.total_size());
-    int64_t reduce_len = arg.in_indexer.total_size() / arg.out_indexer.total_size();
 
     // Iterate over output dimensions
     for (auto it_out = arg.out_indexer.It(0); it_out; ++it_out) {
-        it_in.Restart(it_out.raw_index());
-        auto accum = PairwiseReduction<In, ReductionImpl, InNdim, decltype(impl.Identity())>(arg.in, it_in, impl, reduce_len);
+        auto accum = impl.Identity();
+
+        int64_t i_reduce{0};
+        for (it_in.Restart(it_out.raw_index()); it_in; ++it_in, ++i_reduce) {
+            impl.Reduce(impl.MapIn(native_internal::StorageToDataType<const In>(arg.in[it_in]), i_reduce), accum);
+        }
+
         arg.out[it_out] = native_internal::DataToStorageType<Out>(impl.MapOut(accum));
     }
 }
