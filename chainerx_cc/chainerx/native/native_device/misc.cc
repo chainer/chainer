61824144897d2eb91e4e1e2c1d15fabf3dd58483
dido1998 <adidolkar123@gmail.com>
2019-05-10 01:47:19 +0530

    add missing override
    Co-Authored-By: Hiroyuki Vincent Yamazaki <hiroyuki.vincent.yamazaki@gmail.com>

--- chainerx_cc/chainerx/native/native_device/misc.cc
+++ chainerx_cc/chainerx/native/native_device/misc.cc
@@ -51,7 +51,7 @@ CHAINERX_NATIVE_REGISTER_KERNEL(PowASKernel, NativePowASKernel);
 
 class NativePowSAKernel : public PowSAKernel {
 public:
-    void Call(Scalar x1, const Array& x2, const Array& out) {
+    void Call(Scalar x1, const Array& x2, const Array& out) override {
         x2.device().CheckDevicesCompatible(x2, out);
         VisitNumericDtype(out.dtype(), [&](auto pt) {
             using T = typename decltype(pt)::type;
