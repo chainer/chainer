9de3f5eb01054faed8178e50aa2bddc77df32186
Akifumi Imanishi <imanishi@preferred.jp>
2019-05-14 04:58:51 +0000

    Rename param names

--- tests/chainerx_tests/unit_tests/routines_tests/test_binary.py
+++ tests/chainerx_tests/unit_tests/routines_tests/test_binary.py
@@ -11,23 +11,6 @@ from chainerx_tests import math_utils
 from chainerx_tests import op_utils
 
 
-_in_out_dtypes_array_int_scalar = [
-    # Int scalar.
-    (('int8',), int, 'int8'),
-    (('int16',), int, 'int16'),
-    (('int32',), int, 'int32'),
-    (('int64',), int, 'int64'),
-    (('uint8',), int, 'uint8'),
-    (('float16',), int, 'float16'),
-    (('float32',), int, 'float32'),
-    (('float64',), int, 'float64'),
-    (('int16',), numpy.int16, 'int16'),
-    (('uint8',), numpy.int8, 'uint8'),
-    (('float64',), numpy.int8, 'float64'),
-    (('float16',), numpy.int64, 'float16'),
-]
-
-
 _in_out_dtypes_bitwise = dtype_utils._permutate_dtype_mapping([
     # Same dtypes
     (('bool_', 'bool_'), 'bool_'),
@@ -54,7 +37,7 @@ _in_out_dtypes_bitwise = dtype_utils._permutate_dtype_mapping([
     (('int64', 'uint8'), 'int64'),
 ])
 
-_inplace_invalid_bitwise = [
+_in_out_dtypes_inplace_bitwise_invalid = [
     (('bool_', 'int8'), 'int8'),
     (('bool_', 'int16'), 'int16'),
     (('bool_', 'int32'), 'int32'),
@@ -62,9 +45,19 @@ _inplace_invalid_bitwise = [
     (('bool_', 'uint8'), 'uint8'),
 ]
 
-_in_out_inplace_dtypes_bitwise = [
+_in_out_dtypes_inplace_bitwise = [
     dtypes for dtypes in _in_out_dtypes_bitwise
-    if dtypes not in _inplace_invalid_bitwise
+    if dtypes not in _in_out_dtypes_inplace_bitwise_invalid
+]
+
+_in_out_dtypes_bitwise_scalar = [
+    (('int8',), int, 'int8'),
+    (('int16',), int, 'int16'),
+    (('int32',), int, 'int32'),
+    (('int64',), int, 'int64'),
+    (('uint8',), int, 'uint8'),
+    (('int16',), numpy.int16, 'int16'),
+    (('uint8',), numpy.int8, 'uint8'),
 ]
 
 _scalar_invalid_bitwise = [
@@ -75,10 +68,6 @@ _scalar_invalid_bitwise = [
     (('float16',), numpy.int64, 'float16'),
 ]
 
-_in_out_scalar_dtypes_bitwise = [
-    dtypes for dtypes in _in_out_dtypes_array_int_scalar
-    if dtypes not in _scalar_invalid_bitwise
-]
 
 _params_bitwise = (
     # Special shapes
@@ -125,7 +114,7 @@ _inplace_params_bitwise = (
     # Dtype combinations
     + chainer.testing.product({
         'in_shapes': [((2, 3), (2, 3))],
-        'in_dtypes,out_dtype': _in_out_inplace_dtypes_bitwise,
+        'in_dtypes,out_dtype': _in_out_dtypes_inplace_bitwise,
         'input_lhs': ['random'],
         'input_rhs': ['random'],
     })
@@ -145,7 +134,7 @@ _scalar_params_bitwise = (
     # Special shapes
     chainer.testing.product({
         'shape': [(), (0,), (1,), (2, 0, 3), (1, 1, 1), (2, 3)],
-        'in_dtypes,scalar_type,out_dtype': _in_out_scalar_dtypes_bitwise,
+        'in_dtypes,scalar_type,out_dtype': _in_out_dtypes_bitwise_scalar,
         'input': ['random'],
         'scalar_value': [1],
         'is_module': [False],
@@ -154,7 +143,7 @@ _scalar_params_bitwise = (
     # Type combinations
     + chainer.testing.product({
         'shape': [(2, 3)],
-        'in_dtypes,scalar_type,out_dtype': _in_out_scalar_dtypes_bitwise,
+        'in_dtypes,scalar_type,out_dtype': _in_out_dtypes_bitwise_scalar,
         'input': ['random'],
         'scalar_value': [1],
         'is_module': [False],
@@ -163,7 +152,7 @@ _scalar_params_bitwise = (
     # is_module
     + chainer.testing.product({
         'shape': [(2, 3)],
-        'in_dtypes,scalar_type,out_dtype': _in_out_scalar_dtypes_bitwise,
+        'in_dtypes,scalar_type,out_dtype': _in_out_dtypes_bitwise_scalar,
         'input': ['random'],
         'scalar_value': [1],
         'is_module': [True, False],
@@ -173,7 +162,7 @@ _scalar_params_bitwise = (
     + chainer.testing.product({
         'shape': [(2, 3)],
         'in_dtypes,scalar_type,out_dtype':
-            _in_out_scalar_dtypes_bitwise,
+            _in_out_dtypes_bitwise_scalar,
         'input': [float('inf'), -float('inf'), float('nan')],
         'scalar_value': [
             0, -1, 1, 2],
@@ -190,7 +179,7 @@ _inplace_scalar_params_bitwise = (
     chainer.testing.product({
         'shape': [(), (0,), (1,), (2, 0, 3), (1, 1, 1), (2, 3)],
         'in_dtypes,scalar_type,out_dtype':
-            _in_out_scalar_dtypes_bitwise,
+            _in_out_dtypes_bitwise_scalar,
         'input': ['random'],
         'scalar_value': [1],
     })
@@ -198,7 +187,7 @@ _inplace_scalar_params_bitwise = (
     + chainer.testing.product({
         'shape': [(2, 3)],
         'in_dtypes,scalar_type,out_dtype':
-            _in_out_scalar_dtypes_bitwise,
+            _in_out_dtypes_bitwise_scalar,
         'input': ['random'],
         'scalar_value': [1],
     })
@@ -206,7 +195,7 @@ _inplace_scalar_params_bitwise = (
     + chainer.testing.product({
         'shape': [(2, 3)],
         'in_dtypes,scalar_type,out_dtype':
-            _in_out_scalar_dtypes_bitwise,
+            _in_out_dtypes_bitwise_scalar,
         'input': [float('inf'), -float('inf'), float('nan')],
         'scalar_value': [
             0, -1, 1, 2],
@@ -275,7 +264,7 @@ class TestIBitwiseXor(
 
 
 @pytest.mark.parametrize_device(['native:0', 'cuda:0'])
-@pytest.mark.parametrize('dtypes', _inplace_invalid_bitwise)
+@pytest.mark.parametrize('dtypes', _in_out_dtypes_inplace_bitwise_invalid)
 def test_iand_invalid_dtypes(device, dtypes):
     (in_dtype1, in_dtype2), _ = dtypes
     shape = (2, 3)
@@ -286,7 +275,7 @@ def test_iand_invalid_dtypes(device, dtypes):
 
 
 @pytest.mark.parametrize_device(['native:0', 'cuda:0'])
-@pytest.mark.parametrize('dtypes', _inplace_invalid_bitwise)
+@pytest.mark.parametrize('dtypes', _in_out_dtypes_inplace_bitwise_invalid)
 def test_ior_invalid_dtypes(device, dtypes):
     (in_dtype1, in_dtype2), _ = dtypes
     shape = (2, 3)
@@ -297,7 +286,7 @@ def test_ior_invalid_dtypes(device, dtypes):
 
 
 @pytest.mark.parametrize_device(['native:0', 'cuda:0'])
-@pytest.mark.parametrize('dtypes', _inplace_invalid_bitwise)
+@pytest.mark.parametrize('dtypes', _in_out_dtypes_inplace_bitwise_invalid)
 def test_ixor_invalid_dtypes(device, dtypes):
     (in_dtype1, in_dtype2), _ = dtypes
     shape = (2, 3)
