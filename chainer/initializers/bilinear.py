440d6584c6fa3b9feafa5c3b871245b24d87bf86
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-05-14 14:30:22 +0100

    Generalize Bilinear for ND input

--- chainer/initializers/bilinear.py
+++ chainer/initializers/bilinear.py
@@ -7,16 +7,18 @@ from chainer import initializer
 # https://github.com/shelhamer/fcn.berkeleyvision.org/blob/master/surgery.py
 
 
-def _get_upsampling_filter(size):
+def _get_upsampling_filter(size, ndim):
     """Make a 2D bilinear kernel suitable for upsampling"""
     factor = (size + 1) // 2
     if size % 2 == 1:
         center = factor - 1.
     else:
         center = factor - 0.5
-    og = numpy.ogrid[:size, :size]
-    filt = (1. - abs(og[0] - center) / factor) * \
-           (1. - abs(og[1] - center) / factor)
+    slices = (slice(None, size),) * ndim
+    og = numpy.ogrid[slices]
+    filt = 1
+    for og_i in og:
+        filt = filt * (1. - abs(og_i - center) / factor)
     return filt
 
 
@@ -39,15 +41,21 @@ class Bilinear(initializer.Initializer):
             assert array.dtype == self.dtype
         xp = cuda.get_array_module(array)
 
-        in_c, out_c, kh, kw = array.shape
-        assert kh == kw
+        in_c, out_c = array.shape[:2]
         assert in_c == out_c or out_c == 1
 
-        filt = _get_upsampling_filter(kh)
+        ksize = None
+        for k in array.shape[2:]:
+            if ksize is None:
+                ksize = k
+            else:
+                assert ksize == k
+
+        filt = _get_upsampling_filter(ksize, ndim=array.ndim - 2)
         filt = xp.asarray(filt)
 
         array[...] = 0.
         if out_c == 1:
-            array[range(in_c), 0, :, :] = filt
+            array[range(in_c), 0, ...] = filt
         else:
-            array[range(in_c), range(out_c), :, :] = filt
+            array[range(in_c), range(out_c), ...] = filt
