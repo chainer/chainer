05deb2de16f307134bbc5e0a1777a64b826b475f
Akifumi Imanishi <imanishi@preferred.jp>
2019-05-17 19:23:15 +0900

    Impl LogicBinaryImpl

--- chainerx_cc/chainerx/routines/logic.cc
+++ chainerx_cc/chainerx/routines/logic.cc
@@ -15,39 +15,33 @@ namespace chainerx {
 
 namespace {
 
-void CheckLogicDtypes(const Array& x1, const Array& x2) {
+template <typename KernelType>
+class LogicBinaryImpl {
+public:
+    LogicBinaryImpl() = default;
+    void operator()(const Array& x1, const Array& x2, Array& out) const {
+        NoBackpropModeScope scope{};
+        x1.device().backend().CallKernel<KernelType>(x1, x2, out);
+    }
+};
+
+template <typename KernelType>
+Array LogicBinary(const Array& x1, const Array& x2) {
     if ((x1.dtype() == Dtype::kBool) != (x2.dtype() == Dtype::kBool)) {
         throw DtypeError{"Comparison of ", GetDtypeName(x1.dtype()), " and ", GetDtypeName(x2.dtype()), " is not supported."};
     }
+    return internal::BroadcastBinary(LogicBinaryImpl<KernelType>{}, x1, x2, Dtype::kBool);
 }
 
 }  // namespace
 
-Array Equal(const Array& x1, const Array& x2) {
-    CheckLogicDtypes(x1, x2);
-    auto func = [](const Array& x1, const Array& x2, Array& out) { x1.device().backend().CallKernel<EqualKernel>(x1, x2, out); };
-    return internal::BroadcastBinary(func, x1, x2, Dtype::kBool);
-}
+Array Equal(const Array& x1, const Array& x2) { return LogicBinary<EqualKernel>(x1, x2); }
 
-Array NotEqual(const Array& x1, const Array& x2) {
-    CheckLogicDtypes(x1, x2);
-    auto func = [](const Array& x1, const Array& x2, Array& out) { x1.device().backend().CallKernel<NotEqualKernel>(x1, x2, out); };
-    return internal::BroadcastBinary(func, x1, x2, Dtype::kBool);
-}
+Array NotEqual(const Array& x1, const Array& x2) { return LogicBinary<NotEqualKernel>(x1, x2); }
 
-Array Greater(const Array& x1, const Array& x2) {
-    CheckLogicDtypes(x1, x2);
-    auto func = [](const Array& x1, const Array& x2, Array& out) { x1.device().backend().CallKernel<GreaterKernel>(x1, x2, out); };
-    return internal::BroadcastBinary(func, x1, x2, Dtype::kBool);
-}
+Array Greater(const Array& x1, const Array& x2) { return LogicBinary<GreaterKernel>(x1, x2); }
 
-Array GreaterEqual(const Array& x1, const Array& x2) {
-    CheckLogicDtypes(x1, x2);
-    auto func = [](const Array& x1, const Array& x2, Array& out) {
-        return x1.device().backend().CallKernel<GreaterEqualKernel>(x1, x2, out);
-    };
-    return internal::BroadcastBinary(func, x1, x2, Dtype::kBool);
-}
+Array GreaterEqual(const Array& x1, const Array& x2) { return LogicBinary<GreaterEqualKernel>(x1, x2); }
 
 Array LogicalNot(const Array& x) {
     Array out = Empty(x.shape(), Dtype::kBool, x.device());
@@ -58,27 +52,11 @@ Array LogicalNot(const Array& x) {
     return out;
 }
 
-Array LogicalAnd(const Array& x1, const Array& x2) {
-    CheckLogicDtypes(x1, x2);
-    auto func = [](const Array& x1, const Array& x2, Array& out) {
-        return x1.device().backend().CallKernel<LogicalAndKernel>(x1, x2, out);
-    };
-    return internal::BroadcastBinary(func, x1, x2, Dtype::kBool);
-}
+Array LogicalAnd(const Array& x1, const Array& x2) { return LogicBinary<LogicalAndKernel>(x1, x2); }
 
-Array LogicalOr(const Array& x1, const Array& x2) {
-    CheckLogicDtypes(x1, x2);
-    auto func = [](const Array& x1, const Array& x2, Array& out) { return x1.device().backend().CallKernel<LogicalOrKernel>(x1, x2, out); };
-    return internal::BroadcastBinary(func, x1, x2, Dtype::kBool);
-}
+Array LogicalOr(const Array& x1, const Array& x2) { return LogicBinary<LogicalOrKernel>(x1, x2); }
 
-Array LogicalXor(const Array& x1, const Array& x2) {
-    CheckLogicDtypes(x1, x2);
-    auto func = [](const Array& x1, const Array& x2, Array& out) {
-        return x1.device().backend().CallKernel<LogicalXorKernel>(x1, x2, out);
-    };
-    return internal::BroadcastBinary(func, x1, x2, Dtype::kBool);
-}
+Array LogicalXor(const Array& x1, const Array& x2) { return LogicBinary<LogicalXorKernel>(x1, x2); }
 
 Array All(const Array& a, const OptionalAxes& axis, bool keepdims) {
     Axes sorted_axis = internal::GetSortedAxesOrAll(axis, a.ndim());
