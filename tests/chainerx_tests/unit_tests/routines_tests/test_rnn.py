7adc07ed3ea3d3ab890aba25d2b4aaffcd3f4b3a
aniket <adidolkar123@gmail.com>
2019-05-25 23:56:23 +0530

    fix python formatting

--- tests/chainerx_tests/unit_tests/routines_tests/test_rnn.py
+++ tests/chainerx_tests/unit_tests/routines_tests/test_rnn.py
@@ -1,9 +1,4 @@
-import unittest
-
 import chainer
-from chainer import functions as F
-import numpy
-import pytest
 
 import chainerx
 
@@ -13,10 +8,14 @@ from chainerx_tests import op_utils
 
 n_step_lstm_dtypes_valid = dtype_utils._permutate_dtype_mapping([
     # Floats.
-    (('float16', 'float16', 'float16', 'float16', 'float16'), ('float16', 'float16', 'float16')),
-    (('float32', 'float32', 'float32', 'float32', 'float32'), ('float32', 'float32', 'float32')),
-    (('float64', 'float64', 'float64', 'float64', 'float64'), ('float64', 'float64', 'float64')),
+    (('float16', 'float16', 'float16', 'float16', 'float16'),
+     ('float16', 'float16', 'float16')),
+    (('float32', 'float32', 'float32', 'float32', 'float32'),
+     ('float32', 'float32', 'float32')),
+    (('float64', 'float64', 'float64', 'float64', 'float64'),
+     ('float64', 'float64', 'float64')),
 ])
+
 
 @op_utils.op_test(['native:0', 'cuda:0'])
 @chainer.testing.parameterize(*(
@@ -25,7 +24,7 @@ n_step_lstm_dtypes_valid = dtype_utils._permutate_dtype_mapping([
             'n_layers,hidden_size,input_size,batches', [
                 (1, 2, 1, (1, 1, 1)),
                 (2, 6, 8, (4, 2, 2)),
-                (3, 8, 4 , (4, 2, 1)),
+                (3, 8, 4, (4, 2, 1)),
                 (4, 12, 4, (4, 3, 2)),
 
             ]),
@@ -43,36 +42,37 @@ class TestNStepLstm(op_utils.ChainerOpTest):
             self.check_backward_options.update({
                 'rtol': 1e-2, 'atol': 1e-2})
         self.check_double_backward_options.update({
-                'rtol': 5e-3, 'atol': 5e-2})
+            'rtol': 5e-3, 'atol': 5e-2})
 
     def generate_inputs(self):
         h_shape = (self.n_layers, self.batches[0], self.hidden_size)
-        c_shape = (self.n_layers, self.batches[0], self.hidden_size)
         h_dtype = self.in_dtypes[0]
-        c_dtype = self.in_dtypes[1]
         ws_dtype = self.in_dtypes[2]
         bs_dtype = self.in_dtypes[3]
-        ip_dtype = self.in_dtypes[4]
-        
+
         h = array_utils.uniform(h_shape, h_dtype)
-        c = h.copy()#array_utils.uniform(c_shape, c_dtype)
+        c = h.copy()  # array_utils.uniform(c_shape, c_dtype)
         in_size = self.input_size
         out_size = self.hidden_size
-        xs = [array_utils.uniform((self.batches[b], in_size), ws_dtype) for b in range(len(self.batches))]
-        w_in = lambda i, j: in_size if i == 0 and j < 4 else out_size
-        ws = []
-        bs = []
+        xs = [array_utils.uniform((self.batches[b], in_size), ws_dtype)
+              for b in range(len(self.batches))]
+
+        def w_in(i, j):
+            return in_size if i == 0 and j < 4 else out_size
+
         inputs = []
         inputs.append(h)
         inputs.append(c)
         for i in range(len(self.batches)):
             inputs.append(xs[i])
+
         for n in range(self.n_layers):
             for i in range(8):
-                inputs.append(array_utils.uniform((out_size, w_in(n, i)), ws_dtype) )
+                inputs.append(array_utils.uniform(
+                    (out_size, w_in(n, i)), ws_dtype))
             for i in range(8):
                 inputs.append(array_utils.uniform((out_size,), bs_dtype))
-        return  tuple(inputs)
+        return tuple(inputs)
 
     def process_input(self, inputs):
         h = inputs[0]
@@ -82,24 +82,25 @@ class TestNStepLstm(op_utils.ChainerOpTest):
         bs = []
         index = 2 + len(self.batches)
         for n in range(self.n_layers):
-            ws.append(inputs[index : index + 8])
-            bs.append(inputs[index + 8 : index + 16])   
+            ws.append(inputs[index: index + 8])
+            bs.append(inputs[index + 8: index + 16])
             index += 16
         return h, c, ws, bs, xs
 
     def forward_chainerx(self, inputs):
-        h, c, ws, bs, xs =  self.process_input(inputs)
-        out = chainerx.n_step_lstm(self.n_layers, h, c, ws, bs, xs);
+        h, c, ws, bs, xs = self.process_input(inputs)
+        out = chainerx.n_step_lstm(self.n_layers, h, c, ws, bs, xs)
         rets = []
         rets.append(out[0][0])
         rets.append(out[0][1])
         for i in range(len(out[1])):
             rets.append(out[1][i])
-        return tuple(rets) 
-        
+        return tuple(rets)
+
     def forward_chainer(self, inputs):
-        h, c, ws, bs, xs =  self.process_input(inputs)
-        out = chainer.functions.n_step_lstm(self.n_layers, 0.0, h, c, ws, bs, xs)
+        h, c, ws, bs, xs = self.process_input(inputs)
+        out = chainer.functions.n_step_lstm(
+            self.n_layers, 0.0, h, c, ws, bs, xs)
         rets = []
         rets.append(out[0])
         rets.append(out[1])
