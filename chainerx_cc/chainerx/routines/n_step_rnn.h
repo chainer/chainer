0db2e4236ac307209d619e684d3b90e231be44e3
aniket <adidolkar123@gmail.com>
2019-05-25 19:14:58 +0530

    fix formatting

--- chainerx_cc/chainerx/routines/n_step_rnn.h
+++ chainerx_cc/chainerx/routines/n_step_rnn.h
@@ -8,21 +8,7 @@
 #include "chainerx/array.h"
 
 namespace chainerx {
-/*namespace internal {
 
-Array _stack_weight(std::vector<Array> ws);
-
-std::vector<Array> _lstm(std::vector<Array> xs, Array h, Array c, std::vector<Array> ws, std::vector<Array> b);
-
-template<typename Impl>
-std::vector<std::vector<Array>> _one_directional_loop(Impl&& impl, std::vector<Array> xs, Array h, Array c,
-	std::vector<Array> ws, std::vector<Array> b);
-
-template <typename Impl>
-std::vector<Array> n_step_rnn_impl(Impl&& impl, int64_t n_layers, Array hx, Array cx, std::vector<std::vector<Array>> ws, 
-		std::vector<std::vector<Array>> bs, std::vector<Array> xs, int8_t use_bidirection);
-}*/
-
-std::vector<std::vector<Array>> n_step_lstm(int64_t n_layers, Array hx, Array cx, const std::vector<std::vector<Array>>& ws, 
-		const std::vector<std::vector<Array>>& bs, std::vector<Array>& xs);
+std::vector<std::vector<Array>> n_step_lstm(int64_t n_layers, Array hx, Array cx, const std::vector<std::vector<Array>>& ws,
+    const std::vector<std::vector<Array>>& bs, std::vector<Array>& xs);
 }
