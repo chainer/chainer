f11f4dd04407fe09f5af49e0acfec16059fca2ed
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2019-05-29 16:36:55 +0300

    Wrapped cusolver's cholesky decomposition for doubles

--- chainerx_cc/chainerx/cuda/cuda_device/linalg.cu
+++ chainerx_cc/chainerx/cuda/cuda_device/linalg.cu
@@ -4,6 +4,7 @@
 #include <mutex>
 #include <type_traits>
 
+#include <cusolverDn.h>
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
 #include <cuda_fp16.hpp>
@@ -13,6 +14,7 @@
 #include "chainerx/backend.h"
 #include "chainerx/backend_util.h"
 #include "chainerx/cuda/cublas.h"
+#include "chainerx/cuda/cusolver.h"
 #include "chainerx/cuda/cuda_runtime.h"
 #include "chainerx/cuda/cuda_set_device_scope.h"
 #include "chainerx/cuda/data_type.cuh"
@@ -43,7 +45,51 @@ public:
         CHAINERX_ASSERT(a.ndim() == 2);
         CHAINERX_ASSERT(out.ndim() == 2);
 
-        throw NotImplementedError("cholesky is not yet implemented in CUDA");
+        // potrf (cholesky) stores result in-place, therefore copy ``a`` to ``out`` and then pass ``out`` to the routine
+        device.backend().CallKernel<CopyKernel>(a, out);
+
+        bool is_out_contiguous = out.IsContiguous();
+        Array out_contiguous = is_out_contiguous ? out : AsContiguous(out);
+
+        cuda_internal::DeviceInternals& device_internals = cuda_internal::GetDeviceInternals(static_cast<CudaDevice&>(device));
+
+        // compute workspace size and prepare workspace
+        double* out_ptr = static_cast<double*>(internal::GetRawOffsetData(out_contiguous));
+        int work_size = 0;
+        const int N = a.shape()[0];
+        device_internals.cusolver_handle().Call(
+            cusolverDnDpotrf_bufferSize,
+            CUBLAS_FILL_MODE_UPPER,
+            N,
+            out_ptr,
+            N,
+            &work_size);
+
+        // POTRF execution
+        double *work_space;
+        CheckCudaError(cudaMalloc(&work_space, work_size * sizeof(double)));
+        int *devInfo;
+        CheckCudaError(cudaMalloc(&devInfo, sizeof(int)));
+        device_internals.cusolver_handle().Call(
+            cusolverDnDpotrf,
+            CUBLAS_FILL_MODE_UPPER,
+            N,
+            out_ptr,
+            N,
+            work_space,
+            work_size,
+            devInfo);
+
+        int devInfo_h = 0;
+        CheckCudaError(cudaMemcpy(&devInfo_h, devInfo, sizeof(int), cudaMemcpyDeviceToHost));
+        if (devInfo_h != 0) {
+            std::cout << "Unsuccessful potrf execution\n\n" << "devInfo = " << devInfo_h << "\n\n";
+        }
+
+        if (!is_out_contiguous) {
+            device.backend().CallKernel<CopyKernel>(out_contiguous, out);
+        }
+
     }
 };
 
