e6f9b1784fae013a79d1e8ef8cf79f49c7473711
take-cheeze <takechi101010@gmail.com>
2019-05-31 11:17:16 +0900

    Revert flags for READTHEDOCS

--- setup.py
+++ setup.py
@@ -187,8 +187,7 @@ setup_kwargs = dict(
 
 build_chainerx = 0 != int(os.getenv('CHAINER_BUILD_CHAINERX', '0'))
 if os.getenv('READTHEDOCS', None) == 'True':
-    if 'MAKEFLAGS' not in os.environ:
-        os.environ['MAKEFLAGS'] = '-j2'
+    os.environ['MAKEFLAGS'] = '-j2'
     build_chainerx = True
 
 chainerx_build_helper.config_setup_kwargs(setup_kwargs, build_chainerx)
