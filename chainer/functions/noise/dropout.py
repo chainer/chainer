bdda9d68a5ec278f575cf29854228ed402d45c7d
crcrpar <masaki.kozuki.2014@gmail.com>
2019-06-05 21:06:04 +0900

    use cuDNN by default

--- chainer/functions/noise/dropout.py
+++ chainer/functions/noise/dropout.py
@@ -44,7 +44,7 @@ class Dropout(function_node.FunctionNode):
         return y,
 
     def forward_gpu(self, x):
-        if (chainer.should_use_cudnn('==always', 5000)
+        if (chainer.should_use_cudnn('>=auto', 5000)
                 and x[0].flags.c_contiguous
                 and self.mask is None
                 and not self.return_mask):
