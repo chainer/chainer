cd49ef4cbdc91912fc90eb2ba34ef2793cfbe50d
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-06-07 00:08:19 +0900

    add transpose

---
+++ chainer/iterators/_transpose.py
@@ -0,0 +1,9 @@
+import six
+
+
+def transpose(data):
+    if isinstance(data[0], tuple):
+        return tuple([d[i] for d in data]
+                     for i in six.moves.range(len(data[0])))
+    elif isinstance(data[0], dict):
+        return {k: [d[k] for d in data] for k in data[0].keys()}
