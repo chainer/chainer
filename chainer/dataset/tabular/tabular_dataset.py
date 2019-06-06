819abbd5eeb087854b1b4ee07f396aa56ea76d5d
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-06-06 20:36:53 +0900

    add docs

--- chainer/dataset/tabular/tabular_dataset.py
+++ chainer/dataset/tabular/tabular_dataset.py
@@ -143,6 +143,20 @@ class TabularDataset(dataset_mixin.DatasetMixin):
             return dict(six.moves.zip(self.keys, examples))
 
     def convert(self, data):
+        """Convert fetched data.
+
+        This method takes data fetched by :meth:`fetch` and
+        pre-process them before passing them to models.
+        The default behaviour is converting each column into an ndarray.
+        This behaviour can be overloaded by :meth:`with_converter`.
+
+        Args:
+            data (tuple or dict): Data from :meth:`fetch`.
+
+        Returns:
+            A tuple or dict.
+            Each value is an ndarray.
+        """
         if isinstance(data, tuple):
             return tuple(_as_array(d) for d in data)
         elif isinstance(data, dict):
@@ -191,6 +205,17 @@ class TabularDataset(dataset_mixin.DatasetMixin):
         return chainer.dataset.tabular._join._Join(self, *datasets)
 
     def with_converter(self, converter):
+        """Overload the behaviour of :meth:`convert`.
+
+        This method overloads :meth:`convert`.
+
+        Args:
+            converter (callable): A new converter.
+
+        Returns:
+            A dataset with the new converter.
+        """
+
         return chainer.dataset.tabular._with_converter._WithConverter(
             self, converter)
 
