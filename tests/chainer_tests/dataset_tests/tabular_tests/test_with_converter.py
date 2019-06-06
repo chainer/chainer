41e9ee18db8f174d55bcf5f12f1f7e46441a7603
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-06-06 20:41:38 +0900

    more check

--- tests/chainer_tests/dataset_tests/tabular_tests/test_with_converter.py
+++ tests/chainer_tests/dataset_tests/tabular_tests/test_with_converter.py
@@ -1,9 +1,14 @@
 import unittest
 
+import chainer
 from chainer import testing
 from chainer_tests.dataset_tests.tabular_tests import dummy_dataset
 
 
+@testing.parameterize(
+    {'mode': tuple},
+    {'mode': dict},
+)
 class TestWithConverter(unittest.TestCase):
 
     def test_with_converter(self):
@@ -11,8 +16,13 @@ class TestWithConverter(unittest.TestCase):
             self.assertEqual(data, 'input')
             return 'converted'
 
-        dataset = dummy_dataset.DummyDataset().with_converter(converter)
-        self.assertEqual(dataset.convert('input'), 'converted')
+        dataset = dummy_dataset.DummyDataset(mode=self.mode)
+        view = dataset.with_converter(converter)
+        self.assertIsInstance(view, chainer.dataset.TabularDataset)
+        self.assertEqual(len(view), len(dataset))
+        self.assertEqual(view.keys, dataset.keys)
+        self.assertEqual(view.mode, dataset.mode)
+        self.assertEqual(view.convert('input'), 'converted')
 
 
 testing.run_module(__name__, __file__)
