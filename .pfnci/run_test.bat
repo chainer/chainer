72e6c2f789d8c3964159a30d4b87ccf3eb396dbd
Daisuke Tanaka <duaipp@gmail.com>
2019-06-07 10:15:42 +0900

    split sub routine

--- .pfnci/run_test.bat
+++ .pfnci/run_test.bat
@@ -7,15 +7,13 @@ set CUDA_PATH=CUDA_PATH_V%CUDA_VER%
 set PY_PATH=C:\Development\Python\Python%PY_VER%
 set PATH=%CUDA_PATH%\bin;%CUDA_PATH%\libnvvp;%PY_PATH%;%PY_PATH%\Scripts\%PATH%
 
-set CHAINER_LATEST_MAJOR_VER=7
-set TEST_HOME=%CD%
 set PYTEST_ATTR=not slow and not ideep
 
 curl -o xpytest.exe --insecure -L https://github.com/disktnk/xpytest/releases/download/v0.0.1/xpytest.exe
 
 if %GPU_LIMIT% gtr 0 (
     rem cannot set variable within if block, so use subroutine
-    call :INSTALL_CUPY
+    cmd /c .pfnci\install_cupy.bat
 ) else (
     set PYTEST_ATTR=not gpu and not cudnn and %PYTEST_ATTR%
 )
@@ -25,28 +23,3 @@ pip install scipy
 
 set CHAINER_TEST_GPU_LIMIT=%GPU_LIMIT%
 xpytest --python python -m "%PYTEST_ATTR%" --thread 8 --hint .pfnci/hint_win.pbtxt tests/chainer_tests/**/test_*.py
-
-exit /b
-
-:INSTALL_CUPY
-
-rem get chainer version and set to variable using for-do statement
-set GET_VER_CMD="import os, imp;print(imp.load_source('_version', os.path.join('chainer', '_version.py')).__version__.split('.')[0])"
-for /f "DELIMS=" %%v in (' ^
-python -c %GET_VER_CMD% ^
-') do set CHAINER_MAJOR_VER=%%v
-
-chainer version: %CHAINER_MAJOR_VER%
-cd ..
-if %CHAINER_MAJOR_VER% equ %CHAINER_LATEST_MAJOR_VER% (
-    git clone https://github.com/cupy/cupy.git
-) else (
-    git clone -b v%CHAINER_MAJOR_VER% https://github.com/cupy/cupy.git
-)
-cd cupy
-pip install cython
-pip install -e . -vvv
-
-cd %TEST_HOME%
-
-exit /b
