563fa98493f577e23f11de720d64de33b92d5402
Daisuke Tanaka <duaipp@gmail.com>
2019-06-07 09:02:46 +0900

    add not install cupy mode on cpu

--- .pfnci/run_test.bat
+++ .pfnci/run_test.bat
@@ -12,12 +12,28 @@ set TEST_HOME=%CD%
 
 curl -o xpytest.exe --insecure -L https://github.com/disktnk/xpytest/releases/download/v0.0.1/xpytest.exe
 
+if %GPU_LIMIT% gtr 0 (
+    rem cannot set variable within if block, so use subroutine
+    call :INSTALL_CUPY
+)
+
+pip install -e .[test] -vvv
+pip install scipy
+
+set CHAINER_TEST_GPU_LIMIT=%GPU_LIMIT%
+xpytest --python python -m "not slow and not ideep" --thread 8 --hint .pfnci/hint_win.pbtxt tests/chainer_tests/**/test_*.py
+
+exit /b
+
+:INSTALL_CUPY
+
+rem get chainer version and set to variable using for-do statement
 set GET_VER_CMD="import os, imp;print(imp.load_source('_version', os.path.join('chainer', '_version.py')).__version__.split('.')[0])"
 for /f "DELIMS=" %%v in (' ^
 python -c %GET_VER_CMD% ^
 ') do set CHAINER_MAJOR_VER=%%v
 
-echo %CHAINER_MAJOR_VER%
+chainer version: %CHAINER_MAJOR_VER%
 cd ..
 if %CHAINER_MAJOR_VER% equ %CHAINER_LATEST_MAJOR_VER% (
     git clone https://github.com/cupy/cupy.git
@@ -30,8 +46,4 @@ pip install -e . -vvv
 
 cd %TEST_HOME%
 
-pip install -e .[test] -vvv
-pip install scipy
-
-set CHAINER_TEST_GPU_LIMIT=%GPU_LIMIT%
-xpytest --python python -m "not slow and not ideep" --thread 8 --hint .pfnci/hint_win.pbtxt tests/chainer_tests/**/test_*.py
+exit /b
