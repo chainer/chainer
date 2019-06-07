926480d8ffb7e44dc38e57b158b8c6f016fbaf3d
Masayuki Takagi <kamonama@gmail.com>
2019-06-07 03:10:17 +0000

    Support default dtype in sentiment example's recursive minibatch version

--- examples/sentiment/train_recursive_minibatch.py
+++ examples/sentiment/train_recursive_minibatch.py
@@ -136,8 +136,8 @@ class ThinStackRecursiveNet(chainer.Chain):
         count = 0
         correct = 0
 
-        stack = self.xp.zeros(
-            (batch, maxlen * 2, self.n_units), self.xp.float32)
+        dtype = chainer.get_dtype()
+        stack = self.xp.zeros((batch, maxlen * 2, self.n_units), dtype)
         for i, (word, label) in enumerate(zip(sequences, leaf_labels)):
             batch = word.shape[0]
             es = self.leaf(word)
