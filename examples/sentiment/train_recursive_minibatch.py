d7e98d36f39314eda7e9d6c5b1cf347f74f44db6
Masayuki Takagi <kamonama@gmail.com>
2019-06-10 04:17:20 +0000

    Warn possible NaN in FP16 mode in sentiment's recursive minibatch example

--- examples/sentiment/train_recursive_minibatch.py
+++ examples/sentiment/train_recursive_minibatch.py
@@ -1,4 +1,5 @@
 import argparse
+import warnings
 
 import numpy
 
@@ -194,6 +195,10 @@ def main():
                        type=int, nargs='?', const=0,
                        help='GPU ID (negative value indicates CPU)')
     args = parser.parse_args()
+
+    if chainer.get_dtype() == numpy.float16:
+        warnings.warn(
+            'This example may cause NaN in FP16 mode.', RuntimeWarning)
 
     vocab = {}
     max_size = None
