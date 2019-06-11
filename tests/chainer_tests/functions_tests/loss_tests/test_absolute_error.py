e60a9c97bdcac90bee0dcd83883a0384a9da8359
crcrpar <masaki.kozuki.2014@gmail.com>
2019-06-11 13:42:32 +0900

    separate non default gpu backward test

--- tests/chainer_tests/functions_tests/loss_tests/test_absolute_error.py
+++ tests/chainer_tests/functions_tests/loss_tests/test_absolute_error.py
@@ -1,3 +1,5 @@
+import unittest
+
 import numpy
 
 import chainer
@@ -41,20 +43,22 @@ from chainer import utils
 class TestAbsoluteError(testing.FunctionTestCase):
 
     def setUp(self):
-        # Declare x0 and x1 here for `test_backward_non_default_gpu`
-        self.x0 = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
-        # Add sufficient margin to prevent computational error
-        diff = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
-        diff[abs(diff) < 0.02] = 0.5
-        self.x1 = numpy.asarray(self.x0 + diff)
         if self.dtype == numpy.float16:
             self.check_forward_options.update({'atol': 1e-3, 'rtol': 1e-3})
             self.check_backward_options.update({'atol': 5e-2, 'rtol': 5e-2})
             self.check_double_backward_options.update(
                 {'atol': 5e-2, 'rtol': 5e-2})
+        if self.double_backward_options:
+            self.check_double_backward_options.udpate(
+                self.double_backward_options)
 
     def generate_inputs(self):
-        return (self.x0, self.x1)
+        x0 = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        # Add sufficient margin to prevent computational error
+        diff = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        diff[abs(diff) < 0.02] = 0.5
+        x1 = numpy.asarray(x0 + diff)
+        return (x0, x1)
 
     def forward_expected(self, inputs):
         x0, x1 = inputs
@@ -64,13 +68,47 @@ class TestAbsoluteError(testing.FunctionTestCase):
         x0, x1 = inputs
         return functions.absolute_error(x0, x1),
 
-    # test for #4669
+
+@testing.parameterize(*testing.product_dict(
+    [
+        {'dtype': numpy.float16},
+        {'dtype': numpy.float32},
+        {'dtype': numpy.float64},
+    ],
+    [
+        {'shape': (4, 3)},
+        {'shape': (4, 3, 2)},
+        {'shape': (4,)},
+        {'shape': ()},
+        {'shape': (1,)},
+        {'shape': (1, 1)},
+    ]
+))
+@testing.inject_backend_tests(
+    'test_backward_non_default_gpu',
+    testing.product({
+        'use_cuda': [True],
+        'use_cudnn': ['never', 'always'],
+    })
+)
+class TestNonDefaultGPU(unittest.TestCase):
+
+    # This test is for https://github.com/chainer/chainer/issues/4669
+
+    def setUp(self):
+        self.x0 = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        # Add sufficient margin to prevent computational error
+        diff = numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        diff[abs(diff) < 0.02] = 0.5
+        self.x1 = numpy.asarray(self.x0 + diff)
+        self.gy = numpy.random.random(self.shape).astype(self.dtype)
+
     @attr.multi_gpu(2)
-    def test_backward_non_default_gpu(self):
+    def test_backward_non_default_gpu(self, backend_config):
         x0 = chainer.Variable(cuda.to_gpu(self.x0, 1))
         x1 = chainer.Variable(cuda.to_gpu(self.x1, 1))
         gy = cuda.to_gpu(self.gy, 1)
-        with cuda.get_device_from_id(0):
+        with cuda.get_device_from_id(0), backend_config:
             y = functions.absolute_error(x0, x1)
             y.grad = gy
             y.backward()
