a66bcb947c27f52b9b2858a7f7563b449986695e
Daisuke Tanaka <duaipp@gmail.com>
2019-06-16 14:34:14 +0900

    fix to get chainer version, see git log

--- .pfnci/install_cupy.bat
+++ .pfnci/install_cupy.bat
@@ -1,23 +1,23 @@
 @echo off
 
-set CHAINER_LATEST_MAJOR_VER=7
-set TEST_HOME=%CD%
+pushd ..
 
-rem get chainer version and set to variable using for-do statement
-set GET_VER_CMD="import os, imp;print(imp.load_source('_version', os.path.join('chainer', '_version.py')).__version__.split('.')[0])"
-for /f "DELIMS=" %%v in (' ^
-python -c %GET_VER_CMD% ^
-') do set CHAINER_MAJOR_VER=%%v
+git log | findstr /r /c:"^ *\[backport\]" > nul
+rem if found "backport" in git log, means this branch comes from stable branch
+if %ERRORLEVEL% equ 0 (
+    rem get chainer version and set to variable using for-do statement
+    set GET_VER_CMD="import os, imp;print(imp.load_source('_version', os.path.join('chainer', '_version.py')).__version__.split('.')[0])"
+    for /f "DELIMS=" %%v in (' ^
+    python -c %GET_VER_CMD% ^
+    ') do set CHAINER_MAJOR_VER=%%v
 
-chainer version: %CHAINER_MAJOR_VER%
-cd ..
-if %CHAINER_MAJOR_VER% equ %CHAINER_LATEST_MAJOR_VER% (
-    git clone https://github.com/cupy/cupy.git
-) else (
+    chainer version: %CHAINER_MAJOR_VER%
     git clone -b v%CHAINER_MAJOR_VER% https://github.com/cupy/cupy.git
+) else (
+    git clone https://github.com/cupy/cupy.git
 )
 cd cupy
 pip install cython
 pip install -e . -vvv
 
-cd %TEST_HOME%
+popd
