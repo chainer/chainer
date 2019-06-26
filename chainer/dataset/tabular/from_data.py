0513468746170ff350b95dab61c7676bd3d4ad55
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-06-26 11:37:36 +0900

    support unary mode

--- chainer/dataset/tabular/from_data.py
+++ chainer/dataset/tabular/from_data.py
@@ -17,8 +17,11 @@ def from_data(*args, **kwargs):
             Do not rely on the order.
     Return:
         A :class:`~chainer.dataset.TabularDataset`.
-        If :obj:`args` is specifed, :attr:`mode` is :class:`tuple`.
-        Otherwise, :attr:`mode` is :class:`dict`.
+        If only one argument is given, :attr:`mode` is :obj:`None`.
+        If more than one arguments are given as :obj:`args`,
+        :attr:`mode` is :class:`tuple`.
+        If more than one arguments are given as :obj:`kwargs`,
+        :attr:`mode` is :class:`dict`.
     """
     datasets = []
 
@@ -29,28 +32,33 @@ def from_data(*args, **kwargs):
             key = '_{}'.format(id(data))
 
         if isinstance(data, chainer.get_array_types()):
-            datasets.append(_Array(key, data, tuple))
+            datasets.append(_Array(key, data))
         elif isinstance(data, list):
-            datasets.append(_List(key, data, tuple))
+            datasets.append(_List(key, data))
 
     for key, data in kwargs.items():
         if isinstance(data, chainer.get_array_types()):
-            datasets.append(_Array(key, data, dict))
+            datasets.append(_Array(key, data))
         elif isinstance(data, list):
-            datasets.append(_List(key, data, dict))
+            datasets.append(_List(key, data))
 
-    if len(datasets) == 0:
+    if len(datasets) == 1:
+        return datasets[0]
+    elif args and kwargs:
+        raise ValueError('Mixture of args and kwargs is not supported')
+    elif args:
+        return datasets[0].join(*datasets[1:]).as_tuple()
+    elif kwargs:
+        return datasets[0].join(*datasets[1:]).as_dict()
+    else:
         raise ValueError('At least one data must be passed')
-
-    return datasets[0].join(*datasets[1:])
 
 
 class _Array(tabular_dataset.TabularDataset):
 
-    def __init__(self, key, data, mode):
+    def __init__(self, key, data):
         self._key = key
         self._data = data
-        self._mode = mode
 
     def __len__(self):
         return len(self._data)
@@ -61,7 +69,7 @@ class _Array(tabular_dataset.TabularDataset):
 
     @property
     def mode(self):
-        return self._mode
+        return None
 
     def get_examples(self, indices, key_indices):
         if key_indices is None:
@@ -75,10 +83,9 @@ class _Array(tabular_dataset.TabularDataset):
 
 class _List(tabular_dataset.TabularDataset):
 
-    def __init__(self, key, data, mode):
+    def __init__(self, key, data):
         self._key = key
         self._data = data
-        self._mode = mode
 
     def __len__(self):
         return len(self._data)
@@ -89,7 +96,7 @@ class _List(tabular_dataset.TabularDataset):
 
     @property
     def mode(self):
-        return self._mode
+        return None
 
     def get_examples(self, indices, key_indices):
         if key_indices is None:
--- tests/chainer_tests/dataset_tests/tabular_tests/test_from_data.py
+++ tests/chainer_tests/dataset_tests/tabular_tests/test_from_data.py
@@ -9,62 +9,59 @@ from chainer.dataset import tabular
 
 class TestFromData(unittest.TestCase):
 
-    def test_from_data(self):
-        dataset = tabular.from_data(
-            ('a', np.arange(10)),
-            [3, 1, 4, 5, 9, 2, 6, 8, 7, 0],
-            c=[2, 7, 1, 8, 4, 5, 9, 0, 3, 6],
-            d=-np.arange(10))
+    def test_unary_args(self):
+        dataset = tabular.from_data(np.arange(10))
 
         self.assertIsInstance(dataset, chainer.dataset.TabularDataset)
         self.assertEqual(len(dataset), 10)
-        self.assertEqual(len(dataset.keys), 4)
-        self.assertEqual(dataset.keys[0], 'a')
-        self.assertIn('c', dataset.keys[2:])
-        self.assertIn('d', dataset.keys[2:])
-        self.assertEqual(dataset.mode, tuple)
+        self.assertEqual(len(dataset.keys), 1)
+        self.assertIsNone(dataset.mode)
 
-        output = dataset.slice[[1, 3], ('a', 1, 'c', 'd')].fetch()
-        np.testing.assert_equal(output, ([1, 3], [1, 5], [7, 8], [-1, -3]))
-        self.assertIsInstance(output[0], np.ndarray)
-        self.assertIsInstance(output[1], list)
-        self.assertIsInstance(output[2], list)
-        self.assertIsInstance(output[3], np.ndarray)
+        output = dataset.slice[[1, 3]].fetch()
+        np.testing.assert_equal(output, [1, 3])
+        self.assertIsInstance(output, np.ndarray)
 
-    def test_from_data_args_only(self):
+    def test_unary_args_with_key(self):
+        dataset = tabular.from_data(('a', np.arange(10)))
+
+        self.assertIsInstance(dataset, chainer.dataset.TabularDataset)
+        self.assertEqual(len(dataset), 10)
+        self.assertEqual(dataset.keys, ('a',))
+        self.assertIsNone(dataset.mode)
+
+        output = dataset.slice[[1, 3]].fetch()
+        np.testing.assert_equal(output, [1, 3])
+        self.assertIsInstance(output, np.ndarray)
+
+    def test_unary_kwargs(self):
+        dataset = tabular.from_data(a=np.arange(10))
+
+        self.assertIsInstance(dataset, chainer.dataset.TabularDataset)
+        self.assertEqual(len(dataset), 10)
+        self.assertEqual(dataset.keys, ('a',))
+        self.assertIsNone(dataset.mode)
+
+        output = dataset.slice[[1, 3]].fetch()
+        np.testing.assert_equal(output, [1, 3])
+        self.assertIsInstance(output, np.ndarray)
+
+    def test_tuple(self):
         dataset = tabular.from_data(
-            ('a', np.arange(10)),
-            [3, 1, 4, 5, 9, 2, 6, 8, 7, 0])
+            np.arange(10),
+            ('b', [2, 7, 1, 8, 4, 5, 9, 0, 3, 6]))
 
         self.assertIsInstance(dataset, chainer.dataset.TabularDataset)
         self.assertEqual(len(dataset), 10)
         self.assertEqual(len(dataset.keys), 2)
-        self.assertEqual(dataset.keys[0], 'a')
+        self.assertEqual(dataset.keys[1], 'b')
         self.assertEqual(dataset.mode, tuple)
 
         output = dataset.slice[[1, 3]].fetch()
-        np.testing.assert_equal(output, ([1, 3], [1, 5]))
+        np.testing.assert_equal(output, ([1, 3], [7, 8]))
         self.assertIsInstance(output[0], np.ndarray)
         self.assertIsInstance(output[1], list)
 
-    def test_from_data_kwargs_only(self):
-        dataset = tabular.from_data(
-            c=[2, 7, 1, 8, 4, 5, 9, 0, 3, 6],
-            d=-np.arange(10))
-
-        self.assertIsInstance(dataset, chainer.dataset.TabularDataset)
-        self.assertEqual(len(dataset), 10)
-        self.assertEqual(len(dataset.keys), 2)
-        self.assertIn('c', dataset.keys)
-        self.assertIn('d', dataset.keys)
-        self.assertEqual(dataset.mode, dict)
-
-        output = dataset.slice[[1, 3]].fetch()
-        np.testing.assert_equal(output, {'c': [7, 8], 'd': [-1, -3]})
-        self.assertIsInstance(output['c'], list)
-        self.assertIsInstance(output['d'], np.ndarray)
-
-    def test_from_data_unique_key(self):
+    def test_tuple_unique(self):
         dataset_a = tabular.from_data(
             np.arange(10),
             [3, 1, 4, 5, 9, 2, 6, 8, 7, 0])
@@ -72,6 +69,21 @@ class TestFromData(unittest.TestCase):
             [2, 7, 1, 8, 4, 5, 9, 0, 3, 6],
             -np.arange(10))
         dataset_a.join(dataset_b)
+
+    def test_dict(self):
+        dataset = tabular.from_data(
+            a=np.arange(10),
+            b=[2, 7, 1, 8, 4, 5, 9, 0, 3, 6])
+
+        self.assertIsInstance(dataset, chainer.dataset.TabularDataset)
+        self.assertEqual(len(dataset), 10)
+        self.assertEqual(set(dataset.keys), {'a', 'b'})
+        self.assertEqual(dataset.mode, dict)
+
+        output = dataset.slice[[1, 3]].fetch()
+        np.testing.assert_equal(output, {'a': [1, 3], 'b': [7, 8]})
+        self.assertIsInstance(output['a'], np.ndarray)
+        self.assertIsInstance(output['b'], list)
 
 
 testing.run_module(__name__, __file__)
