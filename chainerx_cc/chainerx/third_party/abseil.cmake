943106150a296b82d45eaeb65dfafa0b4dd347ae
take-cheeze <takechi101010@gmail.com>
2019-06-27 17:50:27 +0900

    Remove nasty workaround and update abseil instead

--- chainerx_cc/chainerx/third_party/abseil.cmake
+++ chainerx_cc/chainerx/third_party/abseil.cmake
@@ -4,7 +4,7 @@ project(abseil-download NONE)
 include(ExternalProject)
 ExternalProject_Add(pybind11
     GIT_REPOSITORY    https://github.com/abseil/abseil-cpp.git
-    GIT_TAG           20181200
+    GIT_TAG           72e09a54d993b192db32be14c65adf7e9bd08c31
     SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/abseil"
     BINARY_DIR        ""
     CONFIGURE_COMMAND ""
