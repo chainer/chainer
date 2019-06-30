2e3c85383fb3b8c8d9fc42abca1824f052d12886
kshitij12345 <kshitijkalambarkar@gmail.com>
2019-06-30 14:34:39 +0530

    update test

--- tests/chainer_tests/optimizer_hooks_tests/test_gradient_hard_clipping.py
+++ tests/chainer_tests/optimizer_hooks_tests/test_gradient_hard_clipping.py
@@ -3,54 +3,90 @@ import unittest
 import numpy as np
 
 import chainer
-from chainer import backend
-import chainer.initializers as I
 from chainer import optimizer_hooks
 from chainer import optimizers
 from chainer import testing
-from chainer.testing import attr
+
+_backend_params = [
+    # NumPy
+    {},
+    # {'use_ideep': 'always'},
+    # CuPy
+    {'use_cuda': True, 'cuda_device': 0},
+    # {'use_cuda': True, 'cuda_device': 1},
+    # ChainerX
+    {'use_chainerx': True, 'chainerx_device': 'native:0'},
+    {'use_chainerx': True, 'chainerx_device': 'cuda:0'},
+    # {'use_chainerx': True, 'chainerx_device': 'cuda:1'},
+]
 
 
 class SimpleLink(chainer.Link):
 
-    def __init__(self, w, g):
+    def __init__(self, params):
         super(SimpleLink, self).__init__()
         with self.init_scope():
-            self.param = chainer.Parameter(I.Zero(), w.shape)
-        self.param.data = w
-        self.param.grad = g
+            for i, p in enumerate(params):
+                setattr(self, 'p{}'.format(i), p)
 
 
+@testing.backend.inject_backend_tests(None, _backend_params)
+@testing.backend.inject_backend_tests(None, _backend_params)
+@testing.backend.inject_backend_tests(None, _backend_params)
 class TestGradientHardClipping(unittest.TestCase):
 
     def setUp(self):
-        self.target = SimpleLink(
-            np.arange(6, dtype=np.float32).reshape(2, 3),
-            np.arange(3, -3, -1, dtype=np.float32).reshape(2, 3))
+        num_params = 3
+        arrs = [
+            np.random.uniform(-3, 3, (2, 3)).astype(np.float32)
+            for _ in range(num_params)]
+        grads = [
+            np.random.uniform(-3, 3, (2, 3)).astype(np.float32)
+            for _ in range(num_params)]
+        params = []
+        for arr, grad in zip(arrs, grads):
+            param = chainer.Parameter(arr)
+            param.grad = grad
+            params.append(param)
 
-    def check_hardclipping(self):
-        w = self.target.param.data
-        g = self.target.param.grad
-        xp = backend.get_array_module(w)
+        self.target = SimpleLink(params)
+
+    def check_hardclipping(self, backend_configs):
+        target = self.target
+        assert len(backend_configs) == len(list(target.params()))
+        devices = [bc.device for bc in backend_configs]
+
         lower_bound = -0.9
         upper_bound = 1.1
-        expect = w - xp.clip(g, lower_bound, upper_bound)
+        expects = []
+        # Compute expected
+        for param, device in zip(target.params(), devices):
+            expects.append(param.array - np.clip(param.grad,
+                                                 lower_bound, upper_bound))
+            param.to_device(device)
 
+        # Apply optimizer_hook
         opt = optimizers.SGD(lr=1)
         opt.setup(self.target)
         opt.add_hook(
             optimizer_hooks.GradientHardClipping(lower_bound, upper_bound))
         opt.update()
 
-        testing.assert_allclose(expect, w)
+        # Validate
+        for expect, param in zip(expects, target.params()):
+            testing.assert_allclose(expect, param.array)
 
-    def test_hardclipping_cpu(self):
-        self.check_hardclipping()
+    def test_hardclipping_cpu(self, backend_config0,
+                              backend_config1, backend_config2):
+        self.check_hardclipping(
+            [backend_config0, backend_config1, backend_config2])
 
-    @attr.gpu
-    def test_hardclipping_gpu(self):
+    @testing.attr.gpu
+    def test_hardclipping_gpu(self, backend_config0,
+                              backend_config1, backend_config2):
         self.target.to_gpu()
-        self.check_hardclipping()
+        self.check_hardclipping(
+            [backend_config0, backend_config1, backend_config2])
 
 
 testing.run_module(__name__, __file__)
