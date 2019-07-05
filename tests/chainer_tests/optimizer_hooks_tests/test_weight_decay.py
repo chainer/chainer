5d387c13673e9e1f621d8c7e0534f185a255c205
kshitij12345 <kshitijkalambarkar@gmail.com>
2019-07-06 02:02:10 +0530

    test for chainerx backend in weight decay

--- tests/chainer_tests/optimizer_hooks_tests/test_weight_decay.py
+++ tests/chainer_tests/optimizer_hooks_tests/test_weight_decay.py
@@ -4,57 +4,88 @@ import numpy as np
 
 import chainer
 import chainer.functions as F
-import chainer.initializers as I
 from chainer import optimizer_hooks
 from chainer import optimizers
 from chainer import testing
-from chainer.testing import attr
+
+
+_backend_params = [
+    # NumPy
+    {},
+    {'use_ideep': 'always'},
+    # CuPy
+    {'use_cuda': True, 'cuda_device': 0},
+    {'use_cuda': True, 'cuda_device': 1},
+    # ChainerX
+    {'use_chainerx': True, 'chainerx_device': 'native:0'},
+    {'use_chainerx': True, 'chainerx_device': 'cuda:0'},
+    {'use_chainerx': True, 'chainerx_device': 'cuda:1'},
+]
 
 
 class SimpleLink(chainer.Link):
 
-    def __init__(self, w, g):
+    def __init__(self, params):
         super(SimpleLink, self).__init__()
         with self.init_scope():
-            self.param = chainer.Parameter(I.Zero(), w.shape)
-        self.param.data = w
-        self.param.grad = g
+            for i, p in enumerate(params):
+                setattr(self, 'p{}'.format(i), p)
 
 
+@testing.backend.inject_backend_tests(None, _backend_params)
+@testing.backend.inject_backend_tests(None, _backend_params)
+@testing.backend.inject_backend_tests(None, _backend_params)
 class TestWeightDecay(unittest.TestCase):
 
     def setUp(self):
-        self.target = SimpleLink(
-            np.arange(6, dtype=np.float32).reshape(2, 3),
-            np.arange(3, -3, -1, dtype=np.float32).reshape(2, 3))
+        self.num_params = 3
+        arrs = [
+            np.random.uniform(-3, 3, (2, 3)).astype(np.float32)
+            for _ in range(self.num_params)]
+        grads = [
+            np.random.uniform(-3, 3, (2, 3)).astype(np.float32)
+            for _ in range(self.num_params)]
+        params = []
+        for arr, grad in zip(arrs, grads):
+            param = chainer.Parameter(arr)
+            param.grad = grad
+            params.append(param)
 
-    def check_weight_decay(self):
-        w = self.target.param.data
-        g = self.target.param.grad
+        self.target = SimpleLink(params)
+
+    def check_weight_decay(self, backend_configs):
+        target = self.target
+        assert len(backend_configs) == len(list(target.params()))
+        devices = [bc.device for bc in backend_configs]
 
         decay = 0.2
-        expect = w - g - decay * w
+
+        # Compute expected
+        expects = []
+        for param, device in zip(target.params(), devices):
+            expects.append(param.array - param.grad - decay * param.array)
+            param.to_device(device)
 
         opt = optimizers.SGD(lr=1)
         opt.setup(self.target)
         opt.add_hook(optimizer_hooks.WeightDecay(decay))
         opt.update()
 
-        testing.assert_allclose(expect, w)
+        # Validate
+        for expect, param in zip(expects, target.params()):
+            testing.assert_allclose(expect, param.array)
 
-    def test_weight_decay_cpu(self):
-        self.check_weight_decay()
-
-    @attr.gpu
-    def test_weight_decay_gpu(self):
-        self.target.to_gpu()
-        self.check_weight_decay()
+    def test_weight_decay(self, backend_config0,
+                          backend_config1, backend_config2):
+        self.check_weight_decay(
+            [backend_config0, backend_config1, backend_config2])
 
 
+# TODO(kshitij12345): Test with chainerx when `loss_scale` in `backward`.
 @testing.inject_backend_tests(
     None,
     # CPU tests
-    [{}]
+    [{}, {'use_ideep': 'always'}]
     # GPU tests
     + testing.product({
         'use_cuda': [True],
