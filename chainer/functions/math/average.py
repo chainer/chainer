41001b2ef780361a009739d25a23924fe8ec647b
Toshiki Kataoka <kataoka@preferred.jp>
2019-07-09 20:52:39 +0900

    copy `Sum` function node

--- chainer/functions/math/average.py
+++ chainer/functions/math/average.py
@@ -1,8 +1,73 @@
 import six
 
+import chainer
+from chainer import function_node
 from chainer.functions.array import broadcast
 from chainer.functions.array import reshape
 from chainer.functions.math import sum as sum_mod
+from chainer.utils import type_check
+import chainerx
+
+
+class Sum(function_node.FunctionNode):
+    """Sum of array elements over a given axis."""
+
+    keepdims = False
+
+    def __init__(self, axis=None, keepdims=False):
+        if axis is None:
+            self.axis = None
+        elif isinstance(axis, six.integer_types):
+            self.axis = (axis,)
+        elif isinstance(axis, tuple) and all(
+                isinstance(a, six.integer_types) for a in axis):
+            if len(set(axis)) != len(axis):
+                raise ValueError('duplicate value in axis: ({})'.format(
+                    ', '.join(map(str, axis))))
+            self.axis = axis
+        else:
+            raise TypeError('None, int or tuple of int are required')
+
+        self.keepdims = keepdims
+
+    def check_type_forward(self, in_types):
+        type_check._argname(in_types, ('x',))
+        type_check.expect(in_types[0].dtype.kind == 'f')
+
+        if self.axis is not None:
+            for axis in self.axis:
+                if axis >= 0:
+                    type_check.expect(
+                        axis < in_types[0].ndim,
+                    )
+                else:
+                    type_check.expect(
+                        -axis - 1 < in_types[0].ndim,
+                    )
+
+    def forward_chainerx(self, inputs):
+        x, = inputs
+        return chainerx.sum(x, axis=self.axis, keepdims=self.keepdims),
+
+    def forward(self, inputs):
+        x, = inputs
+        ret = x.sum(axis=self.axis, keepdims=self.keepdims)
+        if backend.get_array_module(x) is numpy:
+            ret = numpy.asarray(ret)
+        return ret,
+
+    def backward(self, indexes, grad_outputs):
+        gy, = grad_outputs
+        ndim = len(self.inputs[0].shape)
+        if not (ndim == 0 or self.axis is None or self.keepdims):
+            actual_axis = [
+                axis if axis >= 0 else axis + ndim
+                for axis in self.axis]
+            shape = list(gy.shape)
+            for axis in sorted(actual_axis):
+                shape.insert(axis, 1)
+            gy = chainer.functions.reshape(gy, shape)
+        return chainer.functions.broadcast_to(gy, self.inputs[0].shape),
 
 
 def average(x, axis=None, weights=None, keepdims=False):
