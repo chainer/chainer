7d4c843c2a52ef4f0c100b0b64a0385412db127e
Akifumi Imanishi <imanishi@preferred.jp>
2019-07-16 06:15:28 +0000

    Enhanced error message

--- chainerx/__init__.py
+++ chainerx/__init__.py
@@ -6,8 +6,18 @@ try:
 except ImportError:
     chainer_root_dir = os.path.abspath(__file__ + '../../..')
     raise ImportError(
-        'Reinstall Chainer in "editable" mode.\n'
-        '$ pip install -e {}'.format(chainer_root_dir))
+        '''\
+Cannot import chainerx because _build_info.py cannot be found.
+
+The chainerx module being imported was not correctly installed by \
+`pip install`.
+
+It may be caused by either of the following reasons.
+
+1. You are directly importing chainer source files without installing it with \
+`pip install`.
+2. You installed chainer in non-editable mode (`pip install` without -e) and \
+are importing chainer source files instead of the installed module.''')
 
 if _build_info.build_chainerx:
     from chainerx import _core
