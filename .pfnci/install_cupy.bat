5fd468ef771f1af20ed52e1d5fcfbc57c6ccf8c3
Daisuke Tanaka <duaipp@gmail.com>
2019-07-16 11:06:32 +0900

    revert chainer latest major version

--- .pfnci/install_cupy.bat
+++ .pfnci/install_cupy.bat
@@ -1,9 +1,6 @@
 @echo off
 
-rem ref https://github.com/chainer/chainer-test/blob/2b809ed767b219e8a66734e661d879a97486f014/version.py#L80
-rem 1: master branch, 0: backport (stable) branch
-git log | findstr /r /c:"^ *\[backport\]" > nul
-set IS_MASTER_BRANCH=%ERRORLEVEL%
+set CHAINER_LATEST_MAJOR_VER=7
 
 rem get chainer version and set to variable using for-do statement
 set GET_VER_CMD="import os, imp;print(imp.load_source('_version', os.path.join('chainer', '_version.py')).__version__.split('.')[0])"
@@ -13,10 +10,10 @@ python -c %GET_VER_CMD% ^
 echo chainer version: %CHAINER_MAJOR_VER%
 
 pushd ..
-if %IS_MASTER_BRANCH% equ 0 (
-    git clone -b v%CHAINER_MAJOR_VER% https://github.com/cupy/cupy.git
-) else (
+if %CHAINER_MAJOR_VER% equ %CHAINER_LATEST_MAJOR_VER% (
     git clone https://github.com/cupy/cupy.git
+) else (
+    git clone -b v%CHAINER_MAJOR_VER% https://github.com/cupy/cupy.git
 )
 cd cupy
 pip install cython
