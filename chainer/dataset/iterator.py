97c235fb636fe09799518eb3c18c1590f20835bf
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-08-05 16:18:13 +0900

    fix typo

--- chainer/dataset/iterator.py
+++ chainer/dataset/iterator.py
@@ -88,7 +88,7 @@ class Iterator(object):
     def enable_convert(self):
         """Configure the iterator to call a converter in it.
 
-        This method congiures the iterator to call a converter and
+        This method configures the iterator to call a converter and
         return converted examples.
         The concept of converters is similar to
         :func:`chainer.dataset.concat_examples` and
