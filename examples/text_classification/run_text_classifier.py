6d0eadf443da3d6841e8ba80c45852320a4ef857
UmashankarTriforce <umashankarsivakumar@gmail.com>
2019-08-05 13:50:10 +0530

    Changed double quotes to single quotes

--- examples/text_classification/run_text_classifier.py
+++ examples/text_classification/run_text_classifier.py
@@ -36,7 +36,7 @@ def setup_model(device, model_setup):
 
 def run_online(device):
     # predict labels online
-    print("Enter inputs for Online Predictions")
+    print('Enter inputs for Online Predictions')
     for l in sys.stdin:
         l = l.strip()
         if not l:
@@ -67,7 +67,7 @@ def run_batch(device, batchsize=64):
             print('{}\t{:.4f}\t{}'.format(answer, score, ' '.join(words)))
 
     batch = []
-    print("Enter inputs for Batch Predictions")
+    print('Enter inputs for Batch Predictions')
     for l in sys.stdin:
         l = l.strip()
         if not l:
