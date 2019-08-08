e8252be5a3d5517b85bc50a98d70e746f4520eb3
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2019-08-08 08:35:45 +0300

    Skip empty array tests if NumPy version is lower than 1.16
    https://github.com/numpy/numpy/blob/master/doc/release/1.16.0-notes.rst#linalglstsq-linalgqr-and-linalgsvd-now-work-with-empty-arrays

--- tests/chainerx_tests/unit_tests/routines_tests/test_linalg.py
+++ tests/chainerx_tests/unit_tests/routines_tests/test_linalg.py
@@ -9,6 +9,8 @@ from chainerx_tests import array_utils
 from chainerx_tests import dtype_utils
 from chainerx_tests import op_utils
 
+from distutils.version import LooseVersion
+
 
 @op_utils.op_test(['native:0', 'cuda:0'])
 @chainer.testing.parameterize_pytest('a_shape,b_shape', [
@@ -389,6 +391,9 @@ class TestQR(NumpyLinalgOpTest):
 
     def forward_xp(self, inputs, xp):
         a, = inputs
+        if (LooseVersion(numpy.__version__) < LooseVersion('1.16.0')
+                and a.size == 0):
+            pytest.skip('Older NumPy versions do not work with empty arrays')
         out = xp.linalg.qr(a, mode=self.mode)
 
         if self.mode == 'r':
