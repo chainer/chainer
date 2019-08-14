915652640543a56e3e9de106cc4afd35954d2fb3
niboshi <niboshi000@gmail.com>
2019-08-14 17:30:55 +0000

    Merge F.max_pooling_{nd,2d}

--- chainer/functions/pooling/max_pooling_nd.py
+++ chainer/functions/pooling/max_pooling_nd.py
@@ -5,8 +5,9 @@ import numpy
 import six
 
 import chainer
-from chainer.backends import cuda
 from chainer import backend
+from chainer.backends import cuda
+from chainer.backends import intel64
 from chainer import configuration
 from chainer import function_node
 from chainer.functions.pooling import max_pooling_nd_kernel
@@ -52,6 +53,11 @@ class MaxPoolingND(pooling_nd._PoolingND):
         return y,
 
     def forward_cpu(self, x):
+        if (self.ndim == 2
+                and intel64.should_use_ideep('>=auto')
+                and intel64.inputs_all_ready(x)):
+            return self._forward_2d_ideep(x)
+
         ksize = self.ksize
         stride = self.stride
         pad = self.pad
@@ -79,6 +85,38 @@ class MaxPoolingND(pooling_nd._PoolingND):
         self._in_shape = in_shape
         self._in_dtype = in_dtype
         self.indexes = col.argmax(axis=2)
+        return y,
+
+    def _forward_2d_ideep(self, x):
+        assert self.ndim == 2
+        kh, kw = self.ksize
+        sy, sx = self.stride
+        ph, pw = self.pad
+        cover_all = self.cover_all
+
+        self._in_shape = x[0].shape
+        self._in_dtype = x[0].dtype
+        self.retain_inputs((0,))
+
+        n, c, h, w = x[0].shape
+        y_h = conv_nd.get_conv_outsize(h, kh, sy, ph, cover_all)
+        assert y_h > 0, 'Height in the output should be positive.'
+        y_w = conv_nd.get_conv_outsize(w, kw, sx, pw, cover_all)
+        assert y_w > 0, 'Width in the output should be positive.'
+        pd = sy * (y_h - 1) + kh - h - ph
+        pr = sx * (y_w - 1) + kw - w - pw
+
+        pp = intel64.ideep.pooling2DParam(
+            (n, c, y_h, y_w),
+            kh, kw,
+            sy, sx,
+            ph, pw,
+            pd, pr,
+            intel64.ideep.pooling2DParam.pooling_max)
+        y, indexes = intel64.ideep.pooling2D.Forward(
+            intel64.ideep.array(x[0]), pp)
+
+        self.indexes = indexes
         return y,
 
     def forward_gpu(self, x):
@@ -133,6 +171,12 @@ class MaxPoolingNDGrad(function_node.FunctionNode):
 
     def forward_cpu(self, gy):
         func = self.func
+
+        if (func.ndim == 2
+                and intel64.should_use_ideep('>=auto')
+                and intel64.inputs_all_ready(gy)):
+            return self._forward_2d_ideep(gy)
+
         ndim = func.ndim
         ksize = func.ksize
         stride = func.stride
@@ -149,8 +193,9 @@ class MaxPoolingNDGrad(function_node.FunctionNode):
 
         gcol = numpy.zeros(n * c * prod_outs * prod_ksize, dtype=in_dtype)
 
-        indexes = indexes.flatten()
-        indexes += numpy.arange(0, indexes.size * prod_ksize, prod_ksize)
+        indexes = (
+            indexes.flatten()
+            + numpy.arange(0, indexes.size * prod_ksize, prod_ksize))
 
         gcol[indexes] = gy[0].ravel()
         gcol_shape = (n, c) + outs + ksize
@@ -159,6 +204,43 @@ class MaxPoolingNDGrad(function_node.FunctionNode):
             gcol = numpy.swapaxes(gcol, 2 + i, ndim + 2 + i)
 
         gx = conv_nd.col2im_nd_cpu(gcol, stride, pad, dims)
+        return gx,
+
+    def _forward_2d_ideep(self, gy):
+        func = self.func
+
+        # FIXME
+        # Here we expect indexes is returned from MKL-DNN
+        # otherwise, there are dtype mismatch for reorder (int64-->uint8)
+        if not isinstance(func.indexes, intel64.ideep.mdarray):
+            return self.forward_cpu(gy)
+
+        kh, kw = func.ksize
+        sy, sx = func.stride
+        ph, pw = func.pad
+        indexes = func.indexes
+        in_shape = func._in_shape
+
+        n, c, h, w = in_shape
+        y_h, y_w = gy[0].shape[2:]
+        x = func.get_retained_inputs()[0].array
+
+        pd = sy * (y_h - 1) + kh - h - ph
+        pr = sx * (y_w - 1) + kw - w - pw
+
+        pp = intel64.ideep.pooling2DParam(
+            func._in_shape,
+            kh, kw,
+            sy, sx,
+            ph, pw,
+            pd, pr,
+            intel64.ideep.pooling2DParam.pooling_max)
+
+        indexes = intel64.ideep.array(indexes)
+        gx = intel64.ideep.pooling2D.Backward(
+            intel64.ideep.array(x),
+            intel64.ideep.array(gy[0]),
+            indexes, pp)
         return gx,
 
     def forward_gpu(self, gy):
@@ -367,6 +449,49 @@ def max_pooling_1d(x, ksize, stride=None, pad=0, cover_all=True,
         raise ValueError(
             'The number of dimensions under channel dimension of the input '
             '\'x\' should be 1. But the actual ndim was {}.'.format(
+                len(x.shape[2:])))
+    return max_pooling_nd(x, ksize, stride, pad, cover_all, return_indices)
+
+
+def max_pooling_2d(x, ksize, stride=None, pad=0, cover_all=True,
+                   return_indices=False):
+    """Spatial max pooling function.
+
+    This function acts similarly to :func:`~chainer.functions.convolution_2d`,
+    but it computes the maximum of input spatial patch for each channel without
+    any parameter instead of computing the inner products.
+
+    Args:
+        x (~chainer.Variable): Input variable.
+        ksize (int or pair of ints): Size of pooling window. ``ksize=k`` and
+            ``ksize=(k, k)`` are equivalent.
+        stride (int or pair of ints or None): Stride of pooling applications.
+            ``stride=s`` and ``stride=(s, s)`` are equivalent. If ``None`` is
+            specified, then it uses same stride as the pooling window size.
+        pad (int or pair of ints): Spatial padding width for the input array.
+            ``pad=p`` and ``pad=(p, p)`` are equivalent.
+        cover_all (bool): If ``True``, all spatial locations are pooled into
+            some output pixels. It may make the output size larger.
+        return_indices (bool): If ``True``, pooling indices array is returned
+            together with the output variable. The returned indices are
+            expected for use by :func:`chainer.functions.upsampling_2d`.
+            Note that cuDNN will not be used for this function if
+            ``return_indices`` is set to ``True``, as cuDNN does not return
+            indices information.
+
+    Returns:
+        ~chainer.Variable or tuple:
+            When ``return_indices`` is ``False`` (default), returns the output
+            variable.
+            When ``True``, returns the tuple of the output variable and
+            pooling indices (:ref:`ndarray`). Pooling indices will be on the
+            same device as the input.
+
+    """
+    if len(x.shape[2:]) != 2:
+        raise ValueError(
+            'The number of dimensions under channel dimension of the input '
+            '\'x\' should be 2. But the actual ndim was {}.'.format(
                 len(x.shape[2:])))
     return max_pooling_nd(x, ksize, stride, pad, cover_all, return_indices)
 
