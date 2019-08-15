d732c3372e56d27224ed8b71bc5fd8e4404fa79d
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2019-08-15 17:24:32 +0900

    adjust tolerance for double backward test of power

--- tests/chainerx_tests/unit_tests/routines_tests/test_arithmetic.py
+++ tests/chainerx_tests/unit_tests/routines_tests/test_arithmetic.py
@@ -1270,7 +1270,7 @@ class TestPower(math_utils.BinaryMathTestBase, op_utils.NumpyOpTest):
         if in_dtype1 == 'float16' or in_dtype2 == 'float16':
             self.check_backward_options.update({'rtol': 5e-3, 'atol': 5e-3})
             self.check_double_backward_options.update(
-                {'rtol': 5e-3, 'atol': 5e-3})
+                {'rtol': 5e-2, 'atol': 5e-2})
 
     def func(self, xp, a, b):
         if self.is_module:
