8c4841b5e4f2412e83b998a85a773abb606bba0c
aniket <adidolkar123@gmail.com>
2019-08-16 15:06:16 +0530

    cosmetic changes

--- chainerx_cc/chainerx/routines/activation.cc
+++ chainerx_cc/chainerx/routines/activation.cc
@@ -25,6 +25,24 @@
 
 namespace chainerx {
 
+namespace {
+
+    std::vector<Array> ExtractGates(Array x, int64_t n_splits) {
+    StackVector<int64_t, kMaxNdim> shape_vec;
+    shape_vec.emplace_back(x.shape()[0]);
+    shape_vec.emplace_back(n_splits);
+    shape_vec.emplace_back(static_cast<int64_t>(x.shape()[1] / n_splits));
+    for (int64_t i = 2; i < x.ndim(); i++) {
+        shape_vec.emplace_back(x.shape()[i]);
+    }
+    Shape shape{shape_vec};
+    Array x_r = Reshape(x, shape);
+    std::vector<Array> x_split = Split(x_r, n_splits, 1);
+    return x_split;
+}
+
+}  // namespace  
+
 Array ClippedRelu(const Array& x, Scalar z) {
     Dtype dtype = internal::GetMathResultDtype(x.dtype());
     const Array& x_cast = x.dtype() == dtype ? x : x.AsType(dtype);
@@ -68,42 +86,28 @@ Array LeakyRelu(const Array& x, Scalar slope) {
     return Where(x_cast >= zero, x_cast, slope * x_cast);
 }
 
-std::vector<Array> _extract_gates(Array x, int64_t n_splits) {
-    StackVector<int64_t, kMaxNdim> shape_vec;
-    shape_vec.push_back(x.shape()[0]);
-    shape_vec.push_back(n_splits);
-    shape_vec.push_back(static_cast<int>(x.shape()[1] / n_splits));
-    for (int i = 2; i < x.ndim(); i++) {
-        shape_vec.push_back(x.shape()[i]);
-    }
-    Shape shape{shape_vec};
-    Array x_r = Reshape(x, shape);
-    std::vector<Array> x_split = Split(x_r, n_splits, 1);
-    return x_split;
-}
-
 std::vector<Array> TreeLstm(std::vector<Array> arrays) {
-    uint n_ary = arrays.size() - 1;
-    std::vector<Array> gates = _extract_gates(arrays[arrays.size() - 1], 3 + n_ary);
+    size_t n_ary = arrays.size() - 1;
+    std::vector<Array> gates = ExtractGates(arrays[arrays.size() - 1], 3 + n_ary);
 
     Array a = Squeeze(gates[0]);
     Array i = Squeeze(gates[1]);
     Array o = Squeeze(gates[2]);
     std::vector<Array> fs;
-    for (uint i = 3; i < gates.size(); i++) {
-        fs.push_back(Squeeze(gates[i]));
+    for (size_t i = 3; i < gates.size(); i++) {
+        fs.emplace_back(Squeeze(gates[i]));
     }
     Array a_ = Tanh(a);
     Array i_ = Sigmoid(i);
     Array o_ = Sigmoid(o);
     std::vector<Array> fs_s{};
-    for (uint i = 0; i < fs.size(); i++) {
-        fs_s.push_back(Sigmoid(fs[i]));
+    for (size_t i = 0; i < fs.size(); i++) {
+        fs_s.emplace_back(Sigmoid(fs[i]));
     }
     std::vector<Array> sum;
-    sum.push_back(arrays[0] * fs_s[0]);
-    for (uint i = 1; i < arrays.size() - 1; i++) {
-        sum.push_back(sum[i - 1] + arrays[i] * fs_s[i]);
+    sum.emplace_back(arrays[0] * fs_s[0]);
+    for (size_t i = 1; i < arrays.size() - 1; i++) {
+        sum.emplace_back(sum[i - 1] + arrays[i] * fs_s[i]);
     }
     Array c = a_ * i_ + sum[sum.size() - 1];
     Array h = o_ * Tanh(c);
