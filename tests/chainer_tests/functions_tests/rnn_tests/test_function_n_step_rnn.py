3fdf2796dce95833f65275999e16843ae5330033
Masayuki Takagi <kamonama@gmail.com>
2019-09-02 08:05:46 +0000

    Support all float dtyeps in F.n_step_rnn and F.n_step_birnn

--- tests/chainer_tests/functions_tests/rnn_tests/test_function_n_step_rnn.py
+++ tests/chainer_tests/functions_tests/rnn_tests/test_function_n_step_rnn.py
@@ -27,6 +27,11 @@ def array(shape, dtype):
             'batches': (3, 2, 1), 'activation': 'relu'},
         {'n_layers': 2, 'hidden_size': 2, 'input_size': 3,
             'batches': (3, 2, 1), 'activation': 'tanh'},
+    ],
+    [
+        {'dtype': numpy.float16},
+        {'dtype': numpy.float32},
+        {'dtype': numpy.float64},
     ]))
 @testing.fix_random()
 @backend.inject_backend_tests(
@@ -58,7 +63,6 @@ def array(shape, dtype):
 class TestNStepRNN(testing.FunctionTestCase):
 
     dodge_nondifferentiable = True
-    numerical_grad_dtype = None
 
     def setUp(self):
         self.check_forward_options = {'atol': 1e-3, 'rtol': 1e-2}
@@ -68,7 +72,7 @@ class TestNStepRNN(testing.FunctionTestCase):
 
     def generate_inputs(self):
         h_shape = (self.n_layers, self.batches[0], self.hidden_size)
-        dtype = numpy.float32
+        dtype = self.dtype
 
         h = array(h_shape, dtype)
         in_size = self.input_size
@@ -154,6 +158,11 @@ class TestNStepRNN(testing.FunctionTestCase):
             'batches': (3, 2, 1), 'activation': 'relu'},
         {'n_layers': 2, 'hidden_size': 2, 'input_size': 3,
             'batches': (3, 2, 1), 'activation': 'tanh'},
+    ],
+    [
+        {'dtype': numpy.float16},
+        {'dtype': numpy.float32},
+        {'dtype': numpy.float64},
     ]))
 @testing.fix_random()
 @backend.inject_backend_tests(
@@ -179,7 +188,6 @@ class TestNStepRNN(testing.FunctionTestCase):
         })]))
 class TestNStepBiRNN(testing.FunctionTestCase):
     dodge_nondifferentiable = True
-    numerical_grad_dtype = None
 
     def setUp(self):
         self.check_forward_options = {'atol': 1e-3, 'rtol': 1e-2}
@@ -189,7 +197,7 @@ class TestNStepBiRNN(testing.FunctionTestCase):
 
     def generate_inputs(self):
         h_shape = (self.n_layers * 2, self.batches[0], self.hidden_size)
-        dtype = numpy.float32
+        dtype = self.dtype
 
         h = array(h_shape, dtype)
         in_size = self.input_size
