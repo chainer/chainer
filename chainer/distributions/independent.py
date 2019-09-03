e47791a453d7117a88760a02d90e7a1ec28212c5
himkt <himkt@klis.tsukuba.ac.jp>
2019-09-03 23:14:38 +0900

    Replace num_reduce_dims with n_reduce_dims
    [memo] I'm not sure that it needs `n_` for variable of dimensionalities.

--- chainer/distributions/independent.py
+++ chainer/distributions/independent.py
@@ -236,8 +236,8 @@ def _kl_independent_independent(dist1, dist2):
     # KL(p || q) and do a `reduce_sum` on the reinterpreted batch dimensions.
     if dist1.event_shape == dist2.event_shape:
         if p.event_shape == q.event_shape:
-            num_reduce_dims = len(dist1.event_shape) - len(p.event_shape)
-            reduce_dims = tuple([-i - 1 for i in range(0, num_reduce_dims)])
+            n_reduce_dims = len(dist1.event_shape) - len(p.event_shape)
+            reduce_dims = tuple([-i - 1 for i in range(0, n_reduce_dims)])
 
             return sum_mod.sum(
                 distribution.kl_divergence(p, q), axis=reduce_dims)
