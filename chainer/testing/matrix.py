ce5c26ea73ac9764dc45d6f0cef7b7958dc06a30
Seiya Tokui <beam.web@gmail.com>
2019-09-06 05:38:06 +0000

    FIx minor bugs and styles

--- chainer/testing/matrix.py
+++ chainer/testing/matrix.py
@@ -4,7 +4,7 @@ from chainer.utils import argument
 
 
 def generate_matrix(shape, dtype=float, **kwargs):
-    """Generates a random matrix with given singular values.
+    r"""Generates a random matrix with given singular values.
 
     This function generates a random NumPy matrix (or a set of matrices) that
     has specified singular values. It can be used to generate the inputs for a
@@ -22,21 +22,23 @@ def generate_matrix(shape, dtype=float, **kwargs):
 
     """
     singular_values, = argument.parse_kwargs(
-        kwargs, singular_values=None,
+        kwargs, ('singular_values', None),
     )
 
     if len(shape) <= 1:
         raise ValueError(
-            'shpae {} is invalid for matrices: too few axes'.format(shape))
+            'shpae {} is invalid for matrices: too few axes'.format(shape)
+        )
     k_shape = shape[:-2] + (min(shape[-2:]),)
     # TODO(beam2d): consider supporting integer/boolean matrices
+    dtype = numpy.dtype(dtype)
     if dtype.kind not in 'fc':
         raise ValueError('dtype {} is not supported'.format(dtype))
 
     if singular_values is None:
         raise TypeError('singular_values is not given')
     singular_values = numpy.asarray(singular_values)
-    if any(singular_values < 0):
+    if (singular_values < 0).any():
         raise ValueError('negative singular value is given')
 
     # Generate random matrices with given singular values. We simply generate
@@ -44,7 +46,7 @@ def generate_matrix(shape, dtype=float, **kwargs):
     # with the given singular values.
     a = numpy.random.randn(*shape)
     if dtype.kind == 'c':
-        a += 1j * numpy.random.randn(*shape)
+        a = a + 1j * numpy.random.randn(*shape)
     u, _, vh = numpy.linalg.svd(a, full_matrices=False)
     a = numpy.einsum('...ik,...k,...kj->...ij', u, singular_values, vh)
-    return a.astype(dtype, copy=False)
+    return a.astype(dtype)
