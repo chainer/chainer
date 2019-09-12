1b13b99d53a36fd857857ceb29451648c16b5cf1
UmashankarTriforce <umashankarsivakumar@gmail.com>
2019-09-12 21:04:04 +0530

    Added test for zeta

---
+++ tests/chainer_tests/functions_tests/math_tests/test_zeta.py
@@ -0,0 +1,79 @@
+import unittest
+
+import numpy
+
+import chainer
+import chainer.functions as F
+from chainer import testing
+
+
+@testing.parameterize(*testing.product({
+    'dtype': [numpy.float16, numpy.float32, numpy.float64]
+}))
+@testing.inject_backend_tests(
+    None,
+    # CPU tests
+    [
+        {},
+    ]
+    # GPU tests
+    + testing.product({
+        'use_cuda': [True],
+        'cuda_device': [0, 1],
+    })
+    # ChainerX tests
+    + testing.product({
+        'use_chainerx': [True],
+        'chainerx_device': ['native:0', 'cuda:0', 'cuda:1']
+    })
+)
+@testing.with_requires('scipy')
+class TestZeta(testing.FunctionTestCase):
+
+    def setUp(self):
+        if self.dtype == numpy.float16:
+            self.check_forward_options = {'atol': 1e-2, 'rtol': 1e-2}
+        else:
+            self.check_forward_options = {'atol': 1e-4, 'rtol': 1e-4}
+        self.check_backward_options = {'atol': 1e-4, 'rtol': 1e-4}
+        self.check_double_backward_options = {'atol': 1e-4, 'rtol': 1e-4}
+
+    def generate_inputs(self):
+        x = numpy.array(numpy.random.rand(1).astype(numpy.float64))
+        q = numpy.array(numpy.random.uniform(1, 10))
+        return x, q
+
+    def forward_expected(self, inputs):
+        x, q = inputs
+        import scipy
+        y_expect = scipy.special.zeta(x, q)
+        return y_expect.astype(numpy.float64),
+
+    def forward(self, inputs, device):
+        x, q = inputs
+        y = F.zeta(x, q)
+        return y,
+
+
+@testing.parameterize(*testing.product({
+    'dtype': [numpy.float16, numpy.float32, numpy.float64]
+}))
+@testing.without_requires('scipy')
+class TestZetaExceptions(unittest.TestCase):
+    def setUp(self):
+        self.x = \
+            numpy.array(numpy.random.uniform(1, 10))
+        self.q = numpy.array(numpy.random.rand(1).astype(numpy.float64))
+        self.func = F.zeta
+
+    def check_forward(self, q_data, x_data):
+        x = chainer.Variable(x_data)
+        q = chainer.Variable(q_data)
+        with self.assertRaises(ImportError):
+            self.func(q, x)
+
+    def test_zeta_forward_cpu(self):
+        self.check_forward(self.q, self.x)
+
+
+testing.run_module(__name__, __file__)
