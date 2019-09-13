75387042b43e30f42d887b2a0902654cc52c1a2a
take-cheeze <takechi101010@gmail.com>
2019-09-13 13:00:51 +0900

    Remove TODO comment

--- chainerx_cc/third_party/abseil.cmake
+++ chainerx_cc/third_party/abseil.cmake
@@ -4,8 +4,6 @@ project(abseil-download NONE)
 include(ExternalProject)
 ExternalProject_Add(pybind11
     GIT_REPOSITORY    https://github.com/abseil/abseil-cpp.git
-    # TODO(take-cheeze): Use tag instead as soon as 201906 is out
-    # https://github.com/abseil/abseil-cpp/releases
     GIT_TAG           20190808
     SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/abseil"
     BINARY_DIR        ""
