ffd71f3ac516fd05ffec7bd48304360458bdc348
take-cheeze <takechi101010@gmail.com>
2019-09-13 11:59:41 +0900

    Update abseil to new release

--- chainerx_cc/third_party/abseil.cmake
+++ chainerx_cc/third_party/abseil.cmake
@@ -6,7 +6,7 @@ ExternalProject_Add(pybind11
     GIT_REPOSITORY    https://github.com/abseil/abseil-cpp.git
     # TODO(take-cheeze): Use tag instead as soon as 201906 is out
     # https://github.com/abseil/abseil-cpp/releases
-    GIT_TAG           72e09a54d993b192db32be14c65adf7e9bd08c31
+    GIT_TAG           20190808
     SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/abseil"
     BINARY_DIR        ""
     CONFIGURE_COMMAND ""
