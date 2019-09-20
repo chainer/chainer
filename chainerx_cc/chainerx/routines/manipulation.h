67d7ddfd825b34b30d3432a9f16ebd69063492f2
ishanrai05 <ishanrai05@gmail.com>
2019-09-20 12:18:37 +0530

    [chainerx::hsplit] Implements hsplit in chainerx

--- chainerx_cc/chainerx/routines/manipulation.h
+++ chainerx_cc/chainerx/routines/manipulation.h
@@ -59,6 +59,10 @@ std::vector<Array> VSplit(const Array& ary, int64_t sections);
 
 std::vector<Array> VSplit(const Array& ary, std::vector<int64_t> indices);
 
+std::vector<Array> HSplit(const Array& ary, int64_t sections);
+
+std::vector<Array> HSplit(const Array& ary, std::vector<int64_t> indices);
+
 Array Swapaxes(const Array& a, int8_t axis1, int8_t axis2);
 
 Array Repeat(const Array& a, int64_t repeats, absl::optional<int8_t> axis);
