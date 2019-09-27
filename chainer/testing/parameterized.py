2880b4e5f84a5b2f49ec8a6aab9d50cd21d70ca3
Hiroyuki Vincent Yamazaki <hiroyuki.vincent.yamazaki@gmail.com>
2019-09-27 16:28:57 +0900

    Merge pull request #8169 from toslunar/fix-str-parameterized
    Fix `__str__` of parameterized class

--- chainer/testing/parameterized.py
+++ chainer/testing/parameterized.py
@@ -45,51 +45,52 @@ def _parameterize_test_case_generator(base, params):
     # Defines the logic to generate parameterized test case classes.
 
     for i, param in enumerate(params):
-        cls_name = _make_class_name(base.__name__, i, param)
+        yield _parameterize_test_case(base, i, param)
 
-        def __str__(self):
-            name = base.__str__(self)
-            return '%s  parameter: %s' % (name, param)
 
-        mb = {'__str__': __str__}
-        for k, v in sorted(param.items()):
-            if isinstance(v, types.FunctionType):
+def _parameterize_test_case(base, i, param):
+    cls_name = _make_class_name(base.__name__, i, param)
 
-                def create_new_v():
-                    f = v
+    def __str__(self):
+        name = base.__str__(self)
+        return '%s  parameter: %s' % (name, param)
 
-                    def new_v(self, *args, **kwargs):
-                        return f(*args, **kwargs)
-                    return new_v
+    mb = {'__str__': __str__}
+    for k, v in sorted(param.items()):
+        if isinstance(v, types.FunctionType):
 
-                mb[k] = create_new_v()
-            else:
-                mb[k] = v
+            def create_new_v():
+                f = v
 
-        def method_generator(base_method):
-            # Generates a wrapped test method
+                def new_v(self, *args, **kwargs):
+                    return f(*args, **kwargs)
+                return new_v
 
-            # Bind to a new variable.
-            param2 = param
+            mb[k] = create_new_v()
+        else:
+            mb[k] = v
 
-            @functools.wraps(base_method)
-            def new_method(self, *args, **kwargs):
-                try:
-                    return base_method(self, *args, **kwargs)
-                except unittest.SkipTest:
-                    raise
-                except Exception as e:
-                    s = six.StringIO()
-                    s.write('Parameterized test failed.\n\n')
-                    s.write('Base test method: {}.{}\n'.format(
-                        base.__name__, base_method.__name__))
-                    s.write('Test parameters:\n')
-                    for k, v in sorted(param2.items()):
-                        s.write('  {}: {}\n'.format(k, v))
-                    utils._raise_from(e.__class__, s.getvalue(), e)
-            return new_method
+    def method_generator(base_method):
+        # Generates a wrapped test method
 
-        yield (cls_name, mb, method_generator)
+        @functools.wraps(base_method)
+        def new_method(self, *args, **kwargs):
+            try:
+                return base_method(self, *args, **kwargs)
+            except unittest.SkipTest:
+                raise
+            except Exception as e:
+                s = six.StringIO()
+                s.write('Parameterized test failed.\n\n')
+                s.write('Base test method: {}.{}\n'.format(
+                    base.__name__, base_method.__name__))
+                s.write('Test parameters:\n')
+                for k, v in sorted(param.items()):
+                    s.write('  {}: {}\n'.format(k, v))
+                utils._raise_from(e.__class__, s.getvalue(), e)
+        return new_method
+
+    return (cls_name, mb, method_generator)
 
 
 def parameterize(*params):
