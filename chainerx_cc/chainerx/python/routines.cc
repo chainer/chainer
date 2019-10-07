84eef5eedd35d9df49e62ab28e03be70fb7608cd
Masayuki Takagi <kamonama@gmail.com>
2019-10-07 08:19:20 +0000

    Implement python binding for chainerx::SoftmaxCrossEntropy

--- chainerx_cc/chainerx/python/routines.cc
+++ chainerx_cc/chainerx/python/routines.cc
@@ -1314,6 +1314,25 @@ void InitChainerxLoss(pybind11::module& m) {
           [](const ArrayBodyPtr& x1, const ArrayBodyPtr& x2) { return MoveArrayBody(SigmoidCrossEntropy(Array{x1}, Array{x2})); },
           "x1"_a,
           "x2"_a);
+    m.def("softmax_cross_entropy",
+          [](const ArrayBodyPtr& x1, const ArrayBodyPtr& x2, const std::string& reduce) {
+              Array x1_array{x1};
+              Array x2_array{x2};
+
+              SoftmaxCrossEntropyReduceMode reduce_mode{};
+              if (reduce == "mean") {
+                  reduce_mode = SoftmaxCrossEntropyReduceMode::kMean;
+              } else if (reduce == "no") {
+		  reduce_mode = SoftmaxCrossEntropyReduceMode::kNo;
+              } else {
+                  throw py::value_error{"reduce_mode must be either of 'mean' or 'no'"};
+              }
+
+              return MoveArrayBody(SoftmaxCrossEntropy(x1_array, x2_array, reduce_mode));
+          },
+          "x1"_a,
+          "x2"_a,
+          "reduce"_a = "mean");
     m.def("hinge",
           [](const ArrayBodyPtr& x1, const ArrayBodyPtr& x2, double norm) { return MoveArrayBody(Hinge(Array{x1}, Array{x2}, norm)); },
           "x1"_a,
