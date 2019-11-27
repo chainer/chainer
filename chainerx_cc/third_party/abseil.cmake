69e2a7f79187cc42817b00360aa3c857746eabc0
niboshi <niboshi000@gmail.com>
2019-11-27 06:33:41 +0000

    Remove --allow-multiple-definition

--- chainerx_cc/third_party/abseil.cmake
+++ chainerx_cc/third_party/abseil.cmake
@@ -4,7 +4,8 @@ project(abseil-download NONE)
 include(ExternalProject)
 ExternalProject_Add(abseil
     GIT_REPOSITORY    https://github.com/abseil/abseil-cpp.git
-    GIT_TAG           20190808
+    # TODO(niboshi): Should use a tagged version. 20190808 has a problem causing duplicate symbol errors.
+    GIT_TAG           0514227d2547793b23e209809276375e41c76617
     SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/abseil"
     BINARY_DIR        ""
     CONFIGURE_COMMAND ""
