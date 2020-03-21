4860ce9b9d3b3511ed3bed3cae3bf655dc88e40c
Masayuki Takagi <kamonama@gmail.com>
2020-03-21 08:09:14 +0000

    Copy array content instead of setting array itself

--- chainer/optimizer.py
+++ chainer/optimizer.py
@@ -298,7 +298,7 @@ class UpdateRule(object):
         # Convert back to the original dtype
         if fp32_converted:
             if is_initialized:
-                param.array = fp32_param.array.astype(param.dtype)
+                param.array[:] = fp32_param.array.astype(param.dtype)
             fp32_param.grad = None
 
     def _create_uninitialized_parameter(self, dtype, name):
