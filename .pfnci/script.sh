98af39b9701897a6957fc0ca0ed21c45b82c636e
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-06-23 14:05:15 +0900

    use v7 for base branch detection

--- .pfnci/script.sh
+++ .pfnci/script.sh
@@ -156,7 +156,7 @@ prepare_docker() {
 # base development branch.
 is_known_base_branch() {
   local branch="${1##refs/heads/}"
-  for BASE_BRANCH in master v6; do
+  for BASE_BRANCH in master v7; do
     if [ "${branch}" = "${BASE_BRANCH}" ]; then
       return 0
     fi
@@ -166,7 +166,7 @@ is_known_base_branch() {
 
 # get_base_branch returns the base development branch for the current HEAD.
 get_base_branch() {
-  for BASE_BRANCH in master v6; do
+  for BASE_BRANCH in master v7; do
     run git merge-base --is-ancestor "origin/${BASE_BRANCH}" HEAD && echo "${BASE_BRANCH}" && return 0
   done
   echo "Base branch of HEAD is not valid." >&2
