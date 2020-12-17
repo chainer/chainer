b45dd49c7f19f73c6de230e722c769ec219319e3
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-12-17 23:28:01 +0900

    use CuPy v7

--- onnx_chainer/.flexci/run_test.bat
+++ onnx_chainer/.flexci/run_test.bat
@@ -6,7 +6,7 @@ set "CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v%CUDA_VER%"
 set PY_PATH=C:\Development\Python\Python%PY_VER%
 set PATH=%CUDA_PATH%\bin;%CUDA_PATH%\libnvvp;%PY_PATH%;%PY_PATH%\Scripts\%PATH%
 
-pip install cupy-cuda101
+pip install "cupy-cuda101<8.0.0"
 pip install -e .[test]
 pip install "onnx<1.7.0" onnxruntime==1.2.0 pytest-cov
 pip list -v
